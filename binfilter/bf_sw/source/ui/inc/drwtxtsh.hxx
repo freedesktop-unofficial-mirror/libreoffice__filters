7e3229f82e1c12e880fb167e7b8dcf3a00015a39
Rüdiger Timm <rt@openoffice.org>
2004-05-04 14:02:00 +0000

    #i10000# Class SwDrawTextShell had no user-defined constructor.

--- binfilter/bf_sw/source/ui/inc/drwtxtsh.hxx
+++ binfilter/bf_sw/source/ui/inc/drwtxtsh.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: drwtxtsh.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:55:09 $
+ *  last change: $Author: rt $ $Date: 2004-05-04 15:02:00 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -96,7 +96,7 @@ public:
     SwView	   &GetView() { return rView; }
     SwWrtShell &GetShell();
 
-//STRIP001 				SwDrawTextShell(SwView &rView);
+                 SwDrawTextShell(SwView &rView);
 //STRIP001 	virtual		~SwDrawTextShell();
 
     void		StateDisableItems(SfxItemSet &){DBG_ASSERT(0, "STRIP");}; //STRIP001 void		StateDisableItems(SfxItemSet &);
