730fc68c78963037d765dc59248a6358cfdfeef7
Kurt Zenker <kz@openoffice.org>
2004-07-30 13:51:55 +0000

    INTEGRATION: CWS gcc340fixes01 (1.2.4); FILE MERGED 2004/07/16 13:16:17 cmc 1.2.4.1: #i31580# minor gcc34 tweaks

--- binfilter/bf_sc/source/ui/inc/AccessibleText.hxx
+++ binfilter/bf_sc/source/ui/inc/AccessibleText.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: AccessibleText.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:29:32 $
+ *  last change: $Author: kz $ $Date: 2004-07-30 14:51:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -98,18 +98,18 @@ public:
                         ScAccessibleTextData() {}
     virtual				~ScAccessibleTextData() {}
 
-    virtual	ScAccessibleTextData* Clone() const = NULL;
+    virtual	ScAccessibleTextData* Clone() const = 0;
 
     virtual void		Notify( SfxBroadcaster& rBC, const SfxHint& rHint ) {}
 
-    virtual SvxTextForwarder* GetTextForwarder() = NULL;
-    virtual SvxViewForwarder* GetViewForwarder() = NULL;
-    virtual SvxEditViewForwarder* GetEditViewForwarder( sal_Bool bCreate ) = NULL;
+    virtual SvxTextForwarder* GetTextForwarder() = 0;
+    virtual SvxViewForwarder* GetViewForwarder() = 0;
+    virtual SvxEditViewForwarder* GetEditViewForwarder( sal_Bool bCreate ) = 0;
     virtual SfxBroadcaster& GetBroadcaster() const { return maBroadcaster; }
 
-    virtual void				UpdateData() = NULL;
-    virtual void				SetDoUpdate(sal_Bool bValue) = NULL;
-    virtual sal_Bool			IsDirty() const = NULL;
+    virtual void				UpdateData() = 0;
+    virtual void				SetDoUpdate(sal_Bool bValue) = 0;
+    virtual sal_Bool			IsDirty() const = 0;
 
 private:
     mutable SfxBroadcaster maBroadcaster;
