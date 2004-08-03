1c64e5016a7589f04b41b5c3ba12ab1ccc535dcc
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 11:47:46 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/08 16:25:55 aw 1.3.20.1: #117295#

--- binfilter/bf_sch/source/core/sch_objadj.cxx
+++ binfilter/bf_sch/source/core/sch_objadj.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sch_objadj.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-05 16:39:53 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 12:47:46 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -63,6 +63,11 @@
 
 #include "objadj.hxx"
 #include "glob.hxx"
+
+#ifndef _TOOLS_DEBUG_HXX
+#include <tools/debug.hxx>
+#endif
+
 namespace binfilter {
 
 
