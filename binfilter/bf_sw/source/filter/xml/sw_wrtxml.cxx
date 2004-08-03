f65dc725112e280d88ee02df3296f6425fe52387
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 17:22:30 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:30:50 aw 1.2.20.1: #117295#

--- binfilter/bf_sw/source/filter/xml/sw_wrtxml.cxx
+++ binfilter/bf_sw/source/filter/xml/sw_wrtxml.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sw_wrtxml.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:53:35 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 18:22:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -96,9 +96,19 @@
 #ifndef _SFXDOCFILE_HXX //autogen wg. SfxMedium
 #include <bf_sfx2/docfile.hxx>
 #endif
+
+#ifndef _ERRHDL_HXX
+#include <errhdl.hxx>
+#endif
+
 #ifndef _PAM_HXX //autogen wg. SwPaM
 #include <pam.hxx>
 #endif
+
+#ifndef _HORIORNT_HXX
+#include <horiornt.hxx>
+#endif
+
 #ifndef _DOC_HXX //autogen wg. SwDoc
 #include <doc.hxx>
 #endif
