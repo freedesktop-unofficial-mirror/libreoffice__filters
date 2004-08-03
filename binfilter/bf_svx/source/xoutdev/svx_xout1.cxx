79b61f4367711e33152f6b9fc4ba776334e46b40
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 15:52:33 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:45:26 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_svx/source/xoutdev/svx_xout1.cxx
+++ binfilter/bf_svx/source/xoutdev/svx_xout1.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svx_xout1.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:47:40 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 16:52:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,7 +64,7 @@
 #include "xoutx.hxx"
 
 #ifdef JOEDEBUG
-#include "Q:\joedebug.hxx"
+// auto strip #include "Q:\joedebug.hxx"
 #endif
 namespace binfilter {
 
