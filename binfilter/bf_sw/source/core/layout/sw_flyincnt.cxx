7ee6145ad5a92118668166a1cc744e6b6fbfd252
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 16:35:17 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/14 15:00:02 aw 1.2.20.3: #117295# Corrections after phase2, unxlngi5 and unxsols4 compile 2004/07/13 08:46:25 aw 1.2.20.2: #117295# minimize_includes step2 2004/07/08 16:29:48 aw 1.2.20.1: #117295#

--- binfilter/bf_sw/source/core/layout/sw_flyincnt.cxx
+++ binfilter/bf_sw/source/core/layout/sw_flyincnt.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sw_flyincnt.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:50:27 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 17:35:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,9 +62,14 @@
 
 #pragma hdrstop
 
-#include "cntfrm.hxx"
+// auto strip #include "cntfrm.hxx"
+
+#ifndef _HORIORNT_HXX
+#include <horiornt.hxx>
+#endif
+
 #include "doc.hxx"
-#include "flyfrm.hxx"
+// auto strip #include "flyfrm.hxx"
 #include "frmtool.hxx"
 #include "frmfmt.hxx"
 #include "hints.hxx"
