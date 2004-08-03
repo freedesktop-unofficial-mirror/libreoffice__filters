d3a83e28421a9526f53424128a0d926cf17c9210
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:27:58 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:37:54 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/filter/xml/sc_XMLExportSharedData.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_XMLExportSharedData.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_XMLExportSharedData.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:27:59 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:27:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "filt_pch.hxx"
+// auto strip #include "filt_pch.hxx"
 #endif
 
 #pragma hdrstop
