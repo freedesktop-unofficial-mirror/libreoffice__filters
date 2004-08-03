300e2b0802e01c044a0d1b3b1f040d654475fa31
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 11:42:44 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:39:30 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sch/source/core/sch_chartbar.cxx
+++ binfilter/bf_sch/source/core/sch_chartbar.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sch_chartbar.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:32:21 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 12:42:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 #include "chtmodel.hxx"
-#include "chartbar.hxx"
+// auto strip #include "chartbar.hxx"
 #include "chaxis.hxx"
 namespace binfilter {
 
