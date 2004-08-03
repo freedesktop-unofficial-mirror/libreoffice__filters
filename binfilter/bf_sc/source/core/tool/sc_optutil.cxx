b14380480f4b5d444b874397b0600426d852b1ff
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:19:47 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:37:45 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/core/tool/sc_optutil.cxx
+++ binfilter/bf_sc/source/core/tool/sc_optutil.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_optutil.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:26:33 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:19:19 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,12 +60,12 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "core_pch.hxx"
+// auto strip #include "core_pch.hxx"
 #endif
 
 #pragma hdrstop
 
-#include <vcl/svapp.hxx>
+// auto strip #include <vcl/svapp.hxx>
 
 #include "optutil.hxx"
 #include "global.hxx"       // for pSysLocale
--- binfilter/bf_sc/source/core/tool/sc_prnsave.cxx
+++ binfilter/bf_sc/source/core/tool/sc_prnsave.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_prnsave.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:26:33 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:19:47 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "core_pch.hxx"
+// auto strip #include "core_pch.hxx"
 #endif
 
 #pragma hdrstop
