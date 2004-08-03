6d71ced91426eccacedbb10214784d9d20be187f
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:23:49 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:37:50 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/core/tool/sc_zforauto.cxx
+++ binfilter/bf_sc/source/core/tool/sc_zforauto.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_zforauto.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:26:38 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:23:49 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,14 +60,14 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "core_pch.hxx"
+// auto strip #include "core_pch.hxx"
 #endif
 
 #pragma hdrstop
 
 //------------------------------------------------------------------------
 
-#include <svtools/zforlist.hxx>
+// auto strip #include <svtools/zforlist.hxx>
 #include <svtools/zformat.hxx>
 #ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
