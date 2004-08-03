8d97e1adb07b2ac58fc9cedcc0ea1d3622ac6249
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 19:50:52 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/08 16:32:37 aw 1.3.20.1: #117295#

--- binfilter/inc/bf_sc/userlist.hxx
+++ binfilter/inc/bf_sc/userlist.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: userlist.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: er $ $Date: 2004-03-30 13:21:13 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 20:50:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,9 +62,9 @@
 #ifndef SC_USERLIST_HXX
 #define SC_USERLIST_HXX
 
-#ifndef _STREAM_HXX //autogen
-#include <tools/stream.hxx>
-#endif
+// auto strip #ifndef _STREAM_HXX //autogen
+// auto strip #include <tools/stream.hxx>
+// auto strip #endif
 
 #ifndef SC_COLLECT_HXX
 #include "collect.hxx"
