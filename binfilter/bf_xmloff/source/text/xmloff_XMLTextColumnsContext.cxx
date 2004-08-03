9874e375e5e8530f7a2c8bcaf7a6600c9ce4dd64
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 19:30:58 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:49:36 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_xmloff/source/text/xmloff_XMLTextColumnsContext.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLTextColumnsContext.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmloff_XMLTextColumnsContext.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: aw $ $Date: 2004-04-19 10:23:12 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 20:30:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,22 +62,22 @@
 #ifndef _COM_SUN_STAR_TEXT_XTEXTCOLUMNS_HPP_ 
 #include <com/sun/star/text/XTextColumns.hpp>
 #endif
-#ifndef _COM_SUN_STAR_TEXT_TEXTCOLUMN_HPP_ 
-#include <com/sun/star/text/TextColumn.hpp>
-#endif
-#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
-#include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_TEXT_TEXTCOLUMN_HPP_ 
+// auto strip #include <com/sun/star/text/TextColumn.hpp>
+// auto strip #endif
+// auto strip #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+// auto strip #include <com/sun/star/lang/XMultiServiceFactory.hpp>
+// auto strip #endif
 #ifndef _COM_SUN_STAR_STYLE_VERTICALALIGNMENT_HPP_ 
 #include <com/sun/star/style/VerticalAlignment.hpp>
 #endif
-#ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_ 
-#include <com/sun/star/beans/XPropertySet.hpp>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_ 
+// auto strip #include <com/sun/star/beans/XPropertySet.hpp>
+// auto strip #endif
 
-#ifndef _XMLOFF_XMLTKMAP_HXX
-#include "xmltkmap.hxx"
-#endif
+// auto strip #ifndef _XMLOFF_XMLTKMAP_HXX
+// auto strip #include "xmltkmap.hxx"
+// auto strip #endif
 #ifndef _XMLOFF_XMLUCONV_HXX 
 #include "xmluconv.hxx"
 #endif
@@ -90,9 +90,9 @@
 #ifndef _XMLOFF_XMLIMP_HXX
 #include "xmlimp.hxx"
 #endif
-#ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
-#endif
+// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
+// auto strip #include "xmltoken.hxx"
+// auto strip #endif
 #ifndef _XMLTEXTCOLUMNSCONTEXT_HXX
 #include "XMLTextColumnsContext.hxx"
 #endif
