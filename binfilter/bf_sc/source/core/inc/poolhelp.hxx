350b446f2e965926bdb44c1d300e0304a81b9160
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:12:15 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:25:00 aw 1.2.20.1: #117295#

--- binfilter/bf_sc/source/core/inc/poolhelp.hxx
+++ binfilter/bf_sc/source/core/inc/poolhelp.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: poolhelp.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:26:21 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:12:15 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,9 +66,9 @@
 #include <vos/refernce.hxx>
 #endif
 
-#ifndef _LINK_HXX
-#include <tools/link.hxx>
-#endif
+// auto strip #ifndef _LINK_HXX
+// auto strip #include <tools/link.hxx>
+// auto strip #endif
 class SvNumberFormatter;
 class SfxItemPool;
 
