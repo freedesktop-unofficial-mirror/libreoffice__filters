598f43dfdbe37cafefc45be801755b7d87e248e7
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 16:55:30 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:47:00 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sw/source/core/text/sw_txtcache.cxx
+++ binfilter/bf_sw/source/core/text/sw_txtcache.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sw_txtcache.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:51:24 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 17:55:30 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,8 +62,8 @@
 
 #pragma hdrstop
 
-#include "errhdl.hxx"
-#include "segmentc.hxx"
+// auto strip #include "errhdl.hxx"
+// auto strip #include "segmentc.hxx"
 
 #include "txtcache.hxx"
 #include "txtfrm.hxx"
