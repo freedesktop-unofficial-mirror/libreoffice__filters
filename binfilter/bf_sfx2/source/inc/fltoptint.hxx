5046f0181dd3766865fe7d61217d621dc654dfad
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 13:56:27 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:27:16 aw 1.2.20.1: #117295#

--- binfilter/bf_sfx2/source/inc/fltoptint.hxx
+++ binfilter/bf_sfx2/source/inc/fltoptint.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fltoptint.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:39:50 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 14:56:27 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,9 @@
  *
  ************************************************************************/
 
-#ifndef _COM_SUN_STAR_DOCUMENT_FILTEROPTIONSREQUEST_HPP_
-#include <com/sun/star/document/FilterOptionsRequest.hpp>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_DOCUMENT_FILTEROPTIONSREQUEST_HPP_
+// auto strip #include <com/sun/star/document/FilterOptionsRequest.hpp>
+// auto strip #endif
 #ifndef _COM_SUN_STAR_DOCUMENT_XINTERACTIONFILTEROPTIONS_HPP_
 #include <com/sun/star/document/XInteractionFilterOptions.hpp>
 #endif
