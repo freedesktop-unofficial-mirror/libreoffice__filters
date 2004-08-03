cd857161b9de4c91ae7264bf6dd4bad411501281
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 20:42:36 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/08 16:33:38 aw 1.3.20.1: #117295#

--- binfilter/inc/bf_sw/fmtlsplt.hxx
+++ binfilter/inc/bf_sw/fmtlsplt.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtlsplt.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: os $ $Date: 2004-04-22 15:42:09 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 21:42:36 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,9 +64,9 @@
 #ifndef _SFXENUMITEM_HXX //autogen
 #include <svtools/eitem.hxx>
 #endif
-#ifndef _HINTIDS_HXX
-#include <hintids.hxx>
-#endif
+// auto strip #ifndef _HINTIDS_HXX
+// auto strip #include <hintids.hxx>
+// auto strip #endif
 #ifndef _FORMAT_HXX //autogen
 #include <format.hxx>
 #endif
