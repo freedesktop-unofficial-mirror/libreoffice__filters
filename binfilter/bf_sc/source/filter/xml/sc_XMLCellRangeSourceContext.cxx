7145b79cc635be559dcf1355c964c58228bd4a36
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:25:02 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/13 08:37:50 aw 1.3.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/filter/xml/sc_XMLCellRangeSourceContext.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_XMLCellRangeSourceContext.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_XMLCellRangeSourceContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: aw $ $Date: 2003-12-05 15:10:16 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:25:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "filt_pch.hxx"
+// auto strip #include "filt_pch.hxx"
 #endif
 
 #pragma hdrstop
