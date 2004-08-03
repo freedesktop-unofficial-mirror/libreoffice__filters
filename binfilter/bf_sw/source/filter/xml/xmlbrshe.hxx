698e9a762b1a48eb535b04aa26fafa568d2c3aae
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 17:28:28 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/08 16:30:54 aw 1.3.20.1: #117295#

--- binfilter/bf_sw/source/filter/xml/xmlbrshe.hxx
+++ binfilter/bf_sw/source/filter/xml/xmlbrshe.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmlbrshe.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: aw $ $Date: 2003-12-05 15:11:08 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 18:28:28 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -66,9 +66,9 @@
 #include <rtl/ustring.hxx>
 #endif
 
-#ifndef _XMLOFF_ATTRLIST_HXX
-#include <bf_xmloff/attrlist.hxx>
-#endif
+// auto strip #ifndef _XMLOFF_ATTRLIST_HXX
+// auto strip #include <bf_xmloff/attrlist.hxx>
+// auto strip #endif
 namespace binfilter {
 
 
