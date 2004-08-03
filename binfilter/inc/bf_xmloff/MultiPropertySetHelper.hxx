37bd477320998cd782d043eef37bbc865c1ca18e
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 20:51:25 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:33:47 aw 1.2.20.1: #117295#

--- binfilter/inc/bf_xmloff/MultiPropertySetHelper.hxx
+++ binfilter/inc/bf_xmloff/MultiPropertySetHelper.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: MultiPropertySetHelper.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: aw $ $Date: 2003-12-05 09:17:47 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 21:51:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,9 @@
 #ifndef _XMLOFF_CONDITIONALMULTIPROPERTYSETHELPER_HXX
 #define _XMLOFF_CONDITIONALMULTIPROPERTYSETHELPER_HXX
 
-#ifndef _RTL_USTRING_HXX_
-#include <rtl/ustring.hxx>
-#endif
+// auto strip #ifndef _RTL_USTRING_HXX_
+// auto strip #include <rtl/ustring.hxx>
+// auto strip #endif
 
 #ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
 #include <com/sun/star/uno/Sequence.hxx>
