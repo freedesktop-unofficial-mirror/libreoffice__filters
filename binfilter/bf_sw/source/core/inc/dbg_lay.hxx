44ea0963a5fe6d9ac7dea10d075f54fa1b1dbc27
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 16:31:43 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/08 16:29:43 aw 1.3.20.1: #117295#

--- binfilter/bf_sw/source/core/inc/dbg_lay.hxx
+++ binfilter/bf_sw/source/core/inc/dbg_lay.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: dbg_lay.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-05 16:40:58 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 17:31:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -100,7 +100,7 @@
 
 #ifndef PRODUCT
 
-#include "swtypes.hxx"
+// auto strip #include "swtypes.hxx"
 #include <tools/debug.hxx>
 namespace binfilter {
 class SwImplProtocol;
