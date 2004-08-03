d505a7b735181a5eed9be62e3ec19bce554eabd8
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 15:19:05 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/08 16:28:08 aw 1.3.20.1: #117295#

--- binfilter/bf_svx/source/svdraw/svx_svdattr.cxx
+++ binfilter/bf_svx/source/svdraw/svx_svdattr.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svx_svdattr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-05 16:40:38 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 16:19:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -103,6 +103,10 @@
 
 #ifndef _APP_HXX //autogen
 #include <vcl/svapp.hxx>
+#endif
+
+#ifndef _XDEF_HXX
+#include "xdef.hxx"
 #endif
 
 #include "svdattr.hxx"
