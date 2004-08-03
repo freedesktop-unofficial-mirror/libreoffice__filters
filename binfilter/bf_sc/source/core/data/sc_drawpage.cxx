38a9396dd2351f1c762b5ebbd786f0bac43ff5e9
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:06:44 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:37:26 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/core/data/sc_drawpage.cxx
+++ binfilter/bf_sc/source/core/data/sc_drawpage.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_drawpage.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:26:04 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:06:44 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,18 +60,18 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "core_pch.hxx"
+// auto strip #include "core_pch.hxx"
 #endif
 
 #pragma hdrstop
 
 // INCLUDE ---------------------------------------------------------------
 
-#include <bf_sfx2/objsh.hxx>
+// auto strip #include <bf_sfx2/objsh.hxx>
 
 #include "drawpage.hxx"
 #include "drwlayer.hxx"
-#include "document.hxx"
+// auto strip #include "document.hxx"
 #include "pageuno.hxx"
 namespace binfilter {
 
