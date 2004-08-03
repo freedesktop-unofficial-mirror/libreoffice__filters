62b8eb49cb9744cb50770392d6b69afaf1636847
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 19:04:51 +0000

    INTEGRATION: CWS bff2 (1.1.20); FILE MERGED 2004/07/13 08:48:58 aw 1.1.20.1: #117295# minimize_includes step2

--- binfilter/bf_xmloff/source/style/xmloff_DashStyle.cxx
+++ binfilter/bf_xmloff/source/style/xmloff_DashStyle.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: xmloff_DashStyle.cxx,v $
  *
- *  $Revision: 1.1 $
+ *  $Revision: 1.2 $
  *
- *  last change: $Author: aw $ $Date: 2003-12-05 09:36:21 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 20:04:51 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,9 +59,9 @@
  *
  ************************************************************************/
 
-#ifndef _COM_SUN_STAR_DRAWING_DASHSTYLE_HPP_ 
-#include <com/sun/star/drawing/DashStyle.hpp>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_DRAWING_DASHSTYLE_HPP_ 
+// auto strip #include <com/sun/star/drawing/DashStyle.hpp>
+// auto strip #endif
 
 #ifndef _COM_SUN_STAR_DRAWING_LINEDASH_HPP_ 
 #include <com/sun/star/drawing/LineDash.hpp>
@@ -71,9 +71,9 @@
 #include "DashStyle.hxx"
 #endif
 
-#ifndef _XMLOFF_ATTRLIST_HXX
-#include "attrlist.hxx"
-#endif
+// auto strip #ifndef _XMLOFF_ATTRLIST_HXX
+// auto strip #include "attrlist.hxx"
+// auto strip #endif
 
 #ifndef _XMLOFF_NMSPMAP_HXX
 #include "nmspmap.hxx"
@@ -87,9 +87,9 @@
 #include "xmlnmspe.hxx"
 #endif
 
-#ifndef _XMLOFF_XMLTOKEN_HXX
-#include "xmltoken.hxx"
-#endif
+// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
+// auto strip #include "xmltoken.hxx"
+// auto strip #endif
 
 #ifndef _XMLOFF_XMLEXP_HXX
 #include "xmlexp.hxx"
@@ -99,9 +99,9 @@
 #include "xmlimp.hxx"
 #endif
 
-#ifndef _RTL_USTRBUF_HXX_
-#include <rtl/ustrbuf.hxx>
-#endif
+// auto strip #ifndef _RTL_USTRBUF_HXX_
+// auto strip #include <rtl/ustrbuf.hxx>
+// auto strip #endif
 
 #ifndef _RTL_USTRING_
 #include <rtl/ustring>
@@ -111,9 +111,9 @@
 #include <tools/debug.hxx>
 #endif
 
-#ifndef _XMLOFF_XMLTKMAP_HXX
-#include "xmltkmap.hxx"
-#endif
+// auto strip #ifndef _XMLOFF_XMLTKMAP_HXX
+// auto strip #include "xmltkmap.hxx"
+// auto strip #endif
 namespace binfilter {
 
 using namespace ::com::sun::star;
