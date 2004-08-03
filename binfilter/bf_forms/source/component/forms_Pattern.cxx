62db3d0dc6cc6fc1aefb8ac89cf1265d916597eb
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 09:51:54 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:37:07 aw 1.2.20.2: #117295# minimize_includes step2 2004/07/08 16:24:48 aw 1.2.20.1: #117295#

--- binfilter/bf_forms/source/component/forms_Pattern.cxx
+++ binfilter/bf_forms/source/component/forms_Pattern.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: forms_Pattern.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:25:10 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 10:51:54 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -59,13 +59,30 @@
  *
  ************************************************************************/
 
+#ifndef _COMPHELPER_PROPERTY_ARRAY_HELPER_HXX_
+#include <comphelper/proparrhlp.hxx>
+#endif
+
 #ifndef _FORMS_PATTERN_HXX_
 #include "Pattern.hxx"
 #endif
 
-#ifndef _TOOLS_DEBUG_HXX
-#include <tools/debug.hxx>
+// auto strip #ifndef _TOOLS_DEBUG_HXX
+// auto strip #include <tools/debug.hxx>
+// auto strip #endif
+
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
+#include <com/sun/star/beans/PropertyAttribute.hpp>
 #endif
+
+#ifndef _FRM_SERVICES_HXX_
+#include "services.hxx"
+#endif
+
+#ifndef _COM_SUN_STAR_FORM_FORMCOMPONENTTYPE_HPP_
+#include <com/sun/star/form/FormComponentType.hpp>
+#endif
+
 namespace binfilter {
 
 //.........................................................................
@@ -75,7 +92,7 @@ namespace frm
 using namespace ::com::sun::star::uno;
 using namespace ::com::sun::star::sdb;
 using namespace ::com::sun::star::sdbc;
-using namespace ::com::sun::star::sdbcx;
+//using namespace ::com::sun::star::sdbcx;
 using namespace ::com::sun::star::beans;
 using namespace ::com::sun::star::container;
 using namespace ::com::sun::star::form;
