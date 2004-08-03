19554ab6195f86dd9d6092d1a22e9b03eb457e52
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 15:09:10 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/14 14:59:57 aw 1.2.20.2: #117295# Corrections after phase2, unxlngi5 and unxsols4 compile 2004/07/13 08:44:05 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_svx/source/options/svx_asiancfg.cxx
+++ binfilter/bf_svx/source/options/svx_asiancfg.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svx_asiancfg.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:45:43 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 16:09:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -67,9 +67,9 @@
 #ifndef _SVARRAY_HXX //autogen
 #include <svtools/svarray.hxx>
 #endif
-#ifndef _COM_SUN_STAR_UNO_ANY_HXX_
-#include <com/sun/star/uno/Any.hxx>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_UNO_ANY_HXX_
+// auto strip #include <com/sun/star/uno/Any.hxx>
+// auto strip #endif
 #ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
 #include <com/sun/star/uno/Sequence.hxx>
 #endif
