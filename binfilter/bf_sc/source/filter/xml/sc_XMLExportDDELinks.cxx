8c703ae68ac997b56e3a6cb2f7c9dea2820f8e98
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:27:05 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/13 08:37:53 aw 1.3.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/filter/xml/sc_XMLExportDDELinks.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_XMLExportDDELinks.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_XMLExportDDELinks.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: aw $ $Date: 2003-12-05 15:10:17 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:27:05 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "filt_pch.hxx"
+// auto strip #include "filt_pch.hxx"
 #endif
 
 #pragma hdrstop
@@ -71,15 +71,15 @@
 #include "XMLExportDDELinks.hxx"
 #endif
 
-#ifndef _XMLOFF_XMLTOKEN_HXX
-#include <bf_xmloff/xmltoken.hxx>
-#endif
+// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
+// auto strip #include <bf_xmloff/xmltoken.hxx>
+// auto strip #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include <bf_xmloff/xmlnmspe.hxx>
 #endif
-#ifndef _XMLOFF_NMSPMAP_HXX
-#include <bf_xmloff/nmspmap.hxx>
-#endif
+// auto strip #ifndef _XMLOFF_NMSPMAP_HXX
+// auto strip #include <bf_xmloff/nmspmap.hxx>
+// auto strip #endif
 #ifndef _XMLOFF_XMLUCONV_HXX
 #include <bf_xmloff/xmluconv.hxx>
 #endif
