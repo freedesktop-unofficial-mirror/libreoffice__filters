d83f49202aebc7463376af3430d78711ff609c25
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 14:35:59 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/13 08:43:11 aw 1.3.20.1: #117295# minimize_includes step2

--- binfilter/bf_svx/source/editeng/svx_forbiddencharacterstable.cxx
+++ binfilter/bf_svx/source/editeng/svx_forbiddencharacterstable.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svx_forbiddencharacterstable.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2004-05-05 16:40:26 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 15:35:59 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,9 +64,9 @@
 #include <unotools/localedatawrapper.hxx>
 #include <unolingu.hxx>
 
-#ifndef _COM_SUN_STAR_LANG_XSINGLESERVICEFACTORY_HPP_
-#include <com/sun/star/lang/XMultiServiceFactory.hpp>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_LANG_XSINGLESERVICEFACTORY_HPP_
+// auto strip #include <com/sun/star/lang/XMultiServiceFactory.hpp>
+// auto strip #endif
 /*STRIP001*/#include <tools/debug.hxx>
 namespace binfilter {
 /*N*/ SvxForbiddenCharactersTable::SvxForbiddenCharactersTable( ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory > xMSF, USHORT nISize, USHORT nGrow )
