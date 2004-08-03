202f2ba115879932305c65b6c88bc79a583a2711
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 11:50:38 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:39:48 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sch/source/ui/app/sch_schresid.cxx
+++ binfilter/bf_sch/source/ui/app/sch_schresid.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sch_schresid.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:32:51 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 12:50:38 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,7 +62,7 @@
 
 #pragma hdrstop
 
-#include <tools/shl.hxx>
+// auto strip #include <tools/shl.hxx>
 #include "schmod.hxx"
 #include "schresid.hxx"
 namespace binfilter {
