e088760913221560e0f0172ca308bbe8ea28bf80
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 17:20:37 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:30:46 aw 1.2.20.1: #117295#

--- binfilter/bf_sw/source/filter/inc/fltbase.hxx
+++ binfilter/bf_sw/source/filter/inc/fltbase.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fltbase.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:52:45 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 18:20:37 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -65,9 +65,9 @@
 #include <tools/stream.hxx>
 #endif
 
-#ifndef _STRING_HXX //autogen
-#include <tools/string.hxx>
-#endif
+// auto strip #ifndef _STRING_HXX //autogen
+// auto strip #include <tools/string.hxx>
+// auto strip #endif
 namespace binfilter {
 
 class SwFilterBase
