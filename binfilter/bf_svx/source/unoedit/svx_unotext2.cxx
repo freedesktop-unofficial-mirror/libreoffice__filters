2e253283e8c16c1f692b84c781fdd55897804357
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 15:48:10 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:45:20 aw 1.2.20.2: #117295# minimize_includes step2 2004/07/08 16:28:24 aw 1.2.20.1: #117295#

--- binfilter/bf_svx/source/unoedit/svx_unotext2.cxx
+++ binfilter/bf_svx/source/unoedit/svx_unotext2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svx_unotext2.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:47:29 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 16:48:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -75,8 +75,21 @@
 #include <eeitem.hxx>
 
 #define ITEMID_FIELD EE_FEATURE_FIELD
-#include "flditem.hxx"
-#include "unofield.hxx"
+
+// auto strip #ifndef _PSTM_HXX //autogen
+// auto strip #include <tools/pstm.hxx>
+// auto strip #endif
+
+#ifndef _SFXPOOLITEM_HXX //autogen
+#include <svtools/poolitem.hxx>
+#endif
+
+#ifndef _SVX_ITEMDATA_HXX
+#include <bf_svx/itemdata.hxx>
+#endif
+
+// auto strip #include "flditem.hxx"
+// auto strip #include "unofield.hxx"
 #include "unotext.hxx"
 namespace binfilter {
 
