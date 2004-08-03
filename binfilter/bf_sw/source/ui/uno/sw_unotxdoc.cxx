6b6ad147624261e180e56e4e9b21595b4932ee35
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 18:30:57 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/08 16:32:07 aw 1.3.20.1: #117295#

--- binfilter/bf_sw/source/ui/uno/sw_unotxdoc.cxx
+++ binfilter/bf_sw/source/ui/uno/sw_unotxdoc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sw_unotxdoc.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: aw $ $Date: 2003-12-10 14:09:01 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 19:30:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -83,6 +83,11 @@
 #ifndef _SWWDOCSH_HXX //autogen
 #include <wdocsh.hxx>
 #endif
+
+#ifndef _HORIORNT_HXX
+#include <horiornt.hxx>
+#endif
+
 #ifndef _WRTSH_HXX //autogen
 #include <wrtsh.hxx>
 #endif
