b2e67f0432febb666e2052d2758fbfb4c6d887f5
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 20:42:02 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:33:37 aw 1.2.20.1: #117295#

--- binfilter/inc/bf_sw/fmtfsize.hxx
+++ binfilter/inc/bf_sw/fmtfsize.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtfsize.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 08:33:48 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 21:41:39 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -64,12 +64,12 @@
 #ifndef _GEN_HXX //autogen
 #include <tools/gen.hxx>
 #endif
-#ifndef _SFXPOOLITEM_HXX //autogen
-#include <svtools/poolitem.hxx>
-#endif
-#ifndef _HINTIDS_HXX
-#include <hintids.hxx>
-#endif
+// auto strip #ifndef _SFXPOOLITEM_HXX //autogen
+// auto strip #include <svtools/poolitem.hxx>
+// auto strip #endif
+// auto strip #ifndef _HINTIDS_HXX
+// auto strip #include <hintids.hxx>
+// auto strip #endif
 #ifndef _SWTYPES_HXX //autogen
 #include <swtypes.hxx>
 #endif
--- binfilter/inc/bf_sw/fmthdft.hxx
+++ binfilter/inc/bf_sw/fmthdft.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmthdft.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 08:33:49 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 21:42:02 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,18 +61,18 @@
 #ifndef _FMTHDFT_HXX
 #define _FMTHDFT_HXX
 
-#ifndef _HINTIDS_HXX
-#include <hintids.hxx>
-#endif
+// auto strip #ifndef _HINTIDS_HXX
+// auto strip #include <hintids.hxx>
+// auto strip #endif
 #ifndef _FORMAT_HXX //autogen
 #include <format.hxx>
 #endif
-#ifndef _SFXPOOLITEM_HXX //autogen
-#include <svtools/poolitem.hxx>
-#endif
-#ifndef _CALBCK_HXX //autogen
-#include <calbck.hxx>
-#endif
+// auto strip #ifndef _SFXPOOLITEM_HXX //autogen
+// auto strip #include <svtools/poolitem.hxx>
+// auto strip #endif
+// auto strip #ifndef _CALBCK_HXX //autogen
+// auto strip #include <calbck.hxx>
+// auto strip #endif
 class IntlWrapper; 
 namespace binfilter {
 
