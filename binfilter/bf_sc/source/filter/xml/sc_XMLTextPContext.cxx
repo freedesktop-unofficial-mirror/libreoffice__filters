6c07571e8b013c63cbbd7efa3a21c60eb1b9c680
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:29:57 +0000

    INTEGRATION: CWS bff2 (1.3.20); FILE MERGED 2004/07/13 08:37:58 aw 1.3.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/filter/xml/sc_XMLTextPContext.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_XMLTextPContext.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_XMLTextPContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: aw $ $Date: 2003-12-05 15:10:19 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:29:57 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "filt_pch.hxx"
+// auto strip #include "filt_pch.hxx"
 #endif
 
 #pragma hdrstop
@@ -80,19 +80,19 @@
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include <bf_xmloff/xmlnmspe.hxx>
 #endif
-#ifndef _XMLOFF_XMLTOKEN_HXX
-#include <bf_xmloff/xmltoken.hxx>
-#endif
+// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
+// auto strip #include <bf_xmloff/xmltoken.hxx>
+// auto strip #endif
 #ifndef _XMLOFF_NMSPMAP_HXX
 #include <bf_xmloff/nmspmap.hxx>
 #endif
-#ifndef _TOOLS_DEBUG_HXX
-#include <tools/debug.hxx>
-#endif
+// auto strip #ifndef _TOOLS_DEBUG_HXX
+// auto strip #include <tools/debug.hxx>
+// auto strip #endif
 
-#ifndef _COM_SUN_STAR_TEXT_XTEXTCURSOR_HPP_
-#include <com/sun/star/text/XTextCursor.hpp>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_TEXT_XTEXTCURSOR_HPP_
+// auto strip #include <com/sun/star/text/XTextCursor.hpp>
+// auto strip #endif
 namespace binfilter {
 
 using namespace ::com::sun::star;
