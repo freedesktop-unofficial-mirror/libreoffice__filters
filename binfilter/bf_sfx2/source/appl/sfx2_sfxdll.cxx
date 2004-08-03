798200eba5954e41fc546712730813aadeb9f057
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 13:30:12 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:42:01 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sfx2/source/appl/sfx2_sfxdll.cxx
+++ binfilter/bf_sfx2/source/appl/sfx2_sfxdll.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sfx2_sfxdll.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:38:07 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 14:30:12 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -68,7 +68,7 @@
 #pragma hdrstop
 
 #ifdef WIN
-#include "segmentc.hxx"
+// auto strip #include "segmentc.hxx"
 namespace binfilter {
 
 // Statische DLL-Verwaltungs-Variablen
