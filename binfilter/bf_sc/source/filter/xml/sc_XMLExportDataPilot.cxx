c82fc818ecf715ef20034747bec55ef4679be90c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 10:27:17 +0000

    INTEGRATION: CWS bff2 (1.4.20); FILE MERGED 2004/07/13 08:37:53 aw 1.4.20.1: #117295# minimize_includes step2

--- binfilter/bf_sc/source/filter/xml/sc_XMLExportDataPilot.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_XMLExportDataPilot.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sc_XMLExportDataPilot.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: aw $ $Date: 2004-04-19 10:22:39 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 11:27:17 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -60,7 +60,7 @@
  ************************************************************************/
 
 #ifdef PCH
-#include "filt_pch.hxx"
+// auto strip #include "filt_pch.hxx"
 #endif
 
 #pragma hdrstop
@@ -71,9 +71,9 @@
 #include "XMLExportDataPilot.hxx"
 #endif
 
-#ifndef _XMLOFF_XMLTOKEN_HXX
-#include <bf_xmloff/xmltoken.hxx>
-#endif
+// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
+// auto strip #include <bf_xmloff/xmltoken.hxx>
+// auto strip #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include <bf_xmloff/xmlnmspe.hxx>
 #endif
