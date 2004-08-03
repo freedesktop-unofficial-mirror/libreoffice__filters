8a08711ade8f3eba3b88a311e1db973bc5c5c72c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 16:57:41 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/14 15:00:23 aw 1.2.20.2: #117295# Corrections after phase2, unxlngi5 and unxsols4 compile 2004/07/13 08:47:04 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sw/source/core/text/sw_txttab.cxx
+++ binfilter/bf_sw/source/core/text/sw_txttab.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sw_txttab.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:51:26 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 17:57:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -74,16 +74,16 @@
 #ifndef _FRMATR_HXX
 #include <frmatr.hxx>
 #endif
-#ifndef _SW_PORTIONHANDLER_HXX
-#include <SwPortionHandler.hxx>
-#endif
+// auto strip #ifndef _SW_PORTIONHANDLER_HXX
+// auto strip #include <SwPortionHandler.hxx>
+// auto strip #endif
 
-#include "viewopt.hxx"	// SwViewOptions
+// auto strip #include "viewopt.hxx"	// SwViewOptions
 #include "txtcfg.hxx"
 #include "portab.hxx"
-#include "inftxt.hxx"
+// auto strip #include "inftxt.hxx"
 #include "itrform2.hxx"
-#include "txtfrm.hxx"
+// auto strip #include "txtfrm.hxx"
 namespace binfilter {
 
 
