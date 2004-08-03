4ef52d7aa3a3b91775e1da1ef5894d16ab2f4482
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 12:22:24 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:40:10 aw 1.2.20.2: #117295# minimize_includes step2 2004/07/08 16:26:36 aw 1.2.20.1: #117295#

--- binfilter/bf_sch/source/ui/unoidl/sch_ChXDataRow.cxx
+++ binfilter/bf_sch/source/ui/unoidl/sch_ChXDataRow.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sch_ChXDataRow.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:34:06 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 13:22:24 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,27 +62,27 @@
 #include "ChXDataRow.hxx"
 #include "mapprov.hxx"
 #include "ChartLine.hxx"
-#include "ChartArea.hxx"
-#ifndef _CHXCHART_OBJECT_HXX
-#include "ChXChartObject.hxx"
-#endif
+// auto strip #include "ChartArea.hxx"
+// auto strip #ifndef _CHXCHART_OBJECT_HXX
+// auto strip #include "ChXChartObject.hxx"
+// auto strip #endif
 
 // header for SvxServiceInfoHelper
-#ifndef SVX_UNOPROV_HXX
-#include <bf_svx/unoprov.hxx>
-#endif
+// auto strip #ifndef SVX_UNOPROV_HXX
+// auto strip #include <bf_svx/unoprov.hxx>
+// auto strip #endif
 // header for class OGuard
-#ifndef _VOS_MUTEX_HXX_
-#include <vos/mutex.hxx>
-#endif
+// auto strip #ifndef _VOS_MUTEX_HXX_
+// auto strip #include <vos/mutex.hxx>
+// auto strip #endif
 // header for class Application
 #ifndef _SV_SVAPP_HXX
 #include <vcl/svapp.hxx>
 #endif
 // header for SvxUnoTextRangeBase
-#ifndef _SVX_UNOTEXT_HXX
-#include <bf_svx/unotext.hxx>
-#endif
+// auto strip #ifndef _SVX_UNOTEXT_HXX
+// auto strip #include <bf_svx/unotext.hxx>
+// auto strip #endif
 // for OWN_ATTR_...
 #ifndef _SVX_UNOSHPRP_HXX
 #include <bf_svx/unoshprp.hxx>
@@ -114,9 +114,9 @@
 #ifndef _SVX_UNOAPI_HXX_
 #include <bf_svx/unoapi.hxx>
 #endif
-#ifndef _SVX_XBITMAP_HXX 
-#include <bf_svx/xbitmap.hxx>
-#endif
+// auto strip #ifndef _SVX_XBITMAP_HXX 
+// auto strip #include <bf_svx/xbitmap.hxx>
+// auto strip #endif
 
 #ifndef _COM_SUN_STAR_CHART_CHARTDATACAPTION_HPP_
 #include <com/sun/star/chart/ChartDataCaption.hpp>
@@ -124,9 +124,9 @@
 #ifndef _COM_SUN_STAR_CHART_CHARTAXISASSIGN_HPP_
 #include <com/sun/star/chart/ChartAxisAssign.hpp>
 #endif
-#ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
-#include <com/sun/star/beans/PropertyAttribute.hpp>
-#endif
+// auto strip #ifndef _COM_SUN_STAR_BEANS_PROPERTYATTRIBUTE_HPP_
+// auto strip #include <com/sun/star/beans/PropertyAttribute.hpp>
+// auto strip #endif
 #ifndef _COMPHELPER_EXTRACT_HXX_
 #include <comphelper/extract.hxx>
 #endif
@@ -134,11 +134,14 @@
 // header for SvxChartDataDescrItem
 #ifndef _SVX_CHRTITEM_HXX
 #define ITEMID_CHARTDATADESCR   SCHATTR_DATADESCR_DESCR
-#include <bf_svx/chrtitem.hxx>
+// auto strip #include <bf_svx/chrtitem.hxx>
 #endif
 
-#include "chtmodel.hxx"
+// auto strip #include "chtmodel.hxx"
 #include "globfunc.hxx"			// for GlobalGenerate3DAttrDefaultItem
+#ifndef _SCH_UNONAMES_HXX
+#include "unonames.hxx"
+#endif
 namespace binfilter {
 
 extern SchUnoPropertyMapProvider aSchMapProvider;
