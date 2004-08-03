db71a6cde9a6da69803c19ca62bfe364de4ddb41
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 15:34:33 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:28:15 aw 1.2.20.1: #117295#

--- binfilter/bf_svx/source/svxlink/fileobj.hxx
+++ binfilter/bf_svx/source/svxlink/fileobj.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fileobj.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:47:02 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 16:34:33 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -61,9 +61,9 @@
 #ifndef _FILEOBJ_HXX
 #define _FILEOBJ_HXX
 
-#ifndef _STRING_HXX //autogen
-#include <tools/string.hxx>
-#endif
+// auto strip #ifndef _STRING_HXX //autogen
+// auto strip #include <tools/string.hxx>
+// auto strip #endif
 #ifndef _LINKSRC_HXX //autogen
 #include <so3/linksrc.hxx>
 #endif
