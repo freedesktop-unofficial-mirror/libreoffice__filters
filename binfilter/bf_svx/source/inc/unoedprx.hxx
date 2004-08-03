f381a9898e664c30d27b639b13fcfe483bc4cca0
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 15:01:32 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/08 16:27:56 aw 1.2.20.1: #117295#

--- binfilter/bf_svx/source/inc/unoedprx.hxx
+++ binfilter/bf_svx/source/inc/unoedprx.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoedprx.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:44:38 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 16:01:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -72,7 +72,7 @@
 #include "unoedsrc.hxx"
 #endif
 
-#include "editdata.hxx"
+// auto strip #include "editdata.hxx"
 namespace binfilter {
 
 
