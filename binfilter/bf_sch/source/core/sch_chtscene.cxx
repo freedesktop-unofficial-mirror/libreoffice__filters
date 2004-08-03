4cd1b4aa55d88c4f38bdb8707334383a26e0a737
Jens-Heiner Rechtien <hr@openoffice.org>
2004-08-03 11:46:25 +0000

    INTEGRATION: CWS bff2 (1.2.20); FILE MERGED 2004/07/13 08:39:39 aw 1.2.20.1: #117295# minimize_includes step2

--- binfilter/bf_sch/source/core/sch_chtscene.cxx
+++ binfilter/bf_sch/source/core/sch_chtscene.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sch_chtscene.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:32:26 $
+ *  last change: $Author: hr $ $Date: 2004-08-03 12:46:25 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -62,13 +62,13 @@
 #pragma hdrstop
 
 
-#ifndef _XPOLY_HXX //autogen
-#include <bf_svx/xpoly.hxx>
-#endif
+// auto strip #ifndef _XPOLY_HXX //autogen
+// auto strip #include <bf_svx/xpoly.hxx>
+// auto strip #endif
 
 #include "axisid.hxx"
 #include "chtscene.hxx"
-#include "chtmodel.hxx"
+// auto strip #include "chtmodel.hxx"
 namespace binfilter {
 
 #pragma optimize ("",off)
