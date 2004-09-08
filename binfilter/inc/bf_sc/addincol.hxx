bbb933d5199e1a45d2c0f107e8a12aaae2b6bf54
Rüdiger Timm <rt@openoffice.org>
2004-09-08 12:55:43 +0000

    INTEGRATION: CWS ooo20040704 (1.2.14); FILE MERGED 2004/07/02 14:52:16 cmc 1.2.14.2: #i30891# revert header and namespace change 2004/06/28 17:15:29 cmc 1.2.14.1: #i30801# allow using system stl if possible

--- binfilter/inc/bf_sc/addincol.hxx
+++ binfilter/inc/bf_sc/addincol.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: addincol.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 07:59:27 $
+ *  last change: $Author: rt $ $Date: 2004-09-08 13:55:43 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -92,6 +92,7 @@
 #endif
 
 #include <hash_map>
+
 class String;
 namespace binfilter {
 
