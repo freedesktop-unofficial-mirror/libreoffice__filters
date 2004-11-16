4b18211c75a5b97bda4bd9ef7aa6b82bf91595e2
Oliver Bolte <obo@openoffice.org>
2004-11-16 11:27:32 +0000

    INTEGRATION: CWS eforms2 (1.3.2); FILE MERGED 2004/04/23 11:35:00 lo 1.3.2.1: #i26721# put/get/post support for submission node cloning

--- unoxml/source/dom/text.hxx
+++ unoxml/source/dom/text.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: text.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: lo $ $Date: 2004-02-26 14:43:17 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 12:27:32 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
