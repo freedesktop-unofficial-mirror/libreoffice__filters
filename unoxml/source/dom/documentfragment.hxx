0140dbbe81353e07d6161f30acc01e1d49ae7181
Oliver Bolte <obo@openoffice.org>
2004-11-16 11:22:10 +0000

    INTEGRATION: CWS eforms2 (1.2.2); FILE MERGED 2004/04/23 11:34:55 lo 1.2.2.1: #i26721# put/get/post support for submission node cloning

--- unoxml/source/dom/documentfragment.hxx
+++ unoxml/source/dom/documentfragment.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: documentfragment.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: lo $ $Date: 2004-02-26 14:43:15 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 12:21:41 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
--- unoxml/source/dom/documenttype.hxx
+++ unoxml/source/dom/documenttype.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: documenttype.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: lo $ $Date: 2004-02-26 14:43:15 $
+ *  last change: $Author: obo $ $Date: 2004-11-16 12:22:10 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
