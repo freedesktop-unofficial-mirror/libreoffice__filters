796549b39777f6ccd7f3dbd576035691a2f701fe
Oliver Bolte <obo@openoffice.org>
2005-04-13 07:05:42 +0000

    INTEGRATION: CWS visibility03 (1.6.22); FILE MERGED 2005/04/04 17:33:12 mhu 1.6.22.1: #i45006# Replaced svtools/sbx*.hxx with basic/sbx*.hxx

--- binfilter/bf_sfx2/source/doc/sfx2_objmisc.cxx
+++ binfilter/bf_sfx2/source/doc/sfx2_objmisc.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: sfx2_objmisc.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2005-01-18 15:09:32 $
+ *  last change: $Author: obo $ $Date: 2005-04-13 08:05:42 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -99,7 +99,7 @@ using namespace ::com::sun::star::document;
 #include <basic/basmgr.hxx>
 #endif
 #ifndef _SBXCLASS_HXX //autogen
-#include <svtools/sbx.hxx>
+#include <basic/sbx.hxx>
 #endif
 
 #include <unotools/ucbhelper.hxx>
