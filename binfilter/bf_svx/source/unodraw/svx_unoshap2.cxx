1fb2279793116d10634b23f3bf5963a086753c4a
Oliver Bolte <obo@openoffice.org>
2005-04-13 07:10:52 +0000

    INTEGRATION: CWS visibility03 (1.3.82); FILE MERGED 2005/04/04 19:08:42 mhu 1.3.82.1: #i45006# Added missing includes.

--- binfilter/bf_svx/source/unodraw/svx_unoshap2.cxx
+++ binfilter/bf_svx/source/unodraw/svx_unoshap2.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: svx_unoshap2.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-03 16:44:48 $
+ *  last change: $Author: obo $ $Date: 2005-04-13 08:10:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -85,9 +85,9 @@
 #ifndef _UNOTOOLS_LOCALFILEHELPER_HXX
 #include <unotools/localfilehelper.hxx>
 #endif
-// auto strip #ifndef _SV_SVAPP_HXX
-// auto strip #include <vcl/svapp.hxx>
-// auto strip #endif
+#ifndef _SV_SVAPP_HXX
+#include <vcl/svapp.hxx>
+#endif
 // auto strip #ifndef _VOS_MUTEX_HXX_ //autogen
 // auto strip #include <vos/mutex.hxx>
 // auto strip #endif
