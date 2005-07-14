7614334e658a895a0ca94d5520d4941d0c5dd8a8
Kurt Zenker <kz@openoffice.org>
2005-07-14 13:43:52 +0000

    INTEGRATION: CWS mh19104 (1.3.112); FILE MERGED 2005/06/28 10:16:25 mh 1.3.112.1: add: forwards for gcc4, #i50551#

--- binfilter/inc/bf_sw/fmtrfmrk.hxx
+++ binfilter/inc/bf_sw/fmtrfmrk.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fmtrfmrk.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-03 21:43:11 $
+ *  last change: $Author: kz $ $Date: 2005-07-14 14:43:52 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -70,6 +70,7 @@
 #endif
 namespace binfilter {
 
+class SwTxtRefMark;
 // ATT_REFMARK *******************************************************
 
 class SwFmtRefMark : public SfxPoolItem
