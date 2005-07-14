8c0c941ccdd2f20b77127517883e8f83d2bf415c
Kurt Zenker <kz@openoffice.org>
2005-07-14 13:39:29 +0000

    INTEGRATION: CWS mh19104 (1.2.142); FILE MERGED 2005/06/28 10:16:20 mh 1.2.142.1: add: forwards for gcc4, #i50551#

--- binfilter/inc/bf_starmath/symbol.hxx
+++ binfilter/inc/bf_starmath/symbol.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: symbol.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: mwu $ $Date: 2003-11-06 08:27:17 $
+ *  last change: $Author: kz $ $Date: 2005-07-14 14:39:29 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -95,6 +95,8 @@ namespace binfilter {
 
 #define SYMBOLSET_NONE	0xFFFF
 #define SYMBOL_NONE		0xFFFF
+
+class SmSymSetManager;
 
 ////////////////////////////////////////////////////////////////////////////////
 
