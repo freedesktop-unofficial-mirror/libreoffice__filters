4e36003846760e95d4271ce8d30f515c7474aa5c
Kurt Zenker <kz@openoffice.org>
2005-07-14 13:42:09 +0000

    INTEGRATION: CWS mh19104 (1.4.112); FILE MERGED 2005/06/28 10:16:23 mh 1.4.112.1: add: forwards for gcc4, #i50551#

--- binfilter/inc/bf_svx/unoedsrc.hxx
+++ binfilter/inc/bf_svx/unoedsrc.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: unoedsrc.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2004-08-03 21:34:14 $
+ *  last change: $Author: kz $ $Date: 2005-07-14 14:42:09 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -90,6 +90,7 @@
 #ifndef _SFXITEMPOOL_HXX //autogen
 #include <svtools/itempool.hxx>
 #endif
+class OutputDevice;
 class String;
 class SfxItemSet;
 class SvUShorts;
