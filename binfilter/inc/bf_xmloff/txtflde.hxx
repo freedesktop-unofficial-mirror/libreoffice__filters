80559fa3c98164fa598f1c7791992677a4dc7288
Oliver Bolte <obo@openoffice.org>
2006-01-19 16:34:17 +0000

    INTEGRATION: CWS gcc41 (1.3.36); FILE MERGED 2005/12/07 18:01:39 pmladek 1.3.36.1: #i58937# Removed extra qualification to fix build with gcc-4.1

--- binfilter/inc/bf_xmloff/txtflde.hxx
+++ binfilter/inc/bf_xmloff/txtflde.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtflde.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 17:34:38 $
+ *  last change: $Author: obo $ $Date: 2006-01-19 17:34:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -240,7 +240,7 @@ protected:
     SvXMLExport& GetExport() { return rExport; }
 
     /// export a field after <text:span> is already written
-    void XMLTextFieldExport::ExportFieldHelper(
+    void ExportFieldHelper(
         const ::com::sun::star::uno::Reference<
                 ::com::sun::star::text::XTextField> & rTextField,
         const ::com::sun::star::uno::Reference<
