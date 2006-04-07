216840cc73599cbd596ae984a90e7a783ebd3114
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:42:02 +0000

    INTEGRATION: CWS internatiodel (1.6.48); FILE MERGED 2006/02/10 19:26:50 er 1.6.48.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- binfilter/inc/bf_svx/unoedsrc.hxx
+++ binfilter/inc/bf_svx/unoedsrc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoedsrc.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:13:58 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:42:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,8 +39,8 @@
 #ifndef _SOLAR_H
 #include <tools/solar.h>
 #endif
-#ifndef _LANG_HXX //autogen
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 // auto strip #ifndef _LINK_HXX
 // auto strip #include <tools/link.hxx>
