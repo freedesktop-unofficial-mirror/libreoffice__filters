f97677b3185e8f12b8c6e47135e6918cd2bdf33c
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:40:56 +0000

    INTEGRATION: CWS internatiodel (1.4.46); FILE MERGED 2006/02/10 19:26:49 er 1.4.46.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- binfilter/inc/bf_svx/outliner.hxx
+++ binfilter/inc/bf_svx/outliner.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: outliner.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:06:34 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:40:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,8 +48,8 @@
 #ifndef _EDITDATA_HXX //autogen
 #include <bf_svx/editdata.hxx>
 #endif
-#ifndef _LANG_HXX //autogen
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 #ifndef _COLOR_HXX //autogen
 #include <tools/color.hxx>
