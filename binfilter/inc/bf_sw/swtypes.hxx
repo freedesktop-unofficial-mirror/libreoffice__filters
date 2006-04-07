34c9e193e80ffa273c8b827c973d508b6a911e83
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:43:40 +0000

    INTEGRATION: CWS internatiodel (1.4.46); FILE MERGED 2006/02/10 19:26:51 er 1.4.46.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- binfilter/inc/bf_sw/swtypes.hxx
+++ binfilter/inc/bf_sw/swtypes.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swtypes.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:34:03 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:43:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,8 +38,8 @@
 #ifndef _SOLAR_H
 #include <tools/solar.h>
 #endif
-#ifndef _LANG_HXX
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 // auto strip #ifndef _GETPOOLIDFROMNAMEENUM_HXX
 // auto strip #include <SwGetPoolIdFromName.hxx>
