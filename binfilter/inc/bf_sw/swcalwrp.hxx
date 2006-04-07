2d78ae7515a261169bc002605db351165c115f80
Vladimir Glazounov <vg@openoffice.org>
2006-04-07 12:43:19 +0000

    INTEGRATION: CWS internatiodel (1.3.48); FILE MERGED 2006/02/10 19:26:51 er 1.3.48.1: #i52115# move LangIDs and ISO conversion from tools to i18npool; introduce class MsLangId and libi18nisolang

--- binfilter/inc/bf_sw/swcalwrp.hxx
+++ binfilter/inc/bf_sw/swcalwrp.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swcalwrp.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:27:15 $
+ *  last change: $Author: vg $ $Date: 2006-04-07 13:43:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,8 +36,8 @@
 #ifndef _SWCALWRP_HXX
 #define _SWCALWRP_HXX
 
-#ifndef _LANG_HXX
-#include <tools/lang.hxx>
+#ifndef INCLUDED_I18NPOOL_LANG_H
+#include <i18npool/lang.h>
 #endif
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
