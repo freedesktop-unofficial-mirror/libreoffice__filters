784d8d65b3be25589cbf93c1a29cf35460e8ee70
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:24:31 +0000

    INTEGRATION: CWS vgcleanupmacros (1.4.72); FILE MERGED 2006/05/17 11:09:19 vg 1.4.72.1: #i65486# cleanup:remove obsolete macros

--- binfilter/inc/bf_svx/boxitem.hxx
+++ binfilter/inc/bf_svx/boxitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: boxitem.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:05:59 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:23:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,14 +47,10 @@
 #include <bf_svx/svxids.hrc>
 #endif
 
-#ifdef _USE_NAMESPACE
 namespace rtl
 {
-#endif
     class OUString;
-#ifdef _USE_NAMESPACE
 }
-#endif
 namespace binfilter {
 
 // class SvxBoxItem ------------------------------------------------------
