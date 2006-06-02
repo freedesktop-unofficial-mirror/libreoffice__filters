72bbdb481167088c62aded9feaf489725f1ae6cb
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:30:17 +0000

    INTEGRATION: CWS vgcleanupmacros (1.4.72); FILE MERGED 2006/05/17 11:09:23 vg 1.4.72.1: #i65486# cleanup:remove obsolete macros

--- binfilter/inc/bf_svx/widwitem.hxx
+++ binfilter/inc/bf_svx/widwitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: widwitem.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:22:24 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:30:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,14 +45,10 @@
 #include <bf_svx/svxids.hrc>
 #endif
 
-#ifdef _USE_NAMESPACE
 namespace rtl
 {
-#endif
     class OUString;
-#ifdef _USE_NAMESPACE
 }
-#endif
 namespace binfilter {
 class SvXMLUnitConverter;
 
