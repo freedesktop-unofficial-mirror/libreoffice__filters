b441906572c2ea0d77311e51c155a8022bef82eb
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:22:47 +0000

    INTEGRATION: CWS vgcleanupmacros (1.2.74); FILE MERGED 2006/05/17 11:09:16 vg 1.2.74.1: #i65486# cleanup:remove obsolete macros

--- binfilter/bf_xmloff/source/core/xmloff_i18nmap.cxx
+++ binfilter/bf_xmloff/source/core/xmloff_i18nmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_i18nmap.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:45:00 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:22:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,9 +50,7 @@
 #endif
 namespace binfilter {
 
-#ifdef _USE_NAMESPACE
 using namespace rtl;
-#endif
 
 class SvI18NMapEntry_Impl
 {
