4cdd3bebcd627cf53220b3f603c10fd6ee0f6165
Vladimir Glazounov <vg@openoffice.org>
2006-06-02 11:24:44 +0000

    INTEGRATION: CWS vgcleanupmacros (1.5.72); FILE MERGED 2006/05/17 11:09:19 vg 1.5.72.1: #i65486# cleanup:remove obsolete macros

--- binfilter/inc/bf_svx/colritem.hxx
+++ binfilter/inc/bf_svx/colritem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: colritem.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:11:02 $
+ *  last change: $Author: vg $ $Date: 2006-06-02 12:24:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -47,14 +47,10 @@
 #include <tools/color.hxx>
 #endif
 
-#ifdef _USE_NAMESPACE
 namespace rtl
 {
-#endif
     class OUString;
-#ifdef _USE_NAMESPACE
 }
-#endif
 namespace binfilter {
 class SvXMLUnitConverter;
 
