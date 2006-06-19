1b23c240b99b81c78538eb689a16f57e77e19229
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:15:15 +0000

    INTEGRATION: CWS warnings01 (1.3.14); FILE MERGED 2006/04/20 15:00:04 sb 1.3.14.1: #i53898# Made code compile and/or warning-free again after resync to SRC680m162.

--- binfilter/bf_xmloff/source/text/xmloff_XMLTextMarkImportContext.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLTextMarkImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_XMLTextMarkImportContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:32:49 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:15:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,10 +36,7 @@
 
 #include "XMLTextMarkImportContext.hxx"
 
-
-#ifndef _RTL_USTRING
-#include <rtl/ustring>
-#endif
+#include "rtl/ustring.hxx"
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
