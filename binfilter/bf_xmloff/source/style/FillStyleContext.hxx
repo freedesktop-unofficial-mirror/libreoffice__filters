53e7d0f17217f4b3feae0ac30e712d5343747520
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:10:41 +0000

    INTEGRATION: CWS warnings01 (1.2.14); FILE MERGED 2006/04/20 15:00:01 sb 1.2.14.1: #i53898# Made code compile and/or warning-free again after resync to SRC680m162.

--- binfilter/bf_xmloff/source/style/FillStyleContext.hxx
+++ binfilter/bf_xmloff/source/style/FillStyleContext.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: FillStyleContext.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:22:08 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:10:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,8 +44,8 @@
 #include <xmlstyle.hxx>
 #endif
 
-#ifndef _RTL_USTRING_
-#include<rtl/ustring>
+#ifndef _RTL_USTRING_HXX_
+#include "rtl/ustring.hxx"
 #endif
 namespace binfilter {
 
