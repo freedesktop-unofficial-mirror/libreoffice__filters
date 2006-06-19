9fa1f878e5f2672480f28862dd4d9cf7c07f7663
Jens-Heiner Rechtien <hr@openoffice.org>
2006-06-19 09:10:16 +0000

    INTEGRATION: CWS warnings01 (1.2.16); FILE MERGED 2006/04/20 15:00:00 sb 1.2.16.1: #i53898# Made code compile and/or warning-free again after resync to SRC680m162.

--- binfilter/bf_xmloff/source/draw/XMLNumberStylesExport.hxx
+++ binfilter/bf_xmloff/source/draw/XMLNumberStylesExport.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: XMLNumberStylesExport.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:48:49 $
+ *  last change: $Author: hr $ $Date: 2006-06-19 10:10:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,8 +40,8 @@
 #include <sal/types.h>
 #endif
 
-#ifndef _RTL_USTRING_
-#include <rtl/ustring>
+#ifndef _RTL_USTRING_HXX_
+#include "rtl/ustring.hxx"
 #endif
 namespace binfilter {
 
