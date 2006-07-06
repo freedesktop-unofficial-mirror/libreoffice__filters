bb7c8b6d1bd0b0967732be37ce34456353cdcb1d
Kurt Zenker <kz@openoffice.org>
2006-07-06 10:14:42 +0000

    INTEGRATION: CWS bfsixtyfour (1.5.72); FILE MERGED 2006/04/27 15:36:59 cmc 1.5.72.1: #i64815# mirror sfx2 changes in bf_sfx2

--- binfilter/inc/bf_sfx2/docinf.hxx
+++ binfilter/inc/bf_sfx2/docinf.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docinf.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:14:19 $
+ *  last change: $Author: kz $ $Date: 2006-07-06 11:14:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -130,7 +130,7 @@ private:
     String   aDefaultTarget;
     String   aReloadURL;
     BOOL     bReloadEnabled;
-    ULONG    nReloadSecs;
+    sal_uInt32 nReloadSecs;
 
     DateTime aTemplateDate;	// Stamp der Dokumentvorlage
 
