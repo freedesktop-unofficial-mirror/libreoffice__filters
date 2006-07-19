442cf746f0fbabeeebe01ebc19df2f37c8f3d838
Kurt Zenker <kz@openoffice.org>
2006-07-19 14:02:06 +0000

    INTEGRATION: CWS sb58 (1.2.98); FILE MERGED 2006/07/14 15:29:55 sb 1.2.98.1: #i67380# binfilter is treated like an external project, and is not going to be made warning-free.

--- binfilter/bf_sw/source/ui/inc/makefile.mk
+++ binfilter/bf_sw/source/ui/inc/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-09 05:42:12 $
+#   last change: $Author: kz $ $Date: 2006-07-19 15:02:06 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -32,3 +32,4 @@
 #     MA  02111-1307  USA
 #
 #*************************************************************************
+EXTERNAL_WARNINGS_NOT_ERRORS := TRUE
