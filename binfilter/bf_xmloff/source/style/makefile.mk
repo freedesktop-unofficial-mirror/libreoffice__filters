ce985541f7f43815e7b57aecd0db2cf637a526cc
Rüdiger Timm <rt@openoffice.org>
2006-07-25 08:15:59 +0000

    INTEGRATION: CWS rt15 (1.2.90); FILE MERGED 2006/07/03 07:55:19 rt 1.2.90.1: #i66824# Obsolete.

--- binfilter/bf_xmloff/source/style/makefile.mk
+++ binfilter/bf_xmloff/source/style/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.3 $
+#   $Revision: 1.4 $
 #
-#   last change: $Author: kz $ $Date: 2006-07-19 15:06:37 $
+#   last change: $Author: rt $ $Date: 2006-07-25 09:15:59 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,6 @@ PRJ=..$/..$/..
 
 PRJNAME=binfilter
 TARGET=xmloff_style
-AUTOSEG=true
 
 ENABLE_EXCEPTIONS=TRUE
 
