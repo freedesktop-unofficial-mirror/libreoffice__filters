932954251944b9e18d5f6274ad530d678ca19095
Rüdiger Timm <rt@openoffice.org>
2006-07-25 07:51:20 +0000

    INTEGRATION: CWS rt15 (1.4.90); FILE MERGED 2006/07/04 12:51:09 rt 1.4.90.2: Obsolet 2006/07/03 07:55:16 rt 1.4.90.1: #i66824# Obsolete.

--- binfilter/bf_sw/source/ui/utlui/makefile.mk
+++ binfilter/bf_sw/source/ui/utlui/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: kz $ $Date: 2006-07-19 15:04:04 $
+#   last change: $Author: rt $ $Date: 2006-07-25 08:51:20 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,21 +41,13 @@ BFPRJ=..$/..$/..
 PRJNAME=binfilter
 TARGET=sw_utlui
 
-#GEN_HID=TRUE
-#GEN_HID_OTHER=TRUE
 NO_HIDS=TRUE
-
-AUTOSEG=true
 
 # --- Settings -----------------------------------------------------
 
-#.INCLUDE :  $(PRJ)$/inc$/swpre.mk
 .INCLUDE :  $(PRJ)$/inc$/bf_sw$/swpre.mk
 .INCLUDE :  settings.mk
-#.INCLUDE :  $(PRJ)$/inc$/sw.mk
 .INCLUDE :  $(PRJ)$/inc$/bf_sw$/sw.mk
-#IMGLST_SRS=$(SRS)$/utlui.srs
-#BMP_IN=$(PRJ)$/win/imglst
 
 INC+= -I$(PRJ)$/inc$/bf_sw
 
@@ -63,22 +55,16 @@ INC+= -I$(PRJ)$/inc$/bf_sw
 
 CXXFILES = \
         sw_bookctrl.cxx \
-        sw_condedit.cxx \
         sw_content.cxx \
         sw_gloslst.cxx \
-        sw_glbltree.cxx \
         sw_initui.cxx \
         sw_navipi.cxx \
-        sw_navicfg.cxx \
-        sw_numfmtlb.cxx \
         sw_prcntfld.cxx \
         sw_tmplctrl.cxx \
         sw_uitool.cxx \
         sw_uiitems.cxx \
         sw_attrdesc.cxx \
         sw_swstbctl.cxx \
-        sw_shdwcrsr.cxx \
-        sw_unotools.cxx \
         sw_zoomctrl.cxx
 
 SRS1NAME=$(TARGET)
@@ -88,31 +74,22 @@ SRC1FILES =  \
         sw_navipi.src \
         sw_poolfmt.src \
         sw_attrdesc.src \
-        sw_unotools.src \
         sw_utlui.src
 
 SLOFILES =  \
         $(SLO)$/sw_bookctrl.obj \
-        $(SLO)$/sw_condedit.obj \
         $(SLO)$/sw_content.obj \
         $(SLO)$/sw_gloslst.obj \
-        $(SLO)$/sw_glbltree.obj \
         $(SLO)$/sw_initui.obj \
         $(SLO)$/sw_navipi.obj \
-        $(SLO)$/sw_navicfg.obj \
-        $(SLO)$/sw_numfmtlb.obj \
         $(SLO)$/sw_prcntfld.obj \
         $(SLO)$/sw_tmplctrl.obj \
         $(SLO)$/sw_uitool.obj \
         $(SLO)$/sw_uiitems.obj \
         $(SLO)$/sw_attrdesc.obj \
         $(SLO)$/sw_swstbctl.obj \
-        $(SLO)$/sw_shdwcrsr.obj \
-        $(SLO)$/sw_unotools.obj \
         $(SLO)$/sw_zoomctrl.obj
 
-EXCEPTIONSFILES= \
-        $(SLO)$/sw_unotools.obj
 
 # --- Tagets -------------------------------------------------------
 
