c5986f723ceaa9b36cb47406d683923aaf38e9c6
Rüdiger Timm <rt@openoffice.org>
2006-07-25 07:53:29 +0000

    INTEGRATION: CWS rt15 (1.4.90); FILE MERGED 2006/07/03 07:55:17 rt 1.4.90.1: #i66824# Obsolete.

--- binfilter/bf_sw/source/ui/web/makefile.mk
+++ binfilter/bf_sw/source/ui/web/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: kz $ $Date: 2006-07-19 15:04:16 $
+#   last change: $Author: rt $ $Date: 2006-07-25 08:53:29 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -44,8 +44,6 @@ TARGET=sw_web
 #GEN_HID=TRUE
 #GEN_HID_OTHER=TRUE
 NO_HIDS=TRUE
-
-AUTOSEG=true
 
 # --- Settings -----------------------------------------------------
 
