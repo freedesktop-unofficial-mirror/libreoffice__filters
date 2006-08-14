739dc250ae0ae366f42758554d73776ad4a570a8
Jens-Heiner Rechtien <hr@openoffice.org>
2006-08-14 14:14:53 +0000

    INTEGRATION: CWS hr35 (1.6.2); FILE MERGED 2006/08/03 17:01:17 hr 1.6.2.2: #i68002#: remove obsolete files 2006/08/02 17:26:57 hr 1.6.2.1: #i68002#: remove obsolete files

--- binfilter/bf_sd/source/ui/view/makefile.mk
+++ binfilter/bf_sd/source/ui/view/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2006-07-25 08:13:10 $
+#   last change: $Author: hr $ $Date: 2006-08-14 15:14:53 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,66 +55,6 @@ NO_HIDS=TRUE
 INC+= -I$(PRJ)$/inc$/bf_sd
 # --- Files --------------------------------------------------------
 
-CXXFILES = $(BFPRJ)$/util$/sd_sd.cxx \
-        sd_sdview.cxx		\
-        sd_sdview2.cxx 	\
-        sd_sdview3.cxx 	\
-        sd_sdview4.cxx 	\
-        sd_sdview5.cxx 	\
-        sd_viewshel.cxx	\
-        sd_viewshe2.cxx	\
-        sd_viewshe3.cxx	\
-        sd_sdwindow.cxx	\
-        sd_drviewsh.cxx	\
-        sd_drviews1.cxx	\
-        sd_drviews2.cxx	\
-        sd_drviews3.cxx	\
-        sd_drviews4.cxx	\
-        sd_drviews5.cxx	\
-        sd_drviews6.cxx	\
-        sd_drviews7.cxx	\
-        sd_drviews8.cxx	\
-        sd_drviews9.cxx	\
-        sd_drviewsa.cxx	\
-        sd_drviewsb.cxx	\
-        sd_drviewsc.cxx	\
-        sd_drviewsd.cxx	\
-        sd_drviewse.cxx	\
-        sd_drviewsf.cxx	\
-        sd_drviewsg.cxx	\
-        sd_drviewsi.cxx	\
-        sd_drviewsj.cxx	\
-        sd_drvwshrg.cxx	\
-        sd_drawview.cxx	\
-        sd_grviewsh.cxx	\
-        sd_slidvish.cxx	\
-        sd_slidview.cxx	\
-        sd_outlnvsh.cxx	\
-        sd_outlnvs2.cxx	\
-        sd_presvish.cxx	\
-        sd_prvwshll.cxx	\
-        sd_outlview.cxx	\
-        sd_tabcontr.cxx	\
-        sd_layertab.cxx	\
-        sd_showview.cxx	\
-        sd_showwin.cxx 	\
-        sd_bmcache.cxx 	\
-        sd_drstdob.cxx 	\
-        sd_grstdob.cxx 	\
-        sd_drbezob.cxx 	\
-        sd_drglueob.cxx 	\
-        sd_drtxtob.cxx 	\
-        sd_drtxtob1.cxx 	\
-        sd_sdruler.cxx 	\
-        sd_sdoutl.cxx		\
-        sd_SdOutlinerIterator.cxx \
-        sd_frmview.cxx 	\
-        sd_clview.cxx		\
-        sd_zoomlist.cxx	\
-        sd_unmodpg.cxx		\
-        sd_drgrfob.cxx		\
-        sd_WindowUpdater.cxx
-
 EXCEPTIONSFILES= \
         $(SLO)$/sd_sdoutl.obj  \
         $(SLO)$/sd_drviews1.obj \
@@ -132,12 +72,10 @@ SLOFILES =	\
         $(SLO)$/sd_sdview2.obj	\
         $(SLO)$/sd_sdview3.obj	\
         $(SLO)$/sd_sdview4.obj	\
-        $(SLO)$/sd_sdview5.obj	\
         $(SLO)$/sd_viewshel.obj \
         $(SLO)$/sd_viewshe2.obj \
         $(SLO)$/sd_viewshe3.obj \
         $(SLO)$/sd_sdwindow.obj \
-        $(SLO)$/sd_drviewsh.obj \
         $(SLO)$/sd_drviews1.obj \
         $(SLO)$/sd_drviews2.obj \
         $(SLO)$/sd_drviews3.obj \
@@ -146,14 +84,11 @@ SLOFILES =	\
         $(SLO)$/sd_drviews6.obj \
         $(SLO)$/sd_drviews7.obj \
         $(SLO)$/sd_drviews8.obj \
-        $(SLO)$/sd_drviews9.obj \
         $(SLO)$/sd_drviewsa.obj \
         $(SLO)$/sd_drviewsb.obj \
         $(SLO)$/sd_drviewsc.obj \
-        $(SLO)$/sd_drviewsd.obj \
         $(SLO)$/sd_drviewse.obj \
         $(SLO)$/sd_drviewsf.obj \
-        $(SLO)$/sd_drviewsg.obj \
         $(SLO)$/sd_drviewsi.obj \
         $(SLO)$/sd_drviewsj.obj \
         $(SLO)$/sd_drvwshrg.obj \
@@ -168,22 +103,16 @@ SLOFILES =	\
         $(SLO)$/sd_outlview.obj \
         $(SLO)$/sd_tabcontr.obj \
         $(SLO)$/sd_layertab.obj \
-        $(SLO)$/sd_showview.obj \
-        $(SLO)$/sd_showwin.obj	\
-        $(SLO)$/sd_bmcache.obj	\
         $(SLO)$/sd_drstdob.obj	\
         $(SLO)$/sd_grstdob.obj	\
         $(SLO)$/sd_drbezob.obj	\
         $(SLO)$/sd_drglueob.obj	\
         $(SLO)$/sd_drtxtob.obj	\
-        $(SLO)$/sd_drtxtob1.obj	\
         $(SLO)$/sd_sdruler.obj	\
         $(SLO)$/sd_sdoutl.obj  \
         $(SLO)$/sd_SdOutlinerIterator.obj \
         $(SLO)$/sd_frmview.obj	\
-        $(SLO)$/sd_clview.obj  \
         $(SLO)$/sd_zoomlist.obj	\
-        $(SLO)$/sd_unmodpg.obj	\
         $(SLO)$/sd_drgrfob.obj \
         $(SLO)$/sd_WindowUpdater.obj
 
