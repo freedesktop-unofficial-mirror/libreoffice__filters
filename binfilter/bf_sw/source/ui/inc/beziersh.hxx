d4de7ee2ff99ff3f0cd393d37be6f5d8358ff97c
Rüdiger Timm <rt@openoffice.org>
2006-10-27 23:32:18 +0000

    INTEGRATION: CWS hr38 (1.4.116); FILE MERGED 2006/09/11 13:36:42 hr 1.4.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sw/source/ui/inc/beziersh.hxx
+++ binfilter/bf_sw/source/ui/inc/beziersh.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: beziersh.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 05:20:19 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 00:32:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,13 +42,8 @@ class SwBezierShell: public SwBaseShell
 {
 public:
     SFX_DECL_INTERFACE(SW_BEZIERSHELL);
-//STRIP001 	TYPEINFO();
 
             SwBezierShell(SwView &rView);
-//STRIP001 			SwBezierShell(SwView &rView);
-//STRIP001 
-//STRIP001 	void	GetState(SfxItemSet &);
-//STRIP001 	void 	Execute(SfxRequest &);
 };
 
 } //namespace binfilter
