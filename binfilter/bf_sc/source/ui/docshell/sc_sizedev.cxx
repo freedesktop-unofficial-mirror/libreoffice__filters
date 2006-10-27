e3604d536b07fbea118407f065a092b744f59339
Rüdiger Timm <rt@openoffice.org>
2006-10-27 14:49:47 +0000

    INTEGRATION: CWS hr38 (1.4.120); FILE MERGED 2006/09/11 13:31:40 hr 1.4.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/ui/docshell/sc_sizedev.cxx
+++ binfilter/bf_sc/source/ui/docshell/sc_sizedev.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_sizedev.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:11:43 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 15:49:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,6 @@
  ************************************************************************/
 
 #ifdef PCH
-// auto strip #include "core_pch.hxx"
 #endif
 
 #pragma hdrstop
