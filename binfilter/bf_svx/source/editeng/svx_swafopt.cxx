86a1621ca569b8a5626ca7b15c080479040b7153
Rüdiger Timm <rt@openoffice.org>
2006-10-27 19:46:45 +0000

    INTEGRATION: CWS hr38 (1.4.118); FILE MERGED 2006/09/11 13:34:19 hr 1.4.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_svx/source/editeng/svx_swafopt.cxx
+++ binfilter/bf_svx/source/editeng/svx_swafopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_swafopt.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:27:48 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 20:46:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,9 +38,6 @@
 #ifndef _SV_KEYCODES_HXX //autogen
 #include <vcl/keycodes.hxx>
 #endif
-// auto strip #ifndef _STRING_HXX //autogen
-// auto strip #include <tools/string.hxx>
-// auto strip #endif
 
 #include "swafopt.hxx"
 namespace binfilter {
