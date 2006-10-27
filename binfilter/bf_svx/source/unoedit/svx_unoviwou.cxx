7c21264fc307cd15579ae7f771a89477896d69f8
Rüdiger Timm <rt@openoffice.org>
2006-10-27 20:59:43 +0000

    INTEGRATION: CWS hr38 (1.4.116); FILE MERGED 2006/09/11 13:35:24 hr 1.4.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_svx/source/unoedit/svx_unoviwou.cxx
+++ binfilter/bf_svx/source/unoedit/svx_unoviwou.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_unoviwou.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 07:34:21 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 21:59:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,13 +33,7 @@
  *
  ************************************************************************/
 
-// auto strip #ifndef _SV_OUTDEV_HXX
-// auto strip #include <vcl/outdev.hxx>
-// auto strip #endif
 
-// auto strip #ifndef _SV_WINDOW_HXX
-// auto strip #include <vcl/window.hxx>
-// auto strip #endif
 
 #include "unoviwou.hxx"
 #include "outliner.hxx"
