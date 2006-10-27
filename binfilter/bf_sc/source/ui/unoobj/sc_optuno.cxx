ad38abe08a697a00a497902d7032a0189647ee34
Rüdiger Timm <rt@openoffice.org>
2006-10-27 16:05:04 +0000

    INTEGRATION: CWS hr38 (1.4.120); FILE MERGED 2006/09/11 13:32:03 hr 1.4.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/ui/unoobj/sc_optuno.cxx
+++ binfilter/bf_sc/source/ui/unoobj/sc_optuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_optuno.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:16:43 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 17:04:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,19 +34,16 @@
  ************************************************************************/
 
 #ifdef PCH
-// auto strip #include "ui_pch.hxx"
 #endif
 
 #pragma hdrstop
 
-// auto strip #include <svtools/itemprop.hxx>
 
 #include <com/sun/star/util/Date.hpp>
 
 #include "optuno.hxx"
 #include "miscuno.hxx"
 #include "unonames.hxx"
-// auto strip #include "docoptio.hxx"
 #include "unoguard.hxx"
 namespace binfilter {
 
--- binfilter/bf_sc/source/ui/unoobj/sc_servuno.cxx
+++ binfilter/bf_sc/source/ui/unoobj/sc_servuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_servuno.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 21:17:21 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 17:05:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,12 +34,10 @@
  ************************************************************************/
 
 #ifdef PCH
-// auto strip #include "ui_pch.hxx"
 #endif
 
 #pragma hdrstop
 
-// auto strip #include <tools/debug.hxx>
 #include <svtools/unoimap.hxx>
 #include <bf_svx/unofill.hxx>
 
