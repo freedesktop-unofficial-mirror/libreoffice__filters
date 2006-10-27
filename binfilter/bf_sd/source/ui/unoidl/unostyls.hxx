e2d5a11ce90b224579c55d3ec6adc75fad73c829
Rüdiger Timm <rt@openoffice.org>
2006-10-27 17:39:39 +0000

    INTEGRATION: CWS hr38 (1.3.118); FILE MERGED 2006/09/05 11:00:11 hr 1.3.118.1: #i10000#: insert newline at end of file

--- binfilter/bf_sd/source/ui/unoidl/unostyls.hxx
+++ binfilter/bf_sd/source/ui/unoidl/unostyls.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unostyls.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:32:39 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 18:39:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
