980ae41da309440f8257367d3d74805c8f36f94b
Rüdiger Timm <rt@openoffice.org>
2006-10-27 21:48:42 +0000

    INTEGRATION: CWS hr38 (1.4.118); FILE MERGED 2006/09/11 13:35:58 hr 1.4.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sw/source/core/inc/splargs.hxx
+++ binfilter/bf_sw/source/core/inc/splargs.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: splargs.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 09:38:09 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 22:48:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,9 +50,6 @@ class SwTxtNode;
 class SwIndex;
 class SpellCheck;
 
-// auto strip #ifndef _COM_SUN_STAR_LINGUISTIC2_XSPELLALTERNATIVES_HPP_
-// auto strip #include <com/sun/star/linguistic2/XSpellAlternatives.hpp>
-// auto strip #endif
 #ifndef _COM_SUN_STAR_LINGUISTIC2_XSPELLCHECKER1_HPP_
 #include <com/sun/star/linguistic2/XSpellChecker1.hpp>
 #endif
