adef0a153d5ddda483f9e6c8c5877aa536c821c6
Rüdiger Timm <rt@openoffice.org>
2006-10-27 13:33:41 +0000

    INTEGRATION: CWS hr38 (1.4.118); FILE MERGED 2006/09/11 13:31:20 hr 1.4.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/core/tool/sc_indexmap.cxx
+++ binfilter/bf_sc/source/core/tool/sc_indexmap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_indexmap.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 17:10:17 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 14:33:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,6 @@
 
 
 #ifdef PCH
-// auto strip #include "core_pch.hxx"
 #endif
 
 #pragma hdrstop
