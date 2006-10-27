38908130f27b5eb3ee8f580f0ec47583490fe216
Rüdiger Timm <rt@openoffice.org>
2006-10-27 15:32:03 +0000

    INTEGRATION: CWS hr38 (1.3.120); FILE MERGED 2006/09/11 13:31:54 hr 1.3.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/ui/inc/undoolk.hxx
+++ binfilter/bf_sc/source/ui/inc/undoolk.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undoolk.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:27:22 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 16:32:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,10 +45,7 @@ class SdrUndoAction;
 class ScDocument;
 
 SdrUndoAction*	GetSdrUndoAction	( ScDocument* pDoc );
-//STRIP001 void			DoSdrUndoAction 	( SdrUndoAction* pUndoAction );
-//STRIP001 void			RedoSdrUndoAction 	( SdrUndoAction* pUndoAction );
 void			DeleteSdrUndoAction ( SdrUndoAction* pUndoAction );
-//STRIP001 void			EnableDrawAdjust	( ScDocument* pDoc, BOOL bEnable );
 
 
 
