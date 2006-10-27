a7e942f956411ea81c96c3fe639f5f80be4ab6b0
Rüdiger Timm <rt@openoffice.org>
2006-10-27 15:03:12 +0000

    INTEGRATION: CWS hr38 (1.4.120); FILE MERGED 2006/09/11 13:31:44 hr 1.4.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/ui/inc/content.hxx
+++ binfilter/bf_sc/source/ui/inc/content.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: content.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:37:24 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 16:03:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,17 +40,8 @@
 #include <svtools/svtreebx.hxx>
 #endif
 
-// auto strip #ifndef SC_SCGLOB_HXX
-// auto strip #include "global.hxx"
-// auto strip #endif
 namespace binfilter {
 
-//STRIP001 class ScNavigatorDlg;
-//STRIP001 class ScNavigatorSettings;
-//STRIP001 class ScDocument;
-//STRIP001 class ScDocShell;
-//STRIP001 class ScAreaLink;
-//STRIP001 
 /*N*/ #define SC_CONTENT_ROOT			0
 /*N*/ #define SC_CONTENT_TABLE		1
 /*N*/ #define SC_CONTENT_RANGENAME	2
@@ -68,107 +59,6 @@ const ULONG SC_CONTENT_NOCHILD  = ~0UL;
 //	TreeListBox fuer Inhalte
 //
 
-//STRIP001 class ScContentTree : public SvTreeListBox
-//STRIP001 {
-//STRIP001 	ScNavigatorDlg*		pParentWindow;
-//STRIP001 	ImageList			aEntryImages;
-//STRIP001 	ImageList			aHCEntryImages;
-//STRIP001 	SvLBoxEntry*		pRootNodes[SC_CONTENT_COUNT];
-//STRIP001 	USHORT				nRootType;			// als Root eingestellt
-//STRIP001 	String				aManualDoc;			// im Navigator umgeschaltet (Title)
-//STRIP001 	BOOL				bHiddenDoc;			// verstecktes aktiv?
-//STRIP001 	String				aHiddenName;		// URL zum Laden
-//STRIP001 	String				aHiddenTitle;		// fuer Anzeige
-//STRIP001 	ScDocument*			pHiddenDocument;	// temporaer
-//STRIP001 
-//STRIP001 	USHORT				pPosList[SC_CONTENT_COUNT];		// fuer die Reihenfolge
-//STRIP001 
-//STRIP001 	static BOOL	bIsInDrag;		// static, falls der Navigator im ExecuteDrag geloescht wird
-//STRIP001 
-//STRIP001 	ScDocShell* GetManualOrCurrent();
-//STRIP001 
-//STRIP001 	void	InitRoot(USHORT nType);
-//STRIP001 	void	ClearType(USHORT nType);
-//STRIP001 	void	ClearAll();
-//STRIP001 	void	InsertContent( USHORT nType, const String& rValue );
-//STRIP001 	void	GetDrawNames( USHORT nType, USHORT nId );
-//STRIP001 
-//STRIP001 	void	GetTableNames();
-//STRIP001 	void	GetAreaNames();
-//STRIP001 	void	GetDbNames();
-//STRIP001 	void	GetLinkNames();
-//STRIP001 	void	GetGraphicNames();
-//STRIP001 	void	GetOleNames();
-//STRIP001 	void	GetGroupNames();
-//STRIP001 	void	GetNoteStrings();
-//STRIP001 
-//STRIP001 	BOOL	DrawNamesChanged( USHORT nType, USHORT nId );
-//STRIP001 	BOOL	NoteStringsChanged();
-//STRIP001 
-//STRIP001 	ScAddress GetNotePos( ULONG nIndex );
-//STRIP001 	const ScAreaLink* GetLink( ULONG nIndex );
-//STRIP001 
-//STRIP001     /** Returns the indexes of the specified listbox entry.
-//STRIP001         @param rnRootIndex  Root index of specified entry is returned.
-//STRIP001         @param rnChildIndex  Index of the entry inside its root is returned (or SC_CONTENT_NOCHILD if entry is root).
-//STRIP001         @param pEntry  The entry to examine. */
-//STRIP001     void    GetEntryIndexes( USHORT& rnRootIndex, ULONG& rnChildIndex, SvLBoxEntry* pEntry ) const;
-//STRIP001 
-//STRIP001     /** Returns the child index of the specified listbox entry.
-//STRIP001         @param pEntry  The entry to examine or NULL for the selected entry.
-//STRIP001         @return  Index of the entry inside its root or SC_CONTENT_NOCHILD if entry is root. */
-//STRIP001     ULONG   GetChildIndex( SvLBoxEntry* pEntry ) const;
-//STRIP001 
-//STRIP001 	void	DoDrag();
-//STRIP001 	void	AdjustTitle();
-//STRIP001 
-//STRIP001 	ScDocument* GetSourceDocument();
-//STRIP001 
-//STRIP001 	DECL_LINK( DoubleClickHdl, ScContentTree* );
-//STRIP001 	DECL_STATIC_LINK( ScContentTree, ExecDragHdl, void* );
-//STRIP001 
-//STRIP001 protected:
-//STRIP001 //	virtual BOOL	Drop( const DropEvent& rEvt );
-//STRIP001 //	virtual BOOL	QueryDrop( DropEvent& rEvt );
-//STRIP001 
-//STRIP001 	virtual sal_Int8	AcceptDrop( const AcceptDropEvent& rEvt );
-//STRIP001 	virtual sal_Int8    ExecuteDrop( const ExecuteDropEvent& rEvt );
-//STRIP001 	virtual void		StartDrag( sal_Int8 nAction, const Point& rPosPixel );
-//STRIP001 	virtual void		DragFinished( sal_Int8 nAction );
-//STRIP001 
-//STRIP001 	virtual void	Command( const CommandEvent& rCEvt );
-//STRIP001 	virtual void	RequestHelp( const HelpEvent& rHEvt );
-//STRIP001 
-//STRIP001 public:
-//STRIP001 			ScContentTree( Window* pParent, const ResId& rResId );
-//STRIP001 			~ScContentTree();
-//STRIP001 
-//STRIP001     virtual void    MouseButtonDown( const MouseEvent& rMEvt );
-//STRIP001     virtual void    KeyInput( const KeyEvent& rKEvt );
-//STRIP001 
-//STRIP001 	void	InitWindowBits( BOOL bButtons );
-//STRIP001 
-//STRIP001     void    Refresh( USHORT nType = 0 );
-//STRIP001 
-//STRIP001 	void	ToggleRoot();
-//STRIP001 	void	SetRootType( USHORT nNew );
-//STRIP001 	USHORT	GetRootType() const				{ return nRootType; }
-//STRIP001 
-//STRIP001 	void	ActiveDocChanged();
-//STRIP001 	void	ResetManualDoc();
-//STRIP001 	void	SetManualDoc(const String& rName);
-//STRIP001 	BOOL	LoadFile(const String& rUrl);
-//STRIP001 	void	SelectDoc(const String& rName);
-//STRIP001 
-//STRIP001 	const String& GetHiddenTitle() const	{ return aHiddenTitle; }
-//STRIP001 
-//STRIP001     /** Applies the navigator settings to the listbox. */
-//STRIP001     void                        ApplySettings();
-//STRIP001     /** Stores the current listbox state in the navigator settings. */
-//STRIP001     void                        StoreSettings() const;
-//STRIP001 
-//STRIP001 	static BOOL	IsInDrag()	{ return bIsInDrag; }
-//STRIP001 };
 
 
 
