fa1b6fc833b55b2b68d5f616d68ebfed8b99ff7c
Rüdiger Timm <rt@openoffice.org>
2006-10-27 19:40:29 +0000

    INTEGRATION: CWS hr38 (1.3.118); FILE MERGED 2006/09/11 13:34:14 hr 1.3.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_svx/source/editeng/editsel.hxx
+++ binfilter/bf_svx/source/editeng/editsel.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editsel.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 05:18:33 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 20:40:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -80,7 +80,6 @@ public:
                     EditSelectionEngine();
 
     void			SetCurView( EditView* pNewView );
-//STRIP001 	EditView*		GetCurView();
 };
 
 }//end of namespace binfilter
