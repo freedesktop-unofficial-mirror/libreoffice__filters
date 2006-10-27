fac028b647fa8cf3e20fbb7d127ecdb9faf812de
Rüdiger Timm <rt@openoffice.org>
2006-10-27 21:16:38 +0000

    INTEGRATION: CWS hr38 (1.3.118); FILE MERGED 2006/09/11 13:35:31 hr 1.3.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sw/source/core/bastyp/sw_swcache.cxx
+++ binfilter/bf_sw/source/core/bastyp/sw_swcache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_swcache.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 08:21:08 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 22:16:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -656,10 +656,6 @@ void SwCache::Delete( const void *pOwner, const USHORT nIndex )
 |*************************************************************************/
 
 
-//STRIP001 BOOL SwCacheAccess::IsAvailable() const
-//STRIP001 {
-//STRIP001 	return pObj != 0;
-//STRIP001 }
 
 
 
