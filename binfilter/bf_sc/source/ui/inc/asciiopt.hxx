cdbb7063f089a323e5b89bb856222c0dbb41ffe4
Rüdiger Timm <rt@openoffice.org>
2006-10-27 15:00:16 +0000

    INTEGRATION: CWS hr38 (1.3.120); FILE MERGED 2006/09/11 13:31:43 hr 1.3.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/ui/inc/asciiopt.hxx
+++ binfilter/bf_sc/source/ui/inc/asciiopt.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: asciiopt.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:32:45 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 16:00:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -93,14 +93,11 @@ public:
                     ScAsciiOptions(const ScAsciiOptions& rOpt);
                     ~ScAsciiOptions();
 
-//STRIP001 	ScAsciiOptions&	operator=( const ScAsciiOptions& rCpy );
 
-//STRIP001 	BOOL			operator==( const ScAsciiOptions& rCmp ) const;
 
     void			ReadFromString( const String& rString );
     String			WriteToString() const;
 
-//STRIP001 	void			InterpretColumnList( const String& rString );
 
     CharSet				GetCharSet() const		{ return eCharSet; }
     BOOL				GetCharSetSystem() const	{ return bCharSetSystem; }
@@ -121,95 +118,11 @@ public:
     void	SetTextSep( sal_Unicode c )			{ cTextSep = c; }
     void	SetStartRow( long nRow)				{ nStartRow= nRow; }
 
-//STRIP001 	void	SetColInfo( USHORT nCount, const xub_StrLen* pStart, const BYTE* pFormat );
-//STRIP001     void    SetColumnInfo( const ScCsvExpDataVec& rDataVec );
 };
 
 
 // ============================================================================
 
-//STRIP001 class ScImportAsciiDlg : public ModalDialog
-//STRIP001 {
-//STRIP001     SvStream*                   pDatStream;
-//STRIP001     ULONG*                      pRowPosArray;
-//STRIP001     ULONG*                      pRowPosArrayUnicode;
-//STRIP001     USHORT                      nArrayEndPos;
-//STRIP001     USHORT                      nArrayEndPosUnicode;
-//STRIP001     ULONG                       nStreamPos;
-//STRIP001     ULONG                       nStreamPosUnicode;
-//STRIP001     BOOL                        bVFlag;
-//STRIP001 
-//STRIP001     FixedLine                   aFlFieldOpt;
-//STRIP001     FixedText                   aFtCharSet;
-//STRIP001 //STRIP001     SvxTextEncodingBox          aLbCharSet;
-//STRIP001 
-//STRIP001     FixedText                   aFtRow;
-//STRIP001     NumericField                aNfRow;
-//STRIP001 
-//STRIP001     FixedLine                   aFlSepOpt;
-//STRIP001     RadioButton                 aRbFixed;
-//STRIP001     RadioButton                 aRbSeparated;
-//STRIP001 
-//STRIP001     CheckBox                    aCkbTab;
-//STRIP001     CheckBox                    aCkbSemicolon;
-//STRIP001     CheckBox                    aCkbComma;
-//STRIP001     CheckBox                    aCkbSpace;
-//STRIP001     CheckBox                    aCkbOther;
-//STRIP001     Edit                        aEdOther;
-//STRIP001     CheckBox                    aCkbAsOnce;
-//STRIP001     FixedText                   aFtTextSep;
-//STRIP001     ComboBox                    aCbTextSep;
-//STRIP001 
-//STRIP001     FixedLine                   aFlWidth;
-//STRIP001     FixedText                   aFtType;
-//STRIP001     ListBox                     aLbType;
-//STRIP001 
-//STRIP001     ScCsvTableBox               maTableBox;
-//STRIP001 
-//STRIP001     OKButton                    aBtnOk;
-//STRIP001     CancelButton                aBtnCancel;
-//STRIP001     HelpButton                  aBtnHelp;
-//STRIP001 
-//STRIP001     String                      aCharSetUser;
-//STRIP001     String                      aColumnUser;
-//STRIP001     String                      aFldSepList;
-//STRIP001     String                      aTextSepList;
-//STRIP001 
-//STRIP001 	// aPreviewLine contains the byte string as read from the file
-//STRIP001     ByteString                  aPreviewLine[ CSV_PREVIEW_LINES ];
-//STRIP001 	// same for Unicode
-//STRIP001     String                      aPreviewLineUnicode[ CSV_PREVIEW_LINES ];
-//STRIP001 
-//STRIP001     CharSet                     meCharSet;          /// Selected char set.
-//STRIP001     bool                        mbCharSetSystem;    /// Is System char set selected?
-//STRIP001 
-//STRIP001 public:
-//STRIP001                                 ScImportAsciiDlg(
-//STRIP001                                     Window* pParent, String aDatName,
-//STRIP001 									SvStream* pInStream, sal_Unicode cSep = '\t' );
-//STRIP001                                 ~ScImportAsciiDlg();
-//STRIP001 
-//STRIP001     void                        GetOptions( ScAsciiOptions& rOpt );
-//STRIP001 
-//STRIP001 private:
-//STRIP001     /** Sets the selected char set data to meCharSet and mbCharSetSystem. */
-//STRIP001 	void                        SetSelectedCharSet();
-//STRIP001     /** Returns all separator characters in a string. */
-//STRIP001     String                      GetSeparators() const;
-//STRIP001 
-//STRIP001     /** Enables or disables all separator checkboxes and edit fields. */
-//STRIP001     void                        SetupSeparatorCtrls();
-//STRIP001 
-//STRIP001 //STRIP001     void                        UpdateVertical( bool bSwitchToFromUnicode = false );
-//STRIP001 
-//STRIP001                                 DECL_LINK( CharSetHdl, SvxTextEncodingBox* );
-//STRIP001                                 DECL_LINK( FirstRowHdl, NumericField* );
-//STRIP001                                 DECL_LINK( RbSepFixHdl, RadioButton* );
-//STRIP001                                 DECL_LINK( SeparatorHdl, Control* );
-//STRIP001                                 DECL_LINK( LbColTypeHdl, ListBox* );
-//STRIP001                                 DECL_LINK( UpdateTextHdl, ScCsvTableBox* );
-//STRIP001                                 DECL_LINK( ColTypeHdl, ScCsvTableBox* );
-//STRIP001 };
 
 
 // ============================================================================
