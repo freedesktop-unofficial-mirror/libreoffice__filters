1394406e784cd3724b1c481ef594396a1af79f82
Rüdiger Timm <rt@openoffice.org>
2006-10-27 14:24:18 +0000

    INTEGRATION: CWS hr38 (1.4.120); FILE MERGED 2006/09/11 13:31:26 hr 1.4.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/filter/xml/sc_XMLEmptyContext.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_XMLEmptyContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_XMLEmptyContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:23:40 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 15:24:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,6 @@
  ************************************************************************/
 
 #ifdef PCH
-// auto strip #include "filt_pch.hxx"
 #endif
 
 #pragma hdrstop
