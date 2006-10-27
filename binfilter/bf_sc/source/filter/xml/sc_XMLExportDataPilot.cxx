0949240e6d12621583bf4d6b4c3ef2e05684d378
Rüdiger Timm <rt@openoffice.org>
2006-10-27 14:24:46 +0000

    INTEGRATION: CWS hr38 (1.6.120); FILE MERGED 2006/09/11 13:31:26 hr 1.6.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/filter/xml/sc_XMLExportDataPilot.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_XMLExportDataPilot.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_XMLExportDataPilot.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 18:24:15 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 15:24:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,6 @@
  ************************************************************************/
 
 #ifdef PCH
-// auto strip #include "filt_pch.hxx"
 #endif
 
 #pragma hdrstop
@@ -45,9 +44,6 @@
 #include "XMLExportDataPilot.hxx"
 #endif
 
-// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
-// auto strip #include <bf_xmloff/xmltoken.hxx>
-// auto strip #endif
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include <bf_xmloff/xmlnmspe.hxx>
 #endif
