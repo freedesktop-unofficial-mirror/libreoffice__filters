377dcc9d7618acdb79607ddb0a6fa8764526f1e0
Rüdiger Timm <rt@openoffice.org>
2006-10-27 15:28:09 +0000

    INTEGRATION: CWS hr38 (1.3.120); FILE MERGED 2006/09/11 13:31:53 hr 1.3.120.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sc/source/ui/inc/tbinsert.hxx
+++ binfilter/bf_sc/source/ui/inc/tbinsert.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tbinsert.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 20:21:12 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 16:28:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,6 @@ class ScTbxInsertCtrl : public SfxToolBoxControl
 {
     USHORT 					nLastSlotId;
 
-//STRIP001 	virtual void			Select( BOOL bMod1 = FALSE );
 
 public:
     SFX_DECL_TOOLBOX_CONTROL();
@@ -61,8 +60,6 @@ public:
     ScTbxInsertCtrl( USHORT nId, ToolBox& rTbx, SfxBindings& rBind );
     ~ScTbxInsertCtrl();
 
-//STRIP001 	virtual SfxPopupWindowType	GetPopupWindowType() const;
-//STRIP001 	virtual SfxPopupWindow* 	CreatePopupWindow();
     virtual void				StateChanged( USHORT nSID,
                                               SfxItemState eState,
                                               const SfxPoolItem* pState );
@@ -72,29 +69,6 @@ public:
 //
 //----------------------------------------------------------------------------
 
-//STRIP001 class ScTbxInsertPopup : public SfxPopupWindow
-//STRIP001 {
-//STRIP001 	SfxToolBoxManager	aTbx;
-//STRIP001 	ResId 				aRIdWinTemp;
-//STRIP001 	ResId				aRIdTbxTemp;
-//STRIP001 
-//STRIP001 	Link				aTbxClickHdl;
-//STRIP001 
-//STRIP001 	DECL_LINK( TbxSelectHdl, ToolBox* );
-//STRIP001 	DECL_LINK( TbxClickHdl, ToolBox* );
-//STRIP001 
-//STRIP001 protected:
-//STRIP001 	virtual void		PopupModeEnd();
-//STRIP001 
-//STRIP001 public:
-//STRIP001 	ScTbxInsertPopup( USHORT nId, WindowAlign eAlign,
-//STRIP001 						const ResId& rRIdWin, const ResId& rRIdTbx,
-//STRIP001 						SfxBindings& rBindings );
-//STRIP001 	~ScTbxInsertPopup();
-//STRIP001 
-//STRIP001 	virtual SfxPopupWindow* Clone() const;
-//STRIP001 	void StartSelection();
-//STRIP001 };
 
 
 
