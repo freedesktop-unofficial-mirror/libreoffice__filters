2d920e419e8958bb8248252d956ea19b3c9d97d6
Rüdiger Timm <rt@openoffice.org>
2006-10-27 18:36:17 +0000

    INTEGRATION: CWS hr38 (1.5.118); FILE MERGED 2006/09/11 13:33:21 hr 1.5.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sfx2/source/inc/helper.hxx
+++ binfilter/bf_sfx2/source/inc/helper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: helper.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 03:28:35 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 19:36:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,13 +43,7 @@
 
 class String;
 
-// auto strip #ifndef  _COM_SUN_STAR_UCB_NAMECLASH_HPP_
-// auto strip #include <com/sun/star/ucb/NameClash.hpp>
-// auto strip #endif
 
-// auto strip #include <tools/string.hxx>
-// auto strip #include <tools/errcode.hxx>
-// auto strip #include <tools/bigint.hxx>
 
 namespace binfilter {
 
@@ -58,37 +52,16 @@ namespace binfilter {
 class SfxContentHelper
 {
 private:
-//STRIP001     static sal_Bool             Transfer_Impl( const String& rSource, const String& rDest, sal_Bool bMoveData,
-//STRIP001                                                     sal_Int32 nNameClash );
 
 public:
-//STRIP001 	static sal_Bool				IsDocument( const String& rContent );
-//STRIP001 	static sal_Bool				IsFolder( const String& rContent );
-//STRIP001 	static sal_Bool				GetTitle( const String& rContent, String& rTitle );
-//STRIP001 	static sal_Bool				Kill( const String& rContent );
 
-//STRIP001 	static ::com::sun::star::uno::Sequence< ::rtl::OUString >
-//STRIP001                                 GetFolderContents( const String& rFolder, sal_Bool bFolder, sal_Bool bSorted = sal_False );
-//STRIP001 	static ::com::sun::star::uno::Sequence< ::rtl::OUString >
-//STRIP001 								GetFolderContentProperties( const String& rFolder, sal_Bool bFolder );
     static ::com::sun::star::uno::Sequence< ::rtl::OUString >
                                 GetResultSet( const String& rURL );
-//STRIP001 	static ::com::sun::star::uno::Sequence< ::rtl::OUString >
-//STRIP001 								GetHelpTreeViewContents( const String& rURL );
     static String				GetActiveHelpString( const String& rURL );
-//STRIP001 	static sal_Bool				IsHelpErrorDocument( const String& rURL );
 
-//STRIP001 	static sal_Bool				CopyTo( const String& rSource, const String& rDest );
-//STRIP001     static sal_Bool             MoveTo( const String& rSource, const String& rDest, sal_Int32 nNameClash = ::com::sun::star::ucb::NameClash::ERROR );
 
-//STRIP001 	static sal_Bool				MakeFolder( const String& rFolder );
-//STRIP001 	static ErrCode				QueryDiskSpace( const String& rPath, sal_Int64& rFreeBytes );
-//STRIP001 	static ULONG				GetSize( const String& rContent );
-//STRIP001 	static sal_Bool				IsYounger( const String& rIsYoung, const String& rIsOlder );
 
     // please don't use this!
-//STRIP001 	static sal_Bool				Exists( const String& rContent );
-//STRIP001 	static sal_Bool				Find( const String& rFolder, const String& rName, String& rFile );
 };
 
 }//end of namespace binfilter
