332b20ed0408925794de26aa995d449f3cfff2cc
Rüdiger Timm <rt@openoffice.org>
2006-10-27 17:02:40 +0000

    INTEGRATION: CWS hr38 (1.3.116); FILE MERGED 2006/09/11 13:32:30 hr 1.3.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sd/source/core/sd_sdattr.cxx
+++ binfilter/bf_sd/source/core/sd_sdattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sd_sdattr.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 23:23:28 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 18:02:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -48,28 +48,6 @@ using namespace ::com::sun::star;
 /*N*/ TYPEINIT1( DiaEffectItem, SfxEnumItem );
 
 
-//STRIP001 DiaEffectItem::DiaEffectItem( presentation::FadeEffect eFE ) :
-//STRIP001 	SfxEnumItem( ATTR_DIA_EFFECT, eFE )
-//STRIP001 {
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 DiaEffectItem::DiaEffectItem( SvStream& rIn ) :
-//STRIP001 	SfxEnumItem( ATTR_DIA_EFFECT, rIn )
-//STRIP001 {
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 SfxPoolItem* DiaEffectItem::Clone( SfxItemPool* pPool ) const
-//STRIP001 {
-//STRIP001 	return new DiaEffectItem( *this );
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 SfxPoolItem* DiaEffectItem::Create( SvStream& rIn, USHORT nVer ) const
-//STRIP001 {
-//STRIP001 	return new DiaEffectItem( rIn );
-//STRIP001 }
 
 /*************************************************************************
 |*
@@ -79,28 +57,6 @@ using namespace ::com::sun::star;
 /*N*/ TYPEINIT1( DiaSpeedItem, SfxEnumItem );
 
 
-//STRIP001 DiaSpeedItem::DiaSpeedItem( FadeSpeed eFS ) :
-//STRIP001 	SfxEnumItem( ATTR_DIA_SPEED, eFS )
-//STRIP001 {
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 DiaSpeedItem::DiaSpeedItem( SvStream& rIn ) :
-//STRIP001 	SfxEnumItem( ATTR_DIA_SPEED, rIn )
-//STRIP001 {
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 SfxPoolItem* DiaSpeedItem::Clone( SfxItemPool* pPool ) const
-//STRIP001 {
-//STRIP001 	return new DiaSpeedItem( *this );
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 SfxPoolItem* DiaSpeedItem::Create( SvStream& rIn, USHORT nVer ) const
-//STRIP001 {
-//STRIP001 	return new DiaSpeedItem( rIn );
-//STRIP001 }
 
 /*************************************************************************
 |*
@@ -109,28 +65,6 @@ using namespace ::com::sun::star;
 *************************************************************************/
 /*N*/ TYPEINIT1( DiaAutoItem, SfxEnumItem );
 
-//STRIP001 DiaAutoItem::DiaAutoItem( PresChange eChange ) :
-//STRIP001 	SfxEnumItem( ATTR_DIA_AUTO, eChange )
-//STRIP001 {
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 DiaAutoItem::DiaAutoItem( SvStream& rIn ) :
-//STRIP001 	SfxEnumItem( ATTR_DIA_AUTO, rIn )
-//STRIP001 {
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 SfxPoolItem* DiaAutoItem::Clone( SfxItemPool* pPool ) const
-//STRIP001 {
-//STRIP001 	return new DiaAutoItem( *this );
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 SfxPoolItem* DiaAutoItem::Create( SvStream& rIn, USHORT nVer ) const
-//STRIP001 {
-//STRIP001 	return new DiaAutoItem( rIn );
-//STRIP001 }
 
 /*************************************************************************
 |*
@@ -140,22 +74,6 @@ using namespace ::com::sun::star;
 /*N*/ TYPEINIT1( DiaTimeItem, SfxUInt32Item );
 
 
-//STRIP001 DiaTimeItem::DiaTimeItem( UINT32 nValue ) :
-//STRIP001 		SfxUInt32Item( ATTR_DIA_TIME, nValue )
-//STRIP001 {
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 SfxPoolItem* DiaTimeItem::Clone( SfxItemPool* pPool ) const
-//STRIP001 {
-//STRIP001 	return new DiaTimeItem( *this );
-//STRIP001 }
-//STRIP001 
-//STRIP001 
-//STRIP001 int DiaTimeItem::operator==( const SfxPoolItem& rItem ) const
-//STRIP001 {
-//STRIP001 	return( ( (DiaTimeItem&) rItem ).GetValue() == GetValue() );
-//STRIP001 }
 
 
 
