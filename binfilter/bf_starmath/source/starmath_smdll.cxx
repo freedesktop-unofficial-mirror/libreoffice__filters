872b5dae3ac42dd84cb526d3f48d7c86fc8e5f39
Rüdiger Timm <rt@openoffice.org>
2006-10-27 18:57:27 +0000

    INTEGRATION: CWS hr38 (1.4.116); FILE MERGED 2006/09/11 13:33:38 hr 1.4.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_starmath/source/starmath_smdll.cxx
+++ binfilter/bf_starmath/source/starmath_smdll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: starmath_smdll.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:04:40 $
+ *  last change: $Author: rt $ $Date: 2006-10-27 19:57:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,9 +35,6 @@
 
 #pragma hdrstop
 
-// auto strip #ifndef _SOT_FACTORY_HXX //autogen
-// auto strip #include <sot/factory.hxx>
-// auto strip #endif
 #ifndef _SVX_SVXIDS_HRC //autogen
 #include <bf_svx/svxids.hrc>
 #endif
@@ -47,22 +44,13 @@
 #ifndef _SVX_ZOOMCTRL_HXX //autogen
 #include <bf_svx/zoomctrl.hxx>
 #endif
-// auto strip #ifndef _SFX_OBJFAC_HXX
-// auto strip #include <bf_sfx2/docfac.hxx>
-// auto strip #endif
 #ifndef _SVX_LBOXCTRL_HXX_
 #include <bf_svx/lboxctrl.hxx>
 #endif
 
-// auto strip #ifndef _SMDLL_HXX
-// auto strip #include <smdll.hxx>
-// auto strip #endif
 #ifndef DOCUMENT_HXX
 #include <document.hxx>
 #endif
-// auto strip #ifndef TOOLBOX_HXX
-// auto strip #include <toolbox.hxx>
-// auto strip #endif
 #ifndef VIEW_HXX
 #include <view.hxx>
 #endif
@@ -113,9 +101,6 @@ BOOL SmDLL::bInitialized = FALSE;
 /*N*/ 
 /*N*/ 	SmModule::RegisterInterface(pp);
 /*N*/ 
-//STRIP001 /*N*/ 	SmDocShell::Factory().RegisterMenuBar( SmResId(RID_SMMENU) );
-//STRIP001 /*N*/     SmDocShell::Factory().RegisterPluginMenuBar( SmResId(RID_SMPLUGINMENU) );
-//STRIP001 /*N*/ 	SmDocShell::Factory().RegisterAccel ( SmResId(RID_SMACCEL) );
 /*N*/ 	SmDocShell::RegisterInterface(pp);
 /*N*/ 
 /*N*/ 	SmViewShell::RegisterFactory(1);
@@ -126,8 +111,6 @@ BOOL SmDLL::bInitialized = FALSE;
 /*N*/ 	SvxUndoRedoControl::RegisterControl( SID_UNDO, pp );
 /*N*/ 	SvxUndoRedoControl::RegisterControl( SID_REDO, pp );
 /*N*/ 
-//STRIP001 /*N*/ 	SmToolBoxWrapper::RegisterChildWindow(TRUE);
-//STRIP001 /*N*/ 	SmCmdBoxWrapper::RegisterChildWindow(TRUE);
 /*N*/ }
 
 /*************************************************************************
