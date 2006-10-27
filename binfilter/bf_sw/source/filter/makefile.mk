7273f45eb0d3e8db5ccb153072c39dc16bfd4806
Rüdiger Timm <rt@openoffice.org>
2006-10-27 22:25:04 +0000

    INTEGRATION: CWS hr38 (1.5.28); FILE MERGED 2006/09/06 13:17:44 hr 1.5.28.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_sw/source/filter/makefile.mk
+++ binfilter/bf_sw/source/filter/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.5 $
+#   $Revision: 1.6 $
 #
-#   last change: $Author: kz $ $Date: 2006-07-19 14:55:25 $
+#   last change: $Author: rt $ $Date: 2006-10-27 23:25:04 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -41,8 +41,6 @@ BFPRJ=..$/..
 PRJNAME=binfilter
 TARGET=sw_filter
 
-#GEN_HID=TRUE
-#GEN_HID_OTHER=TRUE
 NO_HIDS=TRUE
 
 .IF "$(CALLTARGETS)"=="filter"
@@ -51,27 +49,12 @@ RC_SUBDIRS=
 
 # --- Settings -----------------------------------------------------
 
-#.INCLUDE :	$(PRJ)$/inc$/swpre.mk
 .INCLUDE :	$(PRJ)$/inc$/bf_sw$/swpre.mk
 .INCLUDE :	settings.mk
-#.INCLUDE :	$(PRJ)$/inc$/sw.mk
 .INCLUDE :	$(PRJ)$/inc$/bf_sw$/sw.mk
 INC+= -I$(PRJ)$/inc$/bf_sw
 # --- Files --------------------------------------------------------
 
-
-#SUBLIBS= \
-#	$(SLB)$/ascii.lib \
-#	$(SLB)$/basflt.lib \
-#	$(SLB)$/excel.lib \
-#	$(SLB)$/html.lib \
-#	$(SLB)$/lotus.lib \
-#	$(SLB)$/rtf.lib \
-#	$(SLB)$/w4w.lib \
-#	$(SLB)$/writer.lib \
-#	$(SLB)$/ww1.lib \
-#	$(SLB)$/ww8.lib \
-#	$(SLB)$/xml.lib
 
 SUBLIBS= \
     $(SLB)$/sw_ascii.lib \
@@ -79,20 +62,6 @@ SUBLIBS= \
     $(SLB)$/sw_writer.lib \
     $(SLB)$/sw_xml.lib
     
-.IF "$(product)" == ""
-#.IF "$(GUI)" == "WIN" || "$(GUI)" == "WNT" || "$(GUI)" == "UNX"
-#SUBLIBS+= \
-#	$(SLB)$/debug.lib
-#.ENDIF
-.ENDIF
-.IF "$(compact)" == ""
-#SUBLIBS+= \
-#	 $(SLB)$/sw6.lib
-.ENDIF
-
-# -----------------------------------------------------------
-
-
 ################################################################
 
 LIB1TARGET=$(SLB)$/sw_filter.lib
