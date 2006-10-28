d57314a446f8e11a30b7e95e2d3f866d2849bfeb
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:35:33 +0000

    INTEGRATION: CWS hr38 (1.3.116); FILE MERGED 2006/09/11 13:38:01 hr 1.3.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_sc/drwlayer.hxx
+++ binfilter/inc/bf_sc/drwlayer.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drwlayer.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:12:53 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:35:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -101,38 +101,27 @@ private:
 private:
     void			MoveAreaTwips( USHORT nTab, const Rectangle& rArea, const Point& rMove,
                                 const Point& rTopLeft );
-//STRIP001 	void			MoveCells( USHORT nTab, USHORT nCol1,USHORT nRow1, USHORT nCol2,USHORT nRow2,
-//STRIP001 								short nDx,short nDy );
-//STRIP001 	void			RecalcPos( SdrObject* pObj, ScDrawObjData* pData );
 
 public:
                     ScDrawLayer( ScDocument* pDocument, const String& rName );
     virtual			~ScDrawLayer();
 
     virtual SdrPage*  AllocPage(FASTBOOL bMasterPage);
-//STRIP001 	virtual SdrModel* AllocModel() const;
     virtual void	SetChanged( FASTBOOL bFlg = TRUE );
 
-//STRIP001 	virtual Window* GetCurDocViewWin();
     virtual SvStream* GetDocumentStream(SdrDocumentStreamInfo& rStreamInfo) const;
 
-//STRIP001 	virtual SdrLayerID GetControlExportLayerId( const SdrObject & ) const;
 
     void			ReleasePictureStorage();
 
-//STRIP001 	BOOL			HasObjects() const;
 
     void			ScAddPage( USHORT nTab );
-//STRIP001 	void			ScRemovePage( USHORT nTab );
     void			ScRenamePage( USHORT nTab, const String& rNewName );
-//STRIP001 	void			ScMovePage( USHORT nOldPos, USHORT nNewPos );
                     // inkl. Inhalt, bAlloc=FALSE -> nur Inhalt
-//STRIP001 	void			ScCopyPage( USHORT nOldPos, USHORT nNewPos, BOOL bAlloc );
 
     ScDocument*		GetDocument() const { return pDoc; }
 
     void			UpdateBasic();				// DocShell-Basic in DrawPages setzen
-//STRIP001 	void			UseHyphenator();
 
     void			Load( SvStream& rStream );
     void			Store( SvStream& rStream ) const;
@@ -148,35 +137,21 @@ public:
     BOOL			IsRecording()			{ return bRecording; }
     void			AddCalcUndo( SdrUndoAction* pUndo );
 
-//STRIP001 	void			MoveArea( USHORT nTab, USHORT nCol1,USHORT nRow1, USHORT nCol2,USHORT nRow2,
-//STRIP001 								short nDx,short nDy, BOOL bInsDel );
     void			WidthChanged( USHORT nTab, USHORT nCol, long nDifTwips );
     void			HeightChanged( USHORT nTab, USHORT nRow, long nDifTwips );
 
     BOOL			HasObjectsInRows( USHORT nTab, USHORT nStartRow, USHORT nEndRow );
 
-//STRIP001 	void			DeleteObjectsInArea( USHORT nTab, USHORT nCol1,USHORT nRow1,
-//STRIP001 											USHORT nCol2,USHORT nRow2 );
-//STRIP001 	void			DeleteObjectsInSelection( const ScMarkData& rMark );
-//STRIP001 	void			DeleteObjects( USHORT nTab );
 
-//STRIP001 	void			CopyToClip( ScDocument* pClipDoc, USHORT nTab, const Rectangle& rRange );
-//STRIP001 	void			CopyFromClip( ScDrawLayer* pClipModel,
-//STRIP001 									USHORT nSourceTab, const Rectangle& rSourceRange,
-//STRIP001 									const ScAddress& rDestPos, const Rectangle& rDestRange );
 
     void			SetPageSize( USHORT nPageNo, const Size& rSize );
-//STRIP001 	ULONG			GetDefTextHeight() const;
 
                     //	GetVisibleName: name for navigator etc: GetPersistName or GetName
                     //	(ChartListenerCollection etc. must use GetPersistName directly)
-//STRIP001 	static String	GetVisibleName( SdrObject* pObj );
 
     SdrObject*		GetNamedObject( const String& rName, USHORT nId, USHORT& rFoundTab ) const;
                     // if pnCounter != NULL, the search for a name starts with this index + 1,
                     // and the index really used is returned.
-//STRIP001     String          GetNewGraphicName( long* pnCounter = NULL ) const;
-//STRIP001 	void			EnsureGraphicNames();
 
     // Verankerung setzen und ermitteln
     static void		SetAnchor( SdrObject*, ScAnchorType );
@@ -188,14 +163,10 @@ public:
     // Image-Map
     static ScIMapInfo* GetIMapInfo( SdrObject* pObj );
 
-//STRIP001 	static Graphic	GetGraphicFromOle2Obj( const SdrOle2Obj* pOle2Obj );
-//STRIP001 	static IMapObject* GetHitIMapObject( SdrObject* pObject,
-//STRIP001 							const Point& rWinPoint, const Window& rCmpWnd );
 
 private:
     static SvPersist* pGlobalDrawPersist;			// fuer AllocModel
 public:
-//STRIP001 	static void		SetGlobalDrawPersist(SvPersist* pPersist);
 };
 
 
