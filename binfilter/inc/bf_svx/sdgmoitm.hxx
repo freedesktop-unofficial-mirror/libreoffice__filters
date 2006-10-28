7cfd18e18386db44d337e383610b778fada6164b
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:58:06 +0000

    INTEGRATION: CWS hr38 (1.3.114); FILE MERGED 2006/09/11 13:39:01 hr 1.3.114.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/sdgmoitm.hxx
+++ binfilter/inc/bf_svx/sdgmoitm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdgmoitm.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 14:19:26 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:58:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -65,11 +65,6 @@ public:
     virtual USHORT			GetValueCount() const;
     GraphicDrawMode			GetValue() const { return (GraphicDrawMode) SfxEnumItem::GetValue(); }
 
-//STRIP001 	virtual String			GetValueTextByPos( USHORT nPos ) const;
-//STRIP001 	virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
-//STRIP001 												 SfxMapUnit eCoreMetric,
-//STRIP001 												 SfxMapUnit ePresMetric,
-//STRIP001                                                  String& rText, const IntlWrapper * = 0) const;
 
 };
 
