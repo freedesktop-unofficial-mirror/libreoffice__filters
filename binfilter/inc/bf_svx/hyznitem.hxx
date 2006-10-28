a211064c226c1be37a54076cf3fdce12c6371af6
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:38:44 +0000

    INTEGRATION: CWS hr38 (1.5.42); FILE MERGED 2006/09/11 13:38:46 hr 1.5.42.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/hyznitem.hxx
+++ binfilter/inc/bf_svx/hyznitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hyznitem.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-06-02 12:26:05 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:38:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,10 +79,6 @@ public:
     virtual	sal_Bool        	 QueryValue( ::com::sun::star::uno::Any& rVal, BYTE nMemberId = 0 ) const;
     virtual	sal_Bool			 PutValue( const ::com::sun::star::uno::Any& rVal, BYTE nMemberId = 0 );
 
-//STRIP001 	virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
-//STRIP001 									SfxMapUnit eCoreMetric,
-//STRIP001 									SfxMapUnit ePresMetric,
-//STRIP001                                     String &rText, const IntlWrapper * = 0 ) const;
 
     virtual SfxPoolItem*	 Clone( SfxItemPool *pPool = 0 ) const;
     virtual SfxPoolItem*	 Create(SvStream &, USHORT) const;
