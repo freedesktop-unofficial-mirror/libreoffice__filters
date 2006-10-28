2d5ab389fe9642b27fa195ac291b406f4ddde43e
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:12:41 +0000

    INTEGRATION: CWS hr38 (1.3.118); FILE MERGED 2006/09/11 13:38:26 hr 1.3.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_starmath/config.hxx
+++ binfilter/inc/bf_starmath/config.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: config.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:47:27 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:12:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -69,7 +69,6 @@ public:
 
     SmFontPickList & GetFontPickList(USHORT nIdent) { return vFontPickList[nIdent]; }
     
-//STRIP001 	void ItemSetToConfig(const SfxItemSet &rSet);
     void ConfigToItemSet(SfxItemSet &rSet) const;
 };
 
