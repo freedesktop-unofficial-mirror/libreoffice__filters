4c3f2cce91a66d7519fc0493e5328cba2c2b28ce
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:13:46 +0000

    INTEGRATION: CWS hr38 (1.5.76); FILE MERGED 2006/09/11 13:38:27 hr 1.5.76.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_starmath/rect.hxx
+++ binfilter/inc/bf_starmath/rect.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rect.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-03-07 10:14:10 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:13:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -233,7 +233,6 @@ public:
     inline	Rectangle	AsRectangle() const;
             SmRect      AsGlyphRect() const;
 
-//STRIP001 			void 		Draw(OutputDevice &rDev, const Point &rPosition, int nFlags) const;
 };
 
 
