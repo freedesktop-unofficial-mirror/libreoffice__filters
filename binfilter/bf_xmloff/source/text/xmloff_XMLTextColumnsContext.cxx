2684145cc466a5cd4f3cd5ef67b30981501cbbfd
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:16:57 +0000

    INTEGRATION: CWS hr38 (1.4.116); FILE MERGED 2006/09/11 13:37:44 hr 1.4.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_xmloff/source/text/xmloff_XMLTextColumnsContext.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLTextColumnsContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_XMLTextColumnsContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:30:04 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:16:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,22 +36,10 @@
 #ifndef _COM_SUN_STAR_TEXT_XTEXTCOLUMNS_HPP_ 
 #include <com/sun/star/text/XTextColumns.hpp>
 #endif
-// auto strip #ifndef _COM_SUN_STAR_TEXT_TEXTCOLUMN_HPP_ 
-// auto strip #include <com/sun/star/text/TextColumn.hpp>
-// auto strip #endif
-// auto strip #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
-// auto strip #include <com/sun/star/lang/XMultiServiceFactory.hpp>
-// auto strip #endif
 #ifndef _COM_SUN_STAR_STYLE_VERTICALALIGNMENT_HPP_ 
 #include <com/sun/star/style/VerticalAlignment.hpp>
 #endif
-// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_ 
-// auto strip #include <com/sun/star/beans/XPropertySet.hpp>
-// auto strip #endif
 
-// auto strip #ifndef _XMLOFF_XMLTKMAP_HXX
-// auto strip #include "xmltkmap.hxx"
-// auto strip #endif
 #ifndef _XMLOFF_XMLUCONV_HXX 
 #include "xmluconv.hxx"
 #endif
@@ -64,9 +52,6 @@
 #ifndef _XMLOFF_XMLIMP_HXX
 #include "xmlimp.hxx"
 #endif
-// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
-// auto strip #include "xmltoken.hxx"
-// auto strip #endif
 #ifndef _XMLTEXTCOLUMNSCONTEXT_HXX
 #include "XMLTextColumnsContext.hxx"
 #endif
