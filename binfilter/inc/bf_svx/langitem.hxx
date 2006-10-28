6fa5cb3dc69b46791c54402ad24292dd22e88bff
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:41:10 +0000

    INTEGRATION: CWS hr38 (1.6.42); FILE MERGED 2006/09/11 13:38:47 hr 1.6.42.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/langitem.hxx
+++ binfilter/inc/bf_svx/langitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: langitem.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2006-06-02 12:26:46 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:41:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -69,10 +69,6 @@ public:
                      const USHORT nId = ITEMID_LANGUAGE );
 
     // "pure virtual Methoden" vom SfxPoolItem
-//STRIP001 	virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
-//STRIP001 									SfxMapUnit eCoreMetric,
-//STRIP001 									SfxMapUnit ePresMetric,
-//STRIP001                                     String &rText, const IntlWrapper * = 0 ) const;
 
     virtual SfxPoolItem*    Clone( SfxItemPool *pPool = 0 ) const;
     virtual SfxPoolItem*    Create(SvStream &, USHORT) const;
