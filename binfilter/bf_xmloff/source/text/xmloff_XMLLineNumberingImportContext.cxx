4bd8d85b395db7457665ce0bdcfa6f79bacd4f50
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:15:16 +0000

    INTEGRATION: CWS hr38 (1.3.116); FILE MERGED 2006/09/11 13:37:43 hr 1.3.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_xmloff/source/text/xmloff_XMLLineNumberingImportContext.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLLineNumberingImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_XMLLineNumberingImportContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:26:27 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:15:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,9 +41,6 @@
 #include "XMLLineNumberingSeparatorImportContext.hxx"
 #endif
 
-// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
-// auto strip #include "com/sun/star/beans/XPropertySet.hpp"
-// auto strip #endif
 
 #ifndef _COM_SUN_STAR_TEXT_XLINENUMBERINGPROPERTIES_HPP_
 #include "com/sun/star/text/XLineNumberingProperties.hpp"
@@ -52,9 +49,6 @@
 #ifndef _COM_SUN_STAR_STYLE_LINENUMBERPOSITION_HPP_
 #include <com/sun/star/style/LineNumberPosition.hpp>
 #endif
-// auto strip #ifndef _COM_SUN_STAR_STYLE_NUMBERINGTYPE_HPP_
-// auto strip #include <com/sun/star/style/NumberingType.hpp>
-// auto strip #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
 #include "xmlimp.hxx"
@@ -72,9 +66,6 @@
 #include "nmspmap.hxx"
 #endif
 
-// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
-// auto strip #include "xmltoken.hxx"
-// auto strip #endif
 
 #ifndef _XMLOFF_XMLNUMI_HXX
 #include "xmlnumi.hxx"
--- binfilter/bf_xmloff/source/text/xmloff_XMLLineNumberingSeparatorImportContext.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLLineNumberingSeparatorImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_XMLLineNumberingSeparatorImportContext.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:26:41 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:15:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,9 +53,6 @@
 #include "xmlnmspe.hxx"
 #endif
 
-// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
-// auto strip #include "xmltoken.hxx"
-// auto strip #endif
 
 #ifndef _XMLOFF_XMLUCONV_HXX
 #include "xmluconv.hxx"
