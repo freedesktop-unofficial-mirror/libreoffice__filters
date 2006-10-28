a9b7040cddf509261ac67a436430393d84967605
Rüdiger Timm <rt@openoffice.org>
2006-10-28 03:19:56 +0000

    INTEGRATION: CWS hr38 (1.4.118); FILE MERGED 2006/09/11 13:39:17 hr 1.4.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/txencbox.hxx
+++ binfilter/inc/bf_svx/txencbox.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txencbox.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:10:59 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 04:19:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,92 +45,12 @@
 #endif
 namespace binfilter {
 
-//STRIP001 class SvxTextEncodingTable;
 
 class SvxTextEncodingBox : public ListBox
 {
-//STRIP001 private:
-//STRIP001 	const SvxTextEncodingTable*		m_pEncTable;
-//STRIP001 
-//STRIP001 	USHORT				EncodingToPos_Impl( rtl_TextEncoding nEnc ) const;
-//STRIP001 
 public:
      SvxTextEncodingBox( Window* pParent, const ResId& rResId );
     ~SvxTextEncodingBox(){DBG_BF_ASSERT(0, "STRIP");} //STRIP001 ;
-//STRIP001 
-//STRIP001 	/** Fill with all known encodings but exclude those matching one or more
-//STRIP001 		given flags as defined in rtl/tencinfo.h
-//STRIP001         
-//STRIP001 	 	<p> If nButIncludeInfoFlags is given, encodings are included even if they
-//STRIP001 	 	match nExcludeInfoFlags. Thus it is possible to exclude 16/32-bit
-//STRIP001 	 	Unicode with RTL_TEXTENCODING_INFO_UNICODE but to include UTF7 and UTF8
-//STRIP001 	 	with RTL_TEXTENCODING_INFO_MIME </p>
-//STRIP001         
-//STRIP001         @param bExcludeImportSubsets
-//STRIP001             If <TRUE/>, some specific encodings are not listed, as they are a
-//STRIP001             subset of another encoding. This is the case for
-//STRIP001             RTL_TEXTENCODING_GB_2312, RTL_TEXTENCODING_GBK,
-//STRIP001             RTL_TEXTENCODING_MS_936, which are covered by
-//STRIP001             RTL_TEXTENCODING_GB_18030. Normally, this flag should be set to
-//STRIP001             <TRUE/> whenever the box is used in import dialogs. */
-//STRIP001 	void				FillFromTextEncodingTable(
-//STRIP001                             sal_Bool bExcludeImportSubsets = FALSE,
-//STRIP001 							sal_uInt32 nExcludeInfoFlags = 0,
-//STRIP001 							sal_uInt32 nButIncludeInfoFlags = 0
-//STRIP001 							);
-//STRIP001 
-//STRIP001     /** Fill with all encodings known to the dbtools::OCharsetMap but exclude
-//STRIP001         those matching one or more given flags as defined in rtl/tencinfo.h
-//STRIP001         
-//STRIP001 	 	<p> If nButIncludeInfoFlags is given, encodings are included even if they
-//STRIP001 	 	match nExcludeInfoFlags. Thus it is possible to exclude 16/32-bit
-//STRIP001 	 	Unicode with RTL_TEXTENCODING_INFO_UNICODE but to include UTF7 and UTF8
-//STRIP001 	 	with RTL_TEXTENCODING_INFO_MIME </p>
-//STRIP001         
-//STRIP001         @param bExcludeImportSubsets
-//STRIP001             If <TRUE/>, some specific encodings are not listed, as they are a
-//STRIP001             subset of another encoding. This is the case for
-//STRIP001             RTL_TEXTENCODING_GB_2312, RTL_TEXTENCODING_GBK,
-//STRIP001             RTL_TEXTENCODING_MS_936, which are covered by
-//STRIP001             RTL_TEXTENCODING_GB_18030. Normally, this flag should be set to
-//STRIP001             <TRUE/> whenever the box is used in import dialogs. */
-//STRIP001     void                FillFromDbTextEncodingMap(
-//STRIP001                             sal_Bool bExcludeImportSubsets = FALSE,
-//STRIP001 							sal_uInt32 nExcludeInfoFlags = 0,
-//STRIP001 							sal_uInt32 nButIncludeInfoFlags = 0
-//STRIP001 							);
-//STRIP001 
-//STRIP001     /** Fill with all known MIME encodings and select the best according to
-//STRIP001         <method>GetBestMimeEncoding</method>
-//STRIP001 	 */
-//STRIP001 	void				FillWithMimeAndSelectBest();
-//STRIP001 
-//STRIP001     /** Get the best MIME encoding matching the system locale, or if that isn't
-//STRIP001         determinable one that matches the UI locale, or UTF8 if everything else
-//STRIP001         fails.
-//STRIP001      */
-//STRIP001     static  rtl_TextEncoding    GetBestMimeEncoding();
-//STRIP001 
-//STRIP001 	const SvxTextEncodingTable*		GetTextEncodingTable() const
-//STRIP001 							{ return m_pEncTable; }
-//STRIP001 
-//STRIP001 	void				InsertTextEncoding( const rtl_TextEncoding nEnc,
-//STRIP001 							USHORT nPos = LISTBOX_APPEND );
-//STRIP001 
-//STRIP001 	void				InsertTextEncoding( const rtl_TextEncoding nEnc,
-//STRIP001 							const String& rEntry,
-//STRIP001 							USHORT nPos = LISTBOX_APPEND );
-//STRIP001 
-//STRIP001 	void				RemoveTextEncoding( const rtl_TextEncoding nEnc );
-//STRIP001 
-//STRIP001 	void				SelectTextEncoding( const rtl_TextEncoding nEnc,
-//STRIP001 							BOOL bSelect = TRUE );
-//STRIP001 
-//STRIP001 	rtl_TextEncoding	GetSelectTextEncoding() const;
-//STRIP001 
-//STRIP001 	const String&		GetSelectTextString() const;
-//STRIP001 
-//STRIP001 	BOOL				IsTextEncodingSelected( const rtl_TextEncoding nEnc ) const;
 };
 
 }//end of namespace binfilter
