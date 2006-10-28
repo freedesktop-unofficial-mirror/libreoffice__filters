413ccabb614767b19cafe8bb414fa65c6130b853
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:28:27 +0000

    INTEGRATION: CWS hr38 (1.4.118); FILE MERGED 2006/09/11 13:38:36 hr 1.4.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/drawuiks.hxx
+++ binfilter/inc/bf_svx/drawuiks.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: drawuiks.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:18:33 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:28:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,6 @@
 #ifndef _SDUIKS_HXX
 #define _SDUIKS_HXX
 
-// auto strip #include <usr/uiks.hxx>
 namespace binfilter {
 
 #define UIK_SHAPEFACTORY					UIK_DRAWING(00)
