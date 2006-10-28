caee968b1fa8704a0204bab559f50f797306934f
Rüdiger Timm <rt@openoffice.org>
2006-10-28 03:22:38 +0000

    INTEGRATION: CWS hr38 (1.5.70); FILE MERGED 2006/09/11 13:39:19 hr 1.5.70.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/unolingu.hxx
+++ binfilter/inc/bf_svx/unolingu.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unolingu.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: vg $ $Date: 2006-04-07 13:42:20 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 04:22:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,28 +42,13 @@
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
 #endif
-// auto strip #ifndef _VOS_REFERNCE_HXX_
-// auto strip #include <vos/refernce.hxx>
-// auto strip #endif
 
-// auto strip #ifndef _COM_SUN_STAR_UTIL_LANGUAGE_HPP_
-// auto strip #include <com/sun/star/util/Language.hpp>
-// auto strip #endif
-// auto strip #ifndef _COM_SUN_STAR_LANG_LOCALE_HPP_
-// auto strip #include <com/sun/star/lang/Locale.hpp>
-// auto strip #endif
 #ifndef _COM_SUN_STAR_LINGUISTIC2_XLINGUSERVICEMANAGER_HPP_
 #include <com/sun/star/linguistic2/XLinguServiceManager.hpp>
 #endif
 #ifndef _COM_SUN_STAR_LINGUISTIC2_XSPELLCHECKER1_HPP_
 #include <com/sun/star/linguistic2/XSpellChecker1.hpp>
 #endif
-// auto strip #ifndef _COM_SUN_STAR_LINGUISTIC2_XHYPHENATOR_HPP_
-// auto strip #include <com/sun/star/linguistic2/XHyphenator.hpp>
-// auto strip #endif
-// auto strip #ifndef _COM_SUN_STAR_LINGUISTIC2_XTHESAURUS_HPP_
-// auto strip #include <com/sun/star/linguistic2/XThesaurus.hpp>
-// auto strip #endif
 #ifndef _COM_SUN_STAR_LINGUISTIC2_XDICTIONARYLIST_HPP_
 #include <com/sun/star/linguistic2/XDictionaryList.hpp>
 #endif
@@ -143,14 +128,6 @@ class LinguMgr
         ::com::sun::star::linguistic2::XThesaurus > 	GetThes();
     static ::com::sun::star::uno::Reference<
         ::com::sun::star::linguistic2::XDictionaryList > GetDicList();
-//STRIP001 	static ::com::sun::star::uno::Reference<
-//STRIP001 		::com::sun::star::beans::XPropertySet > 		GetProp();
-//STRIP001 	static ::com::sun::star::uno::Reference<
-//STRIP001 		::com::sun::star::linguistic2::XDictionary1 >	GetStandard();
-//STRIP001 	static ::com::sun::star::uno::Reference<
-//STRIP001 		::com::sun::star::linguistic2::XDictionary1 >	GetIgnoreAll();
-//STRIP001 	static ::com::sun::star::uno::Reference<
-//STRIP001 		::com::sun::star::linguistic2::XDictionary1 >	GetChangeAll();
 
     // disallow access to copy-constructor and assignment-operator
     LinguMgr(const LinguMgr &);
@@ -169,15 +146,6 @@ public:
     static ::com::sun::star::uno::Reference<
         ::com::sun::star::beans::XPropertySet > 		GetLinguPropertySet();
 
-//STRIP001 	static ::com::sun::star::uno::Reference<
-//STRIP001 		::com::sun::star::linguistic2::XLinguServiceManager > GetLngSvcMgr();
-//STRIP001 
-//STRIP001 	static ::com::sun::star::uno::Reference<
-//STRIP001 		::com::sun::star::linguistic2::XDictionary1 > 	GetStandardDic();
-//STRIP001 	static ::com::sun::star::uno::Reference< 
-//STRIP001 		::com::sun::star::linguistic2::XDictionary1 > 	GetIgnoreAllList();
-//STRIP001 	static ::com::sun::star::uno::Reference< 
-//STRIP001 		::com::sun::star::linguistic2::XDictionary1 > 	GetChangeAllList();
 
     // update all configuration entries
     static void UpdateAll();
@@ -206,50 +174,14 @@ inline SvxAlternativeSpelling::SvxAlternativeSpelling() :
 {
 }
 
-//STRIP001 SvxAlternativeSpelling SvxGetAltSpelling( 
-//STRIP001 		const ::com::sun::star::uno::Reference< 
-//STRIP001 			::com::sun::star::linguistic2::XHyphenatedWord > & rHyphWord );
 
 ///////////////////////////////////////////////////////////////////////////
 
-//STRIP001 class SvxDicListChgClamp
-//STRIP001 {
-//STRIP001 private:
-//STRIP001 	::com::sun::star::uno::Reference< 
-//STRIP001 		::com::sun::star::linguistic2::XDictionaryList > 	xDicList;
-//STRIP001 
-//STRIP001 	// disallow access to copy-constructor and assignment-operator
-//STRIP001 	SvxDicListChgClamp(const SvxDicListChgClamp &);
-//STRIP001 	SvxDicListChgClamp & operator = (const SvxDicListChgClamp &);
-//STRIP001 
-//STRIP001 public:
-//STRIP001 	SvxDicListChgClamp( ::com::sun::star::uno::Reference< 
-//STRIP001 		::com::sun::star::linguistic2::XDictionaryList >  &rxDicList );
-//STRIP001 	~SvxDicListChgClamp();
-//STRIP001 };
 
 ///////////////////////////////////////////////////////////////////////////
 
 //TL:TODO: remove those functions or make them inline
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::linguistic2::XSpellChecker1 >	SvxGetSpellChecker();
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::linguistic2::XHyphenator >  	SvxGetHyphenator();
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::linguistic2::XThesaurus >  	SvxGetThesaurus();
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::linguistic2::XDictionaryList > SvxGetDictionaryList();
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::beans::XPropertySet > 		SvxGetLinguPropertySet();
 //TL:TODO: remove argument or provide SvxGetIgnoreAllList with the same one
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::linguistic2::XDictionary1 >  	SvxGetOrCreatePosDic( 
-//STRIP001 			::com::sun::star::uno::Reference< 
-//STRIP001 				::com::sun::star::linguistic2::XDictionaryList >  xDicList );
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::linguistic2::XDictionary1 >  	SvxGetIgnoreAllList();
-//STRIP001 ::com::sun::star::uno::Reference< 
-//STRIP001 	::com::sun::star::linguistic2::XDictionary1 >  	SvxGetChangeAllList();
 
 ///////////////////////////////////////////////////////////////////////////
 // misc functions
@@ -261,21 +193,9 @@ LanguageType 						SvxLocaleToLanguage(
         ::com::sun::star::lang::Locale& rLocale, LanguageType eLang );
 ::com::sun::star::lang::Locale		SvxCreateLocale( LanguageType eLang );
 
-//STRIP001 String	SvxGetDictionaryURL(const String &rDicName, 
-//STRIP001 							sal_Bool bIsUserDic = sal_True);
 
 //TL:TODO: soll mal den rictigen Rückgabetyp bekommen!
-//STRIP001 sal_Bool	SvxAddEntryToDic( 
-//STRIP001 	::com::sun::star::uno::Reference< 
-//STRIP001 		::com::sun::star::linguistic2::XDictionary >  &rxDic,
-//STRIP001 	const ::rtl::OUString &rWord, sal_Bool bIsNeg,
-//STRIP001 	const ::rtl::OUString &rRplcTxt, sal_Int16 nRplcLang, 
-//STRIP001 	sal_Bool bStripDot = sal_True );
-//STRIP001 short	SvxDicError( Window *pParent, sal_Int16 nError );
 
-//STRIP001 sal_Bool	SvxSaveDictionaries( 
-//STRIP001 	const ::com::sun::star::uno::Reference< 
-//STRIP001 		::com::sun::star::linguistic2::XDictionaryList >  &xDicList );
 
 }//end of namespace binfilter
 #endif
