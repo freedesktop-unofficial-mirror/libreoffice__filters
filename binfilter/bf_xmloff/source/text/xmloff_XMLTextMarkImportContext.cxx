65fdf1ff35795f2deb892dd3cedd125ab9edec26
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:18:27 +0000

    INTEGRATION: CWS hr38 (1.4.38); FILE MERGED 2006/09/11 13:37:45 hr 1.4.38.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_xmloff/source/text/xmloff_XMLTextMarkImportContext.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLTextMarkImportContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_XMLTextMarkImportContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:15:15 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:18:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,9 +46,6 @@
 #include "xmluconv.hxx"
 #endif
 
-// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
-// auto strip #include "xmltoken.hxx"
-// auto strip #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
 #include "xmlimp.hxx"
@@ -62,21 +59,12 @@
 #include "xmlnmspe.hxx"
 #endif
 
-// auto strip #ifndef _COM_SUN_STAR_XML_SAX_XATTRIBUTELIST_HPP_
-// auto strip #include <com/sun/star/xml/sax/XAttributeList.hpp>
-// auto strip #endif
 
 #ifndef _COM_SUN_STAR_TEXT_XTEXTCONTENT_HPP_
 #include <com/sun/star/text/XTextContent.hpp>
 #endif
 
-// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
-// auto strip #include <com/sun/star/beans/XPropertySet.hpp>
-// auto strip #endif
 
-// auto strip #ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
-// auto strip #include <com/sun/star/lang/XMultiServiceFactory.hpp>
-// auto strip #endif
 
 #ifndef _COM_SUN_STAR_CONTAINER_XNAMED_HPP_
 #include <com/sun/star/container/XNamed.hpp>
