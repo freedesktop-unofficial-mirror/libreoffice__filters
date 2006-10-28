e04f156a139305300e92f665edecd98e38b60e4f
Rüdiger Timm <rt@openoffice.org>
2006-10-28 03:31:49 +0000

    INTEGRATION: CWS hr38 (1.4.116); FILE MERGED 2006/09/11 13:39:25 hr 1.4.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/xmlcnitm.hxx
+++ binfilter/inc/bf_svx/xmlcnitm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlcnitm.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:39:12 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 04:31:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,14 +60,7 @@ public:
     virtual ~SvXMLAttrContainerItem();
 
     virtual int operator==( const SfxPoolItem& ) const;
-//STRIP001 	virtual int	Compare( const SfxPoolItem &rWith ) const;
 
-//STRIP001 	virtual SfxItemPresentation GetPresentation(
-//STRIP001 								SfxItemPresentation ePresentation,
-//STRIP001 								SfxMapUnit eCoreMetric,
-//STRIP001 								SfxMapUnit ePresentationMetric,
-//STRIP001 								XubString &rText,
-//STRIP001                                 const IntlWrapper *pIntlWrapper = 0 ) const;
 
     virtual sal_uInt16 GetVersion( sal_uInt16 nFileFormatVersion ) const;
 
