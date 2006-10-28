e4830eaba09bd52db3dea4e2d89607d9efdb4003
Rüdiger Timm <rt@openoffice.org>
2006-10-28 03:23:52 +0000

    INTEGRATION: CWS hr38 (1.6.42); FILE MERGED 2006/09/11 13:39:19 hr 1.6.42.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/wghtitem.hxx
+++ binfilter/inc/bf_svx/wghtitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wghtitem.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2006-06-02 12:29:48 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 04:23:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -70,15 +70,10 @@ public:
                     const USHORT nId = ITEMID_WEIGHT );
 
     // "pure virtual Methoden" vom SfxPoolItem + SfxEnumItem
-//STRIP001 	virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
-//STRIP001 									SfxMapUnit eCoreMetric,
-//STRIP001 									SfxMapUnit ePresMetric,
-//STRIP001                                     String &rText, const IntlWrapper * = 0 ) const;
 
     virtual SfxPoolItem*	Clone( SfxItemPool *pPool = 0 ) const;
     virtual SfxPoolItem*	Create(SvStream &, USHORT) const;
     virtual SvStream&		Store(SvStream &, USHORT nItemVersion) const;
-//STRIP001 	virtual String			GetValueTextByPos( USHORT nPos ) const;
     virtual USHORT			GetValueCount() const;
 
     virtual	sal_Bool        	 QueryValue( ::com::sun::star::uno::Any& rVal, BYTE nMemberId = 0 ) const;
@@ -86,7 +81,6 @@ public:
 
     virtual int 			HasBoolValue() const;
     virtual BOOL			GetBoolValue() const;
-//STRIP001 	virtual void			SetBoolValue( BOOL bVal );
 
     inline SvxWeightItem& operator=(const SvxWeightItem& rWeight) {
             SetValue( rWeight.GetValue() );
