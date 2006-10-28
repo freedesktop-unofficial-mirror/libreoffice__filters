52abc5ae7047cb43ee0803d02abdea6284a5eb09
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:43:13 +0000

    INTEGRATION: CWS hr38 (1.4.116); FILE MERGED 2006/09/11 13:38:49 hr 1.4.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/matrix3d.hxx
+++ binfilter/inc/bf_svx/matrix3d.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: matrix3d.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:50:20 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:43:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,9 +40,6 @@
 #include <goodies/hmatrix.hxx>
 #endif
 
-// auto strip #ifndef _SVX_VECTOR3D_HXX
-// auto strip #include <goodies/vector3d.hxx>
-// auto strip #endif
 namespace binfilter {
 
 /*************************************************************************
