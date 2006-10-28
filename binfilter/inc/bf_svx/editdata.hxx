6a5f5c3ffd571759184c172d992bff6f083dc3d5
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:29:22 +0000

    INTEGRATION: CWS hr38 (1.3.118); FILE MERGED 2006/09/11 13:38:37 hr 1.3.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/editdata.hxx
+++ binfilter/inc/bf_svx/editdata.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editdata.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:20:13 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:29:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -258,7 +258,6 @@ struct EFieldInfo
     ~EFieldInfo();
 
     EFieldInfo( const EFieldInfo& );
-//STRIP001     EFieldInfo& operator= ( const EFieldInfo& );
 };
 
 // -----------------------------------------------------------------------
