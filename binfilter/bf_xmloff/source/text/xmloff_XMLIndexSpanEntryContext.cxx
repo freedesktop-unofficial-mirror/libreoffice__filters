f62cd8b2d326c75109cc1fccb2e0ba292f56456b
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:13:11 +0000

    INTEGRATION: CWS hr38 (1.4.38); FILE MERGED 2006/09/11 13:37:41 hr 1.4.38.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_xmloff/source/text/xmloff_XMLIndexSpanEntryContext.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLIndexSpanEntryContext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_XMLIndexSpanEntryContext.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:14:24 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:13:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,9 +43,6 @@
 #include "XMLIndexTemplateContext.hxx"
 #endif
 
-// auto strip #ifndef _XMLOFF_XMLICTXT_HXX 
-// auto strip #include "xmlictxt.hxx"
-// auto strip #endif
 namespace binfilter {
 
 
