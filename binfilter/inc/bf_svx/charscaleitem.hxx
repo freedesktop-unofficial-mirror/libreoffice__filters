e741d03eca27015d76405ea4ad75592153279d20
Rüdiger Timm <rt@openoffice.org>
2006-10-28 02:22:52 +0000

    INTEGRATION: CWS hr38 (1.3.116); FILE MERGED 2006/09/11 13:38:32 hr 1.3.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_svx/charscaleitem.hxx
+++ binfilter/inc/bf_svx/charscaleitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: charscaleitem.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 13:08:50 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 03:22:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -68,11 +68,6 @@ public:
     virtual SvStream&		Store( SvStream& , USHORT nItemVersion ) const;
     virtual USHORT			GetVersion( USHORT nFileVersion ) const;
 
-//STRIP001 	virtual SfxItemPresentation GetPresentation( SfxItemPresentation ePres,
-//STRIP001 									SfxMapUnit eCoreMetric,
-//STRIP001 									SfxMapUnit ePresMetric,
-//STRIP001 									String &rText,
-//STRIP001                                     const IntlWrapper * = 0 ) const;
 
     virtual sal_Bool PutValue( const ::com::sun::star::uno::Any& rVal,
                                     BYTE nMemberId );
