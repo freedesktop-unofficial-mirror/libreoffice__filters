255a0c54124a2d81d8c3c119b182375e1a6a1a27
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:12:27 +0000

    INTEGRATION: CWS hr38 (1.3.118); FILE MERGED 2006/09/11 13:37:41 hr 1.3.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_xmloff/source/text/xmloff_XMLIndexMarkExport.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_XMLIndexMarkExport.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_XMLIndexMarkExport.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:22:42 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:12:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,25 +41,10 @@
 #include <tools/debug.hxx>
 #endif
 
-// auto strip #ifndef _RTL_USTRING_HXX_
-// auto strip #include <rtl/ustring.hxx>
-// auto strip #endif
 
-// auto strip #ifndef _RTL_USTRBUF_HXX_
-// auto strip #include <rtl/ustrbuf.hxx>
-// auto strip #endif
 
-// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_ 
-// auto strip #include <com/sun/star/beans/XPropertySet.hpp>
-// auto strip #endif
 
-// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSETINFO_HPP_ 
-// auto strip #include <com/sun/star/beans/XPropertySetInfo.hpp>
-// auto strip #endif
 
-// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
-// auto strip #include "xmltoken.hxx"
-// auto strip #endif
 
 #ifndef _XMLOFF_XMLNMSPE_HXX
 #include "xmlnmspe.hxx"
