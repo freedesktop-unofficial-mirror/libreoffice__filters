8b75820d5be660a50342bbf2428fe7e2e375e551
Rüdiger Timm <rt@openoffice.org>
2006-10-28 00:55:00 +0000

    INTEGRATION: CWS hr38 (1.4.118); FILE MERGED 2006/09/11 13:37:28 hr 1.4.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_xmloff/source/style/xmloff_MultiPropertySetHelper.cxx
+++ binfilter/bf_xmloff/source/style/xmloff_MultiPropertySetHelper.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_MultiPropertySetHelper.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 09:38:10 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 01:55:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,9 +37,6 @@
 #include "MultiPropertySetHelper.hxx"
 #endif
 
-// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSETINFO_HPP_
-// auto strip #include <com/sun/star/beans/XPropertySetInfo.hpp>
-// auto strip #endif
 
 #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
 #include <com/sun/star/beans/XPropertySet.hpp>
