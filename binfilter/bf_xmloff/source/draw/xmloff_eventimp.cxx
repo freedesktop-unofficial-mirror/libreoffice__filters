f9de5781bfbd8203d3c3871a2385fb9c6debc4dd
Rüdiger Timm <rt@openoffice.org>
2006-10-28 00:43:33 +0000

    INTEGRATION: CWS hr38 (1.5.38); FILE MERGED 2006/09/11 13:37:19 hr 1.5.38.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/bf_xmloff/source/draw/xmloff_eventimp.cxx
+++ binfilter/bf_xmloff/source/draw/xmloff_eventimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_eventimp.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2006-06-19 10:10:28 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 01:43:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,18 +38,9 @@
 #ifndef _COM_SUN_STAR_DOCUMENT_XEVENTSSUPPLIER_HPP_
 #include <com/sun/star/document/XEventsSupplier.hpp>
 #endif
-// auto strip #ifndef _COM_SUN_STAR_CONTAINER_XNAMEREPLACE_HPP_
-// auto strip #include <com/sun/star/container/XNameReplace.hpp>
-// auto strip #endif
 #ifndef _COM_SUN_STAR_PRESENTATION_ANIMATIONSPEED_HPP_
 #include <com/sun/star/presentation/AnimationSpeed.hpp>
 #endif
-// auto strip #ifndef _COM_SUN_STAR_BEANS_XPROPERTYSET_HPP_
-// auto strip #include <com/sun/star/beans/XPropertySet.hpp>
-// auto strip #endif
-// auto strip #ifndef _COM_SUN_STAR_XML_SAX_XATTRIBUTELIST_HPP_
-// auto strip #include <com/sun/star/xml/sax/XAttributeList.hpp>
-// auto strip #endif
 #ifndef _COM_SUN_STAR_PRESENTATION_CLICKACTION_HPP_
 #include <com/sun/star/presentation/ClickAction.hpp>
 #endif
@@ -62,9 +53,6 @@
 #include <comphelper/extract.hxx>
 #endif
 
-// auto strip #ifndef _XMLOFF_XMLTOKEN_HXX
-// auto strip #include "xmltoken.hxx"
-// auto strip #endif
 
 #ifndef _XMLOFF_XMLIMP_HXX
 #include "xmlimp.hxx"
