00c866507a6b5c4d9e0d94e586e5541d740f8e33
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:39:14 +0000

    INTEGRATION: CWS hr38 (1.4.116); FILE MERGED 2006/09/11 13:38:04 hr 1.4.116.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_sc/printopt.hxx
+++ binfilter/inc/bf_sc/printopt.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: printopt.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:22:32 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:39:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -64,34 +64,12 @@ public:
 
     void	SetDefaults();
 
-//STRIP001 	const ScPrintOptions&	operator=  ( const ScPrintOptions& rCpy );
-//STRIP001 	int						operator== ( const ScPrintOptions& rOpt ) const;
-//STRIP001 	int						operator!= ( const ScPrintOptions& rOpt ) const;
 };
 
 //==================================================================
 // item for the dialog / options page
 //==================================================================
 
-//STRIP001 class ScTpPrintItem : public SfxPoolItem
-//STRIP001 {
-//STRIP001 public:
-//STRIP001 				TYPEINFO();
-//STRIP001 				ScTpPrintItem( USHORT nWhich );
-//STRIP001 				ScTpPrintItem( USHORT nWhich,
-//STRIP001 							   const ScPrintOptions& rOpt );
-//STRIP001 				ScTpPrintItem( const ScTpPrintItem& rItem );
-//STRIP001 				~ScTpPrintItem();
-//STRIP001 
-//STRIP001 	virtual String          GetValueText() const;
-//STRIP001 	virtual int             operator==( const SfxPoolItem& ) const;
-//STRIP001 	virtual SfxPoolItem*    Clone( SfxItemPool *pPool = 0 ) const;
-//STRIP001 
-//STRIP001 	const ScPrintOptions&	GetPrintOptions() const { return theOptions; }
-//STRIP001 
-//STRIP001 private:
-//STRIP001 	ScPrintOptions theOptions;
-//STRIP001 };
 
 //==================================================================
 // config item
@@ -99,15 +77,11 @@ public:
 
 class ScPrintCfg : public ScPrintOptions, public ::utl::ConfigItem
 {
-//STRIP001 	::com::sun::star::uno::Sequence<rtl::OUString> GetPropertyNames();
 
 public:
     ScPrintCfg();
 
     void			SetOptions( const ScPrintOptions& rNew ){DBG_BF_ASSERT(0, "STRIP");} //STRIP001 	void			SetOptions( const ScPrintOptions& rNew );
-//STRIP001 	void			OptionsChanged();	// after direct access to ScPrintOptions base class
-//STRIP001 
-//STRIP001 	virtual void	Commit();
 };
 
 } //namespace binfilter
