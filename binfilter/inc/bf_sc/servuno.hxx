12f4daf014a81ed6e56a30118b95eba02adaa0ce
Rüdiger Timm <rt@openoffice.org>
2006-10-28 01:42:35 +0000

    INTEGRATION: CWS hr38 (1.5.118); FILE MERGED 2006/09/11 13:38:06 hr 1.5.118.1: #i69179#: clean up stripped files; remove obsolete files

--- binfilter/inc/bf_sc/servuno.hxx
+++ binfilter/inc/bf_sc/servuno.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: servuno.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:29:34 $
+ *  last change: $Author: rt $ $Date: 2006-10-28 02:42:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,9 +36,6 @@
 #ifndef SC_SERVUNO_HXX
 #define SC_SERVUNO_HXX
 
-// auto strip #ifndef _COM_SUN_STAR_UNO_XINTERFACE_HPP_
-// auto strip #include <com/sun/star/uno/XInterface.hpp>
-// auto strip #endif
 #ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
 #include <com/sun/star/uno/Sequence.hxx>
 #endif
