6f50df2c2d95fb3541b80ee30f69f7ef7d0778c3
Kurt Zenker <kz@openoffice.org>
2006-11-08 11:46:54 +0000

    INTEGRATION: CWS os71 (1.4.30); FILE MERGED 2006/11/07 09:35:17 os 1.4.30.3: RESYNC: (1.6-1.7); FILE MERGED 2006/08/30 12:46:42 os 1.4.30.2: RESYNC: (1.4-1.6); FILE MERGED 2006/03/28 14:51:27 mba 1.4.30.1: #i63714#: remove superfluous code

--- binfilter/bf_sw/source/ui/index/makefile.mk
+++ binfilter/bf_sw/source/ui/index/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2006-10-28 01:01:45 $
+#   last change: $Author: kz $ $Date: 2006-11-08 12:46:54 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,6 @@ SRC1FILES =  \
         sw_cnttab.src
 
 SLOFILES =  \
-        $(SLO)$/sw_idxmrk.obj \
         $(SLO)$/sw_toxmgr.obj
 
 # --- Tagets -------------------------------------------------------
