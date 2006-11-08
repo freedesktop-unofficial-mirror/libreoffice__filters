c0b68a3667f0299b1ce0f20545b5de401720b2f0
Kurt Zenker <kz@openoffice.org>
2006-11-08 11:42:38 +0000

    INTEGRATION: CWS os71 (1.4.30); FILE MERGED 2006/11/07 08:43:59 os 1.4.30.3: RESYNC: (1.6-1.7); FILE MERGED 2006/08/30 12:42:22 os 1.4.30.2: RESYNC: (1.4-1.6); FILE MERGED 2006/03/28 14:48:04 mba 1.4.30.1: #i63714#: remove superfluous code

--- binfilter/bf_sw/source/ui/config/makefile.mk
+++ binfilter/bf_sw/source/ui/config/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2006-10-28 00:02:24 $
+#   last change: $Author: kz $ $Date: 2006-11-08 12:42:38 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,6 @@ SRC1FILES =	\
         sw_optload.src
 
 SLOFILES =	\
-        $(SLO)$/sw_barcfg.obj \
         $(SLO)$/sw_caption.obj \
         $(SLO)$/sw_dbconfig.obj \
         $(SLO)$/sw_fontcfg.obj \
