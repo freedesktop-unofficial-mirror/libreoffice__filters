56593eeed8d81045bbf5947cb6de3706ff8e4d61
Kurt Zenker <kz@openoffice.org>
2006-11-08 12:10:08 +0000

    INTEGRATION: CWS os71 (1.5.30); FILE MERGED 2006/11/07 13:06:15 os 1.5.30.2: RESYNC: (1.5-1.6); FILE MERGED 2005/12/02 10:24:08 os 1.5.30.1: #i58773# move legacy filters to binfilter - first check-in

--- binfilter/inc/bf_sw/fmtftn.hxx
+++ binfilter/inc/bf_sw/fmtftn.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmtftn.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:43:39 $
+ *  last change: $Author: kz $ $Date: 2006-11-08 13:10:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -75,6 +75,7 @@ public:
 
     void SetNumStr( const String& rStr )	{ aNumber = rStr; }
     void SetNumber( USHORT nNo ) 			{ nNumber = nNo; }
+    void SetEndNote( BOOL b );
 
     void SetNumber( const SwFmtFtn& rFtn )
         { nNumber = rFtn.nNumber; aNumber = rFtn.aNumber; }
