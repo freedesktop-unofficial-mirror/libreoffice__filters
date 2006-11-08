9c65431675a240da7e0907bb43727109e55282a9
Kurt Zenker <kz@openoffice.org>
2006-11-08 12:02:03 +0000

    INTEGRATION: CWS os71 (1.4.30); FILE MERGED 2006/08/30 12:51:53 os 1.4.30.2: RESYNC: (1.4-1.6); FILE MERGED 2006/03/28 14:53:21 mba 1.4.30.1: #i63714#: remove superfluous code

--- binfilter/bf_sw/source/ui/utlui/makefile.mk
+++ binfilter/bf_sw/source/ui/utlui/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.6 $
+#   $Revision: 1.7 $
 #
-#   last change: $Author: rt $ $Date: 2006-07-25 08:51:20 $
+#   last change: $Author: kz $ $Date: 2006-11-08 13:02:03 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,20 +53,6 @@ INC+= -I$(PRJ)$/inc$/bf_sw
 
 # --- Files --------------------------------------------------------
 
-CXXFILES = \
-        sw_bookctrl.cxx \
-        sw_content.cxx \
-        sw_gloslst.cxx \
-        sw_initui.cxx \
-        sw_navipi.cxx \
-        sw_prcntfld.cxx \
-        sw_tmplctrl.cxx \
-        sw_uitool.cxx \
-        sw_uiitems.cxx \
-        sw_attrdesc.cxx \
-        sw_swstbctl.cxx \
-        sw_zoomctrl.cxx
-
 SRS1NAME=$(TARGET)
 SRC1FILES =  \
         sw_initui.src \
@@ -77,19 +63,11 @@ SRC1FILES =  \
         sw_utlui.src
 
 SLOFILES =  \
-        $(SLO)$/sw_bookctrl.obj \
-        $(SLO)$/sw_content.obj \
-        $(SLO)$/sw_gloslst.obj \
-        $(SLO)$/sw_initui.obj \
-        $(SLO)$/sw_navipi.obj \
-        $(SLO)$/sw_prcntfld.obj \
-        $(SLO)$/sw_tmplctrl.obj \
-        $(SLO)$/sw_uitool.obj \
-        $(SLO)$/sw_uiitems.obj \
-        $(SLO)$/sw_attrdesc.obj \
-        $(SLO)$/sw_swstbctl.obj \
-        $(SLO)$/sw_zoomctrl.obj
-
+                $(SLO)$/sw_attrdesc.obj \
+                $(SLO)$/sw_gloslst.obj \
+                $(SLO)$/sw_uiitems.obj \
+                $(SLO)$/sw_uitool.obj \
+                $(SLO)$/sw_initui.obj
 
 # --- Tagets -------------------------------------------------------
 
