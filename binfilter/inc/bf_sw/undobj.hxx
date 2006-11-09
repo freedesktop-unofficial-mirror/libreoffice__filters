aa6683676afa2802060b2fc7aa5a400b25b97a11
Rüdiger Timm <rt@openoffice.org>
2006-11-09 09:31:29 +0000

    #i10000# Remove conflict marker.

--- binfilter/inc/bf_sw/undobj.hxx
+++ binfilter/inc/bf_sw/undobj.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: undobj.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 13:13:33 $
+ *  last change: $Author: rt $ $Date: 2006-11-09 10:31:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -521,13 +521,6 @@ public:
     }
     OUT_UNDOBJ( TblNodesChg )
 };
-=======
-};
-
-
-
-
->>>>>>> 1.5
 
  class SwUndoTblMerge : public SwUndo, private SwUndRng
  {
