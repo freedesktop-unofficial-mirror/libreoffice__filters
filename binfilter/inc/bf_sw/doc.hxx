ffbdf66d1d4424f44d5eec4d1d3847a72b76ff66
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 11:24:55 +0000

    INTEGRATION: CWS aw024 (1.5.68); FILE MERGED 2006/11/10 14:35:29 aw 1.5.68.5: RESYNC: (1.8-1.9); FILE MERGED 2006/11/10 02:12:06 aw 1.5.68.4: RESYNC: (1.7-1.8); FILE MERGED 2006/08/03 14:45:14 aw 1.5.68.3: RESYNC: (1.6-1.7); FILE MERGED 2005/09/17 05:03:38 aw 1.5.68.2: RESYNC: (1.5-1.6); FILE MERGED 2005/03/23 15:41:35 aw 1.5.68.1: #i39528#

--- binfilter/inc/bf_sw/doc.hxx
+++ binfilter/inc/bf_sw/doc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: doc.hxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 13:09:41 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 12:24:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -103,25 +103,26 @@ namespace utl {
 namespace so3 {
     class SvLinkSource;
 };
-class DateTime;
-class JobSetup;
-class Color;
-class KeyCode;
-class OutputDevice;
-class Point;
-class SbxArray;
-class VirtualDevice;
-class SvEmbeddedObjectRef;
-class SvNumberFormatter;
-class SvPersist;
-class SvStorage;
-class SvStrings;
-class SvUShorts;
-class SvUShortsSort;
-class SvxMacro;
-class SvxMacroTableDtor;
-class GraphicObject;
+class DateTime; 
+class JobSetup; 
+class Color; 
+class KeyCode; 
+class OutputDevice; 
+class Point; 
+class SbxArray; 
+class VirtualDevice; 
+class SvEmbeddedObjectRef; 
+class SvNumberFormatter; 
+class SvPersist; 
+class SvStorage; 
+class SvStrings; 
+class SvUShorts; 
+class SvUShortsSort; 
+class SvxMacro; 
+class SvxMacroTableDtor; 
+
 namespace binfilter {
+class BfGraphicObject; 
 
 class SvxForbiddenCharactersTable;
 class SwExtTextInput;
@@ -978,13 +979,13 @@ public:
                         const SfxItemSet* pGrfAttrSet = 0,
                         SwFrmFmt* = 0 );
     SwFlyFrmFmt* Insert(const SwPaM &rRg,
-                        const GraphicObject& rGrfObj,
+                        const BfGraphicObject& rGrfObj,
                         const SfxItemSet* pFlyAttrSet = 0,
                         const SfxItemSet* pGrfAttrSet = 0,
                         SwFrmFmt* = 0 );
         // austauschen einer Grafik (mit Undo)
     void ReRead( SwPaM&, const String& rGrfName, const String& rFltName,
-                const Graphic* pGraphic, const GraphicObject* pGrfObj );
+                const Graphic* pGraphic, const BfGraphicObject* pGrfObj );
 
         //Einfuegen eines DrawObjectes. Das Object muss bereits im DrawModel
         // angemeldet sein.
