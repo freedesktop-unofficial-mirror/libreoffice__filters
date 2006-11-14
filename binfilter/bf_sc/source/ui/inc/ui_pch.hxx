609ab7998a5d0a168be960629d2a434aa85d2f8d
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 10:59:38 +0000

    INTEGRATION: CWS aw024 (1.3.44); FILE MERGED 2006/11/09 16:10:00 aw 1.3.44.5: RESYNC: (1.6-1.8); FILE MERGED 2006/08/03 13:28:12 aw 1.3.44.4: RESYNC: (1.5-1.6); FILE MERGED 2005/09/15 17:43:54 aw 1.3.44.3: RESYNC: (1.4-1.5); FILE MERGED 2005/05/13 13:46:32 aw 1.3.44.2: RESYNC: (1.3-1.4); FILE MERGED 2005/03/23 15:37:31 aw 1.3.44.1: #i39528#

--- binfilter/bf_sc/source/ui/inc/ui_pch.hxx
+++ binfilter/bf_sc/source/ui/inc/ui_pch.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ui_pch.hxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 16:30:27 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 11:59:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -334,7 +334,7 @@
 #include <drawview.hxx>
 #include <bf_svx/view3d.hxx>
 #include <bf_svx/def3d.hxx>
-#include <goodies/vector3d.hxx>
+#include <bf_goodies/vector3d.hxx>
 #include <vcl/floatwin.hxx>
 #include <basic/sbxobj.hxx>
 #include <bf_svx/svxenum.hxx>
