0afdceb1c1b7aee266af1aa78ee787b4c07f6431
Ivo Hinkelmann <ihi@openoffice.org>
2006-11-14 11:16:16 +0000

    INTEGRATION: CWS aw024 (1.1.84); FILE MERGED 2005/09/16 23:56:21 aw 1.1.84.2: RESYNC: (1.1-1.2); FILE MERGED 2005/03/23 15:40:49 aw 1.1.84.1: #i39528#

--- binfilter/bf_xmloff/source/draw/ximp3dobject.hxx
+++ binfilter/bf_xmloff/source/draw/ximp3dobject.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ximp3dobject.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:51:17 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 12:16:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@
 #endif
 
 #ifndef _SVX_VECTOR3D_HXX
-#include <goodies/vector3d.hxx>
+#include <bf_goodies/vector3d.hxx>
 #endif
 
 #ifndef _COM_SUN_STAR_DRAWING_DIRECTION3D_HPP_
