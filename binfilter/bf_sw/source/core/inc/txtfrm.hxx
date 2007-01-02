abbae0e4be9544f1ed29729becc0d8ab08ca3337
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 16:53:57 +0000

    INTEGRATION: CWS bfshrink01 (1.5.32); FILE MERGED 2006/12/05 17:06:44 cmc 1.5.32.1: #i64084# remove unused methods

--- binfilter/bf_sw/source/core/inc/txtfrm.hxx
+++ binfilter/bf_sw/source/core/inc/txtfrm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: txtfrm.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:50:18 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:53:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -461,20 +461,11 @@ public:
     // Calculates the coordinates of a point when switching from
     // vertical to horizontal layout.
     void SwitchVerticalToHorizontal( Point& rPoint ) const;
-    // Calculates the a limit value when switching from
-    // vertical to horizontal layout.
-    long SwitchVerticalToHorizontal( long nLimit ) const;
 
 #ifdef BIDI
-    // Calculates the coordinates of a rectangle when switching from
-    // LTR to RTL layout
-    void SwitchLTRtoRTL( SwRect& rRect ) const;
     // Calculates the coordinates of a point when switching from
     // LTR to RTL layout.
     void SwitchLTRtoRTL( Point& rPoint ) const;
-    // Calculates the coordinates of a rectangle when switching from
-    // RTL to LTR layout
-    inline void SwitchRTLtoLTR( SwRect& rRect ) const { SwitchLTRtoRTL( rRect ); }
     // Calculates the coordinates of a point when switching from
     // RTL to LTR layout.
     inline void SwitchRTLtoLTR( Point& rPoint ) const { SwitchLTRtoRTL( rPoint ); };
