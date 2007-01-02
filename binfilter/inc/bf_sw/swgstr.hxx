d1b8766c9ad2a520710ad4412235d226019c9cdd
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 17:47:05 +0000

    INTEGRATION: CWS bfshrink01 (1.6.14); FILE MERGED 2006/12/05 11:57:11 cmc 1.6.14.1: #i64084# remove unused methods

--- binfilter/inc/bf_sw/swgstr.hxx
+++ binfilter/inc/bf_sw/swgstr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swgstr.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 13:12:55 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 18:47:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -81,7 +81,6 @@ protected:
 public:
     ~swstreambase();
     SvStream& Strm()                    { return *pStrm; }
-    void clear();                       // Puffer loeschen
     sal_Char* GetBuf()                  { return pBuf; }
 
     // Zusatzfunktionen zur I/O von LONGs als 3-Byte-Zahlen
