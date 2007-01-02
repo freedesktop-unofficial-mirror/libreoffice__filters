a42d632cdf5cc09ae7046ed08e0561b35ec06677
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 16:53:01 +0000

    INTEGRATION: CWS bfshrink01 (1.4.32); FILE MERGED 2006/12/05 11:57:07 cmc 1.4.32.1: #i64084# remove unused methods

--- binfilter/bf_sw/source/core/inc/mvsave.hxx
+++ binfilter/bf_sw/source/core/inc/mvsave.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mvsave.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:46:56 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:53:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -98,8 +98,6 @@ void _SaveCntntIdx( SwDoc* pDoc, ULONG nNode, xub_StrLen nCntnt,
 void _RestoreCntntIdx( SwDoc* pDoc, SvULongs& rSaveArr,
 ULONG nNode, xub_StrLen nOffset = 0,
 BOOL bAuto = FALSE );
-void _RestoreCntntIdx( SvULongs& rSaveArr, const SwNode& rNd,
-                        xub_StrLen nLen, xub_StrLen nCorrLen );
 
 
 /** data structure to temporarily hold fly anchor positions relative to some
