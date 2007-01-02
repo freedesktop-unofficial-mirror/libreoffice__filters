87c9bbcf44f0a59eb99fcb650950d36008e304d9
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 17:10:11 +0000

    INTEGRATION: CWS bfshrink01 (1.10.14); FILE MERGED 2006/12/05 17:20:46 cmc 1.10.14.3: #i64084# remove unused methods 2006/12/05 17:13:21 cmc 1.10.14.2: #i64084# remove unused methods 2006/12/05 17:06:46 cmc 1.10.14.1: #i64084# remove unused methods

--- binfilter/bf_sw/source/ui/dochdl/sw_swdtflvr.cxx
+++ binfilter/bf_sw/source/ui/dochdl/sw_swdtflvr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_swdtflvr.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 12:44:12 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 18:10:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -184,145 +184,6 @@ using namespace ::com::sun::star::datatransfer;
 /*N*/ {
 /*N*/ 	return &xRef;
 /*N*/ }
-// -----------------------------------------------------------------------
-
-/*N*/ BOOL SwTransferable::IsPaste( const SwWrtShell& rSh,
-/*N*/ 							  const TransferableDataHelper& rData )
-/*N*/ {
-/*N*/     // Check the common case first: We can always paste our own data!
-/*N*/     // #106503#: If _only_ the internal format can be pasted, this check will
-/*N*/     // yield 'true', while the one below would give a (wrong) result 'false'.
-/*N*/     bool bIsPaste = ( SW_MOD()->pClipboard != NULL );
-/*N*/
-/*N*/     // if it's not our own data, we need to have a closer look:
-/*N*/     if( ! bIsPaste )
-/*N*/     {
-/*N*/         // determine the proper paste action, and return true if we find one
-/*N*/         Reference<XTransferable> xTransferable( rData.GetXTransferable() );
-/*N*/         USHORT nDestination = SwTransferable::GetSotDestination( rSh );
-/*N*/         USHORT nSourceOptions =
-/*N*/ 					(( EXCHG_DEST_DOC_TEXTFRAME == nDestination ||
-/*N*/ 					   EXCHG_DEST_SWDOC_FREE_AREA == nDestination ||
-/*N*/ 					   EXCHG_DEST_DOC_TEXTFRAME_WEB == nDestination ||
-/*N*/ 					   EXCHG_DEST_SWDOC_FREE_AREA_WEB == nDestination )
-/*N*/ 									? EXCHG_IN_ACTION_COPY
-/*N*/                      : EXCHG_IN_ACTION_MOVE);
-/*N*/
-/*N*/         ULONG nFormat;          // output param for GetExchangeAction
-/*N*/         USHORT nEventAction;    // output param for GetExchangeAction
-/*N*/         USHORT nAction = SotExchange::GetExchangeAction(
-/*N*/ 								rData.GetDataFlavorExVector(),
-/*N*/ 								nDestination,
-/*N*/ 								nSourceOptions,  			/* ?? */
-/*N*/ 								EXCHG_IN_ACTION_DEFAULT,    /* ?? */
-/*N*/ 								nFormat, nEventAction, 0,
-/*N*/ 								lcl_getTransferPointer ( xTransferable ) );
-/*N*/
-/*N*/         // if we find a suitable action, we can paste!
-/*N*/         bIsPaste = (EXCHG_INOUT_ACTION_NONE != nAction);
-/*N*/     }
-/*N*/
-/*N*/ 	return bIsPaste;
-/*N*/ }
-
-// -----------------------------------------------------------------------
-
-
-// -----------------------------------------------------------------------
-
-/*N*/  USHORT SwTransferable::GetSotDestination( const SwWrtShell& rSh,
-/*N*/  											const Point* pPt )
-/*N*/  {
-/*N*/  	USHORT nRet = EXCHG_INOUT_ACTION_NONE;
-/*N*/
-/*N*/  	ObjCntType eOType;
-/*N*/  	if( pPt )
-/*N*/  	{
-/*?*/  		DBG_BF_ASSERT(0, "STRIP"); //STRIP001 SdrObject *pObj = 0;
-/*N*/  	}
-/*N*/  	else
-/*N*/  		eOType = rSh.GetObjCntTypeOfSelection();
-/*N*/
-/*N*/  	switch( eOType )
-/*N*/  	{
-/*N*/  	case OBJCNT_GRF:
-/*N*/  		{
-/*?*/  			DBG_BF_ASSERT(0, "STRIP"); //STRIP001 BOOL bIMap, bLink;
-/*N*/  		}
-/*N*/  		break;
-/*N*/
-/*N*/  	case OBJCNT_FLY:
-/*?*/           DBG_BF_ASSERT(0, "STRIP");
-///*N*/       if( rSh.GetView().GetDocShell()->ISA(SwWebDocShell) )
-///*N*/           nRet = EXCHG_DEST_DOC_TEXTFRAME_WEB;
-///*N*/       else
-/*N*/           nRet = EXCHG_DEST_DOC_TEXTFRAME;
-/*N*/  		break;
-/*N*/  	case OBJCNT_OLE:		nRet = EXCHG_DEST_DOC_OLEOBJ;		break;
-/*N*/
-/*N*/  	case OBJCNT_CONTROL:	/* no Action avail */
-/*N*/  	case OBJCNT_SIMPLE:		nRet = EXCHG_DEST_DOC_DRAWOBJ; 		break;
-/*N*/  	case OBJCNT_URLBUTTON:	nRet = EXCHG_DEST_DOC_URLBUTTON; 	break;
-/*N*/  	case OBJCNT_GROUPOBJ:	nRet = EXCHG_DEST_DOC_GROUPOBJ;		break;
-/*N*/
-/*N*/  // was mmchen wir bei Mehrfachselektion???
-/*N*/  //	case OBJCNT_DONTCARE:
-/*N*/  	default:
-/*N*/  		{
-/*
- JP 13.07.98: Bug 52637: es wird ein URL-Feld erkannt also werden nur die
-                         Inhalte zugelassen. Das ist aber bestimmt nicht das
-                         gewollte.
-             SwContentAtPos aCntntAtPos( SwContentAtPos::SW_INETATTR );
-             SfxItemSet aSet( (SfxItemPool&)rSh.GetAttrPool(),
-                             RES_TXTATR_INETFMT, RES_TXTATR_INETFMT );
-             if( pPt ? ((SwWrtShell&)rSh).GetContentAtPos( *pPt, aCntntAtPos, FALSE )
-                      : (rSh.GetAttr( aSet ) && aSet.Count()) )
-                 nRet = EXCHG_DEST_DOC_URLFIELD;
-             else
-*/
-/*?*/           DBG_BF_ASSERT(0, "STRIP");
-/*N*/  				nRet = EXCHG_DEST_SWDOC_FREE_AREA;
-/*N*/  		}
-/*N*/  	}
-/*N*/
-/*N*/  	return nRet;
-/*N*/  }
-
-// -----------------------------------------------------------------------
-
-
-// -----------------------------------------------------------------------
-
-/*N*/ BOOL SwTransferable::IsPasteSpecial( const SwWrtShell& rWrtShell,
-/*N*/ 									 const TransferableDataHelper& rData )
-/*N*/ {
-/*N*/     // we can paste-special if there's an entry in the paste-special-format list
-/*N*/     SvxClipboardFmtItem aClipboardFmtItem(0);
-/*N*/     FillClipFmtItem( rWrtShell, rData, aClipboardFmtItem);
-/*N*/     return aClipboardFmtItem.Count() > 0;
-/*N*/ }
-
-// -----------------------------------------------------------------------
-
-
-// -----------------------------------------------------------------------
-
-/*N*/ int SwTransferable::_TestAllowedFormat( const TransferableDataHelper& rData,
-/*N*/ 										ULONG nFormat, USHORT nDestination )
-/*N*/ {
-/*N*/ 	USHORT nAction = EXCHG_INOUT_ACTION_NONE, nEventAction;
-/*N*/ 	if( rData.HasFormat( nFormat )) {
-/*N*/         Reference<XTransferable> xTransferable( rData.GetXTransferable() );
-/*N*/ 		nAction = SotExchange::GetExchangeAction(
-/*N*/ 						rData.GetDataFlavorExVector(),
-/*N*/ 						nDestination, EXCHG_IN_ACTION_COPY,
-/*N*/ 						EXCHG_IN_ACTION_COPY, nFormat,
-/*N*/ 						nEventAction, nFormat,
-/*N*/ 						lcl_getTransferPointer ( xTransferable ) );
-/*N*/     }
-/*N*/ 	return EXCHG_INOUT_ACTION_NONE != nAction;
-/*N*/ }
 
 // -----------------------------------------------------------------------
 
@@ -348,72 +209,6 @@ using namespace ::com::sun::star::datatransfer;
 /*N*/     0
 /*N*/ };
 
-
-
-/*N*/ void SwTransferable::FillClipFmtItem( const SwWrtShell& rSh,
-/*N*/ 								const TransferableDataHelper& rData,
-/*N*/ 								SvxClipboardFmtItem & rToFill )
-/*N*/ {
-/*N*/ 	USHORT nDest = SwTransferable::GetSotDestination( rSh );
-/*N*/ 	SwTransferable *pClipboard = SW_MOD()->pClipboard;
-/*N*/ 	if( pClipboard )
-/*N*/ 	{
-/*?*/ 		USHORT nResId;
-/*?*/ 		if( pClipboard->eBufferType & TRNSFR_DOCUMENT )
-/*?*/ 			nResId = STR_PRIVATETEXT;
-/*?*/ 		else if( pClipboard->eBufferType & TRNSFR_GRAPHIC )
-/*?*/ 			nResId = STR_PRIVATEGRAPHIC;
-/*?*/ 		else if( pClipboard->eBufferType == TRNSFR_OLE )
-/*?*/ 			nResId = STR_PRIVATEOLE;
-/*?*/ 		else
-/*?*/ 			nResId = 0;
-/*?*/
-/*?*/ 		if( nResId )
-/*?*/ 			rToFill.AddClipbrdFormat( SOT_FORMATSTR_ID_EMBED_SOURCE,
-/*?*/ 										SW_RES( nResId ) );
-/*N*/ 	}
-/*N*/ 	else
-/*N*/ 	{
-/*N*/ 		TransferableObjectDescriptor aDesc;
-/*N*/ 		if( rData.HasFormat( SOT_FORMATSTR_ID_OBJECTDESCRIPTOR ) )
-/*N*/ 			((TransferableDataHelper&)rData).GetTransferableObjectDescriptor(
-/*N*/ 								SOT_FORMATSTR_ID_OBJECTDESCRIPTOR, aDesc );
-/*N*/
-/*N*/ 		if( SwTransferable::_TestAllowedFormat( rData, SOT_FORMATSTR_ID_EMBED_SOURCE, nDest ))
-/*N*/ 			rToFill.AddClipbrdFormat( SOT_FORMATSTR_ID_EMBED_SOURCE,
-/*N*/ 											aDesc.maTypeName );
-/*N*/ 		if( SwTransferable::_TestAllowedFormat( rData, SOT_FORMATSTR_ID_LINK_SOURCE, nDest ))
-/*N*/ 			rToFill.AddClipbrdFormat( SOT_FORMATSTR_ID_LINK_SOURCE );
-/*N*/
-/*N*/ 		SotFormatStringId nFormat;
-/*N*/ 		if ( rData.HasFormat(nFormat = SOT_FORMATSTR_ID_EMBED_SOURCE_OLE) || rData.HasFormat(nFormat = SOT_FORMATSTR_ID_EMBEDDED_OBJ_OLE) )
-/*N*/ 		{
-/*N*/ 			String sName,sSource;
-/*N*/ 			if ( SvPasteObjectDialog::GetEmbeddedName(rData,sName,sSource,nFormat) )
-/*N*/ 				rToFill.AddClipbrdFormat( nFormat, sName );
-/*N*/ 		}
-/*N*/ 	}
-/*N*/
-/*N*/ #ifdef DDE_AVAILABLE
-/*N*/
-/*N*/ 	if( SwTransferable::_TestAllowedFormat( rData, SOT_FORMATSTR_ID_LINK, nDest ))
-/*N*/ 		rToFill.AddClipbrdFormat( SOT_FORMATSTR_ID_LINK, SW_RES(STR_DDEFORMAT) );
-/*N*/
-/*N*/ #endif
-/*N*/
-/*N*/ 	for( USHORT* pIds = aPasteSpecialIds; *pIds; ++pIds )
-/*N*/ 		if( SwTransferable::_TestAllowedFormat( rData, *pIds, nDest ))
-/*N*/ 			rToFill.AddClipbrdFormat( *pIds, aEmptyStr );
-/*N*/ }
-
-
-
-
-
-/*  */
-
-
-
 // Interfaces for Selection
 
 /*N*/ void SwTransferable::ClearSelection( SwWrtShell& rSh,
@@ -427,21 +222,6 @@ using namespace ::com::sun::star::datatransfer;
 /*N*/ 	{
 /*N*/ 		TransferableHelper::ClearSelection( rSh.GetWin() );
 /*N*/ 	}
-/*N*/ }
-/* -----------------3/31/2003 11:46AM----------------
-
- --------------------------------------------------*/
-/*N*/ const Sequence< sal_Int8 >& SwTransferable::getUnoTunnelId()
-/*N*/ {
-/*N*/     static Sequence< sal_Int8 > aSeq;
-/*N*/     if( !aSeq.getLength() )
-/*N*/     {
-/*N*/         static osl::Mutex           aCreateMutex;
-/*N*/         osl::Guard< osl::Mutex >    aGuard( aCreateMutex );
-/*N*/         aSeq.realloc( 16 );
-/*N*/         rtl_createUuid( reinterpret_cast< sal_uInt8* >( aSeq.getArray() ), 0, sal_True );
-/*N*/     }
-/*N*/     return aSeq;
 /*N*/ }
 /* -----------------3/31/2003 11:46AM----------------
 
