0ea0920e1397bf0e831c92453d53540daf2ee363
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 16:35:32 +0000

    INTEGRATION: CWS bfshrink01 (1.5.32); FILE MERGED 2006/12/06 16:51:29 cmc 1.5.32.1: #i64084# remove unused methods

--- binfilter/bf_svx/source/unodraw/shapeimpl.hxx
+++ binfilter/bf_svx/source/unodraw/shapeimpl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: shapeimpl.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 21:53:31 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:34:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -58,7 +58,6 @@ public:
 class SvxOle2Shape : public SvxShape
 {
 public:
-    SvxOle2Shape( SdrObject* pObj ) throw();
     SvxOle2Shape( SdrObject* pObject, const SfxItemPropertyMap* pPropertySet ) throw ();
     virtual ~SvxOle2Shape() throw();
 
--- binfilter/bf_svx/source/unodraw/svx_unofdesc.cxx
+++ binfilter/bf_svx/source/unodraw/svx_unofdesc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_unofdesc.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 21:55:02 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:35:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -234,19 +234,6 @@ void SvxUnoFontDescriptor::FillFromItemSet( const SfxItemSet& rSet, awt::FontDes
     case SFX_ITEM_SET: \
         return beans::PropertyState_DIRECT_VALUE; \
     }
-
-beans::PropertyState SvxUnoFontDescriptor::getPropertyState( const SfxItemSet& rSet )
-{
-    CheckState(rSet.GetItemState( EE_CHAR_FONTINFO, FALSE ));
-    CheckState(rSet.GetItemState( EE_CHAR_FONTHEIGHT, FALSE ));
-    CheckState(rSet.GetItemState( EE_CHAR_ITALIC, FALSE ));
-    CheckState(rSet.GetItemState( EE_CHAR_UNDERLINE, FALSE ));
-    CheckState(rSet.GetItemState( EE_CHAR_WEIGHT, FALSE ));
-    CheckState(rSet.GetItemState( EE_CHAR_STRIKEOUT, FALSE ));
-    CheckState(rSet.GetItemState( EE_CHAR_WLM, FALSE ));
-
-    return beans::PropertyState_DEFAULT_VALUE;
-}
 
 void SvxUnoFontDescriptor::setPropertyToDefault( SfxItemSet& rSet )
 {
