fc646b6bfc6b1c238e5bfc045f77ab6095eadb19
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 17:29:06 +0000

    INTEGRATION: CWS bfshrink01 (1.3.160); FILE MERGED 2006/12/07 15:30:30 cmc 1.3.160.1: #i64084# remove unused methods

--- binfilter/inc/bf_sc/textuno.hxx
+++ binfilter/inc/bf_sc/textuno.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: textuno.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:33:51 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 18:29:06 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -381,7 +381,6 @@ public:
                         ScEditEngineTextObj();
         virtual                         ~ScEditEngineTextObj() throw();
 
-    void				SetText( const String& rStr );
     void				SetText( const EditTextObject& rTextObject );
     EditTextObject*		CreateTextObject();
 };
