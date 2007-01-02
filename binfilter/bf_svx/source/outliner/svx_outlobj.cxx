49bc02690a6c2b2b59350d937c4562a459dea619
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 16:29:19 +0000

    INTEGRATION: CWS bfshrink01 (1.8.32); FILE MERGED 2006/12/11 18:08:55 cmc 1.8.32.1: #i64084# remove unused methods

--- binfilter/bf_svx/source/outliner/svx_outlobj.cxx
+++ binfilter/bf_svx/source/outliner/svx_outlobj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_outlobj.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 21:32:20 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:29:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -104,10 +104,6 @@ namespace binfilter {
 /*N*/ {
 /*N*/ 	DBG_CHKTHIS(OutlinerParaObject,0);
 /*N*/ 	return pText->ChangeStyleSheets( rOldName, eOldFamily, rNewName, eNewFamily );
-/*N*/ }
-
-/*N*/ void OutlinerParaObject::SetStyleSheets( USHORT nLevel, const XubString rNewName, const SfxStyleFamily& rNewFamily )
-/*N*/ {DBG_BF_ASSERT(0, "STRIP"); //STRIP001 
 /*N*/ }
 
 /*N*/ void OutlinerParaObject::Store(SvStream& rStream ) const
