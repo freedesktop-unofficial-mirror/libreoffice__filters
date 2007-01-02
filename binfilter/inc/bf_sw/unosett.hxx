cccb4e063a8695a1a5876d4db2664666bf4b4768
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 17:49:07 +0000

    INTEGRATION: CWS bfshrink01 (1.6.32); FILE MERGED 2006/12/05 16:47:18 cmc 1.6.32.1: #i64084# remove unused methods

--- binfilter/inc/bf_sw/unosett.hxx
+++ binfilter/inc/bf_sw/unosett.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unosett.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 05:01:47 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 18:49:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -211,7 +211,6 @@ class SwXNumberingRules : public cppu::WeakAggImplHelper5
 protected:
     virtual ~SwXNumberingRules();
 public:
-    SwXNumberingRules();
     SwXNumberingRules(SwDocShell& rDocSh);	// chapter numbering
     SwXNumberingRules(const SwNumRule& rRule); // NumRule for paragraphs, numbering styles
     SwXNumberingRules(SwDoc& rDoc); //create a new instance
