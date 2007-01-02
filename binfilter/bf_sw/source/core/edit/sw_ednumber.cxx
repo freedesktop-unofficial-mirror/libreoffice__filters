a62895672a82df01c30daff9376ee477dee94d9d
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 16:49:08 +0000

    INTEGRATION: CWS bfshrink01 (1.6.32); FILE MERGED 2006/12/06 14:50:12 cmc 1.6.32.1: #i64084# remove unused methods

--- binfilter/bf_sw/source/core/edit/sw_ednumber.cxx
+++ binfilter/bf_sw/source/core/edit/sw_ednumber.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_ednumber.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:34:26 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:48:45 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -66,136 +66,9 @@ namespace binfilter {
 /*N*/ SV_IMPL_VARARR_SORT( _SwPamRanges, SwPamRange )
 
 
-
-
-
-
-
-
-
-
-// Numerierung Outline Regelwerk
-
-
-
-
-
-// setzt, wenn noch keine Numerierung, sonst wird geaendert
-// arbeitet mit alten und neuen Regeln, nur Differenzen aktualisieren
-
-// Absaetze ohne Numerierung, aber mit Einzuegen
-
-// Loeschen, Splitten der Aufzaehlungsliste
-
-
-
-// Hoch-/Runterstufen
-
-
-
-
-
-
-
-
-// Abfrage von Oultine Informationen:
-
-
-
-
-
-
-
-
-
-
-
-// Outlines and SubOutline are ReadOnly?
-
-/** Test whether outline may be moved (bCopy == false) 
- *                           or copied (bCopy == true)
- * Verify these conditions:
- * 1) outline must be within main body (and not in redline)
- * 2) outline must not be within table
- * 3) if bCopy is set, outline must not be write protected
- */
-
-
-
-
-
-
-
-
-
-
-/*N*/ BYTE SwEditShell::GetNumLevel( BOOL* pHasChilds ) const
-/*N*/ {
-/*N*/ 	// gebe die akt. Ebene zurueck, auf der sich der Point vom Cursor befindet
-/*N*/ 	BYTE nLevel = NO_NUMBERING;
-/*N*/ 
-/*N*/ 	SwPaM* pCrsr = GetCrsr();
-/*N*/ 	const SwTxtNode* pTxtNd = pCrsr->GetNode()->GetTxtNode();
-/*N*/ 	const SwNumRule* pRule = pTxtNd->GetNumRule();
-/*N*/ 	if( pRule && pTxtNd->GetNum() )
-/*N*/ 	{
-/*N*/ 		nLevel = pTxtNd->GetNum()->GetLevel();
-/*N*/ 		if( pHasChilds )
-/*N*/ 		{
-/*?*/ 			*pHasChilds = FALSE;
-/*?*/ 			// dann teste ob die NumSection noch weitere UnterEbenen hat:
-/*?*/ 			// zuerst ueber alle TextNodes und falls da nichts gefunden
-/*?*/ 			// wurde, ueber die Formate und deren GetInfo bis zu den Nodes
-/*?*/ 
-/*?*/ 			BYTE nLvl = nLevel & ~NO_NUMLEVEL;
-/*?*/ 			if( nLvl + 1 < MAXLEVEL )
-/*?*/ 			{
-/*?*/ 				const String& rRule = pRule->GetName();
-/*?*/ 				SwModify* pMod;
-/*?*/ 				const SfxPoolItem* pItem;
-/*?*/ 				USHORT i, nMaxItems = GetDoc()->GetAttrPool().GetItemCount( RES_PARATR_NUMRULE);
-/*?*/ 				for( i = 0; i < nMaxItems; ++i )
-/*?*/ 					if( 0 != (pItem = GetDoc()->GetAttrPool().GetItem( RES_PARATR_NUMRULE, i ) ) &&
-/*?*/ 						0 != ( pMod = (SwModify*)((SwNumRuleItem*)pItem)->GetDefinedIn()) &&
-/*?*/ 						((SwNumRuleItem*)pItem)->GetValue().Len() &&
-/*?*/ 						((SwNumRuleItem*)pItem)->GetValue() == rRule &&
-/*?*/ 						pMod->IsA( TYPE( SwTxtNode )) &&
-/*?*/ 						((SwTxtNode*)pMod)->GetNodes().IsDocNodes() &&
-/*?*/ 						((SwTxtNode*)pMod)->GetNum() &&
-/*?*/ 						nLvl < ( ((SwTxtNode*)pMod)->GetNum()->GetLevel() & ~NO_NUMLEVEL) )
-/*?*/ 					{
-/*?*/ 						*pHasChilds = TRUE;
-/*?*/ 						break;
-/*?*/ 					}
-/*?*/ 
-/*?*/ 				if( !*pHasChilds )
-/*?*/ 				{
-/*?*/ 					DBG_BF_ASSERT(0, "STRIP"); //STRIP001 SwNRuleLowerLevel aHnt( rRule, nLvl );
-/*?*/ 				}
-/*?*/ 
-/*?*/ 			}
-/*N*/ 		}
-/*N*/ 	}
-/*N*/ 
-/*N*/ 	return nLevel;
-/*N*/ }
-
 /*N*/ const SwNumRule* SwEditShell::GetCurNumRule() const
 /*N*/ {
 /*N*/ 	return GetDoc()->GetCurrNumRule( *GetCrsr()->GetPoint() );
 /*N*/ }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
--- binfilter/bf_sw/source/core/edit/sw_edsect.cxx
+++ binfilter/bf_sw/source/core/edit/sw_edsect.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_edsect.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:34:47 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:49:08 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,92 +57,4 @@
 #endif
 
 namespace binfilter {
-
-
-    // SS fuer Bereiche
-
-
-
-
-/*N*/ const SwSection* SwEditShell::GetCurrSection() const
-/*N*/ {
-/*N*/ 	if( IsTableMode() )
-/*?*/ 		return 0;
-/*N*/ 
-/*N*/ 	return GetDoc()->GetCurrSection( *GetCrsr()->GetPoint() );
-/*N*/ }
-
-/*-----------------17.03.99 11:53-------------------
- * SwEditShell::GetAnySection liefert den fuer Spalten
- * zustaendigen Bereich, bei Fussnoten kann es nicht der
- * Bereich innerhalb der Fussnote sein.
- * --------------------------------------------------*/
-
-
-
-
-/*N*/ BOOL SwEditShell::IsAnySectionInDoc( BOOL bChkReadOnly, BOOL bChkHidden, BOOL bChkTOX ) const
-/*N*/ {
-/*N*/ 	const SwSectionFmts& rFmts = GetDoc()->GetSections();
-/*N*/ 	USHORT nCnt = rFmts.Count();
-        USHORT n=0;
-/*N*/ 	for( n = 0; n < nCnt; ++n )
-/*N*/ 	{
-/*N*/ 		SectionType eTmpType;
-/*N*/ 		const SwSectionFmt* pFmt = rFmts[ n ];
-/*N*/ 		if( pFmt->IsInNodesArr() &&
-/*N*/ 			(bChkTOX  ||
-/*N*/ 				(eTmpType = pFmt->GetSection()->GetType()) != TOX_CONTENT_SECTION
-/*N*/ 				&& TOX_HEADER_SECTION != eTmpType ))
-/*N*/ 		{
-/*N*/ 			const SwSection& rSect = *rFmts[ n ]->GetSection();
-/*N*/ 			if( (!bChkReadOnly && !bChkHidden ) ||
-/*N*/ 				(bChkReadOnly && rSect.IsProtectFlag() ) ||
-/*N*/ 				(bChkHidden && rSect.IsHiddenFlag() ) )
-/*N*/ 				break;
-/*N*/ 		}
-/*N*/ 	}
-/*N*/ 	return n != nCnt;
-/*N*/ }
-
-
-
-
-
-
-
-
-
-
-
-
-// search inside the cursor selection for full selected sections.
-// if any part of section in the selection return 0.
-// if more than one in the selection return the count
-
-
-/**
- * Find the suitable node for a special insert (alt-enter).
- * This should enable inserting text before/after sections and tables.
- *
- * A node is found if:
- * 1) the innermost table/section is not in a write-protected area
- * 2) pCurrentPos is at or just before an end node 
- *    (or at or just after a start node)
- * 3) there are only start/end nodes between pCurrentPos and the innermost
- *    table/section
- *
- * If a suitable node is found, an SwNode* is returned; else it is NULL.
- */
-
-
-/** a node can be special-inserted (alt-Enter) whenever lcl_SpecialInsertNode
-    finds a suitable position
-*/
-
-
-/** check whether a node cen be special-inserted (alt-Enter), and do so. Return
-    whether insertion was possible.
- */
-
 }
