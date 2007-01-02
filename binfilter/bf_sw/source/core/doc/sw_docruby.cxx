b4427dba80dc56d428fe8f729f1a79c28eb29156
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 16:45:39 +0000

    INTEGRATION: CWS bfshrink01 (1.6.32); FILE MERGED 2006/12/06 14:50:09 cmc 1.6.32.1: #i64084# remove unused methods

--- binfilter/bf_sw/source/core/doc/sw_docruby.cxx
+++ binfilter/bf_sw/source/core/doc/sw_docruby.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_docruby.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:25:09 $
+ *  last change: $Author: hr $ $Date: 2007-01-02 17:45:39 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -67,72 +67,6 @@ namespace binfilter {
 /*N*/ SV_IMPL_PTRARR( SwRubyList, SwRubyListEntryPtr )
 
 using namespace ::com::sun::star::i18n;
-
-
-/*
- * Members in the list:
- *   - String - the orig text
- *   - SwFmtRuby - the ruby attribut
- *
- *
- */
-/*N*/ USHORT SwDoc::FillRubyList( const SwPaM& rPam, SwRubyList& rList,
-/*N*/ 							USHORT nMode )
-/*N*/ {
-/*N*/ 	const SwPaM *_pStartCrsr = (SwPaM*)rPam.GetNext(),
-/*N*/ 				*__pStartCrsr = _pStartCrsr;
-/*N*/ 	BOOL bCheckEmpty = &rPam != _pStartCrsr;
-/*N*/ 	do {
-/*N*/ 		const SwPosition* pStt = _pStartCrsr->Start(),
-/*N*/ 				    	* pEnd = pStt == _pStartCrsr->GetPoint()
-/*N*/ 												? _pStartCrsr->GetMark()
-/*N*/ 												: _pStartCrsr->GetPoint();
-/*N*/ 		if( !bCheckEmpty || ( pStt != pEnd && *pStt != *pEnd ))
-/*N*/ 		{
-/*N*/ 			SwPaM aPam( *pStt );
-/*N*/ 			do {
-/*N*/ 				SwRubyListEntry* pNew = new SwRubyListEntry;
-/*N*/ 				if( pEnd != pStt )
-/*N*/ 				{
-/*N*/ 					aPam.SetMark();
-/*N*/ 					*aPam.GetMark() = *pEnd;
-/*N*/ 				}
-/*N*/ 				if( _SelectNextRubyChars( aPam, *pNew, nMode ))
-/*N*/ 				{
-/*N*/ 					rList.Insert( pNew, rList.Count() );
-/*N*/ 					aPam.DeleteMark();
-/*N*/ 				}
-/*N*/ 				else
-/*N*/ 				{
-/*N*/ 					delete pNew;
-/*N*/ 		 			if( *aPam.GetPoint() < *pEnd )
-/*N*/ 		 			{
-/*N*/ 						// goto next paragraph
-/*N*/ 						aPam.DeleteMark();
-/*N*/ 						aPam.Move( fnMoveForward, fnGoNode );
-/*N*/ 		 			}
-/*N*/ 		 			else
-/*N*/ 						break;
-/*N*/ 				}
-/*N*/ 			} while( 30 > rList.Count() && *aPam.GetPoint() < *pEnd );
-/*N*/ 		}
-/*N*/ 	} while( 30 > rList.Count() &&
-/*N*/ 		(_pStartCrsr=(SwPaM *)_pStartCrsr->GetNext()) != __pStartCrsr );
-/*N*/ 
-/*N*/ 	return rList.Count();
-/*N*/ }
-
-/*N*/ USHORT SwDoc::SetRubyList( const SwPaM& rPam, const SwRubyList& rList,
-/*N*/ 							USHORT nMode )
-/*N*/ {
-/*?*/ 	DBG_BF_ASSERT(0, "STRIP"); return 0;//STRIP001 StartUndo( UNDO_SETRUBYATTR );
-/*N*/ }
-
-/*N*/ BOOL SwDoc::_SelectNextRubyChars( SwPaM& rPam, SwRubyListEntry& rEntry,
-/*N*/ 									USHORT nMode )
-/*N*/ {
-    DBG_BF_ASSERT(0, "STRIP"); return FALSE;//STRIP001 // Point must be the startposition, Mark is optional the end position
-/*N*/ }
 
 /*N*/SwRubyListEntry::~SwRubyListEntry()
 /*N*/{
