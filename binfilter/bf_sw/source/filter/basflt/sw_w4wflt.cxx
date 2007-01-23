b9fd966e06a4234687dcd793b7cb9dd12e62ab2f
Oliver Bolte <obo@openoffice.org>
2007-01-23 07:27:19 +0000

    INTEGRATION: CWS swqbf90 (1.8.18); FILE MERGED 2006/12/05 15:11:42 os 1.8.18.1: #i71983# return type added

--- binfilter/bf_sw/source/filter/basflt/sw_w4wflt.cxx
+++ binfilter/bf_sw/source/filter/basflt/sw_w4wflt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_w4wflt.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2006-12-12 15:55:17 $
+ *  last change: $Author: obo $ $Date: 2007-01-23 08:27:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -79,7 +79,7 @@ typedef int (FAR PASCAL *FNw4wf)( LPSTR, LPSTR, LPSTR, LPSTR, FNDisplay );
 
 #endif      // defined( WIN ) || defined( WNT )
 
-// --------------------------------------------------------------------- 
+// ---------------------------------------------------------------------
 
 #ifdef UNX
 #include <stdlib.h>
