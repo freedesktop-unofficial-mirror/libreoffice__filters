824ed4723c65bd478c4dfb1b7009c7d89eb00bd4
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 11:48:02 +0000

    INTEGRATION: CWS ause074 (1.7.98); FILE MERGED 2007/01/30 12:03:23 hjs 1.7.98.1: #i73801# remove remaining usages of sv[pre].mk

--- binfilter/bf_starmath/util/makefile.mk
+++ binfilter/bf_starmath/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: kz $ $Date: 2006-07-19 13:34:44 $
+#   last change: $Author: vg $ $Date: 2007-02-06 12:48:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -49,9 +49,7 @@ LIBTARGET=NO
 
 # --- Settings -----------------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 INC+= -I$(PRJ)$/inc$/bf_starmath
 # --- Files --------------------------------------------------------
 
