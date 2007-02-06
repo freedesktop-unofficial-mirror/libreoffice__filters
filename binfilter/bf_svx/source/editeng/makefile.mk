a4e599cca3b05078bd0403b5b853c520848b25af
Vladimir Glazounov <vg@openoffice.org>
2007-02-06 11:48:46 +0000

    INTEGRATION: CWS ause074 (1.8.2); FILE MERGED 2007/01/30 12:03:23 hjs 1.8.2.1: #i73801# remove remaining usages of sv[pre].mk

--- binfilter/bf_svx/source/editeng/makefile.mk
+++ binfilter/bf_svx/source/editeng/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.8 $
+#   $Revision: 1.9 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 12:00:58 $
+#   last change: $Author: vg $ $Date: 2007-02-06 12:48:46 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -52,9 +52,7 @@ ENABLE_EXCEPTIONS=TRUE
 
 # --- Settings -----------------------------------------------------------
 
-.INCLUDE :  svpre.mk
 .INCLUDE :  settings.mk
-.INCLUDE :  sv.mk
 .INCLUDE :  $(BFPRJ)$/util$/makefile.pmk
 INC+= -I$(PRJ)$/inc$/bf_svx
 
