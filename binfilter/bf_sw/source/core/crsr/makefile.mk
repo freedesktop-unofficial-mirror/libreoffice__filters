84f1ac268a26a25db525f95d7d2398d90f3b5e61
Oliver Bolte <obo@openoffice.org>
2007-03-09 15:34:37 +0000

    INTEGRATION: CWS hr41 (1.6.72); FILE MERGED 2006/12/13 17:00:01 hr 1.6.72.1: #i72414#: remove obsolete file

--- binfilter/bf_sw/source/core/crsr/makefile.mk
+++ binfilter/bf_sw/source/core/crsr/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: hr $ $Date: 2007-01-02 17:41:58 $
+#   last change: $Author: obo $ $Date: 2007-03-09 16:34:37 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,61 +55,21 @@ CDEFS+=-Dmydebug
 
 # --- Files --------------------------------------------------------
 
-CXXFILES = \
-        sw_bookmrk.cxx \
-        sw_callnk.cxx \
-        sw_crbm.cxx \
-        sw_crsrsh.cxx \
-        sw_crstrvl1.cxx \
-        sw_findattr.cxx \
-        sw_findcoll.cxx \
-        sw_findtxt.cxx \
-        sw_pam.cxx \
-        sw_paminit.cxx \
-        sw_swcrsr.cxx \
-        sw_trvlcol.cxx \
-        sw_trvlfnfl.cxx \
-        sw_trvlreg.cxx \
-        sw_trvltbl.cxx \
-        sw_unocrsr.cxx \
-        sw_viscrs.cxx
-
-.IF "$(GUI)"=="MAC"
-CXXFILES += \
-        sw_crsrsh1.cxx
-.ENDIF
-.IF "$(mydebug)" != ""
-CXXFILES += \
-        sw_pamio.cxx
-.ENDIF
-
 SLOFILES =	\
         $(SLO)$/sw_bookmrk.obj \
         $(SLO)$/sw_callnk.obj \
         $(SLO)$/sw_crbm.obj \
         $(SLO)$/sw_crsrsh.obj \
-        $(SLO)$/sw_crstrvl1.obj \
         $(SLO)$/sw_findattr.obj \
         $(SLO)$/sw_findcoll.obj \
         $(SLO)$/sw_findtxt.obj \
         $(SLO)$/sw_pam.obj \
         $(SLO)$/sw_paminit.obj \
         $(SLO)$/sw_swcrsr.obj \
-        $(SLO)$/sw_trvlcol.obj \
-        $(SLO)$/sw_trvlfnfl.obj \
         $(SLO)$/sw_trvlreg.obj \
         $(SLO)$/sw_trvltbl.obj \
         $(SLO)$/sw_unocrsr.obj \
         $(SLO)$/sw_viscrs.obj
-
-.IF "$(GUI)"=="MAC"
-SLOFILES +=  \
-        $(SLO)$/sw_crsrsh1.obj
-.ENDIF
-.IF "$(mydebug)" != ""
-SLOFILES +=  \
-        $(SLO)$/sw_pamio.obj
-.ENDIF
 
 # --- Tagets -------------------------------------------------------
 
