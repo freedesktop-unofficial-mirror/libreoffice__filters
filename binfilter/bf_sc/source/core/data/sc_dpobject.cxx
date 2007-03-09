a8f601388aaa665edbbf0e1e94008890fab97656
Oliver Bolte <obo@openoffice.org>
2007-03-09 13:17:38 +0000

    INTEGRATION: CWS hr41 (1.5.30); FILE MERGED 2007/01/10 15:03:56 hr 1.5.30.2: RESYNC: (1.5-1.6); FILE MERGED 2006/12/11 16:18:23 hr 1.5.30.1: #i69179#: remove obsolete file

--- binfilter/bf_sc/source/core/data/sc_dpobject.cxx
+++ binfilter/bf_sc/source/core/data/sc_dpobject.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_dpobject.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 16:55:27 $
+ *  last change: $Author: obo $ $Date: 2007-03-09 14:17:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -32,12 +32,6 @@
  *    MA  02111-1307  USA
  *
  ************************************************************************/
-
-#ifdef PCH
-#include "core_pch.hxx"
-#endif
-
-#pragma hdrstop
 
 // INCLUDE ---------------------------------------------------------------
 
