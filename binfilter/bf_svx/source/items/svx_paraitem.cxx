daff82861f4e17d03e0072f503ffff82a841ff95
Oliver Bolte <obo@openoffice.org>
2007-03-15 14:27:42 +0000

    INTEGRATION: CWS basmgr02 (1.7.52); FILE MERGED 2007/01/17 11:14:53 fs 1.7.52.1: #i73557# binfilter now uses a stripped, own, frozen version of the 'basic' module, instead of the global (evolving) one

--- binfilter/bf_svx/source/items/svx_paraitem.cxx
+++ binfilter/bf_svx/source/items/svx_paraitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_paraitem.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 21:17:26 $
+ *  last change: $Author: obo $ $Date: 2007-03-15 15:27:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -74,7 +74,7 @@ using namespace ::com::sun::star;
 #define ITEMID_PARAGRID 0
 
 #include <tools/rtti.hxx>
-#include <basic/sbx.hxx>
+#include "bf_basic/sbx.hxx"
 #define GLOBALOVERFLOW3
 
 #define _SVX_PARAITEM_CXX
