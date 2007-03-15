d29af8ecc2e820525043ad55925675de5175b9f2
Oliver Bolte <obo@openoffice.org>
2007-03-15 14:10:34 +0000

    INTEGRATION: CWS basmgr02 (1.1.2); FILE ADDED 2007/02/13 08:29:09 fs 1.1.2.2: #i10000# manual resync 2007/01/17 11:09:33 fs 1.1.2.1: #i73557# stripped version of 'basic' module

---
+++ binfilter/bf_basic/source/runtime/makefile.mk
@@ -0,0 +1,90 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: obo $ $Date: 2007-03-15 15:10:34 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ=..$/..$/..
+
+PRJNAME=binfilter
+TARGET=basic_runtime
+
+NO_HIDS=TRUE
+
+# --- Settings -----------------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+INC+= -I$(PRJ)$/inc$/bf_basic
+
+# --- Allgemein -----------------------------------------------------------
+
+SLOFILES=	\
+    $(SLO)$/basrdll.obj	\
+
+#	$(SLO)$/inputbox.obj	\
+    $(SLO)$/runtime.obj	\
+    $(SLO)$/step0.obj	\
+    $(SLO)$/step1.obj	\
+    $(SLO)$/step2.obj	\
+    $(SLO)$/iosys.obj	\
+    $(SLO)$/stdobj.obj	\
+    $(SLO)$/stdobj1.obj	\
+    $(SLO)$/methods.obj	\
+    $(SLO)$/methods1.obj	\
+    $(SLO)$/props.obj	\
+    $(SLO)$/ddectrl.obj	\
+    $(SLO)$/dllmgr.obj
+
+.IF "$(GUI)$(CPU)" == "WINI"
+#SLOFILES+=	$(SLO)$/win.obj
+.ENDIF
+
+.IF "$(GUI)$(CPU)" == "WNTI"
+#SLOFILES+=	$(SLO)$/wnt.obj
+.ENDIF
+
+.IF "$(GUI)$(CPU)" == "OS2I"
+#SLOFILES+= $(SLO)$/os2.obj
+.ENDIF
+
+#EXCEPTIONSFILES=$(SLO)$/step0.obj	\
+        $(SLO)$/step2.obj	\
+        $(SLO)$/methods.obj	\
+        $(SLO)$/methods1.obj	\
+        $(SLO)$/iosys.obj	\
+        $(SLO)$/runtime.obj
+
+# --- Targets -------------------------------------------------------------
+
+.INCLUDE :  target.mk
+
