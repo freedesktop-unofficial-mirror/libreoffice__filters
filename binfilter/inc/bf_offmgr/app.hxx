b69265c028c6a38e7466762648b2794ba755056d
Oliver Bolte <obo@openoffice.org>
2007-03-15 14:33:25 +0000

    INTEGRATION: CWS basmgr02 (1.6.16); FILE MERGED 2007/01/17 11:14:59 fs 1.6.16.1: #i73557# binfilter now uses a stripped, own, frozen version of the 'basic' module, instead of the global (evolving) one

--- binfilter/inc/bf_offmgr/app.hxx
+++ binfilter/inc/bf_offmgr/app.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: app.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:23:59 $
+ *  last change: $Author: obo $ $Date: 2007-03-15 15:33:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,6 +43,8 @@
 class SvStringsDtor; 
 class SimpleResMgr; 
 namespace binfilter {
+
+class StarBASIC;
 
 // defines ---------------------------------------------------------------
 
