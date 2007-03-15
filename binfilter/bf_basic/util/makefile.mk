f6592b5319d45e15424bb3b7ed4a4438b9be0222
Oliver Bolte <obo@openoffice.org>
2007-03-15 14:17:01 +0000

    INTEGRATION: CWS basmgr02 (1.1.2); FILE ADDED 2007/01/17 11:04:45 fs 1.1.2.1: #i73557# stripped version of 'basic' module

---
+++ binfilter/bf_basic/util/makefile.mk
@@ -0,0 +1,100 @@
+#*************************************************************************
+#
+#   OpenOffice.org - a multi-platform office productivity suite
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: obo $ $Date: 2007-03-15 15:17:01 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+BFPRJ=..
+PRJ=..$/..
+
+PRJNAME=binfilter
+TARGET=bf_sb
+
+# --- Settings ---------------------------------------------------
+
+.INCLUDE :  settings.mk
+
+# --- Allgemein ---------------------------------------------------
+
+LIB1TARGET=$(SLB)$/$(TARGET).lib
+LIB1FILES=      \
+    $(SLB)$/basic_basicmgr.lib \
+    $(SLB)$/basic_sbx.lib \
+    $(SLB)$/basic_classes.lib \
+    $(SLB)$/basic_comp.lib \
+    $(SLB)$/basic_runtime.lib
+
+SHL1TARGET=$(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1IMPLIB=$(TARGET)
+
+SHL1STDLIBS= \
+            $(CPPULIB) \
+            $(CPPUHELPERLIB) \
+            $(TOOLSLIB) \
+            $(SVTOOLLIB) \
+            $(SVLLIB)   \
+            $(SVLIB) \
+            $(VOSLIB) \
+            $(SALLIB) \
+            $(COMPHELPERLIB) \
+            $(UNOTOOLSLIB) \
+            $(SOTLIB) \
+            $(VOSLIB) \
+            $(XMLSCRIPTLIB)
+
+.IF "$(GUI)"=="WNT"
+SHL1STDLIBS+=   \
+    uwinapi.lib \
+    oleaut32.lib
+.ENDIF # WNT
+
+SHL1DEF=    $(MISC)$/$(SHL1TARGET).def
+SHL1LIBS=   $(SLB)$/$(TARGET).lib
+
+DEF1NAME    =$(SHL1TARGET)
+DEF1DEPN    =   \
+    $(MISC)$/$(SHL1TARGET).flt
+
+DEFLIB1NAME =$(TARGET)
+
+# --- Targets -----------------------------------------------------------
+
+.INCLUDE :  target.mk
+ 
+# --- Basic-Filter-Datei ---
+
+$(MISC)$/$(SHL1TARGET).flt: makefile.mk
+    @echo ------------------------------
+    @echo Making: $@
+    @echo __CT >> $@
+
+
