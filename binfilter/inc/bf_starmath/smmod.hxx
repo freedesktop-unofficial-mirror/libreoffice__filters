b4e03d7a6f6b4e1e4bd9036d03470c5a70384f6d
Rüdiger Timm <rt@openoffice.org>
2007-04-26 06:19:00 +0000

    INTEGRATION: CWS residcleanup (1.4.68); FILE MERGED 2007/02/26 21:26:04 pl 1.4.68.1: #i74635# no more ResMgr default

--- binfilter/inc/bf_starmath/smmod.hxx
+++ binfilter/inc/bf_starmath/smmod.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: smmod.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 03:13:57 $
+ *  last change: $Author: rt $ $Date: 2007-04-26 07:19:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -90,7 +90,7 @@ class SmRectCache;
 /*N*/     SmNamesArray( LanguageType nLang, int nRID ) :
 /*N*/         Resource( SmResId(RID_LOCALIZED_NAMES) ),
 /*N*/         nLanguage   (nLang),
-/*N*/         aNamesAry   (ResId(nRID))
+/*N*/         aNamesAry   (SmResId(nRID))
 /*N*/     {
 /*N*/         FreeResource();
 /*N*/     }
