97707114bb5289e33e7e3c2c7f6c5b49261895d6
Ivo Hinkelmann <ihi@openoffice.org>
2007-06-05 13:27:29 +0000

    INTEGRATION: CWS bgdlremove (1.8.4); FILE MERGED 2007/05/18 08:54:23 kso 1.8.4.1: #i77419# - cleanup of ucbhelper namespaces.

--- binfilter/bf_svx/source/dialog/svx_impgrf.cxx
+++ binfilter/bf_svx/source/dialog/svx_impgrf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_impgrf.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:16:03 $
+ *  last change: $Author: ihi $ $Date: 2007-06-05 14:27:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #include <bf_sfx2/docfile.hxx>
 #endif
 
-using namespace ::ucb;
+using namespace ::ucbhelper;
 using namespace ::com::sun::star::uno;
 
 #include "dialogs.hrc"
