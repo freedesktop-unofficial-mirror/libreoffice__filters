f8cb3cd3db992425fdd086abac82a631c9f4eef9
Rüdiger Timm <rt@openoffice.org>
2007-07-13 08:26:58 +0000

    #i10000# Remove extra semicolon to avoid Sun compiler warning about 'Empty declaration'

--- binfilter/inc/bf_svx/unolingu.hxx
+++ binfilter/inc/bf_svx/unolingu.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unolingu.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:22:38 $
+ *  last change: $Author: rt $ $Date: 2007-07-13 09:26:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -155,7 +155,7 @@ public:
 }//end of namespace binfilter
 namespace com { namespace sun { namespace star { namespace linguistic2 {
     class XHyphenatedWord;
-}}}};
+}}}}
 namespace binfilter {
 struct SvxAlternativeSpelling
 {
