d48b29e5d7aa6be50c0825a7e78d72b76fd0cb88
Oliver Bolte <obo@openoffice.org>
2007-07-17 09:59:45 +0000

    INTEGRATION: CWS basemodelrefactoring (1.7.70); FILE MERGED 2007/05/10 20:36:01 mba 1.7.70.1: #i76195#: all sfx resources removed

--- binfilter/bf_sfx2/source/explorer/makefile.mk
+++ binfilter/bf_sfx2/source/explorer/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2006-10-27 19:31:40 $
+#   last change: $Author: obo $ $Date: 2007-07-17 10:59:45 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,10 +55,6 @@ INC+= -I$(PRJ)$/inc$/bf_sfx2
 
 SLOFILES = \
         $(SLO)$/sfx2_nochaos.obj
-
-SRS1NAME=$(TARGET)
-SRC1FILES = \
-        sfx2_explorer.src
 
 # --- Targets ------------------------------------------------------
 
