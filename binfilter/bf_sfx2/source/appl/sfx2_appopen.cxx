5b63702181ad83c58a2e61d39b10eaf227d75d6a
Oliver Bolte <obo@openoffice.org>
2007-07-17 09:35:49 +0000

    INTEGRATION: CWS basemodelrefactoring (1.13.4); FILE MERGED 2007/05/10 20:35:58 mba 1.13.4.6: #i76195#: all sfx resources removed 2007/05/05 12:55:52 mba 1.13.4.5: #i76195#: remove superfluous code 2007/05/05 12:45:18 mba 1.13.4.4: #i76195#: remove superfluous code 2007/05/01 20:57:51 mba 1.13.4.3: #i76195#: remove view code 2007/04/30 12:12:08 mba 1.13.4.2: #i76195#: remove view code 2007/04/08 18:08:31 mba 1.13.4.1: #i76195#: remove some Sfx view classes

--- binfilter/bf_sfx2/source/appl/sfx2_appopen.cxx
+++ binfilter/bf_sfx2/source/appl/sfx2_appopen.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_appopen.cxx,v $
  *
- *  $Revision: 1.13 $
+ *  $Revision: 1.14 $
  *
- *  last change: $Author: obo $ $Date: 2007-03-15 15:21:31 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 10:35:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -92,14 +92,8 @@
 #include <cppuhelper/implbase1.hxx>
 #endif
 
-#ifndef _SV_WRKWIN_HXX
-#include <vcl/wrkwin.hxx>
-#endif
 #ifndef _SFXINTITEM_HXX //autogen
 #include <svtools/intitem.hxx>
-#endif
-#ifndef _SV_MSGBOX_HXX
-#include <vcl/msgbox.hxx>
 #endif
 #ifndef _SFXSTRITEM_HXX //autogen
 #include <svtools/stritem.hxx>
@@ -122,6 +116,8 @@
 #include <svtools/urihelper.hxx>
 #include <unotools/localfilehelper.hxx>
 
+#include <svtools/itemset.hxx>
+
 #ifndef INCLUDED_SVTOOLS_PATHOPTIONS_HXX
 #include <svtools/pathoptions.hxx>
 #endif
@@ -141,27 +137,20 @@
 
 #include "app.hxx"
 #include "appdata.hxx"
-#include "bindings.hxx"
 #include "cfgmgr.hxx"
-#include "dispatch.hxx"
 #include "docfile.hxx"
 #include "docinf.hxx"
 #include "fcontnr.hxx"
-#include "fsetobsh.hxx"
-#include "loadenv.hxx"
 #include "objitem.hxx"
 #include "objsh.hxx"
 #include "objshimp.hxx"
 #include "openflag.hxx"
 #include "referers.hxx"
 #include "request.hxx"
-#include "sfxresid.hxx"
-#include "viewsh.hxx"
-#include "app.hrc"
-#include "topfrm.hxx"
 #include "appimp.hxx"
 #include "appuno.hxx"
-#include "objface.hxx"
+#include "docfac.hxx"
+#include "sfxsids.hrc"
 
 #define _SVSTDARR_STRINGSDTOR
 #include <svtools/svstdarr.hxx>
@@ -252,236 +241,5 @@ using namespace ::cppu;
 /*N*/     return nRet;
 /*N*/ }
 
-//--------------------------------------------------------------------
-/*N*/ SfxObjectShellLock SfxApplication::NewDoc_Impl( const String& rFact, const SfxItemSet *pSet )
-/*N*/ {
-/*N*/     SfxObjectShellLock xDoc;
-/*N*/ 	const SfxObjectFactory* pFactory = 0;
-/*N*/ 	String aFact( rFact );
-/*N*/ 	String aPrefix = String::CreateFromAscii( "private:factory/" );
-/*N*/ 	if ( aPrefix.Len() == aFact.Match( aPrefix ) )
-/*?*/ 		aFact.Erase( 0, aPrefix.Len() );
-/*N*/ 	USHORT nPos = aFact.Search( '?' );
-/*N*/ 	String aParam;
-/*N*/ 	if ( nPos != STRING_NOTFOUND )
-/*N*/ 	{
-/*N*/ 		aParam = aFact.Copy( nPos, aFact.Len() );
-/*N*/ 		aFact.Erase( nPos, aFact.Len() );
-/*N*/ 		aParam.Erase(0,1);
-/*N*/ 	}
-/*N*/
-/*N*/ 	WildCard aSearchedFac( aFact.EraseAllChars('4').ToUpperAscii() );
-/*N*/     for( USHORT n = SfxObjectFactory::GetObjectFactoryCount_Impl(); !pFactory && n--; )
-/*N*/ 	{
-/*?*/         pFactory = &SfxObjectFactory::GetObjectFactory_Impl( n );
-/*?*/ 		if( !aSearchedFac.Matches( String::CreateFromAscii( pFactory->GetShortName() ).ToUpperAscii() ) )
-/*?*/ 			pFactory = 0;
-/*?*/ 	}
-/*N*/
-/*N*/ 	if( !pFactory )
-/*N*/ 	{
-/*?*/ 		DBG_ERROR("Unknown factory!");
-/*?*/ 		pFactory = &SfxObjectFactory::GetDefaultFactory();
-/*N*/ 	}
-/*N*/
-/*N*/     xDoc = pFactory->CreateObject();
-/*N*/     aParam = INetURLObject::decode( aParam, '%', INetURLObject::DECODE_WITH_CHARSET );
-/*N*/ 	if( xDoc.Is() )
-/*N*/ 		xDoc->DoInitNew_Impl( aParam );
-/*N*/
-/*N*/     if ( xDoc.Is() )
-/*N*/     {
-/*N*/ 		if ( pSet )
-/*N*/ 		{
-/*N*/ 			SFX_ITEMSET_ARG( pSet, pTitleItem, SfxStringItem, SID_DOCINFO_TITLE, FALSE );
-/*N*/ 			if ( pTitleItem )
-/*?*/ 				xDoc->GetMedium()->GetItemSet()->Put( *pTitleItem );
-/*N*/ 		}
-/*N*/
-/*N*/         ::com::sun::star::uno::Reference< ::com::sun::star::frame::XModel >  xModel ( xDoc->GetModel(), ::com::sun::star::uno::UNO_QUERY );
-/*N*/ 		if ( xModel.is() )
-/*N*/ 		{
-/*N*/             SfxItemSet* pNew = xDoc->GetMedium()->GetItemSet()->Clone();
-/*N*/             pNew->ClearItem( SID_PROGRESS_STATUSBAR_CONTROL );
-/*N*/ 			::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue > aArgs;
-/*N*/             TransformItems( SID_OPENDOC, *pNew, aArgs );
-/*N*/
-/*N*/             sal_Int32 nLength = aArgs.getLength();
-/*N*/             aArgs.realloc( nLength + 1 );
-/*N*/
-/*N*/             aArgs[nLength].Name = DEFINE_CONST_UNICODE("Title");
-/*N*/             aArgs[nLength].Value <<= ::rtl::OUString( xDoc->GetTitle( SFX_TITLE_DETECT ) );
-/*N*/
-/*N*/             xModel->attachResource( ::rtl::OUString(), aArgs );
-/*N*/             delete pNew;
-/*N*/ 		}
-/*N*/     }
-/*N*/
-/*N*/ 	return xDoc;
-/*N*/ }
 
-/*N*/ void SfxApplication::NewDocDirectExec_Impl( SfxRequest& rReq )
-/*N*/ {
-/*N*/     DBG_MEMTEST();
-/*N*/
-/*N*/ 	SFX_REQUEST_ARG(rReq, pHidden, SfxBoolItem, SID_HIDDEN, FALSE);
-//(mba)/task
-/*
-    if ( !pHidden || !pHidden->GetValue() )
-        Application::GetAppWindow()->EnterWait();
-*/
-/*N*/     SfxObjectShellLock xDoc;
-/*N*/     BOOL bNewView = TRUE;
-/*N*/
-/*N*/     // Factory-RegNo kann per Parameter angegeben sein
-/*N*/     SfxErrorContext aEc(ERRCTX_SFX_NEWDOCDIRECT);
-/*N*/     const SfxItemSet *pArgs = rReq.GetArgs();
-/*N*/ 	const SfxObjectFactory* pFactory = 0;
-/*N*/ 	String aFactory;
-/*N*/ 	rReq.AppendItem( SfxBoolItem( SID_TEMPLATE, TRUE ) );
-/*N*/ 	SFX_REQUEST_ARG( rReq, pFactoryName, SfxStringItem, SID_NEWDOCDIRECT, FALSE );
-/*N*/ 	if( pFactoryName )
-/*N*/ 		aFactory = pFactoryName->GetValue();
-/*N*/     else
-/*N*/     {
-/*?*/         SvtModuleOptions aOpt;
-/*?*/
-/*?*/         if (aOpt.IsModuleInstalled(SvtModuleOptions::E_SWRITER))
-/*?*/             aFactory = aOpt.GetFactoryShortName(SvtModuleOptions::E_WRITER);
-/*?*/         else
-/*?*/         if (aOpt.IsModuleInstalled(SvtModuleOptions::E_SCALC))
-/*?*/             aFactory = aOpt.GetFactoryShortName(SvtModuleOptions::E_CALC);
-/*?*/         else
-/*?*/         if (aOpt.IsModuleInstalled(SvtModuleOptions::E_SDRAW))
-/*?*/             aFactory = aOpt.GetFactoryShortName(SvtModuleOptions::E_DRAW);
-/*?*/         else
-/*?*/         if (aOpt.IsModuleInstalled(SvtModuleOptions::E_SIMPRESS))
-/*?*/             aFactory = aOpt.GetFactoryShortName(SvtModuleOptions::E_IMPRESS);
-/*?*/         else
-/*?*/         if (aOpt.IsModuleInstalled(SvtModuleOptions::E_SMATH))
-/*?*/             aFactory = aOpt.GetFactoryShortName(SvtModuleOptions::E_MATH);
-/*?*/         else
-/*?*/         if (aOpt.IsModuleInstalled(SvtModuleOptions::E_SWRITER))
-/*?*/             aFactory = aOpt.GetFactoryShortName(SvtModuleOptions::E_WRITERGLOBAL);
-/*?*/         else
-/*?*/         if (aOpt.IsModuleInstalled(SvtModuleOptions::E_SWRITER))
-/*?*/             aFactory = aOpt.GetFactoryShortName(SvtModuleOptions::E_WRITERWEB);
-/*N*/     }
-/*N*/
-/*N*/     SFX_REQUEST_ARG( rReq, pFileFlagsItem, SfxStringItem, SID_OPTIONS, FALSE);
-/*N*/     if ( pFileFlagsItem )
-/*N*/     {
-/*N*/         // Werte auf einzelne Items verteilen
-/*?*/         String aFileFlags = pFileFlagsItem->GetValue();
-/*?*/         aFileFlags.ToUpperAscii();
-/*?*/         if ( STRING_NOTFOUND != aFileFlags.Search( 0x0054 ) )               // T = 54h
-/*?*/             rReq.AppendItem( SfxBoolItem( SID_TEMPLATE, TRUE ) );
-/*?*/         if ( STRING_NOTFOUND != aFileFlags.Search( 0x0048 ) )               // H = 48h
-/*?*/             rReq.AppendItem( SfxBoolItem( SID_HIDDEN, TRUE ) );
-/*?*/         if ( STRING_NOTFOUND != aFileFlags.Search( 0x0052 ) )               // R = 52h
-/*?*/             rReq.AppendItem( SfxBoolItem( SID_DOC_READONLY, TRUE ) );
-/*?*/         if ( STRING_NOTFOUND != aFileFlags.Search( 0x0042 ) )               // B = 42h
-/*?*/             rReq.AppendItem( SfxBoolItem( SID_PREVIEW, TRUE ) );
-/*?*/         if ( STRING_NOTFOUND != aFileFlags.Search( 0x0053 ) )               // S = 53h
-/*?*/             rReq.AppendItem( SfxBoolItem( SID_SILENT, TRUE ) );
-/*N*/     }
-/*N*/
-/*N*/ 	xDoc = NewDoc_Impl( aFactory, rReq.GetArgs() );
-/*N*/ 	if ( xDoc.Is() )
-/*N*/ 	{
-/*N*/ 		SFX_REQUEST_ARG(rReq, pReadonly, SfxBoolItem, SID_DOC_READONLY, FALSE);
-/*N*/ 		if ( pReadonly )
-/*?*/ 			xDoc->GetMedium()->GetItemSet()->Put( *pReadonly );
-/*N*/
-/*N*/ 		SFX_REQUEST_ARG(rReq, pPreview, SfxBoolItem, SID_PREVIEW, FALSE);
-/*N*/ 		if ( pPreview )
-/*?*/ 			xDoc->GetMedium()->GetItemSet()->Put( *pPreview );
-/*N*/
-/*N*/         SFX_REQUEST_ARG(rReq, pSilent, SfxBoolItem, SID_SILENT, FALSE);
-/*N*/         if ( pSilent )
-/*?*/             xDoc->GetMedium()->GetItemSet()->Put( *pSilent );
-/*N*/
-/*N*/         SFX_REQUEST_ARG(rReq, pFlags, SfxStringItem, SID_OPTIONS, FALSE);
-/*N*/         if ( pFlags )
-/*?*/             xDoc->GetMedium()->GetItemSet()->Put( *pFlags );
-/*N*/ 	}
-/*N*/
-/*N*/     // View erzeugen
-/*N*/ 	SfxViewFrame* pViewFrame = 0;
-/*N*/     if ( xDoc.Is() )
-/*N*/     {
-/*N*/ 		SFX_REQUEST_ARG(rReq, pHidden, SfxBoolItem, SID_HIDDEN, FALSE);
-/*N*/         BOOL bHidden = FALSE;
-/*N*/ 		if ( pHidden )
-/*N*/         {
-/*N*/ 			xDoc->GetMedium()->GetItemSet()->Put( *pHidden, SID_HIDDEN );
-/*N*/             bHidden = pHidden->GetValue();
-/*N*/         }
-/*N*/
-/*N*/ 		SFX_REQUEST_ARG(rReq, pViewId, SfxUInt16Item, SID_VIEW_ID, FALSE);
-/*N*/         USHORT nViewId = 0;
-/*N*/ 		if ( pViewId )
-/*N*/         {
-/*?*/ 			xDoc->GetMedium()->GetItemSet()->Put( *pViewId, SID_VIEW_ID );
-/*?*/             nViewId = pViewId->GetValue();
-/*N*/         }
-/*N*/
-/*N*/ 	    xDoc->SetActivateEvent_Impl( SFX_EVENT_CREATEDOC );
-/*N*/ //		xDoc->Get_Impl()->nLoadedFlags = SFX_LOADED_ALL;
-/*N*/ 		const SfxItemSet* pInternalArgs = rReq.GetInternalArgs_Impl();
-/*N*/ 		if( pInternalArgs )
-/*?*/ 			xDoc->GetMedium()->GetItemSet()->Put( *pInternalArgs );
-/*N*/ 		BOOL bOwnsFrame = FALSE;
-/*N*/         SFX_REQUEST_ARG(rReq, pFrameItem, SfxFrameItem, SID_DOCFRAME, FALSE);
-/*N*/
-/*N*/         SfxFrame* pFrame = NULL;
-/*N*/         if (pFrameItem)
-/*N*/             pFrame = pFrameItem->GetFrame();
-/*N*/         else
-/*?*/             {DBG_BF_ASSERT(0, "STRIP");}//STRIP001 pFrame = (SfxFrame*)SfxTopFrame::Create(xDoc, nViewId, bHidden, pInternalArgs);
-/*N*/         if ( pFrame )
-/*N*/         {
-/*N*/             if ( pFrame->GetCurrentDocument() == xDoc || pFrame->PrepareClose_Impl( TRUE, TRUE ) == TRUE )
-/*N*/             {
-/*N*/                 if (bHidden)
-/*N*/                 {
-/*N*/                     xDoc->RestoreNoDelete();
-/*N*/                     xDoc->OwnerLock( TRUE );
-/*N*/                     xDoc->Get_Impl()->bHiddenLockedByAPI = TRUE;
-/*N*/                 }
-/*N*/
-/*N*/                 if ( pFrame->GetCurrentDocument() != xDoc )
-/*N*/                     pFrame->InsertDocument( xDoc );
-/*N*/                 pViewFrame = pFrame->GetCurrentViewFrame();
-/*N*/             }
-/*N*/             else
-/*?*/                 xDoc.Clear();
-/*N*/         }
-/*N*/
-/*N*/         rReq.SetReturnValue( SfxFrameItem( 0, pFrame ) );
-/*N*/     }
-/*N*/
-//(mba)/task
-/*
-    if ( !pHidden || !pHidden->GetValue() )
-        Application::GetAppWindow()->LeaveWait();
- */
-/*N*/ }
-
-//--------------------------------------------------------------------
-
-/*?*/ void SfxApplication::NewDocExec_Impl( SfxRequest& rReq )
-/*?*/ {DBG_BF_ASSERT(0, "STRIP"); //STRIP001
-/*?*/ }
-
-//---------------------------------------------------------------------------
-
-/*?*/ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
-/*?*/ {DBG_BF_ASSERT(0, "STRIP");//STRIP001
-    /* Attention!
-            There exist two possibilities to open hyperlinks:
-            a) using SID_OPENHYPERLINK (new)
-            b) using SID_BROWSE        (old)
-     */
-/*?*/ }
 }
