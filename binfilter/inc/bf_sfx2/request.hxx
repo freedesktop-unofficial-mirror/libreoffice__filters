a4ca41db9bfb43f032998513b7bd7c4259ef2769
Oliver Bolte <obo@openoffice.org>
2007-07-17 11:42:29 +0000

    INTEGRATION: CWS basemodelrefactoring (1.5.70); FILE MERGED 2007/05/01 20:57:58 mba 1.5.70.2: #i76195#: remove view code 2007/04/30 12:13:26 mba 1.5.70.1: #i76195#: remove view code

--- binfilter/inc/bf_sfx2/request.hxx
+++ binfilter/inc/bf_sfx2/request.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: request.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 03:07:10 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 12:42:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,107 +35,21 @@
 #ifndef _SFXREQUEST_HXX
 #define _SFXREQUEST_HXX
 
-#ifndef _SFXITEMSET_HXX //autogen
-#include <svtools/itemset.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_FRAME_XDISPATCHRECORDER_HPP_
-#include <com/sun/star/frame/XDispatchRecorder.hpp>
-#endif
-class SfxPoolItem; 
-class SfxAllItemSet;
+class SfxPoolItem;
 class SfxItemSet;
-class SfxItemPool;
-class String;
+
+#include <tools/rtti.hxx>
+
 namespace binfilter {
 
-class SfxMacro;
-
-class SfxShell;
-class SfxSlot;
-class SfxMacroStatement;
-class SfxArguments;
-class SfxViewFrame;
-struct SfxRequest_Impl;
-
-//==================================================================
-
-class SfxRequest: public SfxHint
+class SfxRequest
 {
-friend struct SfxRequest_Impl;
-
-    USHORT				nSlot;
-    SfxAllItemSet*		pArgs;
-    SfxRequest_Impl*	pImp;
-
-    //---------------------------------------------------------------------
-#if _SOLAR__PRIVATE
 public:
-    void                Record_Impl( SfxShell &rSh, const SfxSlot &rSlot,
-                                     ::com::sun::star::uno::Reference< ::com::sun::star::frame::XDispatchRecorder > xRecorder,
-                                     SfxViewFrame* );
-private:
-    void				Done_Impl( const SfxItemSet *pSet );
-
-#endif
-    //---------------------------------------------------------------------
-
-public:
-                        SfxRequest( SfxViewFrame*, USHORT nSlotId );
-                        SfxRequest( USHORT nSlot, USHORT nCallMode, SfxItemPool &rPool );
-                        SfxRequest( USHORT nSlot, USHORT nCallMode, const SfxAllItemSet& rSfxArgs );
-                        SfxRequest( const SfxRequest& rOrig );
-                        ~SfxRequest();
-
-    USHORT				GetSlot() const { return nSlot; }
-    void				SetSlot(USHORT nNewSlot) { nSlot = nNewSlot; }
-
-    USHORT              GetModifier() const;
-    void                SetModifier( USHORT nModi );
-    void                SetInternalArgs_Impl( const SfxAllItemSet& rArgs );
-    const SfxItemSet*   GetInternalArgs_Impl() const;
-    const SfxItemSet* 	GetArgs() const { return pArgs; }
-    void				SetArgs( const SfxAllItemSet& rArgs );
-    void				AppendItem(const SfxPoolItem &);
-    void				RemoveItem( USHORT nSlotId );
-
     static const SfxPoolItem* GetItem( const SfxItemSet*, USHORT nSlotId,
                                        FASTBOOL bDeep = FALSE,
                                        TypeId aType = 0 );
-    const SfxPoolItem*  GetArg( USHORT nSlotId, FASTBOOL bDeep = FALSE, TypeId aType = 0 ) const;
-    void                ReleaseArgs()
-                        { DELETEZ(pArgs); }
-
-    void				SetReturnValue(const SfxPoolItem &);
-    const SfxPoolItem*	GetReturnValue() const;
-
-    static SfxMacro*	GetRecordingMacro();
-    static BOOL         HasMacroRecorder( SfxViewFrame* pFrame=NULL );
-    USHORT  			GetCallMode() const;
-    void                AllowRecording( BOOL );
-    BOOL                AllowsRecording() const;
-    BOOL				IsAPI() const;
-    BOOL                IsSynchronCall() const;
-    void                SetSynchronCall( BOOL bSynchron );
-    void				SetTarget( const String &rTarget );
-
-    BOOL				IsDone() const;
-    void				Done( BOOL bRemove = FALSE );
-
-    void				Ignore();
-    void				Cancel();
-    BOOL				IsCancelled() const;
-    void				Done(const SfxItemSet &, FASTBOOL bKeep = TRUE );
-
-private:
-    const SfxRequest&	operator=(const SfxRequest &); // n.i.!!
 };
 
-//------------------------------------------------------------------------
-
-#define SFX_REQUEST_ARG(rReq, pItem, ItemType, nSlotId, bDeep) \
-        const ItemType *pItem = (const ItemType*) \
-                rReq.GetArg( nSlotId, bDeep, TYPE(ItemType) )
 #define SFX_ITEMSET_ARG(pArgs, pItem, ItemType, nSlotId, bDeep) \
     const ItemType *pItem = (const ItemType*) \
         SfxRequest::GetItem( pArgs, nSlotId, bDeep, TYPE(ItemType) )
