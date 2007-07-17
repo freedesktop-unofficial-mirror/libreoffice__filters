473b5c930aa3abb9059a3d4ba0b1ffc9a7b138a8
Oliver Bolte <obo@openoffice.org>
2007-07-17 08:49:09 +0000

    INTEGRATION: CWS basemodelrefactoring (1.6.70); FILE MERGED 2007/05/08 20:30:49 mba 1.6.70.4: RESYNC: (1.6-1.7); FILE MERGED 2007/05/01 20:57:48 mba 1.6.70.3: #i76195#: remove view code 2007/04/30 14:35:39 mba 1.6.70.2: #i76195#: remove view code 2007/04/28 15:20:24 mba 1.6.70.1: #i76195#: remove code for UI controls

--- binfilter/bf_sch/source/ui/app/sch_schmod.cxx
+++ binfilter/bf_sch/source/ui/app/sch_schmod.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_schmod.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:11:21 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 09:49:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,21 +39,9 @@
 
 
 #include <bf_svx/svxids.hrc>
-
-
-#ifndef _SFXOBJFACE_HXX //autogen
-#include <bf_sfx2/objface.hxx>
-#endif
-
-#ifndef _SFXDISPATCH_HXX //autogen
-#include <bf_sfx2/dispatch.hxx>
-#endif
-#ifndef _SV_STATUS_HXX //autogen
+#include <bf_sfx2/app.hxx>
 #include <vcl/status.hxx>
-#endif
-#ifndef _SVDOBJ_HXX //autogen
 #include <bf_svx/svdobj.hxx>
-#endif
 
 #define _SCH_DLL                 // fuer SD_MOD()
 #include "schmod.hxx"
@@ -64,26 +52,10 @@
 #include "strings.hrc"
 #include "res_bmp.hrc"
 
-//STRIP008 /*N*/ TYPEINIT1( SchModuleDummy, SfxModule );
-//STRIP008 /*N*/ TYPEINIT1( SchModule, SchModuleDummy );
-
-
-#define SchModule
-//STRIP008 #include "schslots.hxx"
-
-#ifndef _SVX_MODCTRL_HXX //autogen
-#include <bf_svx/modctrl.hxx>
-#endif
 namespace binfilter {
-#include "schslots.hxx"
+
 /*N*/ TYPEINIT1( SchModuleDummy, SfxModule );
 /*N*/ TYPEINIT1( SchModule, SchModuleDummy );
-
-/*N*/ SFX_IMPL_INTERFACE( SchModule, SfxModule, SchResId( STR_APPLICATIONOBJECTBAR ) )
-/*N*/ // from macro: void SchModule::InitInterface_Impl()
-/*N*/ {
-/*N*/ 	SFX_STATUSBAR_REGISTRATION( SchResId( RID_STATUSBAR ) );
-/*N*/ }
 
 /*************************************************************************
 |*
@@ -102,21 +74,21 @@ namespace binfilter {
 /*N*/ 	pChartOptions( NULL )
 /*N*/ {
 /*N*/ 	pSchObjFactory = new SchObjFactory();
-/*N*/ 
+/*N*/
 /*N*/ 	if (!pSchObjFactory->GetInserted())
 /*N*/ 	{
 /*N*/ 		SdrObjFactory::InsertMakeObjectHdl(LINK(pSchObjFactory, SchObjFactory, MakeObject));
 /*N*/ 		SdrObjFactory::InsertMakeUserDataHdl(LINK(pSchObjFactory, SchObjFactory, MakeUserData));
-/*N*/ 
+/*N*/
 /*N*/ 		pSchObjFactory->SetInserted(TRUE);
 /*N*/ 	}
-/*N*/ 
+/*N*/
 /*N*/ 	// #62493# this factory exists already
 /*N*/ 	//pE3dFactory = new E3dObjFactory();
 /*N*/ 	//SdrObjFactory::InsertMakeObjectHdl(LINK(pE3dFactory, E3dObjFactory, MakeObject));
-/*N*/ 
+/*N*/
 /*N*/ 	SetName( String( RTL_CONSTASCII_USTRINGPARAM( "StarChart" )) );
-/*N*/ 
+/*N*/
 /*N*/ 	// establish listener, so that the module is informed
 /*N*/ 	// right before the application is going to be shut down
 /*N*/ 	StartListening( *SFX_APP() );
@@ -137,94 +109,23 @@ namespace binfilter {
 /*N*/ 			SdrObjFactory::RemoveMakeObjectHdl(LINK(pSchObjFactory, SchObjFactory, MakeObject));
 /*N*/ 			SdrObjFactory::RemoveMakeUserDataHdl(LINK(pSchObjFactory, SchObjFactory, MakeUserData));
 /*N*/ 		}
-/*N*/ 
+/*N*/
 /*N*/ 		delete pSchObjFactory;
 /*N*/ 	}
 /*N*/ }
 
-/*************************************************************************
-|*
-|* SfxRequests bearbeiten
-|*
-\************************************************************************/
-
-void SchModule::Execute(SfxRequest& rReq)
-{
-    DBG_BF_ASSERT(0, "STRIP");//STRIP001 	switch (rReq.GetSlot())
-}
-
-/*************************************************************************
-|*
-|*
-|*
-\************************************************************************/
-
-/*?*/ void SchModule::GetState(SfxItemSet& rItemSet)
-/*?*/ {
-/*?*/ 	DBG_BF_ASSERT(0, "STRIP"); //STRIP001 	rItemSet.DisableItem(SID_CONFIGEVENT);
-/*?*/ }
-
-/*************************************************************************
-|*
-|*
-|*
-\************************************************************************/
-
-
-/*************************************************************************
-|*
-|* Statusbar erzeugen; virtuell, wird vom SFX gerufen
-|*
-\************************************************************************/
-
-/*N*/ void SchModule::FillStatusBar(StatusBar &rBar)
-/*N*/ {
-/*N*/ 	String aDummy;
-/*N*/ 	aDummy.Fill( 20, (sal_Unicode)('x') );
-/*N*/ 	long nWidth = rBar.GetTextWidth( aDummy );
-/*N*/ 
-/*N*/ 	rBar.InsertItem(SID_CONTEXT, nWidth, SIB_IN | SIB_AUTOSIZE | SIB_LEFT);
-/*N*/ 	rBar.InsertItem(SID_CONTEXT_TYPE, nWidth, SIB_IN | SIB_AUTOSIZE | SIB_LEFT);
-/*N*/ 	rBar.InsertItem(SID_DOC_MODIFIED, SvxModifyControl::GetDefItemWidth(rBar));
-/*N*/ 	rBar.SetHelpId( SID_DOC_MODIFIED, SID_DOC_MODIFIED );
-/*N*/ 
-/*N*/ 	aDummy.Fill( 30, (sal_Unicode)('x') );
-/*N*/ 	nWidth = rBar.GetTextWidth( aDummy );
-/*N*/ 
-/*N*/ 	rBar.InsertItem(SID_ATTR_SIZE, nWidth, SIB_IN | SIB_USERDRAW);
-/*N*/ }
-
-/*************************************************************************
-|*
-|* Lade Module von aussen
-|*
-\************************************************************************/
 
 /*?*/ SfxModule *SchModuleDummy::Load()
 /*?*/ {
 /*?*/ 	return NULL;
 /*?*/ }
 
-/*************************************************************************
-|*
-|* Lade Module von aussen
-|*
-\************************************************************************/
-
-
-/*************************************************************************
-|*
-|* Loesche Module
-|*
-\************************************************************************/
-
-
 /*N*/ SchOptions* SchModule::GetSchOptions()
 /*N*/ {
 /*N*/ 	if( ! pChartOptions )
 /*N*/ 		pChartOptions = new SchOptions();
-/*N*/ 
-/*N*/ 	return pChartOptions;	
+/*N*/
+/*N*/ 	return pChartOptions;
 /*N*/ }
 
 /*************************************************************************
@@ -235,7 +136,7 @@ void SchModule::Execute(SfxRequest& rReq)
 
 /*N*/ void SchModule::Notify( SfxBroadcaster& rBC, const SfxHint& rHint )
 /*N*/ {
-/*N*/ 	if( rHint.ISA( SfxSimpleHint ) && 
+/*N*/ 	if( rHint.ISA( SfxSimpleHint ) &&
 /*N*/ 		( (SfxSimpleHint&) rHint ).GetId() == SFX_HINT_DEINITIALIZING )
 /*N*/ 	{
 /*N*/ 		delete pChartOptions, pChartOptions = NULL;
