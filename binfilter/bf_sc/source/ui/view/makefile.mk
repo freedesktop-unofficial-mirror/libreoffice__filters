e2de2ce3b3d6087c627b1db482f13b8cf6bc795f
Oliver Bolte <obo@openoffice.org>
2007-07-17 08:35:32 +0000

    INTEGRATION: CWS basemodelrefactoring (1.9.4); FILE MERGED 2007/04/30 14:35:37 mba 1.9.4.3: #i76195#: remove view code 2007/04/28 17:52:39 mba 1.9.4.2: #i76195#: remove view code 2007/04/28 12:24:42 mba 1.9.4.1: #i76764#: remove unused code

--- binfilter/bf_sc/source/ui/view/makefile.mk
+++ binfilter/bf_sc/source/ui/view/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.9 $
+#   $Revision: 1.10 $
 #
-#   last change: $Author: obo $ $Date: 2007-03-09 14:25:39 $
+#   last change: $Author: obo $ $Date: 2007-07-17 09:35:32 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,8 @@
 EXTERNAL_WARNINGS_NOT_ERRORS := TRUE
 PRJ=..$/..$/..$/..
 BFPRJ=..$/..$/..
-
+ENABLE_EXCEPTIONS=TRUE
+ 
 PRJNAME=binfilter
 TARGET=sc_view
 
@@ -56,64 +57,9 @@ PROJECTPCHSOURCE=..\pch\sc_ui_pch
 INC+= -I$(PRJ)$/inc$/bf_sc
 
 SLOFILES =  \
-        $(SLO)$/sc_tabview.obj \
-        $(SLO)$/sc_tabview2.obj \
-        $(SLO)$/sc_tabview3.obj \
-        $(SLO)$/sc_tabview4.obj \
-        $(SLO)$/sc_tabview5.obj \
-        $(SLO)$/sc_viewfunc.obj \
-        $(SLO)$/sc_viewfun2.obj \
-        $(SLO)$/sc_dbfunc.obj \
-        $(SLO)$/sc_dbfunc2.obj \
-        $(SLO)$/sc_dbfunc3.obj \
-        $(SLO)$/sc_tabvwsh.obj \
-        $(SLO)$/sc_tabvwsh2.obj \
-        $(SLO)$/sc_tabvwsh4.obj \
-        $(SLO)$/sc_tabvwsh5.obj \
-        $(SLO)$/sc_tabvwsh9.obj \
-        $(SLO)$/sc_tabvwsha.obj \
-        $(SLO)$/sc_tabvwshf.obj \
-        $(SLO)$/sc_tabvwshh.obj \
         $(SLO)$/sc_printfun.obj \
-        $(SLO)$/sc_pfuncache.obj \
-        $(SLO)$/sc_prevwsh.obj \
-        $(SLO)$/sc_editsh.obj \
-        $(SLO)$/sc_pivotsh.obj \
-        $(SLO)$/sc_auditsh.obj \
-        $(SLO)$/sc_gridwin.obj \
-        $(SLO)$/sc_gridwin3.obj \
-        $(SLO)$/sc_gridwin4.obj \
-        $(SLO)$/sc_gridwin5.obj \
-        $(SLO)$/sc_drawview.obj \
-        $(SLO)$/sc_drawvie2.obj \
-        $(SLO)$/sc_drawvie3.obj \
-        $(SLO)$/sc_drawutil.obj \
-        $(SLO)$/sc_output.obj \
-        $(SLO)$/sc_output2.obj \
-        $(SLO)$/sc_output3.obj \
-        $(SLO)$/sc_gridmerg.obj \
-        $(SLO)$/sc_invmerge.obj \
-        $(SLO)$/sc_select.obj \
-        $(SLO)$/sc_olinewin.obj \
-        $(SLO)$/sc_tabcont.obj \
-        $(SLO)$/sc_tabsplit.obj \
-        $(SLO)$/sc_viewdata.obj \
-        $(SLO)$/sc_viewutil.obj \
-        $(SLO)$/sc_hdrcont.obj \
-        $(SLO)$/sc_colrowba.obj \
-        $(SLO)$/sc_reffact.obj \
-        $(SLO)$/sc_cellsh.obj \
-        $(SLO)$/sc_cellsh2.obj\
-        $(SLO)$/sc_formatsh.obj\
-        $(SLO)$/sc_pgbrksh.obj
+        $(SLO)$/sc_viewdata.obj 
 
-EXCEPTIONSFILES=\
-        $(SLO)$/sc_viewdata.obj
-
-NOOPTFILES=\
-    $(SLO)$/sc_drawview.obj	\
-    $(SLO)$/sc_dbfunc2.obj     \
-    $(SLO)$/sc_tabvwsh2.obj
 
 # --- Targets -------------------------------------------------------
 
