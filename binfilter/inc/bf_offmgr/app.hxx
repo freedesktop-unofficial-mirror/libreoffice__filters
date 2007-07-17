4be6e1616aaa10b2be6780f3e4c70905ed51d483
Oliver Bolte <obo@openoffice.org>
2007-07-17 11:19:27 +0000

    INTEGRATION: CWS basemodelrefactoring (1.7.4); FILE MERGED 2007/05/10 14:16:56 mba 1.7.4.2: #i76195#: remove superfluous code 2007/05/01 20:57:57 mba 1.7.4.1: #i76195#: remove view code

--- binfilter/inc/bf_offmgr/app.hxx
+++ binfilter/inc/bf_offmgr/app.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: app.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2007-03-15 15:33:25 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 12:19:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,142 +40,36 @@
 #ifndef _SFXAPP_HXX //autogen
 #include <bf_sfx2/app.hxx>
 #endif
-class SvStringsDtor; 
-class SimpleResMgr; 
+
+class ResMgr; 
+
 namespace binfilter {
 
-class StarBASIC;
-
-// defines ---------------------------------------------------------------
-
-#define SFX_INTERFACE_OFFAPP SFX_INTERFACE_OFA_START
-
-#define ADDRPI_BTN_INSERT	1
-#define ADDRPI_BTN_MERGE	2
 
 #define OFA_REFERER			"private:user"
 
 // forward ---------------------------------------------------------------
 
-class MailServer;
-class OfaHtmlOptions;
-class OfaAutoCorrCfg;
-class SvxAutoCorrect;
-
-//class BasicIDE;
-class SchDLL;
-class SimDLL;
-class SmDLL;
 class XColorTable;
-class XOutdevItemPool;
-class OfaTabAppearanceCfg;
-class CntAnchor;
-class SfxTabPage;
-
-class OfaFilterOptions;
-
 class  OfficeData_Impl;
-struct ConvertData;
-
-// struct OfficeAppl_Impl ------------------------------------------------
-
-#if _SOLAR__PRIVATE
-struct OfficeAppl_Impl
-{
-    ResMgr*				pResMgr;
-    SfxObjectFactory*	pOffMessageDoc_ImplFactory;
-    SimpleResMgr*		m_pThreadSafeRessources;
-
-    OfficeAppl_Impl() :
-        pResMgr( NULL ),
-        pOffMessageDoc_ImplFactory( NULL ),
-        m_pThreadSafeRessources( NULL )
-    {}
-};
-#else
-struct OfficeAppl_Impl;
-#endif
 
 // class OfficeApplication -----------------------------------------------
 
 class OfficeApplication : public SfxApplication
 {
 private:
-    OfficeAppl_Impl*	pImpl;			  // "offentlich f"ur SFX-Makros
-    OfficeData_Impl*	pDataImpl;		  // privat
-    USHORT              nAddrButtonFlags; // welche Buttons vom Adressbuch sind
-    Link				aSbaCreatedLink;
+    OfficeData_Impl*			pDataImpl;		
 
-#if _SOLAR__PRIVATE
-                                 DECL_LINK( ImplGetAutoCorrect, void* );
-                                DECL_LINK( ChangeUserDataHdl_Impl, void* );
-                                DECL_LINK( GlobalBasicErrorHdl, StarBASIC* );
-#endif
-
-#if _SOLAR__PRIVATE
-    void						CreateDataImpl();
-    void						DeleteDataImpl();
-
-    void *						GetSSOCreator( void );
-
-public:
-    void						WriterExec_Impl( SfxRequest &rReq );
-    void						DrawExec_Impl( SfxRequest &rReq );
-    void 						BaseExec_Impl( SfxRequest &rReq );
-    void						ModuleState_Impl( SfxItemSet &rState );
-    void						Execute_Impl( SfxRequest &rReq );
-     void						GetState_Impl( SfxItemSet &rState );
-     void						ExecuteApp_Impl( SfxRequest &rReq );
-    void						GetStateApp_Impl( SfxItemSet &rState );
-#endif
-
-protected:
-#if SUPD<590
-    virtual void                Main( );
-#endif
     virtual void                Init( );
-    virtual void				Exit();
 
 public:
                                 TYPEINFO();
-                                SFX_DECL_INTERFACE(SFX_INTERFACE_OFFAPP);
 
                                 OfficeApplication();
                                 ~OfficeApplication();
 
-#if SUPD<590
-    virtual SbxObject*          GetSbxObject();
-#endif
-
     ResMgr* 					GetOffResManager();
-
-    OfaHtmlOptions*				GetHtmlOptions();
-    OfaFilterOptions*			GetFilterOptions();
-    //BasicIDE*					GetBasicIDE();
-
-    // Autokorrektur
-    OfaAutoCorrCfg*				GetAutoCorrConfig();
-    SvxAutoCorrect*				GetAutoCorrect();
-
-    // Config
-#ifdef WNT
-       OfaTabAppearanceCfg*            GetTabAppearanceConfig();
-#endif
-    void                        UseFontSubst();
-
-    // ColorTable
     XColorTable*				GetStdColorTable();
-
-    static void					InitMailServer( MailServer* pServer );
-    static BOOL					GetEmailNames( SvStringsDtor& rNameList );
-
-
-    //TreeOptionsDialog
-
-#if _SOLAR__PRIVATE
-    OfficeAppl_Impl*			Get_Impl() { return pImpl; }
-#endif
-
 };
 
 #define OFF_APP()	( (OfficeApplication*)SfxGetpApp() )
