5306c9f35e9363ad374e14a90b3af16b356c5bd8
Oliver Bolte <obo@openoffice.org>
2007-07-17 09:34:23 +0000

    INTEGRATION: CWS basemodelrefactoring (1.8.70); FILE MERGED 2007/05/10 20:35:58 mba 1.8.70.4: #i76195#: all sfx resources removed 2007/05/05 12:45:18 mba 1.8.70.3: #i76195#: remove superfluous code 2007/05/01 20:57:51 mba 1.8.70.2: #i76195#: remove view code 2007/04/30 12:12:07 mba 1.8.70.1: #i76195#: remove view code

--- binfilter/bf_sfx2/source/appl/sfx2_appdata.cxx
+++ binfilter/bf_sfx2/source/appl/sfx2_appdata.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_appdata.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 18:53:48 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 10:34:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,15 +37,12 @@
 #endif
 
 #define _SVSTDARR_STRINGS
+#include <svtools/svstdarr.hxx>
 
-
-
+#include "app.hxx"
 #include "appdata.hxx"
-#include "dispatch.hxx"
 #include "doctempl.hxx"
-#include "arrdecl.hxx"
 #include "docfile.hxx"
-#include "app.hrc"
 #include "imestatuswindow.hxx"
 
 #ifndef _LEGACYBINFILTERMGR_HXX
@@ -54,118 +51,32 @@
 namespace binfilter {
 
 /*N*/ SfxAppData_Impl::SfxAppData_Impl( SfxApplication* pApp ) :
-/*N*/ 		bServer( false ),
 /*N*/ 		pProgress(0),
 /*N*/ 		pPool(0),
-/*N*/ 		pFactArr(0),
 /*N*/ 		pDdeService( 0 ),
 /*N*/ 		pDocTopics( 0 ),
 /*N*/ 		pEventConfig(0),
-/*N*/ 		nAsynchronCalls(0),
-/*N*/ 		pVerbs(0),
 /*N*/ 		nBasicCallLevel(0),
 /*N*/ 		nRescheduleLocks(0),
 /*N*/ 		nInReschedule(0),
 /*N*/ 		pInitLinkList(0),
+            pSfxFrameObjectFactoryPtr(0),
+            pThisDocument(0),
 /*N*/ 		pMatcher( 0 ),
-/*N*/ 		pSfxPlugInObjectShellFactory( 0 ),
-/*N*/ 		pDefFocusWin( 0 ),
-/*N*/ 		pSfxFrameObjectFactoryPtr( 0 ),
 /*N*/ 		pCancelMgr( 0 ),
-/*N*/ 		nDocModalMode(0),
-/*N*/ 		pDisabledSlotList( 0 ),
-/*N*/ 		pFilterIni( 0 ),
-/*N*/ 		pSfxPluginObjectFactoryPtr( 0 ),
-/*N*/ 		pTemplateCommon( 0 ),
-/*N*/ 		pLabelResMgr( 0 ),
-/*N*/ 		pTopFrames( new SfxFrameArr_Impl ),
-/*N*/ 		pSecureURLs(0),
-/*N*/ 		nAutoTabPageId(0),
-/*N*/ 		nExecutingSID( 0 ),
-/*N*/ 		pAppDispatch(NULL),
 /*N*/ 		pTriggerTopic(0),
 /*N*/ 		pDdeService2(0),
 /*N*/ 		pMiscConfig(0),
-/*N*/ 		pThisDocument(0),
-/*N*/         bPlugged(sal_False),
-/*N*/ 		bOLEResize(sal_False),
-//STRIP007 		bDirectAliveCount(sal_False),
 /*N*/ 		bInQuit(sal_False),
-/*N*/ 		bInvalidateOnUnlock(sal_False),
-/*N*/ 		bBean( sal_False ),
-/*N*/ 		bMinimized( sal_False ),
-/*N*/ 		bInvisible( sal_False ),
 /*N*/ 		bInException( sal_False ),
-/*N*/         nAppEvent( 0 ),
-/*N*/         pTemplates( 0 ),
-/*N*/         pSaveOptions( 0 ),
-/*N*/         pUndoOptions( 0 ),
-/*N*/         pHelpOptions( 0 ),
-/*N*/         m_xImeStatusWindow(new sfx2::appl::ImeStatusWindow(
+/*N*/       m_xImeStatusWindow(new sfx2::appl::ImeStatusWindow(
 /*N*/                                *pApp, ::legacy_binfilters::getLegacyProcessServiceFactory()))
 /*N*/ {
-/*N*/ 	StartListening( *pApp );
 /*N*/ }
 
 /*N*/ SfxAppData_Impl::~SfxAppData_Impl()
 /*N*/ {
-/*N*/ //#ifdef DBG_UTIL
-/*N*/     DeInitDDE();
-/*N*/ 	delete pTopFrames;
+/*N*/   DeInitDDE();
 /*N*/ 	delete pCancelMgr;
-/*N*/ 	delete pFilterIni;
-/*N*/ 	delete pSecureURLs;
-/*N*/ //#endif
 /*N*/ }
-
-/*N*/ IMPL_STATIC_LINK( SfxAppData_Impl, CreateDocumentTemplates, void*, EMPTYARG)
-/*N*/ {
-/*N*/     pThis->GetDocumentTemplates();
-/*N*/ 	return 0;
-/*N*/ }
-
-/*N*/ void SfxAppData_Impl::UpdateApplicationSettings( sal_Bool bDontHide )
-/*N*/ {
-/*N*/ 	AllSettings aAllSet = Application::GetSettings();
-/*N*/ 	StyleSettings aStyleSet = aAllSet.GetStyleSettings();
-/*N*/ 	sal_uInt32 nStyleOptions = aStyleSet.GetOptions();
-/*N*/ 	if ( bDontHide )
-/*N*/ 		nStyleOptions &= ~STYLE_OPTION_HIDEDISABLED;
-/*N*/ 	else
-/*?*/ 		nStyleOptions |= STYLE_OPTION_HIDEDISABLED;
-/*N*/ 	aStyleSet.SetOptions( nStyleOptions );
-/*N*/ 	aAllSet.SetStyleSettings( aStyleSet );
-/*N*/ 	Application::SetSettings( aAllSet );
-/*N*/ }
-
-/*N*/ SfxDocumentTemplates* SfxAppData_Impl::GetDocumentTemplates()
-/*N*/ {
-/*N*/     if ( !pTemplates )
-/*N*/         pTemplates = new SfxDocumentTemplates;
-/*N*/     else
-/*?*/         {DBG_BF_ASSERT(0, "STRIP");} //STRIP001 pTemplates->ReInitFromComponent();
-/*N*/     return pTemplates;
-/*N*/ }
-
-/*N*/ void SfxAppData_Impl::Notify( SfxBroadcaster &rBC, const SfxHint &rHint )
-/*N*/ {
-/*N*/ 	SfxSimpleHint* pHint = PTR_CAST( SfxSimpleHint, &rHint );
-/*N*/     if( pHint && pHint->GetId() == SFX_HINT_CANCELLABLE )
-/*N*/ 	{
-/*
-        // vom Cancel-Manager
-        for ( SfxViewFrame *pFrame = SfxViewFrame::GetFirst();
-              pFrame;
-              pFrame = SfxViewFrame::GetNext(*pFrame) )
-        {
-            SfxBindings &rBind = pFrame->GetBindings();
-            rBind.Invalidate( SID_BROWSE_STOP );
-            if ( !rBind.IsInRegistrations() )
-                rBind.Update( SID_BROWSE_STOP );
-            rBind.Invalidate( SID_BROWSE_STOP );
-        }
- */
-/*N*/ 	}
-/*N*/ }
-
 }
