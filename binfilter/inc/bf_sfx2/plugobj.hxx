d4e7f8c52126ed91351e3b4c255f8ac77233cdf2
Oliver Bolte <obo@openoffice.org>
2007-07-17 11:41:50 +0000

    INTEGRATION: CWS basemodelrefactoring (1.3.198); FILE MERGED 2007/04/30 12:13:26 mba 1.3.198.1: #i76195#: remove view code

--- binfilter/inc/bf_sfx2/plugobj.hxx
+++ binfilter/inc/bf_sfx2/plugobj.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: plugobj.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 12:31:42 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 12:41:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,6 +40,7 @@
 #include <so3/plugin.hxx>
 #endif
 #include "bf_sfx2/app.hxx"
+
 namespace binfilter {
 
 //=========================================================================
@@ -82,29 +83,13 @@ public:
     static SfxPluginObjectFactoryPtr*
                             GetFactoryPtr();
                             SfxPluginObject();
-    SfxFrame*				GetFrame() const;
 
-
-                            // Macro mu\s exportiert werden, sonst geht das
-                            // SO2-RTTI schief, da GetFactoryAdress() die
-                            // Factory-Adresse der Basisklasse liefert.
-                            // Da das zweite Argument in einer Inline-Funktion
-                            // verwendet wird, mu\s dieses exportierbare
-                            // Funktionsaufrufe verwenden, also nicht z.B.
-                            // SFX_APP()->Get_Impl()
                             SO2_DECL_BASIC_CLASS_DLL(SfxPluginObject, GetFactoryPtr())
-
-#if _SOLAR__PRIVATE
-    void					ReleaseFrame_Impl();
-    BOOL					ConstructFrame();
-#endif
 };
 
 inline SfxPluginObjectFactoryPtr::SfxPluginObjectFactoryPtr()
     : pSfxPluginObjectFactory(0) 	// sonst funzt ClassFactory() nicht!
 {
-    // hier Factory nicht erzeugen, da GetFactoryPtr() erst nach Beenden dieses
-    // ctors einen vern"unftigen Wert liefern kann!
 }
 
 SO2_DECL_IMPL_REF(SfxPluginObject)
