fc552abbac345c237d27c441b578eded47d026d2
Oliver Bolte <obo@openoffice.org>
2007-07-17 09:57:51 +0000

    INTEGRATION: CWS basemodelrefactoring (1.10.4); FILE MERGED 2007/05/10 20:36:01 mba 1.10.4.5: #i76195#: all sfx resources removed 2007/05/01 20:57:52 mba 1.10.4.4: #i76195#: remove view code 2007/04/30 12:12:16 mba 1.10.4.3: #i76195#: remove view code 2007/04/28 12:50:52 mba 1.10.4.2: #i76764#: remove unused code 2007/04/08 18:08:33 mba 1.10.4.1: #i76195#: remove some Sfx view classes

--- binfilter/bf_sfx2/source/doc/sfx2_objcont.cxx
+++ binfilter/bf_sfx2/source/doc/sfx2_objcont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_objcont.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2007-03-15 15:24:26 $
+ *  last change: $Author: obo $ $Date: 2007-07-17 10:57:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,15 +42,11 @@
 #ifndef _CACHESTR_HXX //autogen
 #include <tools/cachestr.hxx>
 #endif
-#ifndef _SV_MSGBOX_HXX //autogen wg. QueryBox
-#include <vcl/msgbox.hxx>
-#endif
 #ifndef _SFXSTYLE_HXX //autogen
 #include <svtools/style.hxx>
 #endif
-#ifndef _SV_WRKWIN_HXX //autogen
-#include <vcl/wrkwin.hxx>
-#endif
+
+#include <bf_sfx2/app.hxx>
 
 #pragma hdrstop
 
@@ -75,36 +71,24 @@
 #include <svtools/useroptions.hxx>
 #include <unotools/localfilehelper.hxx>
 
-#include "sfxresid.hxx"
-#include "stbmgr.hxx"
-#include "dinfdlg.hxx"
+#include "docinf.hxx"
 #include "fltfnc.hxx"
 #include "docfac.hxx"
 #include "cfgmgr.hxx"
-#include "viewsh.hxx"
 #include "objsh.hxx"
 #include "objshimp.hxx"
 #include "cfgitem.hxx"
 #include "evntconf.hxx"
 #include "interno.hxx"
-#include "sfxhelp.hxx"
-#include "dispatch.hxx"
-#include "urlframe.hxx"
 #include "printer.hxx"
-#include "topfrm.hxx"
 #include "doctempl.hxx"
-#include "doc.hrc"
 #include "appdata.hxx"
 #include "sfxbasemodel.hxx"
-#include "accmgr.hxx"
-#include "mnumgr.hxx"
-#include "imgmgr.hxx"
 
 #ifndef _SVTOOLS_IMGDEF_HXX
 #include <svtools/imgdef.hxx>
 #endif
 
-#include "tbxconf.hxx"
 #include "docfile.hxx"
 #include "objuno.hxx"
 #include "request.hxx"
@@ -136,315 +120,11 @@ using namespace ::com::sun::star::uno;
 
 /*N*/ FASTBOOL SfxObjectShell::SaveWindows_Impl( SvStorage &rStor ) const
 /*N*/ {
-/*N*/ 	SvStorageStreamRef xStream = rStor.OpenStream( DEFINE_CONST_UNICODE( SFX_WINDOWS_STREAM ),
-/*N*/ 									STREAM_TRUNC | STREAM_STD_READWRITE);
-/*N*/ 	if ( !xStream )
 /*N*/ 		return FALSE;
-/*N*/ 
-/*N*/ 	xStream->SetBufferSize(1024);
-/*N*/     xStream->SetVersion( rStor.GetVersion() );
-/*N*/ 
-/*N*/     // "uber alle Fenster iterieren (aber aktives Window zuletzt)
-/*N*/     SfxViewFrame *pActFrame = SfxViewFrame::Current();
-/*N*/     if ( !pActFrame || pActFrame->GetObjectShell() != this )
-/*N*/         pActFrame = SfxViewFrame::GetFirst(this);
-/*N*/ 
-/*N*/     String aActWinData;
-/*N*/     for ( SfxViewFrame *pFrame = SfxViewFrame::GetFirst(this, TYPE(SfxTopViewFrame) ); pFrame;
-/*N*/             pFrame = SfxViewFrame::GetNext(*pFrame, this, TYPE(SfxTopViewFrame) ) )
-/*N*/     {
-/*N*/         // Bei Dokumenten, die Outplace aktiv sind, kann beim Speichern auch schon die View weg sein!
-/*N*/         if ( pFrame->GetViewShell() )
-/*N*/         {
-/*N*/             SfxTopFrame* pTop = (SfxTopFrame*) pFrame->GetFrame();
-/*N*/             Window* pWin = pTop->GetTopWindow_Impl();
-/*N*/ 
-/*N*/ #if SUPD<613//MUSTINI
-/*N*/ 			char cToken = SfxIniManager::GetToken();
-/*N*/ #else
-/*N*/ 			char cToken = ',';
-/*N*/ #endif
-/*N*/             const BOOL bActWin = pActFrame == pFrame;
-/*N*/             String aUserData;
-/*N*/             pFrame->GetViewShell()->WriteUserData(aUserData);
-/*N*/ 
-/*N*/             // assemble ini-data
-/*N*/             String aWinData;
-/*N*/             aWinData += String::CreateFromInt32( pFrame->GetCurViewId() );
-/*N*/             aWinData += cToken;
-/*
-            if ( !pWin || pWin->IsMaximized() )
-                aWinData += SFX_WINSIZE_MAX;
-            else if ( pWin->IsMinimized() )
-                aWinData += SFX_WINSIZE_MIN;
-            else
-*/
-/*N*/ #if SUPD<613//MUSTINI
-/*N*/             aWinData += SfxIniManager::GetString( pWin->GetPosPixel(), pWin->GetSizePixel() );
-/*N*/ #endif
-/*N*/             aWinData += cToken;
-/*N*/             aWinData += aUserData;
-/*N*/ 
-/*N*/             // aktives kennzeichnen
-/*N*/             aWinData += cToken;
-/*N*/             aWinData += bActWin ? '1' : '0';
-/*N*/ 
-/*N*/             // je nachdem merken oder abspeichern
-/*N*/             if ( bActWin  )
-/*N*/                 aActWinData = aWinData;
-/*N*/             else
-/*N*/                 xStream->WriteByteString( aWinData );
-/*N*/         }
-/*N*/     }
-/*N*/ 
-/*N*/     // aktives Window hinterher
-/*N*/     xStream->WriteByteString( aActWinData );
-/*N*/ 	return !xStream->GetError();
 /*N*/ }
 
 //====================================================================
 
-/*N*/ SfxViewFrame* SfxObjectShell::LoadWindows_Impl( SfxTopFrame *pPreferedFrame )
-/*N*/ {
-/*N*/     DBG_ASSERT( pPreferedFrame, "Can't be implemented in StarPortal!" );
-/*N*/     if ( pImp->bLoadingWindows || !pPreferedFrame )
-/*N*/ 		return NULL;
-/*N*/ 
-/*N*/     DBG_ASSERT( GetMedium(), "A Medium should exist here!");
-/*N*/     if( !GetMedium() )
-/*N*/ 		return 0;
-/*N*/ 
-/*N*/ 	// get correct mode
-/*N*/ 	SfxApplication *pSfxApp = SFX_APP();
-/*N*/ 	SfxViewFrame *pPrefered = pPreferedFrame ? pPreferedFrame->GetCurrentViewFrame() : 0;
-/*N*/     SvtSaveOptions aOpt;
-/*N*/     BOOL bLoadDocWins = aOpt.IsSaveDocWins() && !pPrefered;
-/*N*/     BOOL bLoadDocView = aOpt.IsSaveDocView();
-/*N*/ 
-/*N*/     // In a StarPortal not possible at the moment
-/*N*/ 	if ( !bLoadDocView )
-/*N*/ 		return 0;
-/*N*/ 
-/*N*/ 	// try to get viewdata information for XML format
-/*N*/ 	REFERENCE < XVIEWDATASUPPLIER > xViewDataSupplier( GetModel(), ::com::sun::star::uno::UNO_QUERY );
-/*N*/ 	REFERENCE < XINDEXACCESS > xViewData;
-/*N*/ 	SvStorageStreamRef xStream;
-/*N*/ 
-/*N*/ 	// get viewdata information for binary format
-/*N*/     SvStorage *pStor = HasName() ? GetStorage() : NULL;
-/*N*/     xStream = pStor ? pStor->OpenStream( DEFINE_CONST_UNICODE( SFX_WINDOWS_STREAM ), STREAM_STD_READ ) : 0;
-/*N*/ 	if ( xStream.Is() && xStream->GetError() == ERRCODE_NONE )
-/*N*/ 	{
-/*?*/ 		xStream->SetVersion( pStor->GetVersion() );
-/*?*/ 		xStream->SetBufferSize(1024);
-/*N*/ 	}
-/*N*/ 	else if ( xViewDataSupplier.is() )
-/*N*/ 	{
-/*N*/ 		xViewData = xViewDataSupplier->getViewData();
-/*N*/ 		if ( !xViewData.is() )
-/*N*/ 			return NULL;
-/*N*/ 	}
-/*N*/ 	else
-/*N*/ 		return NULL;
-/*N*/ 
-/*?*/     BOOL bOldFormat = TRUE;             // old format : not in StarDesktop 5.x
-/*?*/ 	SfxViewFrame *pActiveFrame = 0;
-/*?*/ 	String aWinData;
-/*?*/ 	char cToken =',';
-/*?*/ 	SfxItemSet *pSet = GetMedium()->GetItemSet();
-/*?*/ 
-/*?*/ 	pImp->bLoadingWindows = TRUE;
-/*?*/ 	BOOL bLoaded = FALSE;
-/*?*/ 	sal_Int32 nView = 0;
-/*?*/ 
-/*?*/ 	// get saved information for all views
-/*?*/ 	while ( TRUE )
-/*?*/ 	{
-/*?*/ 		USHORT nViewId = 0;
-/*?*/         FASTBOOL bActive=FALSE, bMaximized=FALSE;
-/*?*/ 		String aPosSize;
-/*?*/ 		String aUserData;			    	// used in the binary format
-/*?*/     	SEQUENCE < PROPERTYVALUE > aSeq;	// used in the XML format
-/*?*/ 		if ( xViewData.is() )
-/*?*/ 		{
-/*?*/ 			// XML format
-/*?*/ 			// active view is the first view in the container
-/*?*/ 			bActive = ( nView == 0 );
-/*?*/ 
-/*?*/ 			if ( nView == xViewData->getCount() )
-/*?*/ 				// finished
-/*?*/ 				break;
-/*?*/ 
-/*?*/ 			// get viewdata and look for the stored ViewId
-/*?*/ 			::com::sun::star::uno::Any aAny = xViewData->getByIndex( nView++ );
-/*?*/ 			if ( aAny >>= aSeq )
-/*?*/ 			{
-/*?*/ 				for ( sal_Int32 n=0; n<aSeq.getLength(); n++ )
-/*?*/ 				{
-/*?*/ 					const PROPERTYVALUE& rProp = aSeq[n];
-/*?*/ 					if ( rProp.Name.compareToAscii("ViewId") == COMPARE_EQUAL )
-/*?*/ 					{
-/*?*/ 						::rtl::OUString aId;
-/*?*/ 						rProp.Value >>= aId;
-/*?*/ 						String aTmp( aId );
-/*?*/ 						aTmp.Erase( 0, 4 );  // format is like in "view3"
-/*?*/ 						nViewId = (USHORT) aTmp.ToInt32();
-/*?*/ 						break;
-/*?*/ 					}
-/*?*/ 				}
-/*?*/ 			}
-/*?*/ 		}
-/*?*/ 		else
-/*?*/ 		{
-/*?*/ 			// binary format
-/*?*/ 			xStream->ReadByteString( aWinData );
-/*?*/ 			if ( !aWinData.Len() )
-/*?*/ 				// reading finished
-/*?*/ 				break;
-/*?*/ 
-/*?*/ 			if ( aWinData.GetToken( 0, cToken ).EqualsAscii( "TASK" ) )
-/*?*/ 			{
-/*?*/             	// doesn't make any sense with the new task handling using system tasks or browser windows
-/*?*/ 				bOldFormat = FALSE;
-/*?*/ 				continue;
-/*?*/ 			}
-/*?*/ 
-/*?*/ 			nViewId = (USHORT) aWinData.GetToken( 0, cToken ).ToInt32();
-/*?*/             if ( bOldFormat )
-/*?*/ 			{
-/*?*/                 // Old format
-/*?*/ 				aPosSize = aWinData.GetToken( 1, cToken );
-/*?*/ 				aPosSize.ToLowerAscii();
-/*?*/ 				aUserData = aWinData.GetToken( 2, cToken );
-/*?*/ 				bActive = aWinData.GetToken( 3, cToken ).ToInt32();
-/*?*/ 			}
-/*?*/ 			else
-/*?*/ 			{
-/*?*/                 // 5.0-Format, get activity state and UserData
-/*?*/ 				USHORT nPos=0;
-/*?*/ 				bActive = aWinData.GetToken( 3, cToken, nPos ).ToInt32();
-/*?*/ 				aUserData = aWinData.Copy( nPos );
-/*?*/ 			}
-/*?*/ 		}
-/*?*/ 
-/*?*/ 		// load only active view, but current item is not the active one ?
-/*?*/ 		if ( !bLoadDocWins && !bActive )
-/*?*/ 		{
-/*?*/ 			if ( xViewData.is() )
-/*?*/ 				// in XML format the active view is the first one
-/*?*/ 				break;
-/*?*/ 			else
-/*?*/ 				continue;
-/*?*/ 		}
-/*?*/ 
-/*?*/ 		// check for minimized/maximized/size
-/*?*/ 		if ( aPosSize.EqualsAscii( "min" ) )
-/*?*/             bMaximized = TRUE;
-/*?*/ 		else if ( aPosSize.EqualsAscii( "min" ) )
-/*?*/ 		{
-/*?*/             bMaximized = TRUE;
-/*?*/ 			bActive = FALSE;
-/*?*/ 		}
-/*?*/ 		else
-/*?*/             bMaximized = FALSE;
-/*?*/ 
-/*?*/ 		Point aPt;
-/*?*/ 		Size aSz;
-/*?*/ #if SUPD<613//MUSTINI
-/*?*/         if ( !bMaximized )
-/*?*/ 			SfxIniManager::GetPosSize( aPosSize, aPt, aSz );
-/*?*/ #endif
-/*?*/ 
-/*?*/ 		pSet->ClearItem( SID_USER_DATA );
-/*?*/ 		SfxViewFrame *pFrame = 0;
-/*?*/ 		if ( pPrefered )
-/*?*/ 		{
-/*?*/ 			// use the frame from the arguments, but don't set a window size
-/*?*/ 			pFrame = pPrefered;
-/*?*/ 			if ( pFrame->GetViewShell() || !pFrame->GetObjectShell() )
-/*?*/ 			{
-/*?*/ 				pSet->ClearItem( SID_VIEW_POS_SIZE );
-/*?*/ 				pSet->ClearItem( SID_WIN_POSSIZE );
-/*?*/ 				pSet->Put( SfxUInt16Item( SID_VIEW_ID, nViewId ) );
-/*?*/ 
-/*?*/ 				// avoid flickering controllers
-/*?*/                 SfxBindings &rBind = pFrame->GetBindings();
-/*?*/ 				rBind.ENTERREGISTRATIONS();
-/*?*/ 
-/*?*/ 				// set document into frame
-/*?*/ 				pPreferedFrame->InsertDocument( this );
-/*?*/ 
-/*?*/ 				// restart controller updating
-/*?*/ 				rBind.LEAVEREGISTRATIONS();
-/*?*/ 			}
-/*?*/ 			else
-/*?*/ 			{
-/*?*/ 				// create new view
-/*?*/ 				DBG_BF_ASSERT(0, "STRIP"); //STRIP001 pFrame->CreateView_Impl( nViewId );
-/*?*/ 			}
-/*?*/ 		}
-/*?*/ 		else
-/*?*/ 		{
-/*?*/ 			if ( bLoadDocWins )
-/*?*/ 			{
-/*?*/ 				// open in the background
-/*?*/ 				pSet->Put( SfxUInt16Item( SID_VIEW_ZOOM_MODE, 0 ) );
-/*?*/                 if ( !bMaximized )
-/*?*/ 					pSet->Put( SfxRectangleItem( SID_VIEW_POS_SIZE, Rectangle( aPt, aSz ) ) );
-/*?*/ 			}
-/*?*/ 
-/*?*/ 			pSet->Put( SfxUInt16Item( SID_VIEW_ID, nViewId ) );
-/*?*/ 
-/*?*/ 			if ( pPreferedFrame )
-/*?*/ 			{
-/*?*/ 				// Frame "ubergeben, allerdings ist der noch leer
-/*?*/ 				pPreferedFrame->InsertDocument( this );
-/*?*/ 				pFrame = pPreferedFrame->GetCurrentViewFrame();
-/*?*/ 			}
-/*?*/ 			else
-/*?*/ 			{
-/*?*/     			DBG_BF_ASSERT(0, "STRIP"); //STRIP001 pFrame = SfxTopFrame::Create( this, nViewId, FALSE, pSet )->GetCurrentViewFrame();
-/*?*/ 			}
-/*?*/ 
-/*?*/ 			// only temporary data, don't hold it in the itemset
-/*?*/ 			pSet->ClearItem( SID_VIEW_POS_SIZE );
-/*?*/ 			pSet->ClearItem( SID_WIN_POSSIZE );
-/*?*/ 			pSet->ClearItem( SID_VIEW_ZOOM_MODE );
-/*?*/ 		}
-/*?*/ 
-/*?*/ 		bLoaded = TRUE;
-/*?*/ 
-/*?*/ 		// UserData hier einlesen, da es ansonsten immer mit bBrowse=TRUE
-/*?*/ 		// aufgerufen wird, beim Abspeichern wurde aber bBrowse=FALSE verwendet
-/*?*/ 		if ( pFrame && pFrame->GetViewShell() )
-/*?*/ 		{
-/*?*/ 			if ( aUserData.Len() )
-/*?*/ 				pFrame->GetViewShell()->ReadUserData( aUserData, !bLoadDocWins );
-/*?*/ 			else if ( aSeq.getLength() )
-/*?*/ 				pFrame->GetViewShell()->ReadUserDataSequence( aSeq, !bLoadDocWins );
-/*?*/ 		}
-/*?*/ 
-/*?*/ 		// perhaps there are more windows to load
-/*?*/ 		pPreferedFrame = NULL;
-/*?*/ 
-/*?*/ 		if ( bActive )
-/*?*/ 			pActiveFrame = pFrame;
-/*?*/ 
-/*?*/ 		if( pPrefered || !bLoadDocWins )
-/*?*/ 			// load only active window
-/*?*/ 			break;
-/*?*/ 	}
-/*?*/ 
-/*?*/ 	if ( pActiveFrame )
-/*?*/ 	{
-/*?*/ 		if ( !pPrefered )
-/*?*/ 			// activate frame
-/*?*/             pActiveFrame->MakeActive_Impl( TRUE );
-/*?*/ 	}
-/*?*/ 
-/*?*/ 	pImp->bLoadingWindows = FALSE;
-/*?*/ 	return pPrefered && bLoaded ? pPrefered : pActiveFrame;
-/*N*/ }
 
 //====================================================================
 
@@ -748,11 +428,6 @@ using namespace ::com::sun::star::uno;
 /*N*/         if ( bForceCreation || HasStorage() && SfxConfigManager::HasConfiguration( *GetStorage() ) )
 /*N*/ 		{
 /*N*/             pImp->pCfgMgr = new SfxConfigManager( *this );
-/*N*/ 			SfxConfigItem* pItem = GetEventConfig_Impl( FALSE );
-/*N*/ 			if ( pItem && !pItem->GetConfigManager() )
-/*N*/ 				// imported binary format
-/*N*/ 				pItem->Connect( pImp->pCfgMgr );
-/*N*/ 
 /*N*/ 		}
 /*N*/     }
 /*N*/ 
@@ -857,6 +532,15 @@ using namespace ::com::sun::star::uno;
 /*N*/ {DBG_BF_ASSERT(0, "STRIP"); //STRIP001 
 /*N*/ }
 
+
+/*N*/ SvStorageRef SfxObjectShell::GetConfigurationStorage( SotStorage* pStor )
+/*N*/ {DBG_BF_ASSERT(0, "STRIP"); return SvStorageRef();//STRIP001 
+/*N*/ }
+
+/*N*/ SotStorageStreamRef SfxObjectShell::GetConfigurationStream( const String& rName, BOOL bCreate )
+/*N*/ {DBG_BF_ASSERT(0, "STRIP"); return SotStorageStreamRef();//STRIP001 
+/*N*/ }
+
 /*N*/ SfxEventConfigItem_Impl* SfxObjectShell::GetEventConfig_Impl( BOOL bForce )
 /*N*/ {
 /*N*/     if ( bForce && !pImp->pEventConfig )
@@ -869,77 +553,6 @@ using namespace ::com::sun::star::uno;
 /*N*/ 	}
 /*N*/ 
 /*N*/ 	return pImp->pEventConfig;
-/*N*/ }
-
-/*N*/ SvStorageRef SfxObjectShell::GetConfigurationStorage( SotStorage* pStor )
-/*N*/ {DBG_BF_ASSERT(0, "STRIP"); return SvStorageRef();//STRIP001 
-/*N*/ }
-
-/*N*/ SotStorageStreamRef SfxObjectShell::GetConfigurationStream( const String& rName, BOOL bCreate )
-/*N*/ {DBG_BF_ASSERT(0, "STRIP"); return SotStorageStreamRef();//STRIP001 
-/*N*/ }
-
-/*N*/ SfxAcceleratorManager* SfxObjectShell::GetAccMgr_Impl()
-/*N*/ {
-/*N*/ 	// already constructed ?!
-/*N*/ 	if ( pImp->pAccMgr )
-/*N*/ 		return pImp->pAccMgr;
-/*N*/ 
-/*N*/ 	// get the typId ( = ResourceId )
-/*N*/ 	const ResId* pResId = GetFactory().GetAccelId();
-/*N*/ 	if ( !pResId )
-/*N*/ 		return NULL;
-/*N*/ 
-/*N*/ 	if ( GetConfigManager() && pImp->pCfgMgr->HasConfigItem( pResId->GetId() ) )
-/*N*/ 	{
-/*N*/         // document has configuration
-/*N*/         pImp->pAccMgr = new SfxAcceleratorManager( *pResId, pImp->pCfgMgr );
-/*N*/ 		return pImp->pAccMgr;
-/*N*/ 	}
-/*N*/     else
-/*N*/         return GetFactory().GetAccMgr_Impl();
-/*N*/ }
-
-/*N*/ SfxMenuBarManager* SfxObjectShell::CreateMenuBarManager_Impl( SfxViewFrame* pViewFrame )
-/*N*/ {
-/*N*/ 	SfxBindings& rBindings = pViewFrame->GetBindings();
-/*N*/ 	sal_Bool bCheckPlugin = SfxApplication::IsPlugin();
-/*N*/ 	const ResId* pId = bCheckPlugin ? GetFactory().GetPluginMenuBarId() : GetFactory().GetMenuBarId();
-/*N*/ 	DBG_ASSERT( pId && pId->GetId(), "Component must have own window!" );
-/*N*/ 	if ( !pId )
-/*N*/ 		return NULL;
-/*N*/ 
-/*N*/     SfxConfigManager *pCfgMgr = SFX_APP()->GetConfigManager_Impl();
-/*N*/     if ( GetConfigManager() && pImp->pCfgMgr->HasConfigItem( pId->GetId() ) )
-/*N*/         pCfgMgr = pImp->pCfgMgr;
-/*N*/ 
-/*N*/     SfxMenuBarManager* pMgr = new SfxMenuBarManager( *pId, rBindings, pCfgMgr, pViewFrame->ISA( SfxInPlaceFrame ) );
-/*N*/     return pMgr;
-/*N*/ }
-
-/*N*/ SfxImageManager* SfxObjectShell::GetImageManager_Impl()
-/*N*/ {
-/*N*/     if ( pImp->pImageManager )
-/*N*/         return pImp->pImageManager;
-/*N*/ 
-/*N*/     // every document has its own ImageManager, but they may use the global configuration!
-/*N*/     pImp->pImageManager = new SfxImageManager( this );
-/*N*/     return pImp->pImageManager;
-/*N*/ }
-
-/*N*/ SfxObjectShellRef MakeObjectShellForOrganizer_Impl( const String& aTargetURL, BOOL bForWriting )
-/*N*/ {DBG_BF_ASSERT(0, "STRIP"); return SfxObjectShellRef();//STRIP001 
-/*N*/ }
-
-/*N*/ SfxToolBoxConfig* SfxObjectShell::GetToolBoxConfig_Impl()
-/*N*/ {
-/*N*/     if ( !pImp->pTbxConfig )
-/*N*/ 	{
-/*N*/         pImp->pTbxConfig = new SfxToolBoxConfig(
-/*N*/ 			GetConfigManager() ? pImp->pCfgMgr : SFX_APP()->GetConfigManager_Impl() );
-/*N*/ 	}
-/*N*/ 
-/*N*/     return pImp->pTbxConfig;
 /*N*/ }
 
 
