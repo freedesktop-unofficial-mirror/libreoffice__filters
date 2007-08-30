0c6cd5752d0bd618fed08fcd178b0ec12354be2d
Vladimir Glazounov <vg@openoffice.org>
2007-08-30 10:56:44 +0000

    INTEGRATION: CWS bfcoverity01 (1.4.106); FILE MERGED 2007/08/13 11:17:22 jnavrati 1.4.106.1: #i80520# mirror correct initialization values into binfilter according to source in upstream

--- binfilter/bf_xmloff/source/text/xmloff_txtparai.cxx
+++ binfilter/bf_xmloff/source/text/xmloff_txtparai.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_txtparai.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 02:21:28 $
+ *  last change: $Author: vg $ $Date: 2007-08-30 11:56:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -899,6 +899,9 @@ SvXMLImportContext *XMLImpRubyContext_Impl::CreateChildContext(
                                                        rLocalName,
                                                        xAttrList,
                                                        pHint );
+        else
+                pContext = 0; //  TODO What value should this be?
+
     }
     else
         pContext = SvXMLImportContext::CreateChildContext( nPrefix, rLocalName,
