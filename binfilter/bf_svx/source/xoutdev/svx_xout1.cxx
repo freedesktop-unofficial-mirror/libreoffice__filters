ebcdf2760847b9b27195fcb27267f5ff54179111
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:49:34 +0000

    INTEGRATION: CWS mingwport06 (1.6.80); FILE MERGED 2007/08/24 12:46:21 vg 1.6.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_svx/source/xoutdev/svx_xout1.cxx
+++ binfilter/bf_svx/source/xoutdev/svx_xout1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_xout1.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:39:22 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:49:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "xoutx.hxx"
 
