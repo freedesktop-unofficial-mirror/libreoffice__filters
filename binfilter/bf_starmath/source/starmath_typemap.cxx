519ebb134e50a28484f777cab2847b353f004cb2
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:34:28 +0000

    INTEGRATION: CWS mingwport06 (1.3.242); FILE MERGED 2007/08/24 12:46:16 vg 1.3.242.1: #i75499# pragma is for MSVC

--- binfilter/bf_starmath/source/starmath_typemap.cxx
+++ binfilter/bf_starmath/source/starmath_typemap.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: starmath_typemap.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 04:06:32 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:34:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define ITEMID_PTR              0
 #define ITEMID_SHADOW           0
