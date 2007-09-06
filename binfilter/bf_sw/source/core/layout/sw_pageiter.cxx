ba83f41f43a9873c5f33ad903c5a089f703f1e28
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:21:42 +0000

    INTEGRATION: CWS mingwport06 (1.6.102); FILE MERGED 2007/08/24 12:46:29 vg 1.6.102.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/layout/sw_pageiter.cxx
+++ binfilter/bf_sw/source/core/layout/sw_pageiter.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_pageiter.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 12:32:25 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:21:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _HORIORNT_HXX
 #include <horiornt.hxx>
