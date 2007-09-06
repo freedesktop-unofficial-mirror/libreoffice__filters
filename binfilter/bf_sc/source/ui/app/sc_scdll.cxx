73af95ef44b672d7d0acf258d0d41170e9c0a19a
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:02:32 +0000

    INTEGRATION: CWS mingwport06 (1.7.16); FILE MERGED 2007/08/24 12:46:07 vg 1.7.16.1: #i75499# pragma is for MSVC

--- binfilter/bf_sc/source/ui/app/sc_scdll.cxx
+++ binfilter/bf_sc/source/ui/app/sc_scdll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_scdll.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 09:15:11 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:02:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,9 @@
 #ifdef PCH
 #endif
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include <bf_svx/eeitem.hxx>
 #define ITEMID_FIELD EE_FEATURE_FIELD
