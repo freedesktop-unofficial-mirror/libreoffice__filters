57cafca59ead84b90667fe97f1964c656d6b0c51
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:42:01 +0000

    INTEGRATION: CWS mingwport06 (1.5.114); FILE MERGED 2007/08/24 12:46:33 vg 1.5.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/txtnode/sw_atrtox.cxx
+++ binfilter/bf_sw/source/core/txtnode/sw_atrtox.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_atrtox.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 23:17:23 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:42:01 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _HORIORNT_HXX
 #include <horiornt.hxx>
