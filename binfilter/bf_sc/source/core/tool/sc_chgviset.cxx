e492d361df4d0282b99c148d289016d9c32918c1
Kurt Zenker <kz@openoffice.org>
2007-09-06 09:44:09 +0000

    INTEGRATION: CWS mingwport06 (1.7.114); FILE MERGED 2007/08/24 12:46:01 vg 1.7.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sc/source/core/tool/sc_chgviset.cxx
+++ binfilter/bf_sc/source/core/tool/sc_chgviset.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_chgviset.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 14:30:35 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 10:44:09 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,9 @@
 #ifdef PCH
 #endif
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include <unotools/textsearch.hxx>
 
