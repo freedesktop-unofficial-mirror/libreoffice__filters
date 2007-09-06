b0ef5225ef9fd4b2e9facc246243fc3d9334c159
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:20:54 +0000

    INTEGRATION: CWS mingwport06 (1.4.114); FILE MERGED 2007/08/24 12:46:49 vg 1.4.114.1: #i75499# pragma is for MSVC

--- binfilter/inc/bf_svx/xpolyimp.hxx
+++ binfilter/inc/bf_svx/xpolyimp.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xpolyimp.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:33:05 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:20:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,7 +43,9 @@
 #include <bf_svx/xpoly.hxx>
 #endif
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 namespace binfilter {
 
 
