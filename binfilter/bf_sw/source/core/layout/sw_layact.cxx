60e35370916d216fe9c8168bfa75d39ebbe77597
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:20:10 +0000

    INTEGRATION: CWS mingwport06 (1.10.16); FILE MERGED 2007/08/24 12:46:28 vg 1.10.16.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/layout/sw_layact.cxx
+++ binfilter/bf_sw/source/core/layout/sw_layact.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_layact.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 12:02:00 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:20:10 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include <time.h>
 #include "pagefrm.hxx"
