183307e1f736e8ea04de9f76b0b7ceb7bd276697
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:27:44 +0000

    INTEGRATION: CWS mingwport06 (1.7.16); FILE MERGED 2007/08/24 12:46:14 vg 1.7.16.1: #i75499# pragma is for MSVC

--- binfilter/bf_sfx2/source/config/sfx2_cfgitem.cxx
+++ binfilter/bf_sfx2/source/config/sfx2_cfgitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_cfgitem.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 10:42:43 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:26:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "cfgmgr.hxx"
 #include "sfx.hrc"
--- binfilter/bf_sfx2/source/control/sfx2_request.cxx
+++ binfilter/bf_sfx2/source/control/sfx2_request.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_request.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 10:46:21 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:27:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,9 @@
 #include <svtools/itempool.hxx>
 #include <svtools/itemset.hxx>
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "request.hxx"
 
