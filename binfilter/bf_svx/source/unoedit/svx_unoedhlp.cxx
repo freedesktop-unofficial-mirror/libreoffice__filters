bc7198ac9037dff63abaecf9d9626a6fb1c7b6e5
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:47:35 +0000

    INTEGRATION: CWS mingwport06 (1.6.80); FILE MERGED 2007/08/24 12:46:20 vg 1.6.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_svx/source/unoedit/svx_unoedhlp.cxx
+++ binfilter/bf_svx/source/unoedit/svx_unoedhlp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_unoedhlp.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:36:49 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:47:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _TOOLS_DEBUG_HXX
 #include <tools/debug.hxx>
--- binfilter/bf_svx/source/unoedit/svx_unoedprx.cxx
+++ binfilter/bf_svx/source/unoedit/svx_unoedprx.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_unoedprx.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:37:00 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:47:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 //------------------------------------------------------------------------
 //
