b3be8b441be0c0a73cf1801b3cbc6aed071f8bd5
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:08:49 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:25 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/docnode/sw_node2lay.cxx
+++ binfilter/bf_sw/source/core/docnode/sw_node2lay.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_node2lay.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2007-08-30 11:52:01 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:06:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 
 #ifndef _ERRHDL_HXX
--- binfilter/bf_sw/source/core/draw/sw_dview.cxx
+++ binfilter/bf_sw/source/core/draw/sw_dview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_dview.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:31:50 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:08:49 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "hintids.hxx"
 
