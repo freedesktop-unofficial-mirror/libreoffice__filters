e3697f8a8ab9c7233303b6fad7ed51575f833e1e
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:16:43 +0000

    INTEGRATION: CWS mingwport06 (1.10.80); FILE MERGED 2007/08/24 12:46:28 vg 1.10.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/graphic/sw_ndgrf.cxx
+++ binfilter/bf_sw/source/core/graphic/sw_ndgrf.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_ndgrf.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: vg $ $Date: 2007-08-30 11:52:29 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:16:43 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #ifndef _HINTIDS_HXX
