378e58e10f19c5d3f89301df89142b9569a20ae6
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:10:14 +0000

    INTEGRATION: CWS mingwport06 (1.8.16); FILE MERGED 2007/08/24 12:46:09 vg 1.8.16.1: #i75499# pragma is for MSVC

--- binfilter/bf_sc/source/ui/unoobj/sc_docuno.cxx
+++ binfilter/bf_sc/source/ui/unoobj/sc_docuno.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_docuno.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 09:33:49 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:10:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,9 @@
 #ifdef PCH
 #endif
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include <com/sun/star/container/XIndexContainer.hpp>
 
