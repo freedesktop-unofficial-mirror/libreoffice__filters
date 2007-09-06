a3cf572e5bc8ca36118ba09087943db45c1746a3
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:30:47 +0000

    INTEGRATION: CWS mingwport06 (1.6.102); FILE MERGED 2007/08/24 12:46:31 vg 1.6.102.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/swg/sw_rdcont.cxx
+++ binfilter/bf_sw/source/core/swg/sw_rdcont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_rdcont.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 12:33:12 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:28:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _HORIORNT_HXX
 #include <horiornt.hxx>
--- binfilter/bf_sw/source/core/swg/sw_rdpage.cxx
+++ binfilter/bf_sw/source/core/swg/sw_rdpage.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_rdpage.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2006-11-08 12:34:47 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:30:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _HORIORNT_HXX
 #include <horiornt.hxx>
