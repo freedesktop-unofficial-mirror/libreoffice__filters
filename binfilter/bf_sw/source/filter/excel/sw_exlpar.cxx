edefda5e34a383ede98e073be408faa9b19fa0f6
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:58:21 +0000

    INTEGRATION: CWS mingwport06 (1.7.94); FILE MERGED 2007/08/24 12:46:40 vg 1.7.94.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/filter/excel/sw_exlpar.cxx
+++ binfilter/bf_sw/source/filter/excel/sw_exlpar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_exlpar.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2006-11-20 12:38:56 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:57:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _ZFORLIST_HXX //autogen
 #include <svtools/zforlist.hxx>
