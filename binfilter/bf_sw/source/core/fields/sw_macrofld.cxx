e71bcc693d76115242b9d2465fea117b30b9cfb0
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:14:15 +0000

    INTEGRATION: CWS mingwport06 (1.7.114); FILE MERGED 2007/08/24 12:46:27 vg 1.7.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/fields/sw_macrofld.cxx
+++ binfilter/bf_sw/source/core/fields/sw_macrofld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_macrofld.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:38:40 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:14:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 
 #ifndef _HORIORNT_HXX
--- binfilter/bf_sw/source/core/fields/sw_reffld.cxx
+++ binfilter/bf_sw/source/core/fields/sw_reffld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_reffld.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:38:50 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:14:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define _SVSTDARR_USHORTSSORT
 #define _SVSTDARR_USHORTS
