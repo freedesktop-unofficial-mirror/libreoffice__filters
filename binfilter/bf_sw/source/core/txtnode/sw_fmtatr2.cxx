c3fb9b7acdccb6d1d588159a9cdda3f95c7d302b
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:42:31 +0000

    INTEGRATION: CWS mingwport06 (1.7.114); FILE MERGED 2007/08/24 12:46:34 vg 1.7.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/txtnode/sw_fmtatr2.cxx
+++ binfilter/bf_sw/source/core/txtnode/sw_fmtatr2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_fmtatr2.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 23:17:56 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:42:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "unomid.h"
 
