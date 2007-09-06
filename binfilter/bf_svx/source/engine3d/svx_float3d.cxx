7c04facd5ebc8c11eeb5e7ff742812266eb2563f
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:40:32 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:17 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_svx/source/engine3d/svx_float3d.cxx
+++ binfilter/bf_svx/source/engine3d/svx_float3d.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_float3d.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 20:48:51 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:39:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _SFXDISPATCH_HXX //autogen
 #include <bf_sfx2/dispatch.hxx>
--- binfilter/bf_svx/source/form/svx_fmpgeimp.cxx
+++ binfilter/bf_svx/source/form/svx_fmpgeimp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_fmpgeimp.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 20:56:19 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:40:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define _EXTERN_FORMLAYER_
 
