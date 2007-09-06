7d69a23ad2cbbaef5e4ecf5c947c3fc22c32d8fa
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:14:56 +0000

    INTEGRATION: CWS mingwport06 (1.8.114); FILE MERGED 2007/08/24 12:46:10 vg 1.8.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sch/source/core/sch_chartdoc.cxx
+++ binfilter/bf_sch/source/core/sch_chartdoc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_chartdoc.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 17:22:24 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:14:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 
 #ifndef _EEITEM_HXX //autogen
