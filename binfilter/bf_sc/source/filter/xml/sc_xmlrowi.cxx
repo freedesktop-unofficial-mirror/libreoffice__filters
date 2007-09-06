218d6896369774f53131086e23ded3444230c67a
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:00:30 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:05 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sc/source/filter/xml/sc_xmlrowi.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_xmlrowi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_xmlrowi.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 15:31:21 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:00:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,9 @@
 #ifdef PCH
 #endif
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 // INCLUDE ---------------------------------------------------------------
 
