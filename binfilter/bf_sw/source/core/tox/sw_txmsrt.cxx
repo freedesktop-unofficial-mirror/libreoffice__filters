545d1ee6fdca481cdb578292c5a434c63e7fc674
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:41:47 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:33 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/tox/sw_txmsrt.cxx
+++ binfilter/bf_sw/source/core/tox/sw_txmsrt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_txmsrt.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 23:16:16 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:40:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _UNOTOOLS_CHARCLASS_HXX
 #include <unotools/charclass.hxx>
