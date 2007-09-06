26e4a9df9c36bc8f2f275625517b46b8f5f595b5
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:36:18 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:16 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_starmath/source/starmath_symbol.cxx
+++ binfilter/bf_starmath/source/starmath_symbol.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: starmath_symbol.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:58:13 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:34:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 
 
