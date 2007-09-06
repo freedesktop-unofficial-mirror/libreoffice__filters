5da596cef65019639666380b92cc76ad266d4a23
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:31:55 +0000

    INTEGRATION: CWS mingwport06 (1.5.114); FILE MERGED 2007/08/24 12:46:15 vg 1.5.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_starmath/source/cfgitem.hxx
+++ binfilter/bf_starmath/source/cfgitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfgitem.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:54:07 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:31:55 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _MATH_CFGITEM_HXX_
 #define _MATH_CFGITEM_HXX_
