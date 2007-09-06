77eddcc8eb3047c60431a221b6ec11650434f762
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:09:46 +0000

    INTEGRATION: CWS mingwport06 (1.8.16); FILE MERGED 2007/08/24 12:46:43 vg 1.8.16.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/ui/config/sw_viewopt.cxx
+++ binfilter/bf_sw/source/ui/config/sw_viewopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_viewopt.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 12:07:56 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:09:46 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _OFF_APP_HXX //autogen
 #include <bf_offmgr/app.hxx>
