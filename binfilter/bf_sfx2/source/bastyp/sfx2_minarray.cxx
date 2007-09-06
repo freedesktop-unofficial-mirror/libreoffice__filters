538e587e76fab968f140aeae9976a64ec8615f17
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:26:05 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:13 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sfx2/source/bastyp/sfx2_minarray.cxx
+++ binfilter/bf_sfx2/source/bastyp/sfx2_minarray.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_minarray.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:04:58 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:26:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "minarray.hxx"
 namespace binfilter {
