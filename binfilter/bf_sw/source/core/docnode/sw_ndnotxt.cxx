5feacdcd5d0d3f9e91a8f45f58e76ab7feb967b5
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:05:31 +0000

    INTEGRATION: CWS mingwport06 (1.9.16); FILE MERGED 2007/08/24 12:46:25 vg 1.9.16.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/docnode/sw_ndnotxt.cxx
+++ binfilter/bf_sw/source/core/docnode/sw_ndnotxt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_ndnotxt.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 12:01:01 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:05:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 #ifndef _SV_GRAPH_HXX
 #include <vcl/graph.hxx>
 #endif
