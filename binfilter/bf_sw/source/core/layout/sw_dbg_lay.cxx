1fb6c04ebb9c43a5190e3df72012238a78bac707
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:18:57 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:28 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/layout/sw_dbg_lay.cxx
+++ binfilter/bf_sw/source/core/layout/sw_dbg_lay.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_dbg_lay.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:51:51 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:17:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -113,7 +113,9 @@
 #endif
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "dbg_lay.hxx"
 
