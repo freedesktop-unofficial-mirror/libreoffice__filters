98dbb3071da7a54d9551ca9b2322c25f4d0b6922
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:08:33 +0000

    INTEGRATION: CWS mingwport06 (1.6.80); FILE MERGED 2007/08/24 12:46:43 vg 1.6.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/ui/config/sw_dbconfig.cxx
+++ binfilter/bf_sw/source/ui/config/sw_dbconfig.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_dbconfig.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:09:38 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:08:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _DBCONFIG_HXX
 #include <dbconfig.hxx>
