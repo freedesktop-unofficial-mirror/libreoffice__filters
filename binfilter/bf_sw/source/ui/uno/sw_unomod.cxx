bd46664a9cc1881aa5a039df7b234a0a73d1ac1a
Kurt Zenker <kz@openoffice.org>
2007-09-06 12:12:53 +0000

    INTEGRATION: CWS mingwport06 (1.6.14); FILE MERGED 2007/08/24 12:46:46 vg 1.6.14.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/ui/uno/sw_unomod.cxx
+++ binfilter/bf_sw/source/ui/uno/sw_unomod.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_unomod.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 12:12:34 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 13:12:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include <swtypes.hxx>
 #ifndef _TOOLS_DEBUG_HXX //autogen
