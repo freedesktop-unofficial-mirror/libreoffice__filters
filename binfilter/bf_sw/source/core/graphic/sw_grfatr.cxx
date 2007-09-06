864d1db2182a91d1c2bfc71af398bee778c6f99e
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:16:30 +0000

    INTEGRATION: CWS mingwport06 (1.8.96); FILE MERGED 2007/08/24 12:46:28 vg 1.8.96.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/graphic/sw_grfatr.cxx
+++ binfilter/bf_sw/source/core/graphic/sw_grfatr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_grfatr.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 12:07:07 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:16:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _COM_SUN_STAR_TEXT_RELORIENTATION_HPP_
 #include <com/sun/star/text/RelOrientation.hpp>
