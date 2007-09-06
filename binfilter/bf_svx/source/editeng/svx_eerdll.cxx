3a71d9a9471f4d14db148383520a7ec75b9331bc
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:36:54 +0000

    INTEGRATION: CWS mingwport06 (1.7.34); FILE MERGED 2007/08/24 12:46:16 vg 1.7.34.1: #i75499# pragma is for MSVC

--- binfilter/bf_svx/source/editeng/svx_eerdll.cxx
+++ binfilter/bf_svx/source/editeng/svx_eerdll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_eerdll.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:16:16 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:36:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include <svtools/solar.hrc>
 #include <eerdll.hxx>
