efd7567d928bf06f3e1cbe719ca81fcf2fb2d6d4
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:22:35 +0000

    INTEGRATION: CWS mingwport06 (1.4.34); FILE MERGED 2007/08/24 12:46:12 vg 1.4.34.1: #i75499# pragma is for MSVC

--- binfilter/bf_sd/source/ui/app/sd_sdresid.cxx
+++ binfilter/bf_sd/source/ui/app/sd_sdresid.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sd_sdresid.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:12:06 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:22:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _SHL_HXX //autogen
 #include <tools/shl.hxx>
