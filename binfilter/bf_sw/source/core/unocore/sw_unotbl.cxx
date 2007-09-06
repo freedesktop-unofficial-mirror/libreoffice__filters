10dccd82ee0e8e6919a1995253c6a581fbf6084a
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:54:13 +0000

    INTEGRATION: CWS mingwport06 (1.6.80); FILE MERGED 2007/08/24 12:46:36 vg 1.6.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/unocore/sw_unotbl.cxx
+++ binfilter/bf_sw/source/core/unocore/sw_unotbl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_unotbl.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:05:54 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:54:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define ITEMID_BOXINFO SID_ATTR_BORDER_INNER
 
