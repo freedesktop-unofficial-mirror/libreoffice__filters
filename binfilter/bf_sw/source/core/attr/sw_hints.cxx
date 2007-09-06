e007c9cd1abd6b2519957067b33fc51ff8c2e5ec
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:51:25 +0000

    INTEGRATION: CWS mingwport06 (1.5.114); FILE MERGED 2007/08/24 12:46:21 vg 1.5.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/attr/sw_hints.cxx
+++ binfilter/bf_sw/source/core/attr/sw_hints.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_hints.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:14:47 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:50:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _COM_SUN_STAR_I18N_SCRIPTTYPE_HDL_
 #include <com/sun/star/i18n/ScriptType.hdl>
--- binfilter/bf_sw/source/core/bastyp/sw_breakit.cxx
+++ binfilter/bf_sw/source/core/bastyp/sw_breakit.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_breakit.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:15:21 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:51:25 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _COM_SUN_STAR_I18N_SCRIPTTYPE_HDL_
 #include <com/sun/star/i18n/ScriptType.hdl>
