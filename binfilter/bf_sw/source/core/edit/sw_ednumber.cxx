d050a102a761fd1c173ad07cdbe100685e339a17
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:10:48 +0000

    INTEGRATION: CWS mingwport06 (1.7.80); FILE MERGED 2007/08/24 12:46:26 vg 1.7.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/edit/sw_ednumber.cxx
+++ binfilter/bf_sw/source/core/edit/sw_ednumber.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_ednumber.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:48:45 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:10:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 
 #ifndef _HORIORNT_HXX
--- binfilter/bf_sw/source/core/edit/sw_edtab.cxx
+++ binfilter/bf_sw/source/core/edit/sw_edtab.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_edtab.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:49:18 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:10:48 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #ifndef _HINTIDS_HXX
