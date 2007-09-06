7cbc1c40bf7ef64195ab6f75cfc712fb59c94046
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:21:16 +0000

    INTEGRATION: CWS mingwport06 (1.7.114); FILE MERGED 2007/08/24 12:46:29 vg 1.7.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/layout/sw_laycache.cxx
+++ binfilter/bf_sw/source/core/layout/sw_laycache.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_laycache.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:54:09 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:20:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include <hintids.hxx>
 #ifndef _SVX_BRKITEM_HXX //autogen
