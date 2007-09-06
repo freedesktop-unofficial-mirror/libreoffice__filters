f121da545c9bed70222522afe3db768e71d68ed2
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:19:29 +0000

    INTEGRATION: CWS mingwport06 (1.6.34); FILE MERGED 2007/08/24 12:46:11 vg 1.6.34.1: #i75499# pragma is for MSVC

--- binfilter/bf_sch/source/ui/app/sch_schresid.cxx
+++ binfilter/bf_sch/source/ui/app/sch_schresid.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_schresid.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:11:32 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:19:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "schmod.hxx"
 #include "schresid.hxx"
