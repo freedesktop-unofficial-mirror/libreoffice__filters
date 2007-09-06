6d1356d76691777bda40741ee253c5cf83bab109
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:27:56 +0000

    INTEGRATION: CWS mingwport06 (1.11.16); FILE MERGED 2007/08/24 12:46:14 vg 1.11.16.1: #i75499# pragma is for MSVC

--- binfilter/bf_sfx2/source/control/sfx2_shell.cxx
+++ binfilter/bf_sfx2/source/control/sfx2_shell.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_shell.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: obo $ $Date: 2007-07-17 10:46:48 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:27:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,9 @@
 
 #include <svtools/itempool.hxx>
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "app.hxx"
 
