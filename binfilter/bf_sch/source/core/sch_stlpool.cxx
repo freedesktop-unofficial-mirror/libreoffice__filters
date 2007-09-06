96a176a7663b2ab246b251e9b2bd85fef8977cfe
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:18:23 +0000

    INTEGRATION: CWS mingwport06 (1.4.114); FILE MERGED 2007/08/24 12:46:10 vg 1.4.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sch/source/core/sch_stlpool.cxx
+++ binfilter/bf_sch/source/core/sch_stlpool.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_stlpool.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 17:28:10 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:18:23 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 
 #include "stlpool.hxx"
