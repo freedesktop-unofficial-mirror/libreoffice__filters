c7376262ee7123e766f3b319bec7abdb273a6890
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:44:00 +0000

    INTEGRATION: CWS mingwport06 (1.8.114); FILE MERGED 2007/08/24 12:46:18 vg 1.8.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_svx/source/items/svx_svxfont.cxx
+++ binfilter/bf_svx/source/items/svx_svxfont.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_svxfont.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 21:18:20 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:44:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,7 +44,9 @@
 #ifndef _UNO_LINGU_HXX
 #include <unolingu.hxx>
 #endif
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define ITEMID_ESCAPEMENT	0
 
