767945d1bed82d3b7b7cb766026237da9d6c8c93
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:20:32 +0000

    INTEGRATION: CWS mingwport06 (1.4.114); FILE MERGED 2007/08/24 12:46:11 vg 1.4.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sd/source/core/sd_sdattr.cxx
+++ binfilter/bf_sd/source/core/sd_sdattr.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sd_sdattr.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 18:02:40 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:20:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "sdattr.hxx"
 namespace binfilter {
