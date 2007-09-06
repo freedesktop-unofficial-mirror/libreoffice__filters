ee5d5c8da9d36967158b55443848841476891bce
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:17:45 +0000

    INTEGRATION: CWS mingwport06 (1.6.114); FILE MERGED 2007/08/24 12:46:10 vg 1.6.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sch/source/core/sch_objadj.cxx
+++ binfilter/bf_sch/source/core/sch_objadj.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_objadj.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 17:27:24 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:17:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "objadj.hxx"
 #include "glob.hxx"
