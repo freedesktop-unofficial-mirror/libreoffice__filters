dba2ef08fdb3026dc726f94ba5c90920b54b5010
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:00:56 +0000

    INTEGRATION: CWS mingwport06 (1.8.114); FILE MERGED 2007/08/24 12:46:05 vg 1.8.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sc/source/filter/xml/sc_xmlstyli.cxx
+++ binfilter/bf_sc/source/filter/xml/sc_xmlstyli.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_xmlstyli.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 15:32:16 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:00:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _XMLSTYLI_HXX
 #include "xmlstyli.hxx"
