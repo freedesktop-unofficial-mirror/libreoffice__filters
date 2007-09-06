daca782a8556f21eae645807cfbccce96af6ce47
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:26:19 +0000

    INTEGRATION: CWS mingwport06 (1.4.114); FILE MERGED 2007/08/24 12:46:13 vg 1.4.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sfx2/source/bastyp/sfx2_bitset.cxx
+++ binfilter/bf_sfx2/source/bastyp/sfx2_bitset.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_bitset.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:02:34 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:25:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,9 @@
 #ifndef _TOOLS_DEBUG_HXX //autogen
 #include <tools/debug.hxx>
 #endif
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "bitset.hxx"
 
--- binfilter/bf_sfx2/source/bastyp/sfx2_misc.cxx
+++ binfilter/bf_sfx2/source/bastyp/sfx2_misc.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_misc.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:05:15 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:26:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "sfxtypes.hxx"
 namespace binfilter {
