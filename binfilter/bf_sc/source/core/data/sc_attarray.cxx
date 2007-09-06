02f6de506248d58294758e03b347463e078c6ae7
Kurt Zenker <kz@openoffice.org>
2007-09-06 09:30:14 +0000

    INTEGRATION: CWS mingwport06 (1.9.80); FILE MERGED 2007/08/24 12:45:59 vg 1.9.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_sc/source/core/data/sc_attarray.cxx
+++ binfilter/bf_sc/source/core/data/sc_attarray.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_attarray.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 16:52:17 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 10:30:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,9 @@
 #ifdef PCH
 #endif
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 //------------------------------------------------------------------------
 
