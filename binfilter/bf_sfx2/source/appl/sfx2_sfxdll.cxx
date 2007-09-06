c4ff75a5f66fbbbf3b263e37a95c9e75e2b73272
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:25:40 +0000

    INTEGRATION: CWS mingwport06 (1.5.156); FILE MERGED 2007/08/24 12:46:13 vg 1.5.156.1: #i75499# pragma is for MSVC

--- binfilter/bf_sfx2/source/appl/sfx2_sfxdll.cxx
+++ binfilter/bf_sfx2/source/appl/sfx2_sfxdll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_sfxdll.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-07-25 08:14:13 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:25:40 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,9 @@
 #endif
 #endif
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifdef WIN
 namespace binfilter {
