1b979c445eeed6caee1c6386df7a1b49dcb6cc2b
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:30:22 +0000

    INTEGRATION: CWS mingwport06 (1.3.240); FILE MERGED 2007/08/24 12:46:15 vg 1.3.240.1: #i75499# pragma is for MSVC

--- binfilter/bf_sfx2/source/doc/sfx2_objitem.cxx
+++ binfilter/bf_sfx2/source/doc/sfx2_objitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_objitem.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 03:14:53 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:30:22 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "objsh.hxx"
 //#include "objshimp.hxx"
