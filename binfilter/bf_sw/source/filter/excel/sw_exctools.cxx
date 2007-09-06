f1bfdc4953822fce419c22ed76fb3524c74994b8
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:57:41 +0000

    INTEGRATION: CWS mingwport06 (1.9.84); FILE MERGED 2007/08/24 12:46:40 vg 1.9.84.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/filter/excel/sw_exctools.cxx
+++ binfilter/bf_sw/source/filter/excel/sw_exctools.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_exctools.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2006-12-12 15:59:20 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:57:41 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #define ITEMID_BOXINFO      SID_ATTR_BORDER_INNER
 #include "hintids.hxx"
