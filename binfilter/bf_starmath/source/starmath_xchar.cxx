5351cfe43004176be07e5cdc8fcfa72b3a4b1dc7
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:35:16 +0000

    INTEGRATION: CWS mingwport06 (1.5.114); FILE MERGED 2007/08/24 12:46:16 vg 1.5.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_starmath/source/starmath_xchar.cxx
+++ binfilter/bf_starmath/source/starmath_xchar.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: starmath_xchar.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:58:58 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:34:52 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _STREAM_HXX
 #include <tools/stream.hxx>
