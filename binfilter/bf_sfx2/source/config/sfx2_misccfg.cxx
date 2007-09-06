c071e35023622dde881b3afd8f514bf3f6f4047e
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:27:20 +0000

    INTEGRATION: CWS mingwport06 (1.5.114); FILE MERGED 2007/08/24 12:46:14 vg 1.5.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sfx2/source/config/sfx2_misccfg.cxx
+++ binfilter/bf_sfx2/source/config/sfx2_misccfg.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sfx2_misccfg.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:08:16 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:27:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _ZFORLIST_HXX //autogen
 #include <svtools/zforlist.hxx>
