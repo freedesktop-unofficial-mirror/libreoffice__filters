8e9914b25e95b0e91794039d7aefc2883c3960ad
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:14:51 +0000

    INTEGRATION: CWS mingwport06 (1.8.114); FILE MERGED 2007/08/24 12:46:27 vg 1.8.114.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/fields/sw_expfld.cxx
+++ binfilter/bf_sw/source/core/fields/sw_expfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_expfld.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: vg $ $Date: 2007-08-30 11:52:17 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:13:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _HINTIDS_HXX
 #include <hintids.hxx>
--- binfilter/bf_sw/source/core/fields/sw_usrfld.cxx
+++ binfilter/bf_sw/source/core/fields/sw_usrfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_usrfld.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:39:13 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:14:51 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #ifndef _ZFORMAT_HXX //autogen
 #include <svtools/zformat.hxx>
