7254bd2376ae637fd83832c30c04d599ec7b29f5
Kurt Zenker <kz@openoffice.org>
2007-09-06 10:58:31 +0000

    INTEGRATION: CWS mingwport06 (1.9.80); FILE MERGED 2007/08/24 12:46:23 vg 1.9.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/doc/sw_docfld.cxx
+++ binfilter/bf_sw/source/core/doc/sw_docfld.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_docfld.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:44:45 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 11:58:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,9 @@
  ************************************************************************/
 
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 
 #include <string.h>
