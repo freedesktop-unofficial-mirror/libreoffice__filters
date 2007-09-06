0ed5933ec64c75b7fb4a28e4044909622954b36d
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:07:57 +0000

    INTEGRATION: CWS mingwport06 (1.7.80); FILE MERGED 2007/08/24 12:46:25 vg 1.7.80.1: #i75499# pragma is for MSVC

--- binfilter/bf_sw/source/core/draw/sw_dcontact.cxx
+++ binfilter/bf_sw/source/core/draw/sw_dcontact.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_dcontact.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:47:20 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:07:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,9 @@
  *
  ************************************************************************/
 
+#ifdef _MSC_VER
 #pragma hdrstop
+#endif
 
 #include "hintids.hxx"
 
