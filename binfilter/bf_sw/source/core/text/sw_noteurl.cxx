43e2f1de6cd0182ada23f66bf3e724e90e38251c
Kurt Zenker <kz@openoffice.org>
2007-09-06 11:34:37 +0000

    INTEGRATION: CWS mingwport06 (1.4.114); FILE MERGED 2007/08/27 12:27:34 vg 1.4.114.1: #i75499# pragma for MSVC

--- binfilter/bf_sw/source/core/text/sw_noteurl.cxx
+++ binfilter/bf_sw/source/core/text/sw_noteurl.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_noteurl.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 23:11:00 $
+ *  last change: $Author: kz $ $Date: 2007-09-06 12:34:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,9 @@
 
 
 
+/*N*/ #ifdef _MSC_VER
 /*N*/  #pragma hdrstop
+/*N*/ #endif
 /*N*/  
 /*N*/  #include "swtypes.hxx"
 /*N*/  
