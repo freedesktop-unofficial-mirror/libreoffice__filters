c108cacf38b74ac43478e0b2d8492e97755fd028
Jens-Heiner Rechtien <hr@openoffice.org>
2007-09-27 11:55:39 +0000

    INTEGRATION: CWS swwarnings (1.4.6); FILE MERGED 2007/04/13 11:16:01 os 1.4.6.1: warning removed

--- writerperfect/source/filter/ListStyle.hxx
+++ writerperfect/source/filter/ListStyle.hxx
@@ -1,8 +1,8 @@
-/* ListStyle: Stores (and writes) list-based information that is 
+/* ListStyle: Stores (and writes) list-based information that is
  * needed at the head of an OO document.
  *
  * Copyright (C) 2002-2003 William Lachance (william.lachance@sympatico.ca)
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -21,12 +21,18 @@
  *
  */
 
-/* "This product is not manufactured, approved, or supported by 
+/* "This product is not manufactured, approved, or supported by
  * Corel Corporation or Corel Corporation Limited."
  */
 #ifndef _LISTSTYLE_H
 #define _LISTSTYLE_H
+#if defined _MSC_VER
+#pragma warning( push, 1 )
+#endif
 #include <libwpd/libwpd.h>
+#if defined _MSC_VER
+#pragma warning( pop )
+#endif
 
 #define WP6_NUM_LIST_LEVELS 8 // see WP6FileStructure.h (we shouldn't need to reference this)
 
