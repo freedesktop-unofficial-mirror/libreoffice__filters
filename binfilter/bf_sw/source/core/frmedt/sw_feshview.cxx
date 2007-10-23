a241dccd078e443fd970c15b2f9ad02c5198c15f
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:04:21 +0000

    INTEGRATION: CWS so3deadcorpses (1.7.84); FILE MERGED 2007/09/11 08:30:53 fs 1.7.84.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_sw/source/core/frmedt/sw_feshview.cxx
+++ binfilter/bf_sw/source/core/frmedt/sw_feshview.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_feshview.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 12:15:39 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:04:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,7 +56,7 @@
 #include <bf_svx/protitem.hxx>
 #endif
 #ifndef _IPOBJ_HXX //autogen
-#include <so3/ipobj.hxx>
+#include <bf_so3/ipobj.hxx>
 #endif
 
 #ifndef _POOLFMT_HRC
