0aa304195e1437f7840ea115c1b7f66ec4beafe3
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 12:29:29 +0000

    INTEGRATION: CWS so3deadcorpses (1.10.84); FILE MERGED 2007/09/11 08:30:45 fs 1.10.84.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_sc/source/core/data/sc_table2.cxx
+++ binfilter/bf_sc/source/core/data/sc_table2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_table2.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 10:40:06 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 13:29:29 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -66,7 +66,7 @@
 #include "conditio.hxx"
 #include "chartlis.hxx"
 #include "globstr.hrc"
-#include "so3/staticbaseurl.hxx"
+#include "bf_so3/staticbaseurl.hxx"
 namespace binfilter {
 
 // STATIC DATA -----------------------------------------------------------
