43e23000861b02410408ba40851e29ac1da0f4d4
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:06:05 +0000

    INTEGRATION: CWS so3deadcorpses (1.13.84); FILE MERGED 2007/09/11 08:30:54 fs 1.13.84.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_sw/source/core/sw3io/sw_sw3nodes.cxx
+++ binfilter/bf_sw/source/core/sw3io/sw_sw3nodes.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_sw3nodes.cxx,v $
  *
- *  $Revision: 1.14 $
+ *  $Revision: 1.15 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 12:26:01 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:06:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -171,7 +171,7 @@
 #ifndef _SWSTYLENAMEMAPPER_HXX
 #include <SwStyleNameMapper.hxx>
 #endif
-#include "so3/staticbaseurl.hxx"
+#include "bf_so3/staticbaseurl.hxx"
 namespace binfilter {
 
 #define URL_DECODE 	\
