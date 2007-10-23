f9a9ec4353749a21149461f1dd47f8b541515c02
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:18:32 +0000

    INTEGRATION: CWS so3deadcorpses (1.3.246); FILE MERGED 2007/09/11 08:33:42 fs 1.3.246.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/inc/bf_sch/SchXMLWrapper.hxx
+++ binfilter/inc/bf_sch/SchXMLWrapper.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: SchXMLWrapper.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 11:39:46 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:18:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 #define _SCH_XMLWRP_HXX_
 
 #ifndef _SVSTOR_HXX
-#include <so3/svstor.hxx>
+#include <bf_so3/svstor.hxx>
 #endif
 namespace com { namespace sun { namespace star {
     namespace xml {
