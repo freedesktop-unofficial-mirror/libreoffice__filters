eb963c9db4a80c459bd5440b68603f08eae4b50b
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:10:18 +0000

    INTEGRATION: CWS so3deadcorpses (1.8.168); FILE MERGED 2007/09/11 08:30:56 fs 1.8.168.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_sw/source/filter/xml/sw_xmltble.cxx
+++ binfilter/bf_sw/source/filter/xml/sw_xmltble.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_xmltble.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 13:05:24 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:10:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -133,7 +133,7 @@
 #endif
 
 #ifndef _LINKMGR_HXX
-#include <so3/linkmgr.hxx>	// for cTokenSeperator
+#include <bf_so3/linkmgr.hxx>	// for cTokenSeperator
 #endif
 
 #ifndef _UNOOBJ_HXX
