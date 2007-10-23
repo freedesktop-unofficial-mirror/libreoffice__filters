53fd4213802d15cd8789387bd245866f6bc32bcd
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 12:57:11 +0000

    INTEGRATION: CWS so3deadcorpses (1.5.118); FILE MERGED 2007/09/11 08:30:50 fs 1.5.118.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_starmath/source/mathtype.hxx
+++ binfilter/bf_starmath/source/mathtype.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mathtype.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 19:54:43 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 13:57:11 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #endif
 
 #ifndef _SVSTOR_HXX
-#include <so3/svstor.hxx>
+#include <bf_so3/svstor.hxx>
 #endif
 
 #include <set>
