fff8cce93e3ee5ac40fff046f2c3b18b92a0df0d
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 12:36:53 +0000

    INTEGRATION: CWS so3deadcorpses (1.15.22); FILE MERGED 2007/09/11 08:30:49 fs 1.15.22.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_sd/util/makefile.mk
+++ binfilter/bf_sd/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.15 $
+#   $Revision: 1.16 $
 #
-#   last change: $Author: obo $ $Date: 2007-07-17 10:27:12 $
+#   last change: $Author: vg $ $Date: 2007-10-23 13:36:53 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -78,7 +78,7 @@ SHL1STDLIBS+= \
     $(LEGACYSMGRLIB)	\
     $(BFBASICLIB) \
     $(BFGOODIESLIB) \
-    $(SO2LIB) \
+    $(BFSO3LIB) \
     $(SVTOOLLIB) \
     $(TKLIB) \
     $(VCLLIB) \
