a1e91f55bcc2b0c3e345f4a0bd943296cbe893e1
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 12:34:44 +0000

    INTEGRATION: CWS so3deadcorpses (1.10.118); FILE MERGED 2007/09/11 08:30:48 fs 1.10.118.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_sd/source/core/sd_sdpage2.cxx
+++ binfilter/bf_sd/source/core/sd_sdpage2.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sd_sdpage2.cxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 18:03:14 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 13:34:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -80,7 +80,7 @@
 #ifndef _TOOLS_TENCCVT_HXX
 #include <tools/tenccvt.hxx>
 #endif
-#include "so3/staticbaseurl.hxx"
+#include "bf_so3/staticbaseurl.hxx"
 namespace binfilter {
 
 using namespace ::com::sun::star;
