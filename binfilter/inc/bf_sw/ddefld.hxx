5206732ec3ef6de1178650d9f65fb12dfb4dab6f
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:31:03 +0000

    INTEGRATION: CWS so3deadcorpses (1.4.246); FILE MERGED 2007/09/11 08:33:44 fs 1.4.246.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/inc/bf_sw/ddefld.hxx
+++ binfilter/inc/bf_sw/ddefld.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: ddefld.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 15:53:22 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:31:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 #define _DDEFLD_HXX
 
 #ifndef _LNKBASE_HXX //autogen
-#include <so3/lnkbase.hxx>
+#include <bf_so3/lnkbase.hxx>
 #endif
 #include "fldbas.hxx"
 namespace binfilter {
