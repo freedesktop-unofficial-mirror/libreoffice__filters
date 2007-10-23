7529f1e759b585c6eefbf480170d3575f2d0e42e
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 12:29:12 +0000

    INTEGRATION: CWS so3deadcorpses (1.13.22); FILE MERGED 2007/09/13 08:08:58 fs 1.13.22.2: #i81309# so3 lib not needed 2007/09/11 08:30:45 fs 1.13.22.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/bf_offmgr/util/makefile.mk
+++ binfilter/bf_offmgr/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.13 $
+#   $Revision: 1.14 $
 #
-#   last change: $Author: obo $ $Date: 2007-07-17 09:05:46 $
+#   last change: $Author: vg $ $Date: 2007-10-23 13:29:12 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -88,8 +88,7 @@ SHL2STDLIBS+= \
     $(CPPUHELPERLIB) \
     $(CPPULIB) \
     $(VOSLIB) \
-    $(SALLIB) \
-    $(SO2LIB)
+    $(SALLIB)
 
 .IF "$(GUI)"=="WNT"
 SHL2STDLIBS += $(ADVAPI32LIB)
