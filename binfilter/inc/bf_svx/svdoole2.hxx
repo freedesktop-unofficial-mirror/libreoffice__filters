1c3435c351676be1d63c9ea6b45b8b7e4ea76c72
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:30:20 +0000

    INTEGRATION: CWS so3deadcorpses (1.6.118); FILE MERGED 2007/09/11 08:33:44 fs 1.6.118.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/inc/bf_svx/svdoole2.hxx
+++ binfilter/inc/bf_svx/svdoole2.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdoole2.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:08:57 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:30:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,7 +50,7 @@
 #include <vcl/gdimtf.hxx>
 #endif
 
-#include <so3/svstor.hxx>
+#include <bf_so3/svstor.hxx>
 
 class SvPersist;
 class SvInPlaceObjectRef;
