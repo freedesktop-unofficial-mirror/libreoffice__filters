5a578344761220ea99b20f0707dff2bdddbfa239
Vladimir Glazounov <vg@openoffice.org>
2007-10-23 13:21:35 +0000

    INTEGRATION: CWS so3deadcorpses (1.6.30); FILE MERGED 2007/09/11 08:33:43 fs 1.6.30.1: #i81309# so3 is dead, long live bf_so3

--- binfilter/inc/bf_sfx2/docfile.hxx
+++ binfilter/inc/bf_sfx2/docfile.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docfile.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: ihi $ $Date: 2007-06-05 14:27:55 $
+ *  last change: $Author: vg $ $Date: 2007-10-23 14:21:35 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,7 +57,7 @@
 #include <com/sun/star/task/XInteractionHandler.hpp>
 #endif
 #ifndef _SVSTOR_HXX //autogen
-#include <so3/svstor.hxx>
+#include <bf_so3/svstor.hxx>
 #endif
 #ifndef _STREAM_HXX //autogen
 #include <tools/stream.hxx>
