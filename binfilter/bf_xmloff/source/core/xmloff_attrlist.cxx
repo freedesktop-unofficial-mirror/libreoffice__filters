25d65506fa97bb82a84c0980830d118ec2d0d8fb
Jens-Heiner Rechtien <hr@openoffice.org>
2008-01-04 12:04:31 +0000

    INTEGRATION: CWS gcc430 (1.6.110); FILE MERGED 2007/12/13 10:50:18 cmc 1.6.110.1: #i84505# fix up binfilter for gcc 4.3.0

--- binfilter/bf_xmloff/source/core/xmloff_attrlist.cxx
+++ binfilter/bf_xmloff/source/core/xmloff_attrlist.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_attrlist.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:13:15 $
+ *  last change: $Author: hr $ $Date: 2008-01-04 13:04:31 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,9 @@
 #include <rtl/memory.h>
 
 #if OSL_DEBUG_LEVEL == 0
-#define NDEBUG
+#  ifndef NDEBUG
+#    define NDEBUG
+#  endif
 #endif
 #include <assert.h>
 
