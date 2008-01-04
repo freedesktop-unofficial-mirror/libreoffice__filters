dd6513665e920fc0d98c1da2dd1ae40df3e7d412
Jens-Heiner Rechtien <hr@openoffice.org>
2008-01-04 12:04:44 +0000

    INTEGRATION: CWS gcc430 (1.4.144); FILE MERGED 2007/12/13 10:50:18 cmc 1.4.144.1: #i84505# fix up binfilter for gcc 4.3.0

--- binfilter/bf_xmloff/source/forms/formcellbinding.hxx
+++ binfilter/bf_xmloff/source/forms/formcellbinding.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: formcellbinding.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 01:47:40 $
+ *  last change: $Author: hr $ $Date: 2008-01-04 13:04:44 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -299,4 +299,4 @@ namespace xmloff
 //............................................................................
 
 }//end of namespace binfilter
-#endif XMLOFF_FORMS_FORMCELLBINDING
+#endif
