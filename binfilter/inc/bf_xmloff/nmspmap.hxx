626b1b32d778a2f12cdaf8860dce674cc61b0677
Ivo Hinkelmann <ihi@openoffice.org>
2008-02-04 12:34:59 +0000

    INTEGRATION: CWS gcc430two (1.6.124); FILE MERGED 2008/01/28 09:53:02 rene 1.6.124.1: more gcc 4.3.0 things

--- binfilter/inc/bf_xmloff/nmspmap.hxx
+++ binfilter/inc/bf_xmloff/nmspmap.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: nmspmap.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:52:42 $
+ *  last change: $Author: ihi $ $Date: 2008-02-04 13:34:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,6 +35,8 @@
 
 #ifndef _XMLOFF_NMSPMAP_HXX
 #define _XMLOFF_NMSPMAP_HXX
+
+#include <limits.h>
 
 #ifndef __SGI_STL_HASH_MAP
 #include <hash_map>
