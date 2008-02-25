953fe6edcc2c62cbd5da891c6093d17ee53dd269
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:21:28 +0000

    INTEGRATION: CWS supdremove02 (1.2.42); FILE MERGED 2008/01/31 13:45:58 rt 1.2.42.1: #i85482# Remove UPD from resource name.

--- binfilter/bf_so3/source/misc/factory.cxx
+++ binfilter/bf_so3/source/misc/factory.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: factory.cxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: vg $ $Date: 2007-10-23 13:48:54 $
+ *  last change: $Author: obo $ $Date: 2008-02-25 15:21:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -158,7 +158,6 @@ ResMgr * SoDll::GetResMgr()
     if ( !pResMgr )
     {
         ByteString aMgrName = ByteString( "bf_ofa" );
-        aMgrName += ByteString::CreateFromInt32( SOLARUPD );
         pResMgr = ResMgr::CreateResMgr( aMgrName.GetBuffer() );
     }
     return pResMgr;
