c5a61f9f35e3c66bad183037e47e79980028c121
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:25:43 +0000

    INTEGRATION: CWS supdremove02 (1.7.90); FILE MERGED 2008/01/28 08:29:28 rt 1.7.90.1: #i85482# Remove UPD from library name

--- binfilter/legacysmgr/source/legacy/makefile.mk
+++ binfilter/legacysmgr/source/legacy/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: vg $ $Date: 2007-03-26 13:05:12 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:25:43 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@ INC+= -I$(PRJ)$/inc$/legacysmgr
 SLOFILES = \
     $(SLO)$/legacy_binfilters_smgr.obj
 
-SHL1TARGET = $(TARGET)$(UPD)$(DLLPOSTFIX)
+SHL1TARGET = $(TARGET)$(DLLPOSTFIX)
 
 SHL1STDLIBS = \
     $(CPPUHELPERLIB)	\
