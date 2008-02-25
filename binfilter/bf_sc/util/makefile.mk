7d15de16be212f00d2c4bee4efd8d74c28ea5e77
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:20:02 +0000

    INTEGRATION: CWS supdremove02 (1.20.42); FILE MERGED 2008/01/28 08:29:56 rt 1.20.42.1: #i85482# Remove UPD from library name

--- binfilter/bf_sc/util/makefile.mk
+++ binfilter/bf_sc/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.20 $
+#   $Revision: 1.21 $
 #
-#   last change: $Author: vg $ $Date: 2007-10-23 13:33:12 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:20:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -65,7 +65,7 @@ RESLIB1SRSFILES=\
 
 # --- StarClac DLL
 
-SHL1TARGET= bf_sc$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= bf_sc$(DLLPOSTFIX)
 SHL1VERSIONMAP= bf_sc.map
 SHL1IMPLIB= bf_sci
 
