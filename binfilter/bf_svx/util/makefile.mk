04ca79a2bc844a66b88b00ebd1f2b9d1155c959c
Oliver Bolte <obo@openoffice.org>
2008-02-25 14:23:54 +0000

    INTEGRATION: CWS supdremove02 (1.17.42); FILE MERGED 2008/01/28 08:29:28 rt 1.17.42.1: #i85482# Remove UPD from library name

--- binfilter/bf_svx/util/makefile.mk
+++ binfilter/bf_svx/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.17 $
+#   $Revision: 1.18 $
 #
-#   last change: $Author: vg $ $Date: 2007-10-23 14:02:25 $
+#   last change: $Author: obo $ $Date: 2008-02-25 15:23:54 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,7 +55,7 @@ INC+= -I$(PRJ)$/inc$/bf_svx
 
 # --- Svx - DLL ----------
 
-SHL1TARGET= bf_svx$(UPD)$(DLLPOSTFIX)
+SHL1TARGET= bf_svx$(DLLPOSTFIX)
 SHL1IMPLIB= bf_svx
 SHL1BASE  = 0x1d800000
 
