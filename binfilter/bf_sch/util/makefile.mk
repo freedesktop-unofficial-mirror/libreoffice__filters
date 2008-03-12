888af9df3e4da4c4a468083cbba3a2a84f04b8a2
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:27:20 +0000

    INTEGRATION: CWS impresstables2 (1.13.18); FILE MERGED 2008/03/11 18:13:12 cl 1.13.18.2: RESYNC: (1.13-1.14); FILE MERGED 2008/02/13 09:19:02 cl 1.13.18.1: merged svtools in and stripped

--- binfilter/bf_sch/util/makefile.mk
+++ binfilter/bf_sch/util/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.14 $
+#   $Revision: 1.15 $
 #
-#   last change: $Author: obo $ $Date: 2008-02-25 15:20:30 $
+#   last change: $Author: rt $ $Date: 2008-03-12 07:27:20 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -83,9 +83,8 @@ SHL1STDLIBS=\
     $(LEGACYSMGRLIB)	\
     $(BFSO3LIB)	\
     $(BFGOODIESLIB)	\
-    $(SVTOOLLIB)	\
+    $(BFSVTOOLLIB)	\
     $(VCLLIB)	\
-    $(SVLLIB)	\
     $(SOTLIB)	\
     $(UNOTOOLSLIB) \
     $(TOOLSLIB)	\
