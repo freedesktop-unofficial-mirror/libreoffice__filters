1581123437e2602ffa1d30b97a003f7832c54b2a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 13:02:40 +0000

    INTEGRATION: CWS impresstables2 (1.2.382); FILE MERGED 2008/02/22 23:37:11 cl 1.2.382.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/legacysmgr/legacy_binfilters_smgr.hxx
+++ binfilter/inc/legacysmgr/legacy_binfilters_smgr.hxx
@@ -2,13 +2,8 @@
 #define _LEGACYBINFILTERSMGR_HXX
 
 #include "com/sun/star/lang/XMultiServiceFactory.hpp"
-#ifndef _COM_SUN_STAR_UNO_REFERENCE_HXX_
 #include <com/sun/star/uno/Reference.hxx>
-#endif
 #include <com/sun/star/registry/XRegistryKey.hpp>
-//#ifndef _UNO_ENVIRONMENT_H_
-//#include <uno/environment.h>
-//#endif
 #include "cppuhelper/factory.hxx"
 namespace legacy_binfilters
 {
