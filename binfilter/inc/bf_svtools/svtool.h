c8990f085e0ce233a0186c48b602a51880afe488
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:33:26 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/13 11:05:43 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/svtool.h
@@ -0,0 +1,45 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svtool.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:33:26 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVTOOL_H
+#define _SVTOOL_H
+
+#if defined( W30 ) && defined( _MSC_VER )
+#define SVEXPORT    _export
+#else
+#define SVEXPORT
+#endif
+
+#endif // _SVTOOL_H
