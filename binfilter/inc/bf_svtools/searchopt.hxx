4ddfc9e3c2f323e58f8c795f329b386babc8f08c
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:26:02 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:58 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 11:00:47 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/searchopt.hxx
@@ -0,0 +1,130 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: searchopt.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:26:02 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVT_SEARCHOPT_HXX_
+#define _SVT_SEARCHOPT_HXX_
+
+#ifndef _SOLAR_H
+#include "tools/solar.h"
+#endif
+
+namespace binfilter
+{
+
+class SvtSearchOptions_Impl;
+
+class  SvtSearchOptions
+{
+    SvtSearchOptions_Impl	*pImpl;
+
+    // disallow copy-constructor and assignment-operator for now
+    SvtSearchOptions( const SvtSearchOptions & );
+    SvtSearchOptions & operator = ( const SvtSearchOptions & );
+
+protected:
+
+public:
+    SvtSearchOptions();
+    ~SvtSearchOptions();
+
+    INT32	GetTransliterationFlags() const;
+
+    //
+    // General Options
+    //
+
+    BOOL	IsWholeWordsOnly() const;
+    BOOL	IsBackwards() const;
+    BOOL	IsUseRegularExpression() const;
+    BOOL	IsSearchForStyles() const;
+    BOOL	IsSimilaritySearch() const;
+    BOOL	IsUseAsianOptions() const;
+    BOOL	IsMatchCase() const;			// also Japanese search option
+
+    void	SetWholeWordsOnly( BOOL bVal );
+    void	SetBackwards( BOOL bVal );
+    void	SetUseRegularExpression( BOOL bVal );
+    void	SetSearchForStyles( BOOL bVal );
+    void	SetSimilaritySearch( BOOL bVal );
+    void	SetUseAsianOptions( BOOL bVal );
+    void	SetMatchCase( BOOL bVal );		// also Japanese search option
+
+    //
+    // Japanese search options
+    //
+
+    BOOL	IsMatchFullHalfWidthForms() const;
+    BOOL	IsMatchHiraganaKatakana() const;
+    BOOL	IsMatchContractions() const;
+    BOOL	IsMatchMinusDashChoon() const;
+    BOOL	IsMatchRepeatCharMarks() const;
+    BOOL	IsMatchVariantFormKanji() const;
+    BOOL	IsMatchOldKanaForms() const;
+    BOOL	IsMatchDiziDuzu() const;
+    BOOL	IsMatchBavaHafa() const;
+    BOOL	IsMatchTsithichiDhizi() const;
+    BOOL	IsMatchHyuiyuByuvyu() const;
+    BOOL	IsMatchSesheZeje() const;
+    BOOL	IsMatchIaiya() const;
+    BOOL	IsMatchKiku() const;
+    BOOL	IsIgnorePunctuation() const;
+    BOOL	IsIgnoreWhitespace() const;
+    BOOL	IsIgnoreProlongedSoundMark() const;
+    BOOL	IsIgnoreMiddleDot() const;
+    
+    void	SetMatchFullHalfWidthForms( BOOL bVal );
+    void	SetMatchHiraganaKatakana( BOOL bVal );
+    void	SetMatchContractions( BOOL bVal );
+    void	SetMatchMinusDashChoon( BOOL bVal );
+    void	SetMatchRepeatCharMarks( BOOL bVal );
+    void	SetMatchVariantFormKanji( BOOL bVal );
+    void	SetMatchOldKanaForms( BOOL bVal );
+    void	SetMatchDiziDuzu( BOOL bVal );
+    void	SetMatchBavaHafa( BOOL bVal );
+    void	SetMatchTsithichiDhizi( BOOL bVal );
+    void	SetMatchHyuiyuByuvyu( BOOL bVal );
+    void	SetMatchSesheZeje( BOOL bVal );
+    void	SetMatchIaiya( BOOL bVal );
+    void	SetMatchKiku( BOOL bVal );
+    void	SetIgnorePunctuation( BOOL bVal );
+    void	SetIgnoreWhitespace( BOOL bVal );
+    void	SetIgnoreProlongedSoundMark( BOOL bVal );
+    void	SetIgnoreMiddleDot( BOOL bVal );
+};
+
+}
+
+#endif
+
