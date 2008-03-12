4d96d6ad78aa839a0eb7e2d23aa023373b627e50
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:46:32 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:19 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 09:52:23 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/items/svt_globalnameitem.cxx
@@ -0,0 +1,133 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svt_globalnameitem.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:46:31 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+
+
+#ifndef _COM_SUN_STAR_UNO_ANY_HXX_
+#include <com/sun/star/uno/Any.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_HXX_
+#include <com/sun/star/uno/Sequence.hxx>
+#endif
+#ifndef _COM_SUN_STAR_SCRIPT_XTYPECONVERTER_HPP_
+#include <com/sun/star/script/XTypeConverter.hpp>
+#endif
+
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+
+#include <comphelper/processfactory.hxx>
+
+#include <bf_svtools/globalnameitem.hxx>
+
+// STATIC DATA -----------------------------------------------------------
+
+namespace binfilter
+{
+
+
+// -----------------------------------------------------------------------
+
+TYPEINIT1_AUTOFACTORY(SfxGlobalNameItem, SfxPoolItem);
+
+// -----------------------------------------------------------------------
+
+SfxGlobalNameItem::SfxGlobalNameItem()
+{
+}
+
+// -----------------------------------------------------------------------
+
+SfxGlobalNameItem::SfxGlobalNameItem( USHORT nW, const SvGlobalName& rName )
+:	SfxPoolItem( nW ),
+    m_aName( rName )
+{
+}
+
+// -----------------------------------------------------------------------
+
+SfxGlobalNameItem::~SfxGlobalNameItem()
+{
+}
+
+// -----------------------------------------------------------------------
+
+int SfxGlobalNameItem::operator==( const SfxPoolItem& rItem ) const
+{
+    return ((SfxGlobalNameItem&)rItem).m_aName == m_aName;
+}
+
+// -----------------------------------------------------------------------
+
+SfxPoolItem* SfxGlobalNameItem::Clone(SfxItemPool *) const
+{
+    return new SfxGlobalNameItem( *this );
+}
+
+//----------------------------------------------------------------------------
+// virtual
+BOOL SfxGlobalNameItem::PutValue( const com::sun::star::uno::Any& rVal, BYTE )
+{
+    com::sun::star::uno::Reference < com::sun::star::script::XTypeConverter > xConverter
+            ( ::comphelper::getProcessServiceFactory()->createInstance(::rtl::OUString::createFromAscii("com.sun.star.script.Converter")),
+            com::sun::star::uno::UNO_QUERY );
+    com::sun::star::uno::Sequence< sal_Int8 > aSeq;
+    com::sun::star::uno::Any aNew;
+
+    try { aNew = xConverter->convertTo( rVal, ::getCppuType((const com::sun::star::uno::Sequence < sal_Int8 >*)0) ); }
+    catch (com::sun::star::uno::Exception&) {}
+    aNew >>= aSeq;
+    if ( aSeq.getLength() == 16 )
+    {
+        m_aName.MakeFromMemory( (void*) aSeq.getConstArray() );
+        return TRUE;
+    }
+
+    DBG_ERROR( "SfxGlobalNameItem::PutValue - Wrong type!" );
+    return FALSE;
+}
+
+//----------------------------------------------------------------------------
+// virtual
+BOOL SfxGlobalNameItem::QueryValue( ::com::sun::star::uno::Any& rVal, BYTE ) const
+{
+    ::com::sun::star::uno::Sequence< sal_Int8 > aSeq( 16 );
+    void* pData = ( void* ) &m_aName.GetCLSID();
+    memcpy( aSeq.getArray(), pData, 16 );
+    rVal <<= aSeq;
+    return TRUE;
+}
+
+}
