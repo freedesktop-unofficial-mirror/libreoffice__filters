bab088f3177442aa0fe810ff86182fe66bfbb2f7
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:30:17 +0000

    INTEGRATION: CWS impresstables2 (1.6.70); FILE MERGED 2008/02/13 10:30:27 cl 1.6.70.1: merged svtools in and stripped

--- binfilter/bf_svx/source/inc/unoedprx.hxx
+++ binfilter/bf_svx/source/inc/unoedprx.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unoedprx.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:26:51 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:30:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,7 +39,7 @@
 #include <memory>
 
 #ifndef _SFXBRDCST_HXX
-#include <svtools/brdcst.hxx>
+#include <bf_svtools/brdcst.hxx>
 #endif
 
 #ifndef _SVX_UNOEDSRC_HXX
