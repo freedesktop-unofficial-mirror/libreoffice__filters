8e758d4b083358d074584e2814c212bf6cad055a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:30:57 +0000

    INTEGRATION: CWS impresstables2 (1.5.104); FILE MERGED 2008/02/13 11:17:20 cl 1.5.104.1: merged svtools in and stripped

--- binfilter/inc/bf_svx/xmlcnitm.hxx
+++ binfilter/inc/bf_svx/xmlcnitm.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlcnitm.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:31:49 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:30:57 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _SVX_XMLCNITM_HXX
 
 #ifndef _SFXPOOLITEM_HXX
-#include <svtools/poolitem.hxx>
+#include <bf_svtools/poolitem.hxx>
 #endif
 namespace rtl { class OUString; }
 namespace binfilter {
