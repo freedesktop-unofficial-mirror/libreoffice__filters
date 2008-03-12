c3f90a0b39de623a2c10c20d19cd772b3700bcf9
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:54:55 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:21 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 09:58:52 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/items1/svt_tresitem.cxx
@@ -0,0 +1,90 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svt_tresitem.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:54:55 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+
+
+#ifndef SVTOOLS_TRESITEM_HXX
+#include <bf_svtools/tresitem.hxx>
+#endif
+
+using namespace com::sun::star;
+
+namespace binfilter
+{
+
+//============================================================================
+//
+//  CntTransferResultItem
+//
+//============================================================================
+
+TYPEINIT1_AUTOFACTORY(CntTransferResultItem, SfxPoolItem)
+
+//============================================================================
+// virtual
+int CntTransferResultItem::operator ==(SfxPoolItem const & rItem) const
+{
+    if (CntTransferResultItem * pResultItem = PTR_CAST(CntTransferResultItem,
+                                                       &rItem))
+        return m_aResult.Source == pResultItem->m_aResult.Source
+               && m_aResult.Target == pResultItem->m_aResult.Target
+               && m_aResult.Result == pResultItem->m_aResult.Result;
+    return false;
+}
+
+//============================================================================
+// virtual
+BOOL CntTransferResultItem::QueryValue(uno::Any & rVal, BYTE) const
+{
+    rVal <<= m_aResult;
+    return true;
+}
+
+//============================================================================
+// virtual
+BOOL CntTransferResultItem::PutValue(uno::Any const & rVal, BYTE)
+{
+    return rVal >>= m_aResult;
+}
+
+//============================================================================
+// virtual
+SfxPoolItem * CntTransferResultItem::Clone(SfxItemPool *) const
+{
+    return new CntTransferResultItem(*this);
+}
+
+}
