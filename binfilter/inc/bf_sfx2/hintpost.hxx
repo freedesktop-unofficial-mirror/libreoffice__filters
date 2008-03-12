4baba746048c453eb0ad89c74073d87712c333cf
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:36:56 +0000

    INTEGRATION: CWS impresstables2 (1.4.104); FILE MERGED 2008/02/22 23:36:54 cl 1.4.104.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_sfx2/hintpost.hxx
+++ binfilter/inc/bf_sfx2/hintpost.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: hintpost.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 03:00:53 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:36:56 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,8 +42,9 @@
 #ifndef _REF_HXX //autogen
 #include <tools/ref.hxx>
 #endif
-class SfxHint;
 namespace binfilter {
+
+class SfxHint;
 
 //===================================================================
 
