68ed73c59d3e8f65d200e6f796f4c35c8741c940
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:08:15 +0000

    INTEGRATION: CWS impresstables2 (1.2.86); FILE MERGED 2008/02/13 11:17:03 cl 1.2.86.1: merged svtools in and stripped

--- binfilter/inc/bf_goodies/bucket.hxx
+++ binfilter/inc/bf_goodies/bucket.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: bucket.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: ihi $ $Date: 2006-11-14 12:18:34 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:08:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define _B3D_BUCKET_HXX
 
 #ifndef _SVARRAY_HXX
-#include <svtools/svarray.hxx>
+#include <bf_svtools/svarray.hxx>
 #endif
 
 namespace binfilter {
