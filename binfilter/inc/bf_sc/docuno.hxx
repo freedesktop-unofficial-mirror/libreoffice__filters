b53e68eedcd933ba0d599bca189afe3da2f1bc52
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:15:33 +0000

    INTEGRATION: CWS impresstables2 (1.4.70); FILE MERGED 2008/02/22 23:36:52 cl 1.4.70.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 11:17:04 cl 1.4.70.1: merged svtools in and stripped

--- binfilter/inc/bf_sc/docuno.hxx
+++ binfilter/inc/bf_sc/docuno.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docuno.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:26:26 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:15:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@
 #endif
 
 #ifndef _SFXLSTNER_HXX //autogen
-#include <svtools/lstner.hxx>
+#include <bf_svtools/lstner.hxx>
 #endif
 
 #ifndef _SVX_FMDMOD_HXX //autogen
@@ -127,10 +127,10 @@
 #endif
 
 #ifndef _SFX_ITEMPROP_HXX
-#include <svtools/itemprop.hxx>
+#include <bf_svtools/itemprop.hxx>
 #endif
-class SvEmbeddedObject;
 namespace binfilter {
+class SvEmbeddedObject;
 
 class ScDocShell;
 class ScAnnotationObj;
