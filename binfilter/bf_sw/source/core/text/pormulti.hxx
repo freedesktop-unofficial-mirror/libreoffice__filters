19194236877e710bbdcf359c7c353e4ba37caece
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:17:53 +0000

    INTEGRATION: CWS impresstables2 (1.6.70); FILE MERGED 2008/02/22 23:35:00 cl 1.6.70.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/bf_sw/source/core/text/pormulti.hxx
+++ binfilter/bf_sw/source/core/text/pormulti.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pormulti.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:59:05 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:17:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,9 +38,9 @@
 
 #include "porlay.hxx"
 #include "porexp.hxx"
-class SfxPoolItem; 
 namespace binfilter {
 
+class SfxPoolItem; 
 class SwTxtFormatInfo;
 class SwFldPortion;
 class SwTxtCursor;
