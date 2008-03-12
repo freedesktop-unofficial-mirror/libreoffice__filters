a60727b2cd73861e88df0ba03b5208c5be4db4ef
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:15:24 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:57 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:53:09 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/jpeg.hxx
@@ -0,0 +1,145 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: jpeg.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:15:24 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _JPEG_HXX
+#define _JPEG_HXX
+
+#ifndef _GRAPH_HXX
+#include <vcl/graph.hxx>
+#endif
+#ifndef _FLTCALL_HXX
+#include <bf_svtools/fltcall.hxx>
+#endif
+#ifndef _COM_SUN_STAR_UNO_SEQUENCE_H_
+#include <com/sun/star/uno/Sequence.h>
+#endif
+#ifndef _COM_SUN_STAR_BEANS_PROPERTYVALUE_HPP_
+#include <com/sun/star/beans/PropertyValue.hpp>
+#endif
+#ifndef _COM_SUN_STAR_TASK_XSTATUSINDICATOR_HPP_
+#include <com/sun/star/task/XStatusIndicator.hpp>
+#endif
+
+namespace binfilter
+{
+
+#ifdef _JPEGPRIVATE
+
+// --------
+// - Enum -
+// --------
+
+enum ReadState
+{
+    JPEGREAD_OK,
+    JPEGREAD_ERROR,
+    JPEGREAD_NEED_MORE
+};
+
+// --------------
+// - JPEGReader	-
+// --------------
+
+class JPEGReader : public GraphicReader
+{
+    SvStream&			rIStm;
+    Bitmap				aBmp;
+    Bitmap				aBmp1;
+    BitmapWriteAccess*	pAcc;
+    BitmapWriteAccess*	pAcc1;
+    void*				pBuffer;
+    long				nLastPos;
+    long				nFormerPos;
+    long				nLastLines;
+    sal_Bool            bSetLogSize;
+
+    Graphic				CreateIntermediateGraphic( const Bitmap& rBitmap, long nLines );
+    void				FillBitmap();
+
+public:
+
+    void*				CreateBitmap( void* JPEGCreateBitmapParam );
+
+public:
+
+
+
+                        JPEGReader( SvStream& rStm, void* pCallData, sal_Bool bSetLogSize );
+    virtual				~JPEGReader();
+
+
+    ReadState			Read( Graphic& rGraphic );
+};
+
+// --------------
+// - JPEGWriter	-
+// --------------
+
+class JPEGWriter
+{
+    SvStream&			rOStm;
+    Bitmap				aBmp;
+    BitmapReadAccess*	pAcc;
+    BYTE*				pBuffer;
+    BOOL				bNative;
+
+    sal_Bool			bGreys;
+    sal_Int32			nQuality;
+
+    com::sun::star::uno::Reference< com::sun::star::task::XStatusIndicator > xStatusIndicator;
+
+public:
+
+    void*				GetScanline( long nY );
+
+                        JPEGWriter( SvStream& rOStm, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >* pFilterData );
+                        ~JPEGWriter() {};
+
+    BOOL				Write( const Graphic& rGraphic );
+};
+
+#endif // _JPEGPRIVATE
+
+// ---------------------
+// - Import/ExportJPEG -
+// ---------------------
+
+BOOL ImportJPEG( SvStream& rStream, Graphic& rGraphic, void* pCallerData, sal_Int32 nImportFlags );
+
+BOOL ExportJPEG( SvStream& rStream, const Graphic& rGraphic, const ::com::sun::star::uno::Sequence< ::com::sun::star::beans::PropertyValue >* pFilterData );
+
+}
+
+#endif // _JPEG_HXX
