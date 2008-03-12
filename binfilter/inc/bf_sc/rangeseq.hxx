39f6c189d180a5504cfd2a127d1085caf8646527
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:19:58 +0000

    INTEGRATION: CWS impresstables2 (1.4.104); FILE MERGED 2008/02/22 23:36:52 cl 1.4.104.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_sc/rangeseq.hxx
+++ binfilter/inc/bf_sc/rangeseq.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rangeseq.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 02:40:19 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:19:58 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -43,9 +43,9 @@
 #ifndef _COM_SUN_STAR_UNO_ANY_H_
 #include <com/sun/star/uno/Any.h>
 #endif
-class SvNumberFormatter;
 namespace binfilter {
 
+class SvNumberFormatter;
 class ScDocument;
 class ScRange;
 class ScMatrix;
