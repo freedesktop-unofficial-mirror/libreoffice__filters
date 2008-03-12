0977f4d1f666c847173f3e6d38804c5b3061f538
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:15:02 +0000

    INTEGRATION: CWS impresstables2 (1.4.104); FILE MERGED 2008/02/13 11:17:04 cl 1.4.104.1: merged svtools in and stripped

--- binfilter/inc/bf_sc/docpool.hxx
+++ binfilter/inc/bf_sc/docpool.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: docpool.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 02:33:41 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:15:02 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #define SC_SCDOCPOL_HXX
 
 #ifndef _SFXITEMPOOL_HXX //autogen
-#include <svtools/itempool.hxx>
+#include <bf_svtools/itempool.hxx>
 #endif
 namespace binfilter {
 
