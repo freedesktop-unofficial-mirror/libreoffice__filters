4dee767000ff735d6368c6b8d8670b68d2ef0e28
Rüdiger Timm <rt@openoffice.org>
2008-03-12 05:57:50 +0000

    INTEGRATION: CWS impresstables2 (1.11.10); FILE MERGED 2008/02/13 09:17:37 cl 1.11.10.1: merged svtools in and stripped

--- binfilter/bf_sc/source/core/tool/sc_interpr4.cxx
+++ binfilter/bf_sc/source/core/tool/sc_interpr4.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_interpr4.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 10:47:43 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 06:57:50 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -72,7 +72,7 @@
 #include "bf_basic/sbmod.hxx"
 #include "bf_basic/sbstar.hxx"
 #include "bf_basic/sbx.hxx"
-#include <svtools/zforlist.hxx>
+#include <bf_svtools/zforlist.hxx>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
