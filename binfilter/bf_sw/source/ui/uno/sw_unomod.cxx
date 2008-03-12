5942ad6ec13fab9021a8c5ad02e90b8b96f5c8b7
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:54:21 +0000

    INTEGRATION: CWS impresstables2 (1.5.90); FILE MERGED 2008/02/13 10:36:11 cl 1.5.90.4: merged svtools in and stripped 2007/10/11 21:12:20 cl 1.5.90.3: RESYNC: (1.6-1.7); FILE MERGED 2007/08/01 12:07:38 cl 1.5.90.2: RESYNC: (1.5-1.6); FILE MERGED 2007/07/27 15:17:04 cl 1.5.90.1: fixed namespace issues

--- binfilter/bf_sw/source/ui/uno/sw_unomod.cxx
+++ binfilter/bf_sw/source/ui/uno/sw_unomod.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_unomod.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 13:12:53 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:54:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@
 #include <unomap.hxx>
 #endif
 #ifndef _SFX_ITEMPROP_HXX //autogen
-#include <svtools/itemprop.hxx>
+#include <bf_svtools/itemprop.hxx>
 #endif
 #ifndef _PRTOPT_HXX //autogen
 #include <prtopt.hxx>
