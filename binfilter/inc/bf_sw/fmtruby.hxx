b46401261aae5dc317960f0c5e22ff504fe5ea72
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:41:21 +0000

    INTEGRATION: CWS impresstables2 (1.7.104); FILE MERGED 2008/02/13 11:17:24 cl 1.7.104.1: merged svtools in and stripped

--- binfilter/inc/bf_sw/fmtruby.hxx
+++ binfilter/inc/bf_sw/fmtruby.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: fmtruby.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:45:27 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:41:21 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 
 
 #ifndef _SFXPOOLITEM_HXX
-#include <svtools/poolitem.hxx>
+#include <bf_svtools/poolitem.hxx>
 #endif
 namespace binfilter {
 
