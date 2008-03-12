7f131635e28a1ea7b51dc3ef75f6be1d99ab5cc4
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:50:28 +0000

    INTEGRATION: CWS impresstables2 (1.7.70); FILE MERGED 2008/02/13 11:17:25 cl 1.7.70.1: merged svtools in and stripped

--- binfilter/inc/bf_sw/tox.hxx
+++ binfilter/inc/bf_sw/tox.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: tox.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:48:00 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:50:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 
 
 #ifndef _SFXPOOLITEM_HXX //autogen
-#include <svtools/poolitem.hxx>
+#include <bf_svtools/poolitem.hxx>
 #endif
 
 #ifndef _SWTYPES_HXX
