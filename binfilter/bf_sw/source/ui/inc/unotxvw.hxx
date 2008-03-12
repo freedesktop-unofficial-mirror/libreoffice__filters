109413b1fd6ac9ed1dff6e6b4fafb5967ca5ea85
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:52:42 +0000

    INTEGRATION: CWS impresstables2 (1.3.232); FILE MERGED 2008/02/13 10:36:11 cl 1.3.232.1: merged svtools in and stripped

--- binfilter/bf_sw/source/ui/inc/unotxvw.hxx
+++ binfilter/bf_sw/source/ui/inc/unotxvw.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unotxvw.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 06:01:01 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:52:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -81,7 +81,7 @@
 #include <cppuhelper/implbase8.hxx>	// helper for implementations
 #endif
 #ifndef _SFX_ITEMPROP_HXX
-#include <svtools/itemprop.hxx>
+#include <bf_svtools/itemprop.hxx>
 #endif
 #include "calbck.hxx"
 #ifndef _SW_TEXTCURSORHELPER_HXX
