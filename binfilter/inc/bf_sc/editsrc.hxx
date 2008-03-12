b0d4fd7ae5d6b661251ca9f492bb5bbda9a0e293
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:16:19 +0000

    INTEGRATION: CWS impresstables2 (1.4.70); FILE MERGED 2008/02/13 11:17:04 cl 1.4.70.1: merged svtools in and stripped

--- binfilter/inc/bf_sc/editsrc.hxx
+++ binfilter/inc/bf_sc/editsrc.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: editsrc.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:27:00 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:16:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -45,7 +45,7 @@
 #endif
 
 #ifndef _SFXLSTNER_HXX //autogen
-#include <svtools/lstner.hxx>
+#include <bf_svtools/lstner.hxx>
 #endif
 
 #include <memory>
