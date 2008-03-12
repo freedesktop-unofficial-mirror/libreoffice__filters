1280dac6a1dc5968beb9329a31cb0be41b66ee8a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:24:16 +0000

    INTEGRATION: CWS impresstables2 (1.3.234); FILE MERGED 2008/02/13 09:18:40 cl 1.3.234.1: merged svtools in and stripped

--- binfilter/bf_sch/source/ui/app/sch_globopt.cxx
+++ binfilter/bf_sch/source/ui/app/sch_globopt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_globopt.cxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 22:11:26 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:24:16 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -37,7 +37,7 @@
 #include <stdio.h>
 
 #ifndef INCLUDED_SVTOOLS_SYSLOCALE_HXX
-#include <svtools/syslocale.hxx>
+#include <bf_svtools/syslocale.hxx>
 #endif
 #ifndef _UTL_CONFIGITEM_HXX_
 #include <unotools/configitem.hxx>
