905a955dfbf1759b3bd44c46a0f541f0fd4649fb
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:10:52 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:25 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:15:12 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/svdde/ddemldeb.hxx
@@ -0,0 +1,78 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ddemldeb.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:10:52 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+namespace binfilter
+{
+
+#if defined(OV_DEBUG)
+
+void ImpWriteLogFile(char*,char*);
+void ImpAddHSZ( HSZ, String& );
+void ImpWriteDdeStatus(char*, char* );
+void ImpWriteDdeData(char*, DDESTRUCT*);
+
+#ifdef LOGFILE
+#define WRITELOG(aString) ImpWriteLogFile("\\ddeml.log",aString);
+#else
+#define WRITELOG(bla)
+#endif
+#ifdef STATUSFILE
+#define WRITESTATUS(aContext) ImpWriteDdeStatus("\\ddeml.sts",aContext);
+#else
+#define WRITESTATUS(bla)
+#endif
+#ifdef DDEDATAFILE
+#define WRITEDATA(data) ImpWriteDdeData("\\ddeml.dat",data);
+#else
+#define WRITEDATA(bla)
+#endif
+
+#else
+
+#define WRITELOG(bla)
+#define WRITESTATUS(bla)
+#define WRITEDATA(bla)
+
+#endif
+
+APIRET MyDosAllocSharedMem(void** ppBaseAddress, char* pszName, unsigned long ulObjectSize,
+    unsigned long ulFlags, char* pContextStr );
+
+APIRET MyDosAllocMem(void** ppBaseAddress, unsigned long ulObjectSize,
+    unsigned long ulFlags, char* pContextStr );
+
+APIRET MyDosFreeMem( void* pBaseAddress, char* pContextStr );
+
+}
