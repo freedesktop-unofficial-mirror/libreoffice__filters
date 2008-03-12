bb94d95d56f7e3eb7f51eb4cc749fd17471262fb
Rüdiger Timm <rt@openoffice.org>
2008-03-12 13:02:27 +0000

    INTEGRATION: CWS impresstables2 (1.5.70); FILE MERGED 2008/02/22 23:37:11 cl 1.5.70.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_xmloff/xmlnumfi.hxx
+++ binfilter/inc/bf_xmloff/xmlnumfi.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmlnumfi.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:54:56 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 14:02:27 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -60,9 +60,9 @@
 #endif
 class Color;
 class Color;
+namespace binfilter {
 class SvNumberFormatter;
 class SvtSysLocale;
-namespace binfilter {
 
 #define XML_NUMBERSTYLES "NumberStyles"
 
