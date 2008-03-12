ef359235477e0138bfbb73d5b15ea81066a48e44
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:27:19 +0000

    INTEGRATION: CWS impresstables2 (1.4.232); FILE MERGED 2008/02/13 10:35:54 cl 1.4.232.3: merged svtools in and stripped 2007/10/11 20:58:52 cl 1.4.232.2: RESYNC: (1.4-1.5); FILE MERGED 2007/07/27 15:17:01 cl 1.4.232.1: fixed namespace issues

--- binfilter/bf_sw/source/core/unocore/sw_unoparagraph.cxx
+++ binfilter/bf_sw/source/core/unocore/sw_unoparagraph.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_unoparagraph.cxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 12:51:41 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:27:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -81,7 +81,7 @@
 #endif
 #define _SVSTDARR_USHORTS
 #define _SVSTDARR_USHORTSSORT
-#include <svtools/svstdarr.hxx>
+#include <bf_svtools/svstdarr.hxx>
 
 #ifndef _COM_SUN_STAR_BEANS_PropertyAttribute_HPP_
 #include <com/sun/star/beans/PropertyAttribute.hpp>
