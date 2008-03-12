4822c8209ebb6015137a59a06dbf3fdc51d7ae15
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:18:24 +0000

    INTEGRATION: CWS impresstables2 (1.7.104); FILE MERGED 2008/02/13 11:17:18 cl 1.7.104.1: merged svtools in and stripped

--- binfilter/inc/bf_svx/udlnitem.hxx
+++ binfilter/inc/bf_svx/udlnitem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: udlnitem.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 04:20:45 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:18:24 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 // include ---------------------------------------------------------------
 
 #ifndef _SFXENUMITEM_HXX //autogen
-#include <svtools/eitem.hxx>
+#include <bf_svtools/eitem.hxx>
 #endif
 #ifndef _SVX_SVXIDS_HRC
 #include <bf_svx/svxids.hrc>
