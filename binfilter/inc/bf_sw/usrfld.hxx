e1aae9a196f0787dceb41e28d40c4efd4f0480d2
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:58:53 +0000

    INTEGRATION: CWS impresstables2 (1.6.104); FILE MERGED 2008/02/22 23:37:09 cl 1.6.104.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_sw/usrfld.hxx
+++ binfilter/inc/bf_sw/usrfld.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: usrfld.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 05:01:59 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:58:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,10 +36,9 @@
 #define _USRFLD_HXX
 
 #include "fldbas.hxx"
-class SfxPoolItem;
 namespace binfilter {
 
-
+class SfxPoolItem;
 class SwCalc;
 class SwDoc;
 
