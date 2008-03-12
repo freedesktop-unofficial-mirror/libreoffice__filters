464cb008d78926f5140e7648c0dbc3d110205663
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:35:28 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:59 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 11:07:11 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/tooltiplbox.hxx
@@ -0,0 +1,81 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: tooltiplbox.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:35:28 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SVTOOLS_TOOLTIPLBOX_HXX
+#define SVTOOLS_TOOLTIPLBOX_HXX
+
+#ifndef INCLUDED_SVTDLLAPI_H
+#include "bf_svtools/svtdllapi.h"
+#endif
+
+#ifndef _SV_LSTBOX_HXX
+#include <vcl/lstbox.hxx>
+#endif
+
+namespace binfilter
+{
+
+// ============================================================================
+
+/** ListBox with tool tips for long entries. */
+class ToolTipListBox : public ListBox
+{
+public:
+                                ToolTipListBox( Window* pParent, WinBits nStyle = WB_BORDER );
+                                ToolTipListBox( Window* pParent, const ResId& rResId );
+
+protected:
+    virtual void                RequestHelp( const HelpEvent& rHEvt );
+};
+
+// ----------------------------------------------------------------------------
+
+/** MultiListBox with tool tips for long entries. */
+class  ToolTipMultiListBox : public MultiListBox
+{
+public:
+                                ToolTipMultiListBox( Window* pParent, WinBits nStyle = WB_BORDER );
+                                ToolTipMultiListBox( Window* pParent, const ResId& rResId );
+
+protected:
+    virtual void                RequestHelp( const HelpEvent& rHEvt );
+};
+
+// ============================================================================
+
+}
+
+#endif
+
