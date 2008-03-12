0f6a38aa31838af08014eea51f5a9721d9a806a6
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:42:20 +0000

    INTEGRATION: CWS impresstables2 (1.5.230); FILE MERGED 2008/02/13 10:36:06 cl 1.5.230.1: merged svtools in and stripped

--- binfilter/bf_sw/source/filter/xml/xmltbli.hxx
+++ binfilter/bf_sw/source/filter/xml/xmltbli.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmltbli.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 03:47:43 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:42:20 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,7 +46,7 @@
 #define _SVSTDARR_USHORTS
 #define _SVSTDARR_BOOLS
 #define _SVSTDARR_STRINGSDTOR
-#include <svtools/svstdarr.hxx>
+#include <bf_svtools/svstdarr.hxx>
 #endif
 namespace binfilter {
 
