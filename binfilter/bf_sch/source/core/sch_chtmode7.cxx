3ff91c2381207b7e3814a194a68efb656b05d9ec
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:22:30 +0000

    INTEGRATION: CWS impresstables2 (1.7.10); FILE MERGED 2008/02/13 09:18:40 cl 1.7.10.1: merged svtools in and stripped

--- binfilter/bf_sch/source/core/sch_chtmode7.cxx
+++ binfilter/bf_sch/source/core/sch_chtmode7.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_chtmode7.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 11:15:48 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:22:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,7 +52,7 @@
 #define ITEMID_CHARTDATADESCR   SCHATTR_DATADESCR_DESCR
 
 #ifndef _SFXENUMITEM_HXX
-#include <svtools/eitem.hxx>
+#include <bf_svtools/eitem.hxx>
 #endif
 
 #endif
