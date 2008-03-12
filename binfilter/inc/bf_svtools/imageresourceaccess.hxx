172bde68d891bb277fbf92bff1e7761caac749ee
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:08:11 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:57 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:48:51 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/imageresourceaccess.hxx
@@ -0,0 +1,103 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: imageresourceaccess.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:08:11 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SVTOOLS_INC_IMAGERESOURCEACCESS_HXX
+#define SVTOOLS_INC_IMAGERESOURCEACCESS_HXX
+
+#ifndef INCLUDED_SVTDLLAPI_H
+#include "bf_svtools/svtdllapi.h"
+#endif
+
+/** === begin UNO includes === **/
+#ifndef _COM_SUN_STAR_IO_XINPUTSTREAM_HPP_
+#include <com/sun/star/io/XInputStream.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+/** === end UNO includes === **/
+
+class SvStream;
+//........................................................................
+namespace binfilter
+{
+//........................................................................
+
+    //====================================================================
+    //= ImageResourceAccess
+    //====================================================================
+    /** helper class for obtaining streams (which also can be used with the ImageProducer)
+        from a resource
+    */
+    class ImageResourceAccess
+    {
+    private:
+        ImageResourceAccess();    // never implemented
+
+    public:
+        /** determines whether the given URL denotes an image within a resource
+        */
+         static  bool        isImageResourceURL( const ::rtl::OUString& _rURL );
+
+        /** for a given URL of an image within a resource, this method retrieves
+            an SvStream for this image.
+
+            This method works for arbitrary URLs denoting an image, since the
+            <type scope="com::sun::star::graphics">GraphicsProvider</type> service is used
+            to resolve the URL. However, obtaining the stream is expensive (since
+            the image must be copied), so you are strongly encouraged to only use it
+            when you know that the image is small enough.
+        */
+         static  SvStream*   getImageStream(
+                    const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB,
+                    const ::rtl::OUString& _rImageResourceURL
+                );
+
+        /** for a given URL of an image within a resource, this method retrieves
+            an <type scope="com::sun::star::io">XInputStream</type> for this image.
+        */
+         static  ::com::sun::star::uno::Reference< ::com::sun::star::io::XInputStream >
+                getImageXStream(
+                    const ::com::sun::star::uno::Reference< ::com::sun::star::lang::XMultiServiceFactory >& _rxORB,
+                    const ::rtl::OUString& _rImageResourceURL
+                );
+    };
+
+//........................................................................
+}
+//........................................................................
+
+#endif // DBA14_SVTOOLS_INC_IMAGERESOURCEACCESS_HXX
+
