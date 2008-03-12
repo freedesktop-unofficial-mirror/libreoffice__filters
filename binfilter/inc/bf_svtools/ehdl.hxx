42625a91b2689d850c262367f21637c465b7ef05
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:56:52 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:56 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:43:59 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/ehdl.hxx
@@ -0,0 +1,102 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: ehdl.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:56:52 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _EHDL_HXX
+#define _EHDL_HXX
+
+#ifndef __RSC
+
+#ifndef INCLUDED_SVTDLLAPI_H
+#include "bf_svtools/svtdllapi.h"
+#endif
+
+#ifndef _EINF_HXX
+#include <tools/errinf.hxx>
+#endif
+
+class Window;
+class ResMgr;
+
+namespace binfilter
+{
+
+class  SfxErrorContext : private ErrorContext
+{
+public:
+    SfxErrorContext(
+            USHORT nCtxIdP, Window *pWin=0,
+            USHORT nResIdP=USHRT_MAX, ResMgr *pMgrP=0);
+    SfxErrorContext(
+            USHORT nCtxIdP, const String &aArg1, Window *pWin=0,
+            USHORT nResIdP=USHRT_MAX, ResMgr *pMgrP=0);
+    virtual BOOL GetString(ULONG nErrId, String &rStr);
+
+private:
+    USHORT nCtxId;
+    USHORT nResId;
+    ResMgr *pMgr;
+    String aArg1;
+};
+
+class  SfxErrorHandler : private ErrorHandler
+{
+public:
+    SfxErrorHandler(USHORT nId, ULONG lStart, ULONG lEnd, ResMgr *pMgr=0);
+    ~SfxErrorHandler();
+
+protected:
+    virtual BOOL     GetErrorString(ULONG lErrId, String &, USHORT&) const;
+    virtual BOOL     GetMessageString(ULONG lErrId, String &, USHORT&) const;
+
+private:
+
+    ULONG            lStart;
+    ULONG            lEnd;
+    USHORT           nId;
+    ResMgr          *pMgr;
+    ResMgr          *pFreeMgr;
+
+    BOOL             GetClassString(ULONG lErrId, String &) const;
+    virtual BOOL     CreateString(
+                         const ErrorInfo *, String &, USHORT &) const;
+};
+
+
+}
+
+#endif
+
+#endif
+
