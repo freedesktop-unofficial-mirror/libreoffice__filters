b5bd245ca87d15088ad5543c3f375f6f39efe763
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:47:36 +0000

    INTEGRATION: CWS impresstables2 (1.6.24); FILE MERGED 2008/02/22 23:37:04 cl 1.6.24.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_svx/dialmgr.hxx
+++ binfilter/inc/bf_svx/dialmgr.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: dialmgr.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2007-04-26 07:19:23 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:47:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,10 +38,9 @@
 // include ---------------------------------------------------------------
 
 class ResMgr;
-class GraphicFilter;
-class GraphicFilter;
 namespace binfilter {
 
+class GraphicFilter;
 // forward ---------------------------------------------------------------
 
 
