161533cc58affc8c80c2bf778158f62c4a07d296
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:55:29 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:21 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 09:59:15 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/items1/whassert.hxx
@@ -0,0 +1,70 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: whassert.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:55:29 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _SFX_WHASSERT_HXX
+#define _SFX_WHASSERT_HXX
+
+#ifndef _TOOLS_DEBUG_HXX //autogen
+#include <tools/debug.hxx>
+#endif
+#ifndef _STRING_HXX //autogen
+#include <tools/string.hxx>
+#endif
+
+namespace binfilter
+{
+
+//------------------------------------------------------------------------
+
+#ifdef DBG_UTIL
+#define SFX_ASSERT( bCondition, nId, sMessage ) 							\
+{																			\
+    if ( DbgIsAssert() )													\
+    {																		\
+        if ( !(bCondition) )												\
+        {																	\
+            ByteString aMsg( sMessage );									\
+            aMsg.Append(RTL_CONSTASCII_STRINGPARAM("\nwith Id/Pos: "));     \
+            aMsg += ByteString::CreateFromInt32( nId );						\
+            DbgOut( aMsg.GetBuffer(), DBG_OUT_ERROR, __FILE__, __LINE__);	\
+        }																	\
+    }																		\
+}
+#else
+#define SFX_ASSERT( bCondition, nId, sMessage )
+#endif
+
+}
+
+#endif
