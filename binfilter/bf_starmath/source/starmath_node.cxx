e25f201a6f461d9ef19d7be752ff0703f1f62633
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:25:15 +0000

    INTEGRATION: CWS impresstables2 (1.11.10); FILE MERGED 2008/02/22 23:27:45 cl 1.11.10.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 09:25:21 cl 1.11.10.1: merged svtools in and stripped

--- binfilter/bf_starmath/source/starmath_node.cxx
+++ binfilter/bf_starmath/source/starmath_node.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: starmath_node.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 11:33:07 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:25:15 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -120,7 +120,7 @@ namespace binfilter {
 /*N*/             if (OUTDEV_WINDOW == rOutDev.GetOutDevType())
 /*N*/                 aBgCol = ((Window &) rOutDev).GetDisplayBackground().GetColor();
 /*N*/
-/*N*/             nNewCol = SM_MOD1()->GetColorConfig().GetColorValue(svtools::FONTCOLOR).nColor;
+/*N*/             nNewCol = SM_MOD1()->GetColorConfig().GetColorValue(FONTCOLOR).nColor;
 /*N*/
 /*N*/             Color aTmpColor( nNewCol );
 /*N*/             if (aBgCol.IsDark() && aTmpColor.IsDark())
