617013af1a373317130c3493585573af7713a86b
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:35:34 +0000

    INTEGRATION: CWS impresstables2 (1.8.70); FILE MERGED 2008/02/13 10:30:28 cl 1.8.70.1: merged svtools in and stripped

--- binfilter/bf_svx/source/svdraw/svx_svdedxv.cxx
+++ binfilter/bf_svx/source/svdraw/svx_svdedxv.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_svdedxv.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:31:01 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:35:34 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -35,7 +35,7 @@
 
 
 #include "svdedxv.hxx"
-#include <svtools/solar.hrc>
+#include <bf_svtools/solar.hrc>
 
 #ifndef _STRING_H
 #include <tools/string.h>
