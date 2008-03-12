c0459ed0df990909d46482ed6064266214da0be7
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:51:18 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:55 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:39:58 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/broadcast.hxx
@@ -0,0 +1,79 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: broadcast.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:51:18 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _SVT_BROADCAST_HXX
+#define _SVT_BROADCAST_HXX
+
+#ifndef _RTTI_HXX //autogen
+#include <tools/rtti.hxx>
+#endif
+
+namespace binfilter {
+
+class SvtListener;
+class SfxHint;
+class SvtListenerBase;
+
+//-------------------------------------------------------------------------
+
+class  SvtBroadcaster
+{
+friend class SvtListener;
+friend class SvtListenerBase;
+friend class SvtListenerIter;
+    SvtListenerBase* pRoot;
+
+    const SvtBroadcaster&	operator=(const SvtBroadcaster &); // verboten
+
+protected:
+    void					Forward( SvtBroadcaster& rBC,
+                                     const SfxHint& rHint );
+    virtual void			ListenersGone();
+
+public:
+                            TYPEINFO();
+
+                            SvtBroadcaster();
+                            SvtBroadcaster( const SvtBroadcaster &rBC );
+    virtual 				~SvtBroadcaster();
+
+    void					Broadcast( const SfxHint &rHint );
+
+    BOOL					HasListeners() const { return 0 != pRoot; }
+};
+
+}
+
+#endif
+
