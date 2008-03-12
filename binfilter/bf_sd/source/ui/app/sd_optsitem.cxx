d2289821cfb2048520dca34bab906243ca5d9251
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:32:03 +0000

    INTEGRATION: CWS impresstables2 (1.8.70); FILE MERGED 2008/02/13 09:22:02 cl 1.8.70.1: merged svtools in and stripped

--- binfilter/bf_sd/source/ui/app/sd_optsitem.cxx
+++ binfilter/bf_sd/source/ui/app/sd_optsitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sd_optsitem.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:10:52 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:32:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,7 +40,7 @@
 #include <vcl/salbtype.hxx>
 #endif
 #ifndef INCLUDED_SVTOOLS_SYSLOCALE_HXX 
-#include <svtools/syslocale.hxx>
+#include <bf_svtools/syslocale.hxx>
 #endif
 
 #include "optsitem.hxx"
