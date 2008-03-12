f9442d9b634918ece415a6499ae2080fe6e78ce2
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:57:36 +0000

    INTEGRATION: CWS impresstables2 (1.3.232); FILE MERGED 2008/02/22 23:26:42 cl 1.3.232.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/bf_sd/source/ui/unoidl/unopback.hxx
+++ binfilter/bf_sd/source/ui/unoidl/unopback.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: unopback.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 01:31:46 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:57:36 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -54,9 +54,9 @@
 #include <unotools/servicehelper.hxx>
 
 #include <cppuhelper/implbase4.hxx>
-class SfxItemSet;
 namespace binfilter {
 
+class SfxItemSet;
 const SfxItemPropertyMap* ImplGetPageBackgroundPropertyMap();
 
 class SdrObject;
