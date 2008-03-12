26e95195a63eaf446500fb0fa97c8f85574b639a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:31:07 +0000

    INTEGRATION: CWS impresstables2 (1.7.10); FILE MERGED 2008/02/13 10:30:27 cl 1.7.10.1: merged svtools in and stripped

--- binfilter/bf_svx/source/items/svx_drawitem.cxx
+++ binfilter/bf_svx/source/items/svx_drawitem.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_drawitem.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 11:41:44 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:31:07 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -50,7 +50,7 @@
 
 
 #ifndef _SFXPOOLITEM_HXX
-#include <svtools/poolitem.hxx>
+#include <bf_svtools/poolitem.hxx>
 #endif
 
 #include "drawitem.hxx"
