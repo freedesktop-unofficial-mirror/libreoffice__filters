ce909670f1dc07f937a38d857dd007c9a34bf559
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:47:19 +0000

    INTEGRATION: CWS impresstables2 (1.6.70); FILE MERGED 2008/02/13 10:30:30 cl 1.6.70.1: merged svtools in and stripped

--- binfilter/bf_svx/source/unoedit/svx_unotext.cxx
+++ binfilter/bf_svx/source/unoedit/svx_unotext.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_unotext.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:37:12 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:47:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -61,7 +61,7 @@
 #endif
 
 #ifndef _SFXINTITEM_HXX //autogen
-#include <svtools/intitem.hxx>
+#include <bf_svtools/intitem.hxx>
 #endif
 
 #define ITEMID_FIELD EE_FEATURE_FIELD
