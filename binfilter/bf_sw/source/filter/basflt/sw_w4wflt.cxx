e1a6f014e47f1f2cd9b773f592602060b9464da1
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:31:19 +0000

    INTEGRATION: CWS impresstables2 (1.11.10); FILE MERGED 2008/02/13 10:36:04 cl 1.11.10.1: merged svtools in and stripped

--- binfilter/bf_sw/source/filter/basflt/sw_w4wflt.cxx
+++ binfilter/bf_sw/source/filter/basflt/sw_w4wflt.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_w4wflt.cxx,v $
  *
- *  $Revision: 1.11 $
+ *  $Revision: 1.12 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 12:56:58 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:31:19 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 #endif
 
 #ifndef INCLUDED_SVTOOLS_PATHOPTIONS_HXX
-#include <svtools/pathoptions.hxx>
+#include <bf_svtools/pathoptions.hxx>
 #endif
 #ifndef _UNOTOOLS_LOCALFILEHELPER_HXX
 #include <unotools/localfilehelper.hxx>
