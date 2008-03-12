02670c143f3f6406581d95ed2dbac2cec98f74df
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:07:32 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:25 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:12:22 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/numbers/svt_nbdll.cxx
@@ -0,0 +1,96 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svt_nbdll.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:07:32 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+
+
+#ifdef WIN
+
+#ifndef _SVWIN_H
+#include <svwin.h>
+#endif
+
+#ifndef _SYSDEP_HXX
+#include <sysdep.hxx>
+#endif
+
+namespace binfilter
+{
+
+
+// Statische DLL-Verwaltungs-Variablen
+static HINSTANCE hDLLInst = 0;      // HANDLE der DLL
+
+
+/***************************************************************************
+|*
+|*    LibMain()
+|*
+|*    Beschreibung       Initialisierungsfunktion der DLL
+|*    Ersterstellung     TH 05.05.93
+|*    Letzte Aenderung   TH 05.05.93
+|*
+***************************************************************************/
+
+extern "C" int CALLBACK LibMain( HINSTANCE hDLL, WORD, WORD nHeap, LPSTR )
+{
+#ifndef WNT
+    if ( nHeap )
+        UnlockData( 0 );
+#endif
+
+    hDLLInst = hDLL;
+
+    return TRUE;
+}
+
+/***************************************************************************
+|*
+|*    WEP()
+|*
+|*    Beschreibung      DLL-Deinitialisierung
+|*    Ersterstellung     TH 05.05.93
+|*    Letzte Aenderung   TH 05.05.93
+|*
+***************************************************************************/
+
+extern "C" int CALLBACK WEP( int )
+{
+    return 1;
+}
+
+}
+
+#endif
