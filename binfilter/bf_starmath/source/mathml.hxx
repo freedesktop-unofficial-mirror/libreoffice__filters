c1431a21b6f9aaf2a2df463ffec499217f2052b9
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:24:00 +0000

    INTEGRATION: CWS impresstables2 (1.6.190); FILE MERGED 2008/02/22 23:27:45 cl 1.6.190.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/bf_starmath/source/mathml.hxx
+++ binfilter/bf_starmath/source/mathml.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: mathml.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: rt $ $Date: 2006-03-07 10:12:46 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:24:00 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,9 +57,9 @@ namespace com { namespace sun { namespace star {
         namespace beans { 
         class XPropertySet; }
 } } }
-class SvStorage; 
 namespace binfilter {
 
+class SvStorage; 
 class SfxMedium;
 
 //STRIP008 namespace com { namespace sun { namespace star {
