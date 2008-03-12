88cde7cfed6787b213fe821b35b419ed88d9f468
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:02:14 +0000

    INTEGRATION: CWS impresstables2 (1.8.10); FILE MERGED 2008/02/13 09:17:43 cl 1.8.10.1: merged svtools in and stripped

--- binfilter/bf_sc/source/ui/app/sc_scdll.cxx
+++ binfilter/bf_sc/source/ui/app/sc_scdll.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_scdll.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 11:02:32 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:02:14 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -62,7 +62,7 @@
 
 
 #ifndef _SVTOOLS_IMGDEF_HXX
-#include <svtools/imgdef.hxx>
+#include <bf_svtools/imgdef.hxx>
 #endif
 
 #include <bf_svx/svdfield.hxx>		//	SdrRegisterFieldClasses
