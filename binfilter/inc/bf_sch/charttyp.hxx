09eceecd7de35e133fe191882713c2afc65f5dd4
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:24:03 +0000

    INTEGRATION: CWS impresstables2 (1.4.104); FILE MERGED 2008/02/13 11:17:05 cl 1.4.104.1: merged svtools in and stripped

--- binfilter/inc/bf_sch/charttyp.hxx
+++ binfilter/inc/bf_sch/charttyp.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: charttyp.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 02:46:08 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:24:03 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -33,7 +33,7 @@
  *
  ************************************************************************/
 #ifndef _SFXITEMSET_HXX //autogen
-#include <svtools/itemset.hxx>
+#include <bf_svtools/itemset.hxx>
 #endif
 
 #ifndef _SVX_CHRTITEM_HXX //autogen
