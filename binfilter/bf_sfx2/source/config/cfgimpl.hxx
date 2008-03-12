29d302f9f5777fdb3c270e392f990453d6cce883
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:03:30 +0000

    INTEGRATION: CWS impresstables2 (1.3.232); FILE MERGED 2008/02/13 09:23:24 cl 1.3.232.1: merged svtools in and stripped

--- binfilter/bf_sfx2/source/config/cfgimpl.hxx
+++ binfilter/bf_sfx2/source/config/cfgimpl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cfgimpl.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-08 02:39:20 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:03:30 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -36,7 +36,7 @@
 // include ---------------------------------------------------------------
 
 #include <tools/string.hxx>
-#include <svtools/svarray.hxx>
+#include <bf_svtools/svarray.hxx>
 
 #include "cfgitem.hxx"
 class SotStorage;
