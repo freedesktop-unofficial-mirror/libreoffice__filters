e7bf5b013ef4cb9e2eae68f80e91109cc72202bf
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:42:54 +0000

    INTEGRATION: CWS impresstables2 (1.2.232); FILE MERGED 2008/02/13 11:17:24 cl 1.2.232.1: merged svtools in and stripped

--- binfilter/inc/bf_sw/helpid.h
+++ binfilter/inc/bf_sw/helpid.h
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: helpid.h,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 16:09:58 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:42:54 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
  ************************************************************************/
 
 #ifndef _SOLAR_HRC
-#include <svtools/solar.hrc>
+#include <bf_svtools/solar.hrc>
 #endif
 
 #define HID_BASE            		HID_APP_START
