c133762ddec4d12af53aa41da8906823d5aa9c04
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:07:10 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:25 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:12:04 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/numbers/numhead.hxx
@@ -0,0 +1,121 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: numhead.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:07:10 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef NF_NUMHEAD_HXX
+#define NF_NUMHEAD_HXX
+
+#ifndef _STREAM_HXX //autogen
+#include <tools/stream.hxx>
+#endif
+
+namespace binfilter
+{
+
+// -----------------------------------------------------------------------
+
+        //	"Automatischer" Record-Header mit Groessenangabe
+
+/* 						wird fuer SvNumberFormatter nicht gebraucht
+class SvNumReadHeader
+{
+private:
+    SvStream&	rStream;
+    ULONG		nDataEnd;
+
+public:
+    SvNumReadHeader(SvStream& rNewStream);
+    ~SvNumReadHeader();
+
+    ULONG	BytesLeft() const;
+};
+
+class SvNumWriteHeader
+{
+private:
+    SvStream&	rStream;
+    ULONG		nDataPos;
+    ULONG		nDataSize;
+
+public:
+    SvNumWriteHeader(SvStream& rNewStream, ULONG nDefault = 0);
+    ~SvNumWriteHeader();
+};
+
+*/
+
+        //	Header mit Groessenangaben fuer mehrere Objekte
+
+class ImpSvNumMultipleReadHeader
+{
+private:
+    SvStream&		rStream;
+    char*			pBuf;
+    SvMemoryStream*	pMemStream;
+    ULONG			nEndPos;
+    ULONG			nEntryEnd;
+
+public:
+    ImpSvNumMultipleReadHeader(SvStream& rNewStream);
+    ~ImpSvNumMultipleReadHeader();
+
+    void	StartEntry();
+    void	EndEntry();
+    ULONG	BytesLeft() const;
+
+    static void Skip( SvStream& );		// komplett ueberspringen
+};
+
+class ImpSvNumMultipleWriteHeader
+{
+private:
+    SvStream&		rStream;
+    SvMemoryStream	aMemStream;
+    ULONG			nDataPos;
+    sal_uInt32		nDataSize;
+    ULONG			nEntryStart;
+
+public:
+    ImpSvNumMultipleWriteHeader(SvStream& rNewStream, ULONG nDefault = 0);
+    ~ImpSvNumMultipleWriteHeader();
+
+    void	StartEntry();
+    void	EndEntry();
+};
+
+}
+
+#endif
+
+
