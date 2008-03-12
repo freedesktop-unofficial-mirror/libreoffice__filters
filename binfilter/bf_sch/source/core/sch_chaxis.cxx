24a98775c28f0289238bac1b3e88ac44232ef3a3
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:19:13 +0000

    INTEGRATION: CWS impresstables2 (1.8.104); FILE MERGED 2008/02/13 09:18:39 cl 1.8.104.1: merged svtools in and stripped

--- binfilter/bf_sch/source/core/sch_chaxis.cxx
+++ binfilter/bf_sch/source/core/sch_chaxis.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_chaxis.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 17:22:48 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:19:13 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -53,7 +53,7 @@
 #ifndef _ZFORLIST_DECLARE_TABLE
 #define _ZFORLIST_DECLARE_TABLE
 #endif
-#include <svtools/zforlist.hxx>
+#include <bf_svtools/zforlist.hxx>
 #endif
 #ifndef INCLUDED_RTL_MATH_HXX
 #include <rtl/math.hxx>
