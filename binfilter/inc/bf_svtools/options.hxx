46e48c3371d96ee1878392edfd5b05ba5dc5401b
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:21:18 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:58 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:57:11 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/options.hxx
@@ -0,0 +1,62 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: options.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:21:18 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2006 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef INCLUDED_SVTOOLS_OPTIONS_HXX
+#define INCLUDED_SVTOOLS_OPTIONS_HXX
+
+#ifndef _SAL_CONFIG_H_
+#include "sal/config.h"
+#endif
+
+namespace binfilter
+{
+
+// A base class for the various option classes supported by
+// svtools/source/config/itemholderbase.hxx (which must be public, as it is
+// shared between svl and svt):
+class  Options {
+public:
+    Options();
+
+    virtual ~Options() = 0;
+
+private:
+     Options(Options &); // not defined
+     void operator =(Options &); // not defined
+};
+
+}
+
+#endif
