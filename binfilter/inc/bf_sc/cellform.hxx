4965ad39fbe56270a97df264278044626fb59a1c
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:11:12 +0000

    INTEGRATION: CWS impresstables2 (1.3.232); FILE MERGED 2008/02/22 23:36:52 cl 1.3.232.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_sc/cellform.hxx
+++ binfilter/inc/bf_sc/cellform.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: cellform.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 10:59:57 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:11:12 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -40,10 +40,10 @@
 #include <tools/solar.h>
 #endif
 class String;
-class SvNumberFormatter;
 class Color;
 namespace binfilter {
 
+class SvNumberFormatter;
 
 class ScBaseCell;
 
