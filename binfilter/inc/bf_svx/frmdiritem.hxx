71697513579b3c6896636d9d709844bce022902d
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:52:53 +0000

    INTEGRATION: CWS impresstables2 (1.5.78); FILE MERGED 2008/02/13 11:17:15 cl 1.5.78.1: merged svtools in and stripped

--- binfilter/inc/bf_svx/frmdiritem.hxx
+++ binfilter/inc/bf_svx/frmdiritem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: frmdiritem.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-12-04 08:08:32 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:52:53 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 // include ---------------------------------------------------------------
 
 #ifndef _SFXINTITEM_HXX
-#include <svtools/intitem.hxx>
+#include <bf_svtools/intitem.hxx>
 #endif
 #ifndef _SVX_FRMDIR_HXX
 #include <bf_svx/frmdir.hxx>
