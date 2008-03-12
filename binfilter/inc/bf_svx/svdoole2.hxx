882f6e2ad7de943632c07029366f9fc5e1faee04
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:11:18 +0000

    INTEGRATION: CWS impresstables2 (1.7.18); FILE MERGED 2008/02/22 23:37:05 cl 1.7.18.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_svx/svdoole2.hxx
+++ binfilter/inc/bf_svx/svdoole2.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdoole2.hxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: vg $ $Date: 2007-10-23 14:30:20 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:11:18 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -52,20 +52,11 @@
 
 #include <bf_so3/svstor.hxx>
 
+namespace binfilter {
+
 class SvPersist;
 class SvInPlaceObjectRef;
 class SvEmbeddedInfoObject;
-namespace binfilter {
-
-//************************************************************
-//   Vorausdeklarationen
-//************************************************************
-
-
-// ***********************************************************
-//  SvInPlaceObject dummy implementation for displaying
-//  plugins with the StarOffice Player without so3
-// ***********************************************************
 
 //************************************************************
 //   SdrOle2Obj
