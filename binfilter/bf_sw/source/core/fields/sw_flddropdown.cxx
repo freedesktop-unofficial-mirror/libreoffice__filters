98e2a02bbcd01ad25bc22c7fb1257f1be3b49c69
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:00:37 +0000

    INTEGRATION: CWS impresstables2 (1.6.70); FILE MERGED 2008/02/13 10:35:47 cl 1.6.70.1: merged svtools in and stripped

--- binfilter/bf_sw/source/core/fields/sw_flddropdown.cxx
+++ binfilter/bf_sw/source/core/fields/sw_flddropdown.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_flddropdown.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 17:50:26 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:00:37 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -34,7 +34,7 @@
  ************************************************************************/
 
 #include <algorithm>
-#include <svtools/poolitem.hxx>
+#include <bf_svtools/poolitem.hxx>
 #ifndef _UNOFLDMID_H
 #include <unofldmid.h>
 #endif
