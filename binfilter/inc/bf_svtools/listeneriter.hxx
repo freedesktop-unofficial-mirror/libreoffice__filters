fbd91cd0459533d1a51aa17ec2f87f6cb57b454e
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:16:41 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:57 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:54:08 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/listeneriter.hxx
@@ -0,0 +1,92 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: listeneriter.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:16:41 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _SVT_LISTENERITER_HXX
+#define _SVT_LISTENERITER_HXX
+
+#ifndef _RTTI_HXX //autogen
+#include <tools/rtti.hxx>
+#endif
+
+namespace binfilter
+{
+
+class SvtListener;
+class SvtListenerBase;
+class SvtBroadcaster;
+
+//-------------------------------------------------------------------------
+
+class  SvtListenerIter
+{
+    friend class SvtListenerBase;
+
+    SvtBroadcaster& rRoot;
+    SvtListenerBase *pAkt, *pDelNext;
+
+    // for the update of all iterator's, if a listener is added or removed
+    // at the same time.
+    static SvtListenerIter *pListenerIters;
+    SvtListenerIter *pNxtIter;
+    TypeId aSrchId;				// fuer First/Next - suche diesen Type
+
+     static void RemoveListener( SvtListenerBase& rDel,
+                                               SvtListenerBase* pNext );
+
+public:
+    SvtListenerIter( SvtBroadcaster& );
+    ~SvtListenerIter();
+
+    const SvtBroadcaster& GetBroadcaster() const	{ return rRoot; }
+          SvtBroadcaster& GetBroadcaster() 			{ return rRoot; }
+
+    SvtListener* GoNext();			// to the next
+    SvtListener* GoPrev();			// to the previous
+
+    SvtListener* GoStart(); 		// to the start of the list
+    SvtListener* GoEnd();			// to the end of the list
+
+    SvtListener* GoRoot(); 			// to the root
+    SvtListener* GetCurr() const;	// returns the current
+
+    int IsChanged() const 		{ return pDelNext != pAkt; }
+
+    SvtListener* First( TypeId nType );
+    SvtListener* Next();
+};
+
+}
+
+#endif
+
