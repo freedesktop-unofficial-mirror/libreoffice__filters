3109166c036a4a82e0a7f82cba6a03796fca996e
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:03:00 +0000

    INTEGRATION: CWS impresstables2 (1.4.104); FILE MERGED 2008/02/22 23:34:58 cl 1.4.104.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/bf_sw/source/core/inc/atrhndl.hxx
+++ binfilter/bf_sw/source/core/inc/atrhndl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: atrhndl.hxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-27 22:42:27 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:02:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -44,9 +44,9 @@
 #ifndef _SWFNTCCH_HXX
 #include <swfntcch.hxx>
 #endif
-class SfxPoolItem;
-namespace binfilter {
 
+namespace binfilter {
+class SfxPoolItem;
 class SwAttrSet;
 class SwDoc;
 class ViewShell;
