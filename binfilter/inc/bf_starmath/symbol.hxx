3b12d22740229e117d6959bd804ec446579a5aed
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:48:38 +0000

    INTEGRATION: CWS impresstables2 (1.5.104); FILE MERGED 2008/02/13 11:17:14 cl 1.5.104.1: merged svtools in and stripped

--- binfilter/inc/bf_starmath/symbol.hxx
+++ binfilter/inc/bf_starmath/symbol.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: symbol.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 03:14:20 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:48:38 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -51,10 +51,10 @@
 #include <tools/dynary.hxx>
 #endif
 #ifndef _SFXLSTNER_HXX //autogen
-#include <svtools/lstner.hxx>
+#include <bf_svtools/lstner.hxx>
 #endif
 #ifndef _SVARRAY_HXX
-#include <svtools/svarray.hxx>
+#include <bf_svtools/svarray.hxx>
 #endif
 
 #ifndef UTILITY_HXX
