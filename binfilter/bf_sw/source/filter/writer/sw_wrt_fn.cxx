e370b46d5e9471e143ac59c7f5e02956c603cc3c
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:36:17 +0000

    INTEGRATION: CWS impresstables2 (1.8.10); FILE MERGED 2008/02/13 10:36:05 cl 1.8.10.1: merged svtools in and stripped

--- binfilter/bf_sw/source/filter/writer/sw_wrt_fn.cxx
+++ binfilter/bf_sw/source/filter/writer/sw_wrt_fn.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sw_wrt_fn.cxx,v $
  *
- *  $Revision: 1.8 $
+ *  $Revision: 1.9 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 13:01:48 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:36:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -39,10 +39,10 @@
 #endif
 
 #ifndef _SFXITEMITER_HXX //autogen
-#include <svtools/itemiter.hxx>
+#include <bf_svtools/itemiter.hxx>
 #endif
 #ifndef _SFX_WHITER_HXX //autogen
-#include <svtools/whiter.hxx>
+#include <bf_svtools/whiter.hxx>
 #endif
 
 
