3567182ff85655aaa2a40df5dbd67b684ea363a3
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:54:09 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:20 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 09:58:20 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/items1/svt_sitem.cxx
@@ -0,0 +1,124 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svt_sitem.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:54:09 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+
+// INCLUDE ---------------------------------------------------------------
+
+#ifndef GCC
+#endif
+
+#include <tools/string.hxx>
+#include <tools/stream.hxx>
+
+#include <bf_svtools/poolitem.hxx>
+#include <bf_svtools/itemset.hxx>
+
+namespace binfilter
+{
+
+// STATIC DATA -----------------------------------------------------------
+
+DBG_NAME(SfxSetItem)
+
+// --------------------------------------------------------------------------
+
+SfxSetItem::SfxSetItem( USHORT which, const SfxItemSet &rSet) :
+    SfxPoolItem(which),
+    pSet(rSet.Clone(TRUE))
+{
+    DBG_CTOR(SfxSetItem, 0);
+}
+
+// --------------------------------------------------------------------------
+
+SfxSetItem::SfxSetItem( USHORT which, SfxItemSet *pS) :
+    SfxPoolItem(which),
+    pSet(pS)
+{
+    DBG_CTOR(SfxSetItem, 0);
+    DBG_ASSERT(pS, "SfxSetItem without set constructed" );
+}
+
+// --------------------------------------------------------------------------
+
+SfxSetItem::SfxSetItem( const SfxSetItem& rCopy, SfxItemPool *pPool ) :
+    SfxPoolItem(rCopy.Which()),
+    pSet(rCopy.pSet->Clone(TRUE, pPool))
+{
+    DBG_CTOR(SfxSetItem, 0);
+}
+
+// --------------------------------------------------------------------------
+
+SfxSetItem::~SfxSetItem()
+{
+    DBG_DTOR(SfxSetItem, 0);
+    delete pSet; pSet = 0;
+}
+
+// --------------------------------------------------------------------------
+
+int SfxSetItem::operator==( const SfxPoolItem& rCmp) const
+{
+    DBG_CHKTHIS(SfxSetItem, 0);
+    DBG_ASSERT( SfxPoolItem::operator==( rCmp ), "unequal type" );
+    return *pSet == *(((const SfxSetItem &)rCmp).pSet);
+}
+
+// --------------------------------------------------------------------------
+
+SfxItemPresentation SfxSetItem::GetPresentation
+(
+    SfxItemPresentation     /*ePresentation*/,
+    SfxMapUnit              /*eCoreMetric*/,
+    SfxMapUnit              /*ePresentationMetric*/,
+    XubString&              /*rText*/,
+    const ::IntlWrapper *
+)   const
+{
+    DBG_CHKTHIS(SfxSetItem, 0);
+    return SFX_ITEM_PRESENTATION_NONE;
+}
+
+// --------------------------------------------------------------------------
+
+SvStream& SfxSetItem::Store(SvStream& rStream, USHORT) const
+{
+    GetItemSet().Store(rStream);
+    return rStream;
+}
+
+}
