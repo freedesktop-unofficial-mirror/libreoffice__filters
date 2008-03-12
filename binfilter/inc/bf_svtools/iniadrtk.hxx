edcf2fb5231edfea808cb746f3a8ae951080f6e0
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:11:51 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:57 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:50:47 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/iniadrtk.hxx
@@ -0,0 +1,73 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: iniadrtk.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:11:51 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SVTOOLS_INIADRTK_HXX
+#define SVTOOLS_INIADRTK_HXX
+
+class String;
+
+namespace binfilter
+{
+
+//============================================================================
+enum AddressToken
+{
+    ADDRESS_COMPANY,
+    ADDRESS_STREET,
+    ADDRESS_COUNTRY,
+    ADDRESS_PLZ,
+    ADDRESS_CITY,
+    ADDRESS_TITLE,
+    ADDRESS_POSITION,
+    ADDRESS_TEL_PRIVATE,
+    ADDRESS_TEL_COMPANY,
+    ADDRESS_FAX,
+    ADDRESS_EMAIL,
+    ADDRESS_STATE,
+    ADDRESS_FATHERSNAME,
+    ADDRESS_APARTMENT
+};
+
+//============================================================================
+class SfxIniManagerAddressEntry
+{
+public:
+    static String get(const String & rAddress, AddressToken eToken);
+};
+
+}
+
+#endif // SVTOOLS_INIADRTK_HXX
+
