2cb90d5a9abb2e92b32bf4b9ece95458d797de7d
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:21:59 +0000

    INTEGRATION: CWS impresstables2 (1.9.10); FILE MERGED 2008/02/13 09:18:40 cl 1.9.10.1: merged svtools in and stripped

--- binfilter/bf_sch/source/core/sch_chtmode5.cxx
+++ binfilter/bf_sch/source/core/sch_chtmode5.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sch_chtmode5.cxx,v $
  *
- *  $Revision: 1.9 $
+ *  $Revision: 1.10 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 11:15:22 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:21:59 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -74,7 +74,7 @@
 #ifndef _ZFORLIST_DECLARE_TABLE
 #define _ZFORLIST_DECLARE_TABLE
 #endif
-#include <svtools/zforlist.hxx>
+#include <bf_svtools/zforlist.hxx>
 #endif
 namespace binfilter {
 
