9d0b106c9c9672e0ab603ed0c6c59713004727d7
Rüdiger Timm <rt@openoffice.org>
2008-03-12 10:01:28 +0000

    INTEGRATION: CWS impresstables2 (1.4.104); FILE MERGED 2008/02/13 10:37:28 cl 1.4.104.1: merged svtools in and stripped

--- binfilter/bf_xmloff/source/style/xmloff_xmltabi.cxx
+++ binfilter/bf_xmloff/source/style/xmloff_xmltabi.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: xmloff_xmltabi.cxx,v $
  *
- *  $Revision: 1.4 $
+ *  $Revision: 1.5 $
  *
- *  last change: $Author: rt $ $Date: 2006-10-28 02:07:47 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 11:01:28 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,7 +56,7 @@
 #include "xmltabi.hxx"
 
 #define _SVSTDARR_USHORTS
-#include <svtools/svstdarr.hxx>
+#include <bf_svtools/svstdarr.hxx>
 namespace binfilter {
 
 using namespace ::rtl;
