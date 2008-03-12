3b4800f89abd1241a28d4a6b4a4547421c6367b5
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:51:41 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:20 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 09:56:51 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/items1/svt_ilstitem.cxx
@@ -0,0 +1,120 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svt_ilstitem.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:51:41 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+
+
+#ifndef _COM_SUN_STAR_SCRIPT_XTYPECONVERTER_HPP_
+#include <com/sun/star/script/XTypeConverter.hpp>
+#endif
+#ifndef _COM_SUN_STAR_LANG_XMULTISERVICEFACTORY_HPP_
+#include <com/sun/star/lang/XMultiServiceFactory.hpp>
+#endif
+
+#include <comphelper/processfactory.hxx>
+
+#include <bf_svtools/ilstitem.hxx>
+
+#define _SVSTDARR_ULONGS
+#include <bf_svtools/svstdarr.hxx>
+
+namespace binfilter
+{
+
+TYPEINIT1_AUTOFACTORY(SfxIntegerListItem, SfxPoolItem);
+
+SfxIntegerListItem::SfxIntegerListItem()
+{
+}
+
+SfxIntegerListItem::SfxIntegerListItem( USHORT which, const SvULongs& rList )
+    : SfxPoolItem( which )
+{
+    m_aList.realloc( rList.Count() );
+    for ( USHORT n=0; n<rList.Count(); n++ )
+        m_aList[n] = rList[n];
+}
+
+SfxIntegerListItem::SfxIntegerListItem( const SfxIntegerListItem& rItem )
+    : SfxPoolItem( rItem )
+{
+    m_aList = rItem.m_aList;
+}
+
+SfxIntegerListItem::~SfxIntegerListItem()
+{
+}
+
+int SfxIntegerListItem::operator==( const SfxPoolItem& rPoolItem ) const
+{
+    if ( !rPoolItem.ISA( SfxIntegerListItem ) )
+        return FALSE;
+
+    const SfxIntegerListItem rItem = (const SfxIntegerListItem&) rPoolItem;
+    return rItem.m_aList == m_aList;
+}
+
+SfxPoolItem* SfxIntegerListItem::Clone( SfxItemPool * ) const
+{
+    return new SfxIntegerListItem( *this );
+}
+
+BOOL SfxIntegerListItem::PutValue  ( const com::sun::star::uno::Any& rVal, BYTE )
+{
+    ::com::sun::star::uno::Reference < ::com::sun::star::script::XTypeConverter > xConverter
+            ( ::comphelper::getProcessServiceFactory()->createInstance(::rtl::OUString::createFromAscii("com.sun.star.script.Converter")),
+            ::com::sun::star::uno::UNO_QUERY );
+    ::com::sun::star::uno::Any aNew;
+    try { aNew = xConverter->convertTo( rVal, ::getCppuType((const ::com::sun::star::uno::Sequence < sal_Int32 >*)0) ); }
+    catch (::com::sun::star::uno::Exception&)
+    {
+        return FALSE;
+    }
+
+    return ( aNew >>= m_aList );
+}
+
+BOOL SfxIntegerListItem::QueryValue( com::sun::star::uno::Any& rVal, BYTE ) const
+{
+    rVal <<= m_aList;
+    return TRUE;
+}
+
+void SfxIntegerListItem::GetList( SvULongs& rList ) const
+{
+    for ( sal_Int32 n=0; n<m_aList.getLength(); n++ )
+        rList.Insert( m_aList[n], sal::static_int_cast< USHORT >(n) );
+}
+}
