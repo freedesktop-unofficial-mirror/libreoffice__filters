3fe06b08b24bd549467c106b5ec04f00532eaf04
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:02:10 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:56 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:46:20 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/folderrestriction.hxx
@@ -0,0 +1,65 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: folderrestriction.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:02:10 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef SVTOOLS_FOLDER_RESTRICTION_HXX
+#define SVTOOLS_FOLDER_RESTRICTION_HXX
+
+#ifndef _STRING_HXX
+#include <tools/string.hxx>
+#endif
+
+#ifndef INCLUDED_VECTOR
+#include <vector>
+#define INCLUDED_VECTOR
+#endif
+
+//........................................................................
+namespace binfilter
+{
+//........................................................................
+
+    /** retrieves a list of folders which's access is not restricted.
+
+        <p>Note that this is not meant as security feature, but only as
+        method to restrict some UI presentation, such as browsing
+        in the file open dialog.</p>
+    */
+     void getUnrestrictedFolders( ::std::vector< String >& _rFolders );
+
+//........................................................................
+}
+//........................................................................
+
+#endif // SVTOOLS_FOLDER_RESTRICTION_HXX
