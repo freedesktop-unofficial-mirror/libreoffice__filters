fcc1d134bf42138f5492958336fef58105357e3e
Rüdiger Timm <rt@openoffice.org>
2008-03-12 06:06:33 +0000

    INTEGRATION: CWS impresstables2 (1.3.234); FILE MERGED 2008/02/13 09:17:44 cl 1.3.234.1: merged svtools in and stripped

--- binfilter/bf_sc/source/ui/inc/DrawModelBroadcaster.hxx
+++ binfilter/bf_sc/source/ui/inc/DrawModelBroadcaster.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: DrawModelBroadcaster.hxx,v $
  *
- *  $Revision: 1.3 $
+ *  $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 19:30:56 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 07:06:33 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -38,7 +38,7 @@
 
 
 #ifndef _SFXLSTNER_HXX 
-#include <svtools/lstner.hxx>
+#include <bf_svtools/lstner.hxx>
 #endif
 #ifndef _CPPUHELPER_INTERFACECONTAINER_H_ 
 #include <cppuhelper/interfacecontainer.h>
