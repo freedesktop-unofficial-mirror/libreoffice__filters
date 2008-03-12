a839d0eee5674f2d074a5aab628d3f9ef6f16de5
Rüdiger Timm <rt@openoffice.org>
2008-03-12 05:51:42 +0000

    INTEGRATION: CWS impresstables2 (1.5.104); FILE MERGED 2008/02/13 09:17:36 cl 1.5.104.3: merged svtools in and stripped 2007/10/11 19:16:33 cl 1.5.104.2: RESYNC: (1.5-1.6); FILE MERGED 2007/07/27 15:17:48 cl 1.5.104.1: fixed namespace issues

--- binfilter/bf_sc/source/core/data/sc_poolhelp.cxx
+++ binfilter/bf_sc/source/core/data/sc_poolhelp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sc_poolhelp.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 10:38:45 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 06:51:42 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -42,7 +42,7 @@
 
 // INCLUDE ---------------------------------------------------------------
 
-#include <svtools/zforlist.hxx>
+#include <bf_svtools/zforlist.hxx>
 #include <bf_svx/editeng.hxx>
 
 #include "poolhelp.hxx"
