c7600f34fba30c3f6956c07a13521202c4d53fa2
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:15:11 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:57 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:52:59 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/javaoptions.hxx
@@ -0,0 +1,89 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: javaoptions.hxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:15:11 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+#ifndef _SVTOOLS_JAVAOPTIONS_HXX
+#define _SVTOOLS_JAVAOPTIONS_HXX
+
+#ifndef _SAL_TYPES_H_
+#include <sal/types.h>
+#endif
+
+#ifndef _UTL_CONFIGITEM_HXX_
+#include <unotools/configitem.hxx>
+#endif
+
+namespace binfilter
+{
+
+// class SvtJavaOptions --------------------------------------------------
+
+struct SvtJavaOptions_Impl;
+
+class  SvtJavaOptions : public utl::ConfigItem
+{
+    SvtJavaOptions_Impl* pImpl;
+public:
+    enum EOption
+    {
+        E_ENABLED,
+        E_SECURITY,
+        E_NETACCESS,
+        E_USERCLASSPATH,
+        E_EXECUTEAPPLETS
+    };
+
+    SvtJavaOptions();
+    ~SvtJavaOptions();
+
+    virtual void    Commit();
+
+    sal_Bool        IsEnabled() const;
+    sal_Bool        IsSecurity()const;
+    sal_Int32       GetNetAccess() const;
+    rtl::OUString&  GetUserClassPath()const;
+    sal_Bool        IsExecuteApplets() const;
+
+    void SetEnabled(sal_Bool bSet) ;
+    void SetSecurity(sal_Bool bSet);
+    void SetNetAccess(sal_Int32 nSet) ;
+    void SetUserClassPath(const rtl::OUString& rSet);
+    void SetExecuteApplets(sal_Bool bSet);
+
+    sal_Bool IsReadOnly( EOption eOption ) const;
+};
+
+}
+
+#endif //
+
