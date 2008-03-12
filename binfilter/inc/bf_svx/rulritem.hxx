4e4ade4ac75bbd6ac6daf44e566c8b1b22b66c57
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:02:17 +0000

    INTEGRATION: CWS impresstables2 (1.5.70); FILE MERGED 2008/02/13 11:17:16 cl 1.5.70.1: merged svtools in and stripped

--- binfilter/inc/bf_svx/rulritem.hxx
+++ binfilter/inc/bf_svx/rulritem.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: rulritem.hxx,v $
  *
- *  $Revision: 1.5 $
+ *  $Revision: 1.6 $
  *
- *  last change: $Author: hr $ $Date: 2007-01-02 18:33:03 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:02:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -41,7 +41,7 @@
 #include <tools/gen.hxx>
 #endif
 #ifndef _SFXPOOLITEM_HXX //autogen
-#include <svtools/poolitem.hxx>
+#include <bf_svtools/poolitem.hxx>
 #endif
 namespace binfilter {
 
