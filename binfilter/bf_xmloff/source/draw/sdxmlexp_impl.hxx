090dff0a4dc5f76131a5f8c850eb26e0b2f687e1
Rüdiger Timm <rt@openoffice.org>
2008-03-12 09:57:32 +0000

    INTEGRATION: CWS impresstables2 (1.2.232); FILE MERGED 2008/02/22 23:35:27 cl 1.2.232.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/bf_xmloff/source/draw/sdxmlexp_impl.hxx
+++ binfilter/bf_xmloff/source/draw/sdxmlexp_impl.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: sdxmlexp_impl.hxx,v $
  *
- *  $Revision: 1.2 $
+ *  $Revision: 1.3 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-09 07:50:36 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 10:57:32 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -56,10 +56,10 @@
 #ifndef _COM_SUN_STAR_DRAWING_XDRAWPAGE_HPP_
 #include <com/sun/star/drawing/XDrawPage.hpp>
 #endif
-class SfxPoolItem;
-class SfxItemSet;
 class Rectangle;
 namespace binfilter {
+class SfxPoolItem;
+class SfxItemSet;
 
 //////////////////////////////////////////////////////////////////////////////
 
