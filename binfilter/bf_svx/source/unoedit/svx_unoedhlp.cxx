48bd457474d88c8177fd9afe28390c2353637302
Rüdiger Timm <rt@openoffice.org>
2008-03-12 08:46:05 +0000

    INTEGRATION: CWS impresstables2 (1.7.10); FILE MERGED 2008/02/13 10:30:30 cl 1.7.10.1: merged svtools in and stripped

--- binfilter/bf_svx/source/unoedit/svx_unoedhlp.cxx
+++ binfilter/bf_svx/source/unoedit/svx_unoedhlp.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svx_unoedhlp.cxx,v $
  *
- *  $Revision: 1.7 $
+ *  $Revision: 1.8 $
  *
- *  last change: $Author: kz $ $Date: 2007-09-06 11:47:22 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 09:46:05 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -46,72 +46,6 @@
 namespace binfilter {
 
 //------------------------------------------------------------------------
-
-TYPEINIT1( SvxEditSourceHint, TextHint );
-
-SvxEditSourceHint::SvxEditSourceHint( ULONG _nId ) : 
-    TextHint( _nId ),
-    mnStart( 0 ),
-    mnEnd( 0 )
-{
-}
-
-SvxEditSourceHint::SvxEditSourceHint( ULONG _nId, ULONG nValue, ULONG nStart, ULONG nEnd ) : 
-    TextHint( _nId, nValue ),
-    mnStart( nStart),
-    mnEnd( nEnd )
-{
-}
-
-//------------------------------------------------------------------------
-
-::std::auto_ptr<SfxHint> SvxEditSourceHelper::EENotification2Hint( EENotify* aNotify )
-{
-    if( aNotify )
-    {
-        switch( aNotify->eNotificationType )
-        {
-            case EE_NOTIFY_TEXTMODIFIED:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_MODIFIED, aNotify->nParagraph ) );
-
-            case EE_NOTIFY_PARAGRAPHINSERTED:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_PARAINSERTED, aNotify->nParagraph ) );
-
-            case EE_NOTIFY_PARAGRAPHREMOVED: 
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_PARAREMOVED, aNotify->nParagraph ) );
-
-            case EE_NOTIFY_PARAGRAPHSMOVED:
-                return ::std::auto_ptr<SfxHint>( new SvxEditSourceHint( EDITSOURCE_HINT_PARASMOVED, aNotify->nParagraph, aNotify->nParam1, aNotify->nParam2 ) );
-
-            case EE_NOTIFY_TEXTHEIGHTCHANGED:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_TEXTHEIGHTCHANGED, aNotify->nParagraph ) );
-
-            case EE_NOTIFY_TEXTVIEWSCROLLED:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_VIEWSCROLLED ) );
-
-            case EE_NOTIFY_TEXTVIEWSELECTIONCHANGED:
-                return ::std::auto_ptr<SfxHint>( new SvxEditSourceHint( EDITSOURCE_HINT_SELECTIONCHANGED ) );
-
-            case EE_NOTIFY_BLOCKNOTIFICATION_START:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_BLOCKNOTIFICATION_START, 0 ) );
-
-            case EE_NOTIFY_BLOCKNOTIFICATION_END:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_BLOCKNOTIFICATION_END, 0 ) );
-
-            case EE_NOTIFY_INPUT_START:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_INPUT_START, 0 ) );
-
-            case EE_NOTIFY_INPUT_END:
-                return ::std::auto_ptr<SfxHint>( new TextHint( TEXT_HINT_INPUT_END, 0 ) );
-
-            default:
-                DBG_ERROR( "SvxEditSourceHelper::EENotification2Hint unknown notification" );
-                break;
-        }
-    }
-
-    return ::std::auto_ptr<SfxHint>( new SfxHint() );
-}
 
 sal_Bool SvxEditSourceHelper::GetAttributeRun( USHORT& nStartIndex, USHORT& nEndIndex, const EditEngine& rEE, USHORT nPara, USHORT nIndex )
 {
