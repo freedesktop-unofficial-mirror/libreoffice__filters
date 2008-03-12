d37488b30a6d53c650933db8f6a31be1318b461a
Rüdiger Timm <rt@openoffice.org>
2008-03-12 11:32:20 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:36:59 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 11:05:02 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/inc/bf_svtools/svipcdef.h
@@ -0,0 +1,81 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svipcdef.h,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 12:32:20 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+#ifndef _SVIPCDEF_H
+#define _SVIPCDEF_H
+
+namespace binfilter
+{
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined (WNT) || defined (WIN)
+#define CDECL __cdecl
+#elif defined CSET
+#define CDECL _System
+#else
+#define CDECL
+#endif
+
+#if defined WIN
+#define _EXTLIBCALL_ _pascal
+#else
+#define _EXTLIBCALL_ CDECL
+#endif
+
+typedef void (CDECL *IPCCallbackFunc)(void*);
+
+extern void _EXTLIBCALL_ IPCFreeMemory(void*);
+extern short _EXTLIBCALL_ IPCGetStatus(void*);
+extern short _EXTLIBCALL_ IPCInit(void);
+extern void  _EXTLIBCALL_ IPCDeInit(void);
+extern void*  _EXTLIBCALL_ IPCConnectServer(const char*, IPCCallbackFunc);
+extern void  _EXTLIBCALL_ IPCDisconnectServer(void *);
+#ifdef WIN
+extern long CDECL IPCCALLFUNCTION(void *,unsigned long,void *,
+                                               short,const char *, char, ...);
+#else
+extern long CDECL IPCCallFunction(void *,unsigned long,void *,
+                                               short,const char *, char, ...);
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+}
+
+#endif
