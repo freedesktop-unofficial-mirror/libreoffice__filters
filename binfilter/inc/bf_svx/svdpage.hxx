e19071feebddc14a246c4c8d53f6e1bdce0e0739
Rüdiger Timm <rt@openoffice.org>
2008-03-12 12:11:47 +0000

    INTEGRATION: CWS impresstables2 (1.6.16); FILE MERGED 2008/02/22 23:37:05 cl 1.6.16.1: added namespace binfilter to bf_svtools and bf_so3

--- binfilter/inc/bf_svx/svdpage.hxx
+++ binfilter/inc/bf_svx/svdpage.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: svdpage.hxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: obo $ $Date: 2008-01-07 08:29:43 $
+ *  last change: $Author: rt $ $Date: 2008-03-12 13:11:47 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -57,10 +57,11 @@
 #ifndef _SVDLAYER_HXX
 #include <bf_svx/svdlayer.hxx>
 #endif
-class SfxItemPool;
 class Color;
-class SfxStyleSheet;
 namespace binfilter {
+
+class SfxItemPool;
+class SfxStyleSheet;
 
 // ------------------------------
 // - intern benutzte Paint-Modi -
