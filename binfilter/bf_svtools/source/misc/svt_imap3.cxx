265da3b0cc3cca574476ee4915f9719070cb44a0
Rüdiger Timm <rt@openoffice.org>
2008-03-12 07:59:12 +0000

    INTEGRATION: CWS impresstables2 (1.1.2); FILE ADDED 2008/02/22 23:29:23 cl 1.1.2.2: added namespace binfilter to bf_svtools and bf_so3 2008/02/13 10:03:55 cl 1.1.2.1: merged svtools in and stripped

---
+++ binfilter/bf_svtools/source/misc/svt_imap3.cxx
@@ -0,0 +1,109 @@
+/*************************************************************************
+ *
+ *  OpenOffice.org - a multi-platform office productivity suite
+ *
+ *  $RCSfile: svt_imap3.cxx,v $
+ *
+ *  $Revision: 1.2 $
+ *
+ *  last change: $Author: rt $ $Date: 2008-03-12 08:59:12 $
+ *
+ *  The Contents of this file are made available subject to
+ *  the terms of GNU Lesser General Public License Version 2.1.
+ *
+ *
+ *    GNU Lesser General Public License Version 2.1
+ *    =============================================
+ *    Copyright 2005 by Sun Microsystems, Inc.
+ *    901 San Antonio Road, Palo Alto, CA 94303, USA
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License version 2.1, as published by the Free Software Foundation.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ ************************************************************************/
+
+// MARKER(update_precomp.py): autogen include statement, do not remove
+
+
+
+#include <bf_svtools/imap.hxx>
+
+#include <tools/debug.hxx>
+
+namespace binfilter
+{
+
+
+/******************************************************************************
+|*
+|* Ctor
+|*
+\******************************************************************************/
+
+IMapCompat::IMapCompat( SvStream& rStm, const USHORT nStreamMode ) :
+            pRWStm		( &rStm ),
+            nStmMode	( nStreamMode )
+{
+    DBG_ASSERT( nStreamMode == STREAM_READ || nStreamMode == STREAM_WRITE, "Wrong Mode!" );
+
+    if ( !pRWStm->GetError() )
+    {
+        if ( nStmMode == STREAM_WRITE )
+        {
+            nCompatPos = pRWStm->Tell();
+            pRWStm->SeekRel( 4 );
+            nTotalSize = nCompatPos + 4;
+        }
+        else
+        {
+            UINT32 nTotalSizeTmp;
+            *pRWStm >> nTotalSizeTmp;
+            nTotalSize = nTotalSizeTmp;
+            nCompatPos = pRWStm->Tell();
+        }
+    }
+}
+
+
+/******************************************************************************
+|*
+|* Dtor
+|*
+\******************************************************************************/
+
+IMapCompat::~IMapCompat()
+{
+    if ( !pRWStm->GetError() )
+    {
+        if ( nStmMode == STREAM_WRITE )
+        {
+            const ULONG	nEndPos = pRWStm->Tell();
+
+            pRWStm->Seek( nCompatPos );
+            *pRWStm << (UINT32) ( nEndPos - nTotalSize );
+            pRWStm->Seek( nEndPos );
+        }
+        else
+        {
+            const ULONG nReadSize = pRWStm->Tell() - nCompatPos;
+
+            if ( nTotalSize > nReadSize )
+                pRWStm->SeekRel( nTotalSize - nReadSize );
+        }
+    }
+}
+
+
+
+}
