892fab3762f08115e8592665cb4568744ec51cf0
Kurt Zenker <kz@openoffice.org>
2008-05-20 15:15:26 +0000

    INTEGRATION: CWS mba30patches01 (1.9.4); FILE MERGED 2008/04/23 11:27:09 mba 1.9.4.2: RESYNC: (1.9-1.10); FILE MERGED 2008/03/19 14:55:07 cmc 1.9.4.1: #i86582# remove unused code, round 1

--- binfilter/bf_svx/source/editeng/editdoc.hxx
+++ binfilter/bf_svx/source/editeng/editdoc.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: editdoc.hxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -302,8 +302,6 @@ struct ExtraPortionInfo
 
     long*    pOrgDXArray;
 
-
-            ExtraPortionInfo();
             ~ExtraPortionInfo();
 
 };
