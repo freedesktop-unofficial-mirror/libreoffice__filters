ddb667932b7707bf9e5f9c39e5c2d1fbaa3c471c
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:36:13 +0000

    INTEGRATION: CWS mba30patches01 (1.4.162); FILE MERGED 2008/04/23 11:42:05 mba 1.4.162.2: RESYNC: (1.4-1.5); FILE MERGED 2008/03/19 14:55:29 cmc 1.4.162.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_sc/scdll.hxx
+++ binfilter/inc/bf_sc/scdll.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: scdll.hxx,v $
- * $Revision: 1.5 $
+ * $Revision: 1.6 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -105,9 +105,6 @@ public:
                     {}
 
     virtual SfxModule* Load();
-
-    static SvGlobalName GetID(USHORT nFileFormat);
-    static USHORT		HasID(const SvGlobalName& rName);
 };
 
 //-------------------------------------------------------------------------
