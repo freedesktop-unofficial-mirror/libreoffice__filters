b722455528c696b838dc34a7a59fb576033d3949
Kurt Zenker <kz@openoffice.org>
2008-05-20 14:03:17 +0000

    INTEGRATION: CWS mba30patches01 (1.9.48); FILE MERGED 2008/04/23 11:16:06 mba 1.9.48.2: RESYNC: (1.9-1.10); FILE MERGED 2008/03/19 14:54:57 cmc 1.9.48.1: #i86582# remove unused code, round 1

--- binfilter/bf_sch/source/ui/app/sch_schlib.cxx
+++ binfilter/bf_sch/source/ui/app/sch_schlib.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: sch_schlib.cxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -148,25 +148,6 @@ extern "C" { static void SAL_CALL thisModule() {} }
 \************************************************************************/
 
 
-/*N*/ void __EXPORT SchDLL::Update( SvInPlaceObjectRef aIPObj, SchMemChart* pData, const SfxItemSet& rAttr, OutputDevice* pOut )
-/*N*/ {
-/*N*/ 	void (__LOADONCALLAPI*fp)(SvInPlaceObjectRef, SchMemChart*, const SfxItemSet&, OutputDevice*);
-/*N*/ 
-/*N*/ 	fp = (void (__LOADONCALLAPI*)(SvInPlaceObjectRef, SchMemChart*, const SfxItemSet&, OutputDevice*))GetFuncSch("SchUpdateAttr");
-/*N*/ 
-/*N*/ 	if (fp)
-/*N*/ 	{
-/*N*/ 	fp(aIPObj, pData, rAttr, pOut);
-/*N*/ 	}
-/*N*/ }
-
-/*************************************************************************
-|*
-|*
-|*
-\************************************************************************/
-
-
 /*N*/ SchMemChart* __EXPORT SchDLL::GetChartData (SvInPlaceObjectRef aIPObj)
 /*N*/ {
 /*N*/ 	SchMemChart* (__LOADONCALLAPI*fp)(SvInPlaceObjectRef);
@@ -176,23 +157,6 @@ extern "C" { static void SAL_CALL thisModule() {} }
 /*N*/ 	return fp
 /*N*/ 			   ? fp (aIPObj)
 /*N*/ 			   : 0;
-/*N*/ }
-
-/*************************************************************************
-|*
-|*
-|*
-\************************************************************************/
-
-/*N*/ SchMemChart* __EXPORT SchDLL::NewMemChart ()
-/*N*/ {
-/*?*/ 	SchMemChart* (__LOADONCALLAPI*fp)();
-/*?*/ 
-/*?*/ 	fp = (SchMemChart* (__LOADONCALLAPI*)())GetFuncSch("SchNewMemChartNone");
-/*?*/ 
-/*?*/ 	return fp
-/*?*/ 			   ? fp ()
-/*?*/ 			   : 0;
 /*N*/ }
 
 /*************************************************************************
@@ -219,57 +183,6 @@ extern "C" { static void SAL_CALL thisModule() {} }
 |*
 |*
 \************************************************************************/
-
-/*?*/ SchMemChart* __EXPORT SchDLL::NewMemChart (const SchMemChart &rMemChart)
-/*?*/ {
-/*?*/ 	SchMemChart* (__LOADONCALLAPI*fp)(const SchMemChart &);
-/*?*/ 
-/*?*/ 	fp = (SchMemChart* (__LOADONCALLAPI*)(const SchMemChart &))GetFuncSch("SchNewMemChartCopy");
-/*?*/ 
-/*?*/ 	if (fp)
-/*?*/ 	{
-/*?*/ 		return fp(rMemChart);
-/*?*/ 	}
-/*?*/ 	else return 0;
-/*?*/ }
-
-/*************************************************************************
-|*
-|*
-|*
-\************************************************************************/
-
-/*?*/ ChartModel __EXPORT *SchDLL::GetModel (SvInPlaceObjectRef aIPObj)
-/*?*/ {
-/*?*/ 	ChartModel *(__LOADONCALLAPI*fp)(SvInPlaceObjectRef);
-/*?*/ 
-/*?*/ 	fp = (ChartModel* (__LOADONCALLAPI*)(SvInPlaceObjectRef))GetFuncSch("SchGetModel");
-/*?*/ 
-/*?*/ 	if (fp)
-/*?*/ 	{
-/*?*/ 		return fp(aIPObj);
-/*?*/ 	}
-/*?*/ 	else return 0;
-/*?*/ }
-
-/*************************************************************************
-|*
-|*
-|*
-\************************************************************************/
-
-
-/*?*/ void __EXPORT SchDLL::SetTransparentBackground( SvInPlaceObjectRef aIPObj, BOOL bTransp )
-/*?*/ {
-/*?*/ 	void (__LOADONCALLAPI*fp)(SvInPlaceObjectRef, BOOL );
-/*?*/ 
-/*?*/ 	fp = (void (__LOADONCALLAPI*)(SvInPlaceObjectRef, BOOL))GetFuncSch("SchSetTransparent");
-/*?*/ 
-/*?*/ 	if (fp)
-/*?*/ 	{
-/*?*/ 	fp(aIPObj, bTransp);
-/*?*/ 	}
-/*?*/ }
 
 /*N*/ void __EXPORT SchDLL::GetDefaultForColumnText( const SchMemChart& rMemChart, sal_Int32 nCol, String& aResult )
 /*N*/ {
@@ -304,37 +217,6 @@ extern "C" { static void SAL_CALL thisModule() {} }
 /*N*/ 	if( fp )
 /*N*/ 		fp( rMemChart, bOldToNew );
 /*N*/ }
-
-/*?*/ void __EXPORT SchDLL::ConvertChartRangeForCalc( SchMemChart& rMemChart,
-/*?*/ 												BOOL bOldToNew )
-/*?*/ {
-/*?*/ 	void (__LOADONCALLAPI* fp )( SchMemChart&, BOOL )
-/*?*/ 		= (void (__LOADONCALLAPI*)( SchMemChart&, BOOL ))
-/*?*/ 								GetFuncSch("SchConvertChartRangeForCalc");
-/*?*/ 	if( fp )
-/*?*/ 		fp( rMemChart, bOldToNew );
-/*?*/ }
-
-/*?*/ void __EXPORT SchDLL::MemChartResetTranslation( SchMemChart& rMemChart,
-/*?*/ 												long *pTable,long nCnt )
-/*?*/ {
-/*?*/ 	void (__LOADONCALLAPI* fp )( SchMemChart&, long*, long )
-/*?*/ 		= (void (__LOADONCALLAPI*)( SchMemChart&, long*, long ))
-/*?*/ 								GetFuncSch("SchMemChartResetTranslation");
-/*?*/ 	if( fp )
-/*?*/ 		fp( rMemChart, pTable, nCnt );
-/*?*/ }
-
-/*?*/ void __EXPORT SchDLL::MemChartUpdateTranslation( SchMemChart& rMemChart,
-/*?*/ 													long *pTable,long nCnt )
-/*?*/ {
-/*?*/ 	void (__LOADONCALLAPI* fp )( SchMemChart&, long*, long )
-/*?*/ 		= (void (__LOADONCALLAPI*)( SchMemChart&, long*, long ))
-/*?*/ 								GetFuncSch("SchMemChartUpdateTranslation");
-/*?*/ 	if( fp )
-/*?*/ 		fp( rMemChart, pTable, nCnt );
-/*?*/ }
-
 
 /*N*/ void __EXPORT SchDLL::MemChartInsertCols( SchMemChart& rMemChart,
 /*N*/ 											short nAtCol, short nCount)
@@ -372,84 +254,6 @@ extern "C" { static void SAL_CALL thisModule() {} }
 /*?*/ 								GetFuncSch("SchMemChartRemoveRows");
 /*?*/ 	if( fp )
 /*?*/ 		fp( rMemChart, nAtRow, nCount );
-/*?*/ }
-
-/*?*/ void __EXPORT SchDLL::MemChartSwapCols( SchMemChart& rMemChart,
-/*?*/ 										int nAtCol1, int nAtCol2 )
-/*?*/ {
-/*?*/ 	void (__LOADONCALLAPI* fp )( SchMemChart&, int, int )
-/*?*/ 		= (void (__LOADONCALLAPI*)( SchMemChart&, int, int ))
-/*?*/ 								GetFuncSch("SchMemChartSwapCols");
-/*?*/ 	if( fp )
-/*?*/ 		fp( rMemChart, nAtCol1, nAtCol2 );
-/*?*/ }
-/*?*/ void __EXPORT SchDLL::MemChartSwapRows( SchMemChart& rMemChart,
-/*?*/ 										int nAtRow1,int nAtRow2 )
-/*?*/ {
-/*?*/ 	void (__LOADONCALLAPI* fp )( SchMemChart&, int, int )
-/*?*/ 		= (void (__LOADONCALLAPI*)( SchMemChart&, int, int ))
-/*?*/ 								GetFuncSch("SchMemChartSwapRows");
-/*?*/ 	if( fp )
-/*?*/ 		fp( rMemChart, nAtRow1, nAtRow2);
-/*?*/ }
-
-
-
-
-/*************************************************************************
-|*
-|*
-|*
-\************************************************************************/
-
-/*?*/ ULONG __EXPORT SchDLL::DetectFilter(
-/*?*/ 	SfxMedium& rMedium,
-/*?*/ 	const SfxFilter** pFilter,
-/*?*/ 	SfxFilterFlags nMust,
-/*?*/ 	SfxFilterFlags nDont )
-/*?*/ {
-/*?*/ 	ULONG nReturn = ERRCODE_ABORT;
-/*?*/ 
-/*?*/ 	if (rMedium.GetError() != SVSTREAM_OK)
-/*?*/ 	{
-/*?*/ 		return (rMedium.GetError());
-/*?*/ 	}
-/*?*/ 
-/*?*/ 	SvStorage* pStorage = rMedium.GetStorage();
-/*?*/ 	if( pStorage )
-/*?*/ 	{
-/*?*/ 		// Detect using contained streams (StarChart 3.0)
-/*?*/ 
-/*?*/ 		String aStreamName( RTL_CONSTASCII_USTRINGPARAM( "StarChartDocument" ));
-/*?*/ 
-/*?*/ 		if (pStorage->IsContained( aStreamName ) &&
-/*?*/ 			pStorage->IsStream( aStreamName ))
-/*?*/ 		{
-/*?*/ 			const SfxFilter* pFilt = NULL;
-/*?*/ 
-/*?*/ 			if (*pFilter)
-/*?*/ 			{
-/*?*/ 				pFilt = SFX_APP()->GetFilter( SchChartDocShell::Factory(), (*pFilter)->GetFilterName());
-/*?*/ 			}
-/*?*/ 			else
-/*?*/ 			{
-/*?*/ 				pFilt = SFX_APP()->GetFilter( SchChartDocShell::Factory(), SCH_FORMAT_STR_50_FORMAT );
-/*?*/ 			}
-/*?*/ 
-/*?*/ 			if (pFilt &&
-/*?*/ 				(pFilt->GetFilterFlags() & nMust) == nMust &&
-/*?*/ 				(pFilt->GetFilterFlags() & nDont) == 0)
-/*?*/ 			{
-/*?*/ 				*pFilter = pFilt;
-/*?*/ 				nReturn = ERRCODE_NONE;
-/*?*/ 			}
-/*?*/ 		}
-/*?*/ 	}
-/*?*/ 	else
-/*?*/ 		nReturn = ULONG_MAX;
-/*?*/ 
-/*?*/ 	return (nReturn);
-/*?*/ //	return (rMedium.GetError());
 /*?*/ }
 
 /*************************************************************************
