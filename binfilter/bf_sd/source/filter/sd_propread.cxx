9b225767ba46cf57cb4e5810919e532ac65f3f82
Kurt Zenker <kz@openoffice.org>
2008-05-20 14:08:19 +0000

    INTEGRATION: CWS mba30patches01 (1.7.128); FILE MERGED 2008/04/23 11:16:57 mba 1.7.128.2: RESYNC: (1.7-1.8); FILE MERGED 2008/03/19 14:54:59 cmc 1.7.128.1: #i86582# remove unused code, round 1

--- binfilter/bf_sd/source/filter/sd_propread.cxx
+++ binfilter/bf_sd/source/filter/sd_propread.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: sd_propread.cxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -36,54 +36,6 @@
 namespace binfilter {
 
 // ------------------------------------------------------------------------
-
-struct PropEntry
-{
-    sal_uInt32	mnId;
-    sal_uInt32	mnSize;
-    sal_uInt16	mnTextEnc;
-    sal_uInt8*	mpBuf;
-
-                        PropEntry( sal_uInt32 nId, const sal_uInt8* pBuf, sal_uInt32 nBufSize, sal_uInt16 nTextEnc );
-                        PropEntry( const PropEntry& rProp );
-                        ~PropEntry() { delete[] mpBuf; } ;
-
-    const PropEntry&	operator=(const PropEntry& rPropEntry);
-};
-
-PropEntry::PropEntry( sal_uInt32 nId, const sal_uInt8* pBuf, sal_uInt32 nBufSize, sal_uInt16 nTextEnc ) :
-    mnId		( nId ),
-    mnSize		( nBufSize ),
-    mnTextEnc	( nTextEnc ),
-    mpBuf		( new sal_uInt8[ nBufSize ] )
-{
-    memcpy( (void*)mpBuf, (void*)pBuf, nBufSize );
-};
-
-PropEntry::PropEntry( const PropEntry& rProp ) :
-    mnId		( rProp.mnId ),
-    mnSize		( rProp.mnSize ),
-    mpBuf		( new sal_uInt8[ mnSize ] ),
-    mnTextEnc	( rProp.mnTextEnc )
-{
-    memcpy( (void*)mpBuf, (void*)rProp.mpBuf, mnSize );
-};
-
-const PropEntry& PropEntry::operator=(const PropEntry& rPropEntry)
-{
-    if ( this != &rPropEntry )
-    {
-        delete[] mpBuf;
-        mnId = rPropEntry.mnId;
-        mnSize = rPropEntry.mnSize;
-        mnTextEnc = rPropEntry.mnTextEnc;
-        mpBuf = new sal_uInt8[ mnSize ];
-        memcpy( (void*)mpBuf, (void*)rPropEntry.mpBuf, mnSize );
-    }
-    return *this;
-}
-
-//	-----------------------------------------------------------------------
 
 PropItem& PropItem::operator=( PropItem& rPropItem )
 {
