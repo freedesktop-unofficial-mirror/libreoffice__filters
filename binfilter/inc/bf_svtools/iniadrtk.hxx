f3292bda3bd36e508ee72bdaf9cf7d4056fe340e
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:03:25 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:47:41 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:19 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/iniadrtk.hxx
+++ binfilter/inc/bf_svtools/iniadrtk.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: iniadrtk.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -53,13 +53,6 @@ enum AddressToken
     ADDRESS_STATE,
     ADDRESS_FATHERSNAME,
     ADDRESS_APARTMENT
-};
-
-//============================================================================
-class SfxIniManagerAddressEntry
-{
-public:
-    static String get(const String & rAddress, AddressToken eToken);
 };
 
 }
