ff8778b7db45e4936632f9e10533691a1564e832
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:10:29 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:49:01 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:20 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/parrtf.hxx
+++ binfilter/inc/bf_svtools/parrtf.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: parrtf.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -41,66 +41,5 @@
 #ifndef _SVARRAY_HXX
 #include <bf_svtools/svarray.hxx>
 #endif
-
-namespace binfilter
-{
-
-struct RtfParserState_Impl
-{
-    rtl_TextEncoding eCodeSet;
-    BYTE nUCharOverread;
-
-    RtfParserState_Impl( BYTE nUOverread, rtl_TextEncoding eCdSt )
-        : eCodeSet( eCdSt ), nUCharOverread( nUOverread )
-    {}
-};
-
-SV_DECL_VARARR( RtfParserStates_Impl, RtfParserState_Impl, 16, 16 )
-
-class  SvRTFParser : public SvParser
-{
-    RtfParserStates_Impl aParserStates;
-
-    int nOpenBrakets;
-    rtl_TextEncoding eCodeSet, eUNICodeSet;
-    BYTE nUCharOverread;
-
-private:
-    static short _inSkipGroup;
-
-protected:
-    sal_Unicode GetHexValue();
-    void ScanText( const sal_Unicode = 0 );
-    void SkipGroup();
-
-    // scanne das naechste Token,
-    virtual int _GetNextToken();
-
-    virtual void ReadUnknownData();
-    virtual void ReadBitmapData();
-    virtual void ReadOLEData();
-
-    virtual ~SvRTFParser();
-
-    rtl_TextEncoding GetCodeSet() const 			{ return eCodeSet; }
-    void SetEncoding( rtl_TextEncoding eEnc );
-
-    rtl_TextEncoding GetUNICodeSet() const 			{ return eUNICodeSet; }
-    void SetUNICodeSet( rtl_TextEncoding eSet )		{ eUNICodeSet = eSet; }
-
-public:
-    SvRTFParser( SvStream& rIn, BYTE nStackSize = 3 );
-
-    virtual SvParserState CallParser();   // Aufruf des Parsers
-
-    int GetOpenBrakets() const { return nOpenBrakets; }
-
-    // fuers asynchrone lesen aus dem SvStream
-//	virtual void SaveState( int nToken );
-//	virtual void RestoreState();
-    virtual void Continue( int nToken );
-};
-
-}
 
 #endif //_PARRTF_HXX
