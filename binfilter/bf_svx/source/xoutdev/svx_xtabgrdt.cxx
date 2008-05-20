0984daedca9ad8d8db4b980a6d82cabd9593d01c
Kurt Zenker <kz@openoffice.org>
2008-05-20 15:33:39 +0000

    INTEGRATION: CWS mba30patches01 (1.10.4); FILE MERGED 2008/04/23 11:30:59 mba 1.10.4.2: RESYNC: (1.10-1.11); FILE MERGED 2008/03/19 14:55:13 cmc 1.10.4.1: #i86582# remove unused code, round 1

--- binfilter/bf_svx/source/xoutdev/svx_xtabgrdt.cxx
+++ binfilter/bf_svx/source/xoutdev/svx_xtabgrdt.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: svx_xtabgrdt.cxx,v $
- * $Revision: 1.11 $
+ * $Revision: 1.12 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -77,20 +77,6 @@ char const aChckXML[]       = { '<', '?', 'x', 'm', 'l' };		// = 6.0
 // ---------------------
 // class XGradientTable
 // ---------------------
-
-/*************************************************************************
-|*
-|* XGradientTable::XGradientTable()
-|*
-*************************************************************************/
-
-/*N*/ XGradientTable::XGradientTable( const String& rPath,
-/*N*/ 							XOutdevItemPool* pInPool,
-/*N*/ 							USHORT nInitSize, USHORT nReSize ) :
-/*N*/ 				XPropertyTable( rPath, pInPool, nInitSize, nReSize)
-/*N*/ {
-/*N*/ 	pBmpTable = new Table( nInitSize, nReSize );
-/*N*/ }
 
 /************************************************************************/
 
