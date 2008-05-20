29445ebb7fbed3120db2d00b8694065d82311364
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:28:14 +0000

    INTEGRATION: CWS mba30patches01 (1.3.24); FILE MERGED 2008/04/23 11:40:16 mba 1.3.24.2: RESYNC: (1.3-1.4); FILE MERGED 2008/03/19 14:55:25 cmc 1.3.24.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_basic/sbxcore.hxx
+++ binfilter/inc/bf_basic/sbxcore.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: sbxcore.hxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -146,7 +146,6 @@ public:
     static SbxBase* Create( UINT16, UINT32=SBXCR_SBX );
     static SbxObject* CreateObject( const String& );
     // Sbx-Loesung als Ersatz fuer SfxBroadcaster::Enable()
-    static void StaticEnableBroadcasting( BOOL bEnable );
     static BOOL StaticIsEnabledBroadcasting( void );
 };
 
