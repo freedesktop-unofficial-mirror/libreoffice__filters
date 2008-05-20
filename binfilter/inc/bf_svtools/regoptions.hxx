6e3fe3562f8cd042496039fae5fe5875e9f927fa
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:12:33 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:49:24 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:20 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/regoptions.hxx
+++ binfilter/inc/bf_svtools/regoptions.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: regoptions.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -31,89 +31,9 @@
 #ifndef SVTOOLS_REGOPTIONS_HXX
 #define SVTOOLS_REGOPTIONS_HXX
 
-#ifndef INCLUDED_SVLDLLAPI_H
-
-#endif
-
 #ifndef _STRING_HXX
 #include <tools/string.hxx>
 #endif
-
-//........................................................................
-namespace binfilter
-{
-//........................................................................
-
-    //====================================================================
-    //= RegOptions
-    //====================================================================
-    class RegOptionsImpl;
-    /** encapsulates access to the configuration settings for registering the product
-    */
-    class  RegOptions
-    {
-    private:
-        RegOptionsImpl*		m_pImpl;
-
-        // the impl class is constructed upon need only (as this is expensive)
-         void ensureImpl( );
-
-    public:
-        RegOptions( );
-        ~RegOptions( );
-
-        // retrieves the URL which should be used for online registration
-        String				getRegistrationURL( ) const;
-
-        // checks if the menu item which can be used to trigger the online registration should be available
-        sal_Bool			allowMenu( ) const;
-
-        enum DialogPermission
-        {						// the registration dialog shall be executed ....
-            dpDisabled,			// ... never - it has been disabled
-            dpNotThisSession,	// ... later - no statement when, but _not now_
-            dpRemindLater,		// ... later at a reminder date
-            dpThisSession		// ... during this session
-        };
-
-        /** checks if the registration dialog is allowed
-            <p>A return value of <TRUE/> does not mean that during this session, the dialog is allowed to
-            be executed. It simply means executing it is allowed <em>in general</em></p>
-        */
-        DialogPermission	getDialogPermission( ) const;
-
-        /** claims that the current session is done with respect to the registration dialog.
-
-            <p>This is not to be called if <method>getDialogPermission</method> returned dpDisabled previously<p>
-            <p>The behaviour changes as follows:
-            <ul>
-                <li>any subsequent calls to <method>getDialogPermission</method> will return dpNotThisSession, given
-                    that it previously returned dpNotThisSession</li>
-                <li>any subsequent calls to <method>getDialogPermission</method> will return dpDisabled, given
-                    that it previously returned dpThisSession</li>
-                <li>an internal (persistent) session counter is updated, so that during the next session,
-                    <method>getDialogPermission</method> <em>may</em> return dpThisSession</li>
-            </ul></p>
-        */
-        void				markSessionDone( );
-
-        /** activates the reminder
-
-            <p>If this method is called, the reminder for the dialog will be activated. This meas that during the
-            next <arg>_nDaysFromNow</arg>-1 days, <method>getDialogPermission</method> will return dpRemindLater</p>,
-            at the <arg>_nDaysFromNow</arg>th day from today onwards it will return dpThisSession.</p>
-
-            <p>It is not allowed to call this method if the dialog is currently disabled.</p>
-
-            <p>If the current session has not been marked as done already (i.e., <method>markSessionDone</method> has
-            not been called, yet), this is done implicitly when you call <method>activateReminder</method>.</p>
-        */
-        void				activateReminder( sal_Int32 _nDaysFromNow );
-    };
-
-//........................................................................
-}
-//........................................................................
 
 #endif // SVTOOLS_REGOPTIONS_HXX
 
