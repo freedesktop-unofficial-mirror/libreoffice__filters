5eedf835d47f749c98c9bdf3ddef4e26063d0b8e
Kurt Zenker <kz@openoffice.org>
2008-05-20 15:25:01 +0000

    INTEGRATION: CWS mba30patches01 (1.7.162); FILE MERGED 2008/04/23 11:29:13 mba 1.7.162.2: RESYNC: (1.7-1.8); FILE MERGED 2008/03/19 14:55:11 cmc 1.7.162.1: #i86582# remove unused code, round 1

--- binfilter/bf_svx/source/svdraw/svx_svdmrkv1.cxx
+++ binfilter/bf_svx/source/svdraw/svx_svdmrkv1.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: svx_svdmrkv1.cxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -31,38 +31,6 @@
 #include "svdmrkv.hxx"
 namespace binfilter {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//  @@@@@  @@  @@ @@  @@ @@  @@ @@@@@@ @@   @@  @@@@  @@@@@  @@  @@ @@ @@@@@ @@@@@  @@  @@ @@  @@  @@@@
-//  @@  @@ @@  @@ @@@ @@ @@  @@   @@   @@@ @@@ @@  @@ @@  @@ @@  @@ @@ @@    @@  @@ @@  @@ @@@ @@ @@  @@
-//  @@  @@ @@  @@ @@@@@@ @@ @@    @@   @@@@@@@ @@  @@ @@  @@ @@ @@  @@ @@    @@  @@ @@  @@ @@@@@@ @@
-//  @@@@@  @@  @@ @@@@@@ @@@@     @@   @@@@@@@ @@@@@@ @@@@@  @@@@   @@ @@@@  @@@@@  @@  @@ @@@@@@ @@ @@@
-//  @@     @@  @@ @@ @@@ @@ @@    @@   @@ @ @@ @@  @@ @@  @@ @@ @@  @@ @@    @@  @@ @@  @@ @@ @@@ @@  @@
-//  @@     @@  @@ @@  @@ @@  @@   @@   @@   @@ @@  @@ @@  @@ @@  @@ @@ @@    @@  @@ @@  @@ @@  @@ @@  @@
-//  @@      @@@@  @@  @@ @@  @@   @@   @@   @@ @@  @@ @@  @@ @@  @@ @@ @@@@@ @@  @@  @@@@  @@  @@  @@@@@
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*N*/ BOOL SdrMarkView::HasMarkablePoints() const
-/*N*/ {
-/*N*/ 	ForceUndirtyMrkPnt();
-/*N*/ 	BOOL bRet=FALSE;
-/*N*/ 	if (!ImpIsFrameHandles()) {
-/*?*/ 		ULONG nMarkAnz=aMark.GetMarkCount();
-/*?*/ 		if (nMarkAnz<=nFrameHandlesLimit) {
-/*?*/ 			for (ULONG nMarkNum=0; nMarkNum<nMarkAnz && !bRet; nMarkNum++) {
-/*?*/ 				const SdrMark* pM=aMark.GetMark(nMarkNum);
-/*?*/ 				const SdrObject* pObj=pM->GetObj();
-/*?*/ 				bRet=pObj->IsPolyObj();
-/*?*/ 			}
-/*N*/ 		}
-/*N*/ 	}
-/*N*/ 	return bRet;
-/*N*/ }
-
-
 /*N*/ BOOL SdrMarkView::HasMarkedPoints() const
 /*N*/ {
 /*N*/ 	ForceUndirtyMrkPnt();
@@ -79,41 +47,6 @@ namespace binfilter {
 /*N*/ 	}
 /*N*/ 	return bRet;
 /*N*/ }
-
-
-
-
-
-/*N*/ BOOL SdrMarkView::MarkPoints(const Rectangle* pRect, BOOL bUnmark)
-/*N*/ {
-/*N*/ 	ForceUndirtyMrkPnt();
-/*N*/ 	BOOL bChgd=FALSE;
-/*N*/ 	aMark.ForceSort();
-/*N*/ 	const SdrObject* pObj0=NULL;
-/*N*/ 	const SdrPageView* pPV0=NULL;
-/*N*/ 	SdrMark* pM=NULL;
-/*N*/ 	aHdl.Sort();
-/*N*/ 	BOOL bHideHdl=IsMarkHdlShown() && IsSolidMarkHdl() && !bPlusHdlAlways;
-/*N*/ 	ULONG nHdlAnz=aHdl.GetHdlCount();
-/*N*/ 	for (ULONG nHdlNum=nHdlAnz; nHdlNum>0;) {DBG_BF_ASSERT(0, "STRIP"); //STRIP001 
-/*N*/ 	}
-/*N*/ 	if (pM!=NULL) {DBG_BF_ASSERT(0, "STRIP"); //STRIP001  // Den zuletzt geaenderten MarkEntry ggf. noch aufraeumen
-/*N*/ 	}
-/*N*/ 	if (bHideHdl) ShowMarkHdl(NULL); // #36987#
-/*N*/ 	if (bChgd) {
-/*?*/ 		MarkListHasChanged();
-/*N*/ 	}
-
-    // #90239# refresh IAOs
-    // added this call again to make visible (un)marking of point(s)
-//STRIP012/*N*/ 	if(bChgd)
-//STRIP012/*?*/ 		RefreshAllIAOManagers();
-/*N*/ 
-/*N*/ 	return bChgd;
-/*N*/ }
-
-
-
 
 /*N*/ void SdrMarkView::SetPlusHandlesAlwaysVisible(BOOL bOn)
 /*N*/ { // HandlePaint optimieren !!!!!!!
@@ -170,46 +103,5 @@ namespace binfilter {
 /*N*/ BOOL SdrMarkView::MarkGluePoints(const Rectangle* pRect, BOOL bUnmark)
 /*N*/ {DBG_BF_ASSERT(0, "STRIP"); return FALSE;//STRIP001 
 /*N*/ }
-
-/*N*/ BOOL SdrMarkView::PickGluePoint(const Point& rPnt, SdrObject*& rpObj, USHORT& rnId, SdrPageView*& rpPV, ULONG nOptions) const
-/*N*/ {
-/*N*/ 	SdrObject* pObj0=rpObj;
-/*N*/ 	SdrPageView* pPV0=rpPV;
-/*N*/ 	USHORT nId0=rnId;
-/*N*/ 	rpObj=NULL; rpPV=NULL; rnId=0;
-/*N*/ 	if (!IsGluePointEditMode()) return FALSE;
-/*N*/ 	BOOL bBack=(nOptions & SDRSEARCH_BACKWARD) !=0;
-/*N*/ 	BOOL bNext=(nOptions & SDRSEARCH_NEXT) !=0;
-/*N*/ 	const OutputDevice* pOut=(const OutputDevice*)pActualOutDev;
-/*N*/ 	if (pOut==NULL) const OutputDevice* pOut=GetWin(0);
-/*N*/ 	if (pOut==NULL) return FALSE;
-/*N*/ 	((SdrMarkView*)this)->aMark.ForceSort();
-/*N*/ 	ULONG nMarkAnz=aMark.GetMarkCount();
-/*N*/ 	ULONG nMarkNum=bBack ? 0 : nMarkAnz;
-/*N*/ 	if (bNext) {
-/*N*/ 		nMarkNum=((SdrMarkView*)this)->aMark.FindObject(pObj0);
-/*N*/ 		if (nMarkNum==CONTAINER_ENTRY_NOTFOUND) return FALSE;
-/*N*/ 		if (!bBack) nMarkNum++;
-/*N*/ 	}
-/*N*/ 	while (bBack ? nMarkNum<nMarkAnz : nMarkNum>0) {
-/*N*/ 		if (!bBack) nMarkNum--;
-/*N*/ 		const SdrMark* pM=aMark.GetMark(nMarkNum);
-/*N*/ 		SdrObject* pObj=pM->GetObj();
-/*N*/ 		SdrPageView* pPV=pM->GetPageView();
-/*N*/ 		const SdrGluePointList* pGPL=pObj->GetGluePointList();
-/*N*/ 		if (pGPL!=NULL) {DBG_BF_ASSERT(0, "STRIP"); //STRIP001 
-/*N*/ 		}
-/*N*/ 		bNext=FALSE; // HitNextGluePoint nur beim ersten Obj
-/*N*/ 		if (bBack) nMarkNum++;
-/*N*/ 	}
-/*N*/ 	return FALSE;
-/*N*/ }
-
-
-
-
-
-
-
 
 }
