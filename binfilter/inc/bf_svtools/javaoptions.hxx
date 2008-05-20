e54114bf7034b27356ca70a797ccbfb362d2a3e2
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:05:47 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:48:07 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:20 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/javaoptions.hxx
+++ binfilter/inc/bf_svtools/javaoptions.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: javaoptions.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -38,47 +38,4 @@
 #include <unotools/configitem.hxx>
 #endif
 
-namespace binfilter
-{
-
-// class SvtJavaOptions --------------------------------------------------
-
-struct SvtJavaOptions_Impl;
-
-class  SvtJavaOptions : public utl::ConfigItem
-{
-    SvtJavaOptions_Impl* pImpl;
-public:
-    enum EOption
-    {
-        E_ENABLED,
-        E_SECURITY,
-        E_NETACCESS,
-        E_USERCLASSPATH,
-        E_EXECUTEAPPLETS
-    };
-
-    SvtJavaOptions();
-    ~SvtJavaOptions();
-
-    virtual void    Commit();
-
-    sal_Bool        IsEnabled() const;
-    sal_Bool        IsSecurity()const;
-    sal_Int32       GetNetAccess() const;
-    rtl::OUString&  GetUserClassPath()const;
-    sal_Bool        IsExecuteApplets() const;
-
-    void SetEnabled(sal_Bool bSet) ;
-    void SetSecurity(sal_Bool bSet);
-    void SetNetAccess(sal_Int32 nSet) ;
-    void SetUserClassPath(const rtl::OUString& rSet);
-    void SetExecuteApplets(sal_Bool bSet);
-
-    sal_Bool IsReadOnly( EOption eOption ) const;
-};
-
-}
-
 #endif //
-
