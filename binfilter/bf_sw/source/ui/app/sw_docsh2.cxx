1aadba1effdae967e5d4d7ace61fa3e797a45459
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:19:09 +0000

    INTEGRATION: CWS mba30patches01 (1.16.4); FILE MERGED 2008/04/23 11:35:46 mba 1.16.4.2: RESYNC: (1.16-1.17); FILE MERGED 2008/03/27 15:49:04 mba 1.16.4.1: #i86582#: remove Undo stuff

--- binfilter/bf_sw/source/ui/app/sw_docsh2.cxx
+++ binfilter/bf_sw/source/ui/app/sw_docsh2.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: sw_docsh2.cxx,v $
- * $Revision: 1.17 $
+ * $Revision: 1.18 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -348,8 +348,6 @@ DBG_BF_ASSERT(0, "STRIP"); //STRIP001 	if( pDocShell )
 /*N*/ 		{
 /*N*/ 			BOOL bOld = pDoc->IsModified();
 /*N*/ 			pDoc->SetModified();
-/*N*/ 			if( !bOld )
-/*?*/ 				pDoc->SetUndoNoResetModified();
 /*N*/ 		}
 /*N*/ 		else
 /*N*/ 			pDoc->ResetModified();
