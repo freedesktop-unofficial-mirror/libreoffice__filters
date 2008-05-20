4dab155dd821ae6a54f82af7529dbad284c62415
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:36:01 +0000

    INTEGRATION: CWS mba30patches01 (1.7.4); FILE MERGED 2008/04/23 11:42:02 mba 1.7.4.2: RESYNC: (1.7-1.8); FILE MERGED 2008/03/19 14:55:29 cmc 1.7.4.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_sc/patattr.hxx
+++ binfilter/inc/bf_sc/patattr.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: patattr.hxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -152,9 +152,6 @@ public:
     ULONG					GetNumberFormat( SvNumberFormatter* ) const;
     ULONG					GetNumberFormat( SvNumberFormatter* pFormatter,
                                                 const SfxItemSet* pCondSet ) const;
-
-    long					GetRotateVal( const SfxItemSet* pCondSet ) const;
-    BYTE					GetRotateDir( const SfxItemSet* pCondSet ) const;
 };
 
 
