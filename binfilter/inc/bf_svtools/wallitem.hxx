68d14225958594c9cbd1db2e88e1bb017eaa1143
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:17:19 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:50:19 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:21 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/wallitem.hxx
+++ binfilter/inc/bf_svtools/wallitem.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: wallitem.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -64,7 +64,6 @@ public:
     virtual CreateSvxBrushTabPage GetBackgroundTabpageCreateFunc() = 0;
     virtual GetSvxBrushTabPageRanges GetBackgroundTabpageRanges() = 0;
     static SfxBrushItemLink* Get() { return *(SfxBrushItemLink**)GetAppData(SHL_BRUSHITEM);	}
-    static void Set( SfxBrushItemLink* pLink );
 };
 
 }
