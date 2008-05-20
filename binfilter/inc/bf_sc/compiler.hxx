e48a538e09c25d81a21af8b7d086a1b5bd863a8d
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:32:18 +0000

    INTEGRATION: CWS mba30patches01 (1.7.128); FILE MERGED 2008/04/23 11:41:11 mba 1.7.128.2: RESYNC: (1.7-1.8); FILE MERGED 2008/03/19 14:55:27 cmc 1.7.128.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_sc/compiler.hxx
+++ binfilter/inc/bf_sc/compiler.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: compiler.hxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -414,7 +414,6 @@ public:
     ScTokenIterator( const ScTokenArray& );
    ~ScTokenIterator();
     void  Reset();
-     const ScToken* First();
     const ScToken* Next();
 };
 
