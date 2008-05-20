5947be0398c2b1d0c62135b2bafddcd6d25639df
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:59:34 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:46:56 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:19 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/helpopt.hxx
+++ binfilter/inc/bf_svtools/helpopt.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: helpopt.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -61,49 +61,6 @@ class  SvtHelpOptions: public Options
 public:
                     SvtHelpOptions();
                     virtual ~SvtHelpOptions();
-
-    void            SetExtendedHelp( sal_Bool b );
-    sal_Bool        IsExtendedHelp() const;
-    void            SetHelpTips( sal_Bool b );
-    sal_Bool        IsHelpTips() const;
-
-    void            SetHelpAgentAutoStartMode( sal_Bool b );
-    sal_Bool        IsHelpAgentAutoStartMode() const;
-    void			SetHelpAgentTimeoutPeriod( sal_Int32 _nSeconds );
-    sal_Int32		GetHelpAgentTimeoutPeriod( ) const;
-    void			SetHelpAgentRetryLimit( sal_Int32 _nTrials );
-    sal_Int32		GetHelpAgentRetryLimit( ) const;
-
-    const String&   GetHelpStyleSheet()const;
-    void            SetHelpStyleSheet(const String& rStyleSheet);
-
-    /** retrieves the help agent's ignore counter for the given URL.
-        <p>	If the counter returned 0, the agent should silently drop any requests for this URL.<br/>
-            If the counter is greater 0, the agent should display the URL and, if the user ignores it,
-            decrement the counter by 1.
-        </p>
-    */
-    sal_Int32		getAgentIgnoreURLCounter( const ::rtl::OUString& _rURL );
-    /** decrements the help agent's ignore counter for the given URL
-        @see getAgentIgnoreURLCounter
-    */
-    void			decAgentIgnoreURLCounter( const ::rtl::OUString& _rURL );
-    /** resets the help agent's ignore counter for the given URL
-    */
-    void			resetAgentIgnoreURLCounter( const ::rtl::OUString& _rURL );
-    /** resets the help agent's ignore counter for all URL's
-    */
-    void			resetAgentIgnoreURLCounter();
-
-    void            SetWelcomeScreen( sal_Bool b );
-    sal_Bool        IsWelcomeScreen() const;
-
-    IdList*         GetPIStarterList();
-    void            AddToPIStarterList( sal_Int32 nId );
-    void            RemoveFromPIStarterList( sal_Int32 nId );
-
-    String			GetLocale() const;
-    String			GetSystem() const;
 };
 
 }
