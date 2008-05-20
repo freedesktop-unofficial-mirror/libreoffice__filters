c28a9c206f037ecdd2608b62db7da786f08f314c
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:43:17 +0000

    INTEGRATION: CWS mba30patches01 (1.5.162); FILE MERGED 2008/04/23 11:54:29 mba 1.5.162.2: RESYNC: (1.5-1.6); FILE MERGED 2008/03/19 14:55:42 cmc 1.5.162.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_xmloff/MultiPropertySetHelper.hxx
+++ binfilter/inc/bf_xmloff/MultiPropertySetHelper.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: MultiPropertySetHelper.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -97,8 +97,6 @@ class MultiPropertySetHelper
 public:
 
     MultiPropertySetHelper( const sal_Char** pNames );
-
-    MultiPropertySetHelper( const ::rtl::OUString* pNames );
 
     ~MultiPropertySetHelper();
 
