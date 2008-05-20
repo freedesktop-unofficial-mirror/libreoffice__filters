1493905a4a41f46273cd307b35646586fc5af8d0
Kurt Zenker <kz@openoffice.org>
2008-05-20 15:33:00 +0000

    INTEGRATION: CWS mba30patches01 (1.8.128); FILE MERGED 2008/04/23 11:30:49 mba 1.8.128.2: RESYNC: (1.8-1.9); FILE MERGED 2008/03/19 14:55:13 cmc 1.8.128.1: #i86582# remove unused code, round 1

--- binfilter/bf_svx/source/xoutdev/svx_xtabbtmp.cxx
+++ binfilter/bf_svx/source/xoutdev/svx_xtabbtmp.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: svx_xtabbtmp.cxx,v $
- * $Revision: 1.9 $
+ * $Revision: 1.10 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -65,20 +65,6 @@ static char const aChckXML[]     = { 'P', 'K', 0x03, 0x04 };		// = 6.0
 // -------------------
 // class XBitmapTable
 // -------------------
-
-/*************************************************************************
-|*
-|* XBitmapTable::XBitmapTable()
-|*
-*************************************************************************/
-
-/*N*/ XBitmapTable::XBitmapTable( const String& rPath,
-/*N*/ 							XOutdevItemPool* pInPool,
-/*N*/ 							USHORT nInitSize, USHORT nReSize ) :
-/*N*/ 				XPropertyTable( rPath, pInPool, nInitSize, nReSize)
-/*N*/ {
-/*N*/ 	pBmpTable = new Table( nInitSize, nReSize );
-/*N*/ }
 
 /************************************************************************/
 
