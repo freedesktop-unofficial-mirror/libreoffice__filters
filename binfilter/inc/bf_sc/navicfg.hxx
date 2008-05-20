a3849dc276bcab27203a8232e95560f9e8ed7cf0
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:35:47 +0000

    INTEGRATION: CWS mba30patches01 (1.5.128); FILE MERGED 2008/04/23 11:41:58 mba 1.5.128.2: RESYNC: (1.5-1.6); FILE MERGED 2008/03/19 14:55:29 cmc 1.5.128.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_sc/navicfg.hxx
+++ binfilter/inc/bf_sc/navicfg.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: navicfg.hxx,v $
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -49,8 +49,6 @@ private:
     USHORT	nRootType;
 
 public:
-            ScNavipiCfg();
-
     USHORT	GetListMode() const			{ return nListMode; }
     USHORT	GetDragMode() const			{ return nDragMode; }
     USHORT	GetRootType() const			{ return nRootType; }
