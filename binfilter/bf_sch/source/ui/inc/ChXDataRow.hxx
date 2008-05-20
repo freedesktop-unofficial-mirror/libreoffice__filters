fa8871d75b096f0bcc7c6fbf6a5d862b682172e4
Kurt Zenker <kz@openoffice.org>
2008-05-20 14:03:58 +0000

    INTEGRATION: CWS mba30patches01 (1.3.290); FILE MERGED 2008/04/23 11:16:26 mba 1.3.290.2: RESYNC: (1.3-1.4); FILE MERGED 2008/03/19 14:54:58 cmc 1.3.290.1: #i86582# remove unused code, round 1

--- binfilter/bf_sch/source/ui/inc/ChXDataRow.hxx
+++ binfilter/bf_sch/source/ui/inc/ChXDataRow.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: ChXDataRow.hxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -78,7 +78,6 @@ public:
     sal_Int32 GetRow() const		{ return mnRow; }
 
     static const ::com::sun::star::uno::Sequence< sal_Int8 > & getUnoTunnelId() throw();
-    static ChXDataRow* getImplementation( ::com::sun::star::uno::Reference< ::com::sun::star::uno::XInterface > ) throw();
 
     // XPropertySet
     virtual ::com::sun::star::uno::Reference< ::com::sun::star::beans::XPropertySetInfo > SAL_CALL getPropertySetInfo()
