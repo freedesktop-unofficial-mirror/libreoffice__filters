d5841221478cb8621305a01a37e283d7df387f2f
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:54:05 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:45:55 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:18 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/defaultoptions.hxx
+++ binfilter/inc/bf_svtools/defaultoptions.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: defaultoptions.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -55,8 +55,6 @@ public:
 
     SvtDefaultOptions();
     virtual ~SvtDefaultOptions();
-
-    String	GetDefaultPath( USHORT nId ) const;
 };
 
 }
