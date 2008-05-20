d6dd2164cac1454472f01fd888f6e5391f516f64
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:59:59 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:47:02 mba 1.2.4.4: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 23:14:39 cmc 1.2.4.3: #i86582# remove another ludicrious amount of unused code, round 4 2008/03/20 20:16:19 cmc 1.2.4.2: #i86582# remove another ludicrious amount of unused code, round 3 2008/03/19 20:57:51 cmc 1.2.4.1: #i86582# remove unused svtools code, round 3

--- binfilter/inc/bf_svtools/htmlout.hxx
+++ binfilter/inc/bf_svtools/htmlout.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: htmlout.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -65,16 +65,6 @@ struct HTMLOutEvent
     USHORT nEvent;
 };
 
-struct  HTMLOutContext
-{
-    rtl_TextEncoding m_eDestEnc;
-    rtl_TextToUnicodeConverter m_hConv;
-    rtl_TextToUnicodeContext   m_hContext;
-
-    HTMLOutContext( rtl_TextEncoding eDestEnc );
-    ~HTMLOutContext();
-};
-
 struct HTMLOutFuncs
 {
 #if defined(MAC) || defined(UNX)
@@ -82,62 +72,7 @@ struct HTMLOutFuncs
 #else
     static const sal_Char __FAR_DATA sNewLine[];	// \015\012
 #endif
-
-     static void ConvertStringToHTML( const String& sSrc, ByteString& rDest,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252,
-                        String *pNonConvertableChars = 0);
-
-     static SvStream& Out_AsciiTag( SvStream&, const sal_Char* pStr,
-                                   BOOL bOn = TRUE,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252);
-     static SvStream& Out_Char( SvStream&, sal_Unicode cChar,
-                        HTMLOutContext& rContext, 
-                        String *pNonConvertableChars = 0 );
-     static SvStream& Out_String( SvStream&, const String&,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252,
-                        String *pNonConvertableChars = 0 );
-     static SvStream& Out_Hex( SvStream&, ULONG nHex, BYTE nLen,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252 );
-     static SvStream& Out_Color( SvStream&, const Color&,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252 );
-     static SvStream& Out_ImageMap( SvStream&, const String&, const ImageMap&, const String&,
-                                   const HTMLOutEvent *pEventTable,
-                                   BOOL bOutStarBasic,
-                                   const sal_Char *pDelim = 0,
-                                   const sal_Char *pIndentArea = 0,
-                                   const sal_Char *pIndentMap = 0,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252,
-                        String *pNonConvertableChars = 0 );
-     static SvStream& FlushToAscii( SvStream&, HTMLOutContext& rContext );
-
-     static SvStream& OutScript( SvStream& rStrm,
-                                const String& rBaseURL,
-                                const String& rSource,
-                                const String& rLanguage,
-                                ScriptType eScriptType,
-                                const String& rSrc,
-                                const String *pSBLibrary = 0,
-                                const String *pSBModule = 0,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252,
-                        String *pNonConvertableChars = 0 );
-
-    // der 3. Parameter ist ein Array von HTMLOutEvents, das mit einem
-    // nur aus 0 bestehen Eintrag terminiert ist.
-     static SvStream& Out_Events( SvStream&, const SvxMacroTableDtor&,
-                                 const HTMLOutEvent*, BOOL bOutStarBasic,
-                        rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252,
-                        String *pNonConvertableChars = 0 );
-
-    // <TD SDVAL="..." SDNUM="...">
-     static ByteString&	CreateTableDataOptionsValNum( ByteString& aStrTD,
-                BOOL bValue, double fVal, ULONG nFormat,
-                SvNumberFormatter& rFormatter,
-                rtl_TextEncoding eDestEnc = RTL_TEXTENCODING_MS_1252,
-                String *pNonConvertableChars = 0 );
-
-     static BOOL PrivateURLToInternalImg( String& rURL );
 };
-
 }
 
 #endif
