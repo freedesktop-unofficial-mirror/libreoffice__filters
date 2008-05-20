6b6b210f3aa3cd338aad87ac2f425f86c07cc57d
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:30:24 +0000

    INTEGRATION: CWS mba30patches01 (1.9.48); FILE MERGED 2008/04/23 11:40:42 mba 1.9.48.2: RESYNC: (1.9-1.10); FILE MERGED 2008/03/19 14:55:26 cmc 1.9.48.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_offmgr/app.hxx
+++ binfilter/inc/bf_offmgr/app.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: app.hxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -63,7 +63,6 @@ public:
                                 OfficeApplication();
                                 ~OfficeApplication();
 
-    ResMgr* 					GetOffResManager();
     XColorTable*				GetStdColorTable();
 };
 
