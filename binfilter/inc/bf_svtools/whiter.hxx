d853d68930d018f3363fa8a0c1bf4e791afc3517
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:17:32 +0000

    INTEGRATION: CWS mba30patches01 (1.2.4); FILE MERGED 2008/04/23 11:50:22 mba 1.2.4.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/20 20:16:21 cmc 1.2.4.1: #i86582# remove another ludicrious amount of unused code, round 3

--- binfilter/inc/bf_svtools/whiter.hxx
+++ binfilter/inc/bf_svtools/whiter.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: whiter.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -61,10 +61,8 @@ public:
 
     USHORT  GetCurWhich() const { return *pRanges + nOfst; }
     USHORT  NextWhich();
-    USHORT  PrevWhich();
 
     USHORT	FirstWhich();
-    USHORT	LastWhich();
 };
 
 }
