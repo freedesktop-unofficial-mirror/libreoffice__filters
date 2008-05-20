33407a333aff96693c6414e3549284912b278d25
Kurt Zenker <kz@openoffice.org>
2008-05-20 14:12:06 +0000

    INTEGRATION: CWS mba30patches01 (1.3.290); FILE MERGED 2008/04/23 11:17:41 mba 1.3.290.2: RESYNC: (1.3-1.4); FILE MERGED 2008/03/19 14:55:00 cmc 1.3.290.1: #i86582# remove unused code, round 1

--- binfilter/bf_sd/source/ui/unoidl/unoobj.hxx
+++ binfilter/bf_sd/source/ui/unoidl/unoobj.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: unoobj.hxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -102,7 +102,6 @@ private:
     ::com::sun::star::uno::Sequence< sal_Int8 >* mpImplementationId;
 
 public:
-    SdXShape() throw();
     SdXShape(SvxShape* pShape, SdXImpressDocument* pModel) throw();
     virtual ~SdXShape() throw();
 
