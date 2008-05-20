d08b4d29085247a250a5ae2bfbaa40a5edf6aa06
Kurt Zenker <kz@openoffice.org>
2008-05-20 16:28:38 +0000

    INTEGRATION: CWS mba30patches01 (1.2.98); FILE MERGED 2008/04/23 11:40:19 mba 1.2.98.2: RESYNC: (1.2-1.3); FILE MERGED 2008/03/19 14:55:25 cmc 1.2.98.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_basic/sbxmeth.hxx
+++ binfilter/inc/bf_basic/sbxmeth.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: sbxmeth.hxx,v $
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -52,7 +52,6 @@ public:
     SbxMethod( const SbxMethod& r ) : SvRefBase( r ), SbxVariable( r ) {}
     SbxMethod& operator=( const SbxMethod& r )
     { SbxVariable::operator=( r ); return *this; }
-    BOOL Run( SbxValues* pValues = NULL );
     virtual SbxClassType GetClass() const;
 };
 
