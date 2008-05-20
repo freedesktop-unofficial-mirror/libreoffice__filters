edb84acbcdd30d92df86f3db58140e9ebdb6b306
Kurt Zenker <kz@openoffice.org>
2008-05-20 17:30:40 +0000

    INTEGRATION: CWS mba30patches01 (1.3.290); FILE MERGED 2008/04/23 11:52:44 mba 1.3.290.2: RESYNC: (1.3-1.4); FILE MERGED 2008/03/19 14:55:39 cmc 1.3.290.1: #i86582# remove unused code, round 1

--- binfilter/inc/bf_svx/swafopt.hxx
+++ binfilter/inc/bf_svx/swafopt.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: swafopt.hxx,v $
- * $Revision: 1.4 $
+ * $Revision: 1.5 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -101,7 +101,6 @@ struct SvxSwAutoFmtFlags
          bDummy8 : 1
          ;
 
-    SvxSwAutoFmtFlags();
     SvxSwAutoFmtFlags( const SvxSwAutoFmtFlags& rAFFlags ) { *this = rAFFlags; }
     SvxSwAutoFmtFlags& operator=( const SvxSwAutoFmtFlags& );
 };
