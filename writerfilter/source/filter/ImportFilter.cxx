406c37763657cc078ee0c3aa2d85123fec2da5ea
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 10:03:48 +0000

    INTEGRATION: CWS xmlfilter05 (1.7.18); FILE MERGED 2008/05/09 08:43:24 os 1.7.18.3: document properties import added 2008/05/02 10:30:15 hbrinkm 1.7.18.2: RESYNC: (1.7-1.8); FILE MERGED 2008/04/17 09:54:27 dr 1.7.18.1: #i86546# #i86549# #i86560# support all file types and templates

--- writerfilter/source/filter/ImportFilter.cxx
+++ writerfilter/source/filter/ImportFilter.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: ImportFilter.cxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -69,17 +69,19 @@ sal_Bool WriterFilter::filter( const uno::Sequence< beans::PropertyValue >& aDes
     }
 
 #ifdef DEBUG_IMPORT
-    TimeValue t1; 
+    TimeValue t1;
     osl_getSystemTime(&t1);
 
     writerfilter::logger("DEBUG", "<out>");
-    writerfilter::logger("DEBUG", string("<starttime>") + string(t1.Seconds) 
+    writerfilter::logger("DEBUG", string("<starttime>") + string(t1.Seconds)
                          + "</starttime>");
 #endif
 
-    writerfilter::dmapper::SourceDocumentType eType = m_sFilterName.equalsAsciiL ( RTL_CONSTASCII_STRINGPARAM ( "writer_MS_Word_2007" ) ) ? 
+    writerfilter::dmapper::SourceDocumentType eType =
+        (m_sFilterName.equalsAsciiL ( RTL_CONSTASCII_STRINGPARAM ( "writer_MS_Word_2007" ) ) ||
+         m_sFilterName.equalsAsciiL ( RTL_CONSTASCII_STRINGPARAM ( "writer_MS_Word_2007_Template" ) )) ?
                 writerfilter::dmapper::DOCUMENT_OOXML : writerfilter::dmapper::DOCUMENT_DOC;
-    writerfilter::Stream::Pointer_t pStream(new writerfilter::dmapper::DomainMapper(m_xContext, m_xDoc, eType));
+    writerfilter::Stream::Pointer_t pStream(new writerfilter::dmapper::DomainMapper(m_xContext, xInputStream, m_xDoc, eType));
     //create the tokenizer and domain mapper
     if( eType == writerfilter::dmapper::DOCUMENT_OOXML )
     {
@@ -106,10 +108,10 @@ sal_Bool WriterFilter::filter( const uno::Sequence< beans::PropertyValue >& aDes
     }
 
 #ifdef DEBUG_IMPORT
-    TimeValue t2; 
+    TimeValue t2;
     osl_getSystemTime(&t2);
 
-    writerfilter::logger("DEBUG", string("<importtime>") 
+    writerfilter::logger("DEBUG", string("<importtime>")
                          + string(t2.Seconds - t1.Seconds) + "</importtime>");
     writerfilter::logger("DEBUG", "</out>");
 #endif
