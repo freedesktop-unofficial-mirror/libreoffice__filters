a6792bca13fac35c648f73a9b3bd2f706eba5dd9
Ivo Hinkelmann <ihi@openoffice.org>
2008-07-16 19:07:07 +0000

    INTEGRATION: CWS cmcfixes47 (1.8.20); FILE MERGED 2008/06/12 10:43:12 cmc 1.8.20.1: #i90652# some more easy-to-remove unused code

--- binfilter/inc/bf_sw/cellatr.hxx
+++ binfilter/inc/bf_sw/cellatr.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: cellatr.hxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -94,10 +94,6 @@ public:
           SwTableBox* GetTableBox();
     const SwTableBox* GetTableBox() const
         { return ((SwTblBoxFormula*)this)->GetTableBox(); }
-
-    // Status aendern
-    void ChangeState( const SfxPoolItem* pItem );
-    // berechne die Formel
 };
 
 class SwTblBoxValue : public SfxPoolItem
