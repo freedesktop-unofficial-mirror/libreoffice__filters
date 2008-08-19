eda084f3df53da13e1167dfcd4a5e61e2af6e91c
Vladimir Glazounov <vg@openoffice.org>
2008-08-19 21:07:33 +0000

    INTEGRATION: CWS aw033 (1.8.86); FILE MERGED 2008/05/14 13:31:07 aw 1.8.86.3: RESYNC: (1.9-1.10); FILE MERGED 2007/10/08 13:39:37 aw 1.8.86.2: RESYNC: (1.8-1.9); FILE MERGED 2007/05/30 14:22:35 aw 1.8.86.1: changes after resync

--- binfilter/bf_svx/source/xoutdev/svx__xpoly.cxx
+++ binfilter/bf_svx/source/xoutdev/svx__xpoly.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: svx__xpoly.cxx,v $
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -28,7 +28,7 @@
  *
  ************************************************************************/
 
-#include <math.h>
+//#include <math.h>
 #include <string.h>
 #include <tools/stream.hxx>
 
