5bfaa56f28d8290e79e00b9f8f00329d40b5bfac
Henning Brinkmann <hbrinkm@openoffice.org>
2010-02-25 15:38:32 +0100

    writerfilter08: keep grid 'type' in SectionPropertyMap and evaluate in closeSectionGroup

--- writerfilter/source/dmapper/DomainMapper.cxx
+++ writerfilter/source/dmapper/DomainMapper.cxx
@@ -2152,8 +2152,18 @@ void DomainMapper::attribute(Id nName, Value & val)
                 if(pSectionContext)
                 {
                     pSectionContext->SetDxtCharSpace( nIntValue );
+                }                
+            }    
+            break;
+            case NS_ooxml::LN_CT_DocGrid_type:
+                /* WRITERFILTERSTATUS: done: 100, planned: 2, spent: 0 */
+            {
+                if (pSectionContext != NULL)
+                {
+                    pSectionContext->SetGridType(nIntValue);
                 }
-            }                
+            }
+            break;
         default:
             {
 #if OSL_DEBUG_LEVEL > 0
