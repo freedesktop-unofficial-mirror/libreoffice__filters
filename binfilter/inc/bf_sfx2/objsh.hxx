4b767867c924a9a80411e9e454c2a1edf6179022
Caolán McNamara <caolanm@redhat.com>
2011-01-04 15:13:01 +0000

    unused method SetHasName

--- binfilter/inc/bf_sfx2/objsh.hxx
+++ binfilter/inc/bf_sfx2/objsh.hxx
@@ -213,7 +213,6 @@ public:
     void                        FlushDocInfo();
     sal_Bool                    HasName() const { return bHasName; }
     virtual String              GetAPIName() const;
-    void                        SetHasName( sal_Bool bSet = sal_True ) { bHasName = bSet; }
     sal_Bool                    IsReadOnly() const;
     sal_Bool                    IsReadOnlyMedium() const;
     sal_Bool                    IsReadOnlyUI() const;
