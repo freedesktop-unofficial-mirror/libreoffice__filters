35a0c9c4d3df97d7550ecdb50f9547c87e397f27
Michael Stahl <mst@openoffice.org>
2011-01-11 13:02:25 +0100

    xmlfix3: #i115995#: unoxml: retain context node in XPath::CNodeList:  prevents the document containing the nodes from being released.

--- unoxml/source/xpath/xpathobject.cxx
+++ unoxml/source/xpath/xpathobject.cxx
@@ -85,9 +85,11 @@ namespace XPath
     */
     Reference< XNodeList > SAL_CALL CXPathObject::getNodeList() throw (RuntimeException)
     {
-        return Reference< XNodeList >(new CNodeList(m_pXPathObj));
+        Reference< XNodeList > const xRet(
+            new CNodeList(m_xContextNode, m_pXPathObj));
+        return xRet;
     }
-    
+
      /**
         get value of a boolean object
      */
