6c7a4a364ad941558013cb54d7ceaf21ad2cde50
Joseph Powers <jpowers27@cox.net>
2011-01-14 22:48:02 -0800

    Remove DECLARE_LIST( ScRangePairListBase, ScRangePair* )

--- binfilter/inc/bf_sc/rangelst.hxx
+++ binfilter/inc/bf_sc/rangelst.hxx
@@ -154,12 +154,12 @@ SV_DECL_IMPL_REF( ScRangeList );
 
 // RangePairList: erster Range (aRange[0]) eigentlicher Range, zweiter
 // Range (aRange[1]) Daten zu diesem Range, z.B. Rows eines ColName
-DECLARE_LIST( ScRangePairListBase, ScRangePair* )
-class ScRangePairList : public ScRangePairListBase, public SvRefBase
+typedef ::std::vector< ScRangePair* > ScRangePairListBase;
+class ScRangePairList : public SvRefBase
 {
-#if defined( ICC ) && defined( OS2 )
-#endif
 private:
+    ScRangePairListBase maPairList;
+    size_t              current;
 
 public:
     virtual 		~ScRangePairList();
@@ -167,20 +167,74 @@ public:
     void			Append( const ScRangePair& rRangePair )
                     {
                         ScRangePair* pR = new ScRangePair( rRangePair );
-                        Insert( pR, LIST_APPEND );
+                        maPairList.push_back( pR );
                     }
+    size_t          Count() const { return maPairList.size(); }
     void			Join( const ScRangePair&, BOOL bIsInList = FALSE );
     BOOL 			UpdateReference( UpdateRefMode, ScDocument*,
                                     const ScRange& rWhere,
                                     short nDx, short nDy, short nDz );
     ScRangePair*	Find( const ScRange& ) const;
-/*N*/ 	BOOL			Load( SvStream&, USHORT nVer );
-/*N*/ 	BOOL			Store( SvStream& ) const;
+    BOOL			Load( SvStream&, USHORT nVer );
+    BOOL			Store( SvStream& ) const;
+
+    size_t          GetPos( ScRangePair* item )
+                    {
+                        for ( size_t i = 0, n = maPairList.size(); i < n; ++i )
+                            if ( maPairList[ i ] == item ) return i;
+                        return size_t(-1);
+                    }
+
+    ScRangePair*    Remove( size_t i )
+                    {
+                        if ( i < maPairList.size() )
+                        {
+                            ScRangePairListBase::iterator it = maPairList.begin();
+                            ::std::advance( it, i );
+                            maPairList.erase( it );
+                            current = i;
+                        }
+                        return NULL;
+                    }
+
+    ScRangePair*    Remove( ScRangePair* item )
+                    {
+                        size_t i = GetPos( item );
+                        return Remove( i );
+                    }
+
+    ScRangePair*    GetObject( size_t i )
+                    {
+                        if ( i >= maPairList.size() ) return NULL;
+                        return maPairList[ i ];
+                    }
+
+    ScRangePair*    GetObject( size_t i ) const
+                    {
+                        if ( i >= maPairList.size() ) return NULL;
+                        return maPairList[ i ];
+                    }
+
+    void            Seek( size_t i ) { current = i; }
+
+    ScRangePair*    First()
+                    {
+                        current = 0;
+                        return maPairList.empty() ? NULL : maPairList[ current ];
+                    }
+
+    ScRangePair*    Next()
+                    {
+                        if ( current+1 < maPairList.size() )
+                        {
+                            ++current;
+                            return maPairList[ current ];
+                        }
+                        return NULL;
+                    }
+
 };
 SV_DECL_IMPL_REF( ScRangePairList );
-
-#if defined( ICC ) && defined( SC_RANGELST_CXX ) && defined( OS2 )
-#endif
 
 
 } //namespace binfilter
