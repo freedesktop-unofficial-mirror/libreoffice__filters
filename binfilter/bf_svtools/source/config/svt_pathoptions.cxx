fc8a217f55b7b12cceffa2cad83c2a8441b66dc5
Joseph Powers <jpowers27@cox.net>
2011-02-06 05:33:35 -0800

    Add #include <hash_map> again, we still use std::hash

--- binfilter/bf_svtools/source/config/svt_pathoptions.cxx
+++ binfilter/bf_svtools/source/config/svt_pathoptions.cxx
@@ -2,7 +2,7 @@
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
  * OpenOffice.org - a multi-platform office productivity suite
@@ -62,6 +62,7 @@
 
 #include <vector>
 #include <boost/unordered_map.hpp>
+#include <hash_map>
 
 using namespace osl;
 using namespace utl;
