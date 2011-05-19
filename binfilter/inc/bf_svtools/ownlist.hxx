6c30a0d5c26314eeb4d6a10a9d661021c111e076
Joseph Powers <jpowers27@cox.net>
2011-05-18 19:42:48 -0700

    Remove dependance on libs-gui/inc/tools/ownlist.hxx
    I've created a private copy for binfilter because I'm remving the one in
    libs-gui.

--- binfilter/inc/bf_svtools/ownlist.hxx
+++ binfilter/inc/bf_svtools/ownlist.hxx
@@ -2,7 +2,7 @@
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * Copyright 2000, 2010 Oracle and/or its affiliates.
  *
  * OpenOffice.org - a multi-platform office productivity suite
@@ -32,7 +32,7 @@
 #include "bf_svtools/svtdllapi.h"
 
 #include <tools/stream.hxx>
-#include <tools/ownlist.hxx>
+#include <bf_tools/ownlist.hxx>
 
 #include <com/sun/star/uno/Sequence.hxx>
 
