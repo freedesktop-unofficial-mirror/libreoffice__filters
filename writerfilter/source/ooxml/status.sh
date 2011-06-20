aba22372234deefa63ad1e64271d6e7ba8535f2f
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-06-20 20:38:45 +0200

    gnumake4: switching gbuildization
    * mostly switching to gnumake4/writerfilter10 gbuild implementation (thanks mst, hb)

--- writerfilter/source/ooxml/status.sh
+++ writerfilter/source/ooxml/status.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-SEARCHIN=$2
+SEARCHIN=$SRC_ROOT/writerfilter/source/dmapper
 
+(
 echo '<?xml version="1.0"?>'
 echo "<stage1>"
 
-cat $1 | tail -n +2
+xsltproc analyzemodel.xsl model.xml | tail -n +2
 
-find $SEARCHIN -name "*.cxx" -exec grep -nH -A 1 "case NS_" {} \; | 
+find $SEARCHIN -name "*.cxx" -exec grep -nH -A 1 "case NS_" {} \; |
 grep -v "//.*case NS_" |
 sed 's#'$SEARCHIN'##' |
 sed 's#\(^[^:]*\):\([0-9]*\):#<qname file="\1" line="\2"#' |
@@ -18,3 +19,6 @@ sed 's#^.*-[0-9][0-9]*-.*$#<nostatus/>#' |
 grep -v "^--"
 
 echo "</stage1>"
+) |
+xsltproc analyzestage2.xsl - |
+xsltproc analyzestage3.xsl -
